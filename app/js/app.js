be056d7a4c7ec7411f533f564509239daad1d380
Vinay Venu <vinayv@thoughtworks.com>
2013-03-01 09:31:47 +0530

    RT, Vinay | #731 | Search UI first cut

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,8 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+angular.module('registration', ['registration.filters', 'registration.services', 'registration.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.when('/search', {templateUrl: 'partials/search.html', controller: SearchPatientController});
+    $routeProvider.otherwise({redirectTo: '/search'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,10 +3,23 @@
 /* Controllers */
 
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
+function SearchPatientController($scope, $http) {
 
+    var search = function() {
+        delete $http.defaults.headers.common['X-Requested-With'];
+        $http({
+            url: "http://localhost:8080/openmrs/ws/rest/v1/patient?q="+$scope.query+"&v=full",
+            method: "GET",
+            withCredentials: true
+        }).success(function(data) {
+                $scope.results=data.results;
+            });
+    }
+    $scope.search = search;
 
-function MyCtrl2() {
+    var resultsPresent = function() {
+        return angular.isDefined($scope.results) && $scope.results.length > 0;
+    }
+
+    $scope.resultsPresent = resultsPresent;
 }
-MyCtrl2.$inject = [];
