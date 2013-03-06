73835a8352a81a0be6f5cd8e5700eaf1f70858f4
arathyjan <arathyja@thoughtworks.com>
2013-03-06 18:27:57 +0530

    RT, Banka | Basic Authenetication in place

--- app/js/app.js
+++ app/js/app.js
@@ -3,12 +3,12 @@
 
 // Declare app level module which depends on filters, and services
 
-angular.module('registration', ['registration.search'])
+angular.module('registration', ['registration.search', 'registration.session'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/search', {templateUrl: 'partials/search.html', controller: 'SearchPatientController'});
         $routeProvider.when('/create', {templateUrl: 'partials/create.html', controller: 'CreateNewPatientController'});
-        $routeProvider.otherwise({redirectTo: '/search'});
+        $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'SessionController'});
+        $routeProvider.otherwise({redirectTo: '/login'});
     }]).run(function($rootScope){
-//        $rootScope.BaseUrl='http://localhost:8080';
         $rootScope.BaseUrl='';
     });
---
+++ app/js/session.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('registration.session', []).controller('SessionController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+    $scope.login = function () {
+        $scope.errorMessage = null
+        return $http.get('/openmrs/ws/rest/v1/session', {
+            headers: {'Authorization': 'Basic ' + window.btoa($scope.username + ':' + $scope.password)},
+            cache: false
+        }).success(function(data) {
+            var authenticationSuccess = data.authenticated;
+            if(authenticationSuccess){
+                $location.path( "/search" );
+            } else {
+                $scope.errorMessage = "Authentication failed. Please try again."
+                $scope.resetForm();
+            }
+        });
+    }
+
+    $scope.logout = function() {
+        $http.delete('/openmrs/ws/rest/v1/session');
+        $location.path( "/login" );
+    }
+
+    $scope.resetForm = function() {
+        $scope.password = "";
+    }
+}]);
