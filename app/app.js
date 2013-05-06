4ae32954557aa98492c2f4c22fcc280d151e3208
Deepak N <endeep123@gmail.com>
2013-05-06 11:26:57 +0530

    D3 | #866 | Fixing new patient data attributes not saved issue

--- app/app.js
+++ app/app.js
@@ -5,12 +5,12 @@ angular.module('registration', ['registration.search', 'registration.navigation'
                                 'registration.visitController', 'infrastructure.httpErrorInterceptor','registration.patientCommon', 'registration.editPatient',
                                 'registration.initialization'])
 angular.module('registration').config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
-        $routeProvider.when('/search', {reloadOnSearch: false, templateUrl: 'modules/patient/views/search.html', controller: 'SearchPatientController'});
-        $routeProvider.when('/patient/new', {templateUrl: 'modules/patient/views/newpatient.html', controller: 'CreatePatientController'});
-        $routeProvider.when('/printPatient', {templateUrl: 'modules/patient/views/print.html'});
-        $routeProvider.when('/patient/:patientUuid', {templateUrl: 'modules/patient/views/editpatient.html', controller: 'EditPatientController', resolve: {initialization: 'initialization'}});
-        $routeProvider.when('/visit/new', {templateUrl: 'modules/patient/views/visit.html', controller: 'VisitController'});
         $routeProvider.when('/login', {templateUrl: 'modules/auth/views/login.html', controller: 'LoginController'});
+        $routeProvider.when('/search', {reloadOnSearch: false, templateUrl: 'modules/patient/views/search.html', controller: 'SearchPatientController', resolve: {initialization: 'initialization'}});
+        $routeProvider.when('/patient/new', {templateUrl: 'modules/patient/views/newpatient.html', controller: 'CreatePatientController', resolve: {initialization: 'initialization'}});
+        $routeProvider.when('/patient/:patientUuid', {templateUrl: 'modules/patient/views/editpatient.html', controller: 'EditPatientController', resolve: {initialization: 'initialization'}});
+        $routeProvider.when('/visit/new', {templateUrl: 'modules/patient/views/visit.html', controller: 'VisitController', resolve: {initialization: 'initialization'}});
+        $routeProvider.when('/printPatient', {templateUrl: 'modules/patient/views/print.html'});
         $routeProvider.when('/patientcommon', {templateUrl: 'modules/patient/views/patientcommon.html'});
         $routeProvider.otherwise({redirectTo: '/login'});
         $httpProvider.defaults.headers.common['Disable-WWW-Authenticate'] = true;
