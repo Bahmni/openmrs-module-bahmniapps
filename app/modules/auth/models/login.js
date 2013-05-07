cbf3859515799777476f0bc7e47265287b566b01
Deepak N <endeep123@gmail.com>
2013-05-07 12:42:30 +0530

    D3 | Redirect to login page if user reloads a page after session timeout

--- app/modules/auth/models/login.js
+++ app/modules/auth/models/login.js
@@ -1,12 +1,14 @@
 'use strict';
 
 angular.module('registration.login', ['registration.sessionService'])
-    .factory('login', ['$rootScope', '$q','sessionService', function ($rootScope, $q, sessionService) {
+    .factory('login', ['$rootScope', '$q', '$location', 'sessionService', function ($rootScope, $q, $location, sessionService) {
         var defer = $q.defer();
 
         sessionService.get().success(function (data) {
             if (data.authenticated) {
                 defer.resolve();
+            } else {
+              $location.path("/login");
             }
         });
 
