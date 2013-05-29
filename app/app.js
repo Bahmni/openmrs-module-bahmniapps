0c5be703c0e079290a328999d236027cc0f526a3
arathyjan <arathyja@thoughtworks.com>
2013-05-29 18:03:23 +0530

    RT, Sush | #989 | getting rid of the unused, undefined module

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-angular.module('opd', ['opd.navigation', 'opd.consultation'])
+angular.module('opd', ['opd.navigation'])
 angular.module('opd').config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/patient/:patientUuid/consultation', {templateUrl: 'modules/opd/views/consultation.html', controller: 'NavigationController'});
         $routeProvider.when('/blank', {templateUrl: 'modules/navigation/views/comingSoon.html'});
