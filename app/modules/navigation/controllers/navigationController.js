8a17b18c9361e17d6427ea24d53cbc0848d5f348
arathyjan <arathyja@thoughtworks.com>
2013-05-31 16:31:47 +0530

    Sush, RT | #989 | more styling

--- app/modules/navigation/controllers/navigationController.js
+++ app/modules/navigation/controllers/navigationController.js
@@ -1,33 +1,36 @@
 'use strict';
 
 angular.module('opd.navigation', [])
-    .controller('NavigationController', ['$scope', '$location','$route', function ($scope, $location, $route) {
-        $scope.blank = function() {
+    .controller('NavigationController', ['$scope', '$location', '$route', function ($scope, $location, $route) {
+        $scope.mainDisplayText = "Consultation";
+        $scope.buttonClass = "button"
+        $scope.blank = function () {
             return $location.url("/blank");
         }
 
-        $scope.treatment= function() {
-            $scope.uuid = $route.current.params.patientUuid;
-            return $location.url("/patient/"+$scope.uuid+"/treatment");
+        $scope.treatment = function () {
+            return buttonClickAction("treatment",false);
         }
 
-        $scope.investigation = function() {
-            $scope.uuid = $route.current.params.patientUuid;
-            return $location.url("/patient/"+$scope.uuid+"/investigation");
+        $scope.investigation = function () {
+            return buttonClickAction("investigation",false);
         }
 
-        $scope.diagnosis = function() {
-            $scope.uuid = $route.current.params.patientUuid;
-            return $location.url("/patient/"+$scope.uuid+"/diagnosis");
+        $scope.diagnosis = function () {
+            return buttonClickAction("diagnosis",false);
         }
 
-        $scope.templates = function() {
-            $scope.uuid = $route.current.params.patientUuid;
-            return $location.url("/patient/"+$scope.uuid+"/templates");
+        $scope.templates = function () {
+            return buttonClickAction("templates",false);
         }
 
-        $scope.consultation = function() {
+        $scope.consultation = function () {
+            return buttonClickAction("consultation",true);
+        }
+
+        var buttonClickAction = function (url, isMainPage) {
+            $scope.mainDisplayText = isMainPage == true ? "Consultation" : "Done";
             $scope.uuid = $route.current.params.patientUuid;
-            return $location.url("/patient/"+$scope.uuid+"/consultation");
+            return $location.url("/patient/" + $scope.uuid + "/" + url);
         }
     }]);
