48ca7eec4e017edc7c9f33d8bb9fb98a7eba301a
arathyjan <arathyja@thoughtworks.com>
2013-06-04 12:05:19 +0530

    RT, Sush | #989 | cleaning up the consultation page 1) added notes page 2) navigation from consultation to subpages using the divs changes button text 3) renamed investigation to investigations

--- app/app.js
+++ app/app.js
@@ -1,12 +1,13 @@
 'use strict';
 
 
-angular.module('opd', ['opd.navigation','opd.diagnosisController','opd.treatmentController','opd.investigationController'])
+angular.module('opd', ['navigation.navigationController','opd.diagnosisController','opd.treatmentController','opd.investigationController'])
 angular.module('opd').config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/patient/:patientUuid/consultation', {templateUrl: 'modules/opd/views/consultation.html', controller: 'NavigationController'});
+        $routeProvider.when('/patient/:patientUuid/consultation', {templateUrl: 'modules/opd/views/consultation.html'});
         $routeProvider.when('/patient/:patientUuid/diagnosis', {templateUrl: 'modules/opd/views/addObservation.html', controller: 'DiagnosisController'});
         $routeProvider.when('/patient/:patientUuid/treatment', {templateUrl: 'modules/opd/views/addObservation.html', controller: 'TreatmentController'});
         $routeProvider.when('/patient/:patientUuid/investigation', {templateUrl: 'modules/opd/views/addObservation.html', controller: 'InvestigationController'});
+        $routeProvider.when('/patient/:patientUuid/notes', {templateUrl: 'modules/opd/views/notes.html'});
         $routeProvider.when('/patient/:patientUuid/templates', {templateUrl: 'modules/navigation/views/comingSoon.html'});
         $routeProvider.otherwise('/blank', {templateUrl: 'modules/navigation/views/comingSoon.html'});
     }]);
--- app/modules/navigation/controllers/navigationController.js
+++ app/modules/navigation/controllers/navigationController.js
@@ -1,35 +1,39 @@
 'use strict';
 
-angular.module('opd.navigation', [])
+angular.module('navigation.navigationController', [])
     .controller('NavigationController', ['$scope', '$location', '$route', function ($scope, $location, $route) {
-        $scope.mainDisplayText = "Consultation";
-        $scope.buttonClass = "button"
+        $scope.mainButtonText = "Consultation"
+
         $scope.blank = function () {
             return $location.url("/blank");
         }
 
         $scope.treatment = function () {
-            return buttonClickAction("treatment",false);
+            return buttonClickAction("treatment", false);
         }
 
         $scope.investigation = function () {
-            return buttonClickAction("investigation",false);
+            return buttonClickAction("investigation", false);
         }
 
         $scope.diagnosis = function () {
-            return buttonClickAction("diagnosis",false);
+            return buttonClickAction("diagnosis", false);
         }
 
         $scope.templates = function () {
-            return buttonClickAction("templates",false);
+            return buttonClickAction("templates", false);
         }
 
         $scope.consultation = function () {
-            return buttonClickAction("consultation",true);
+            return buttonClickAction("consultation", true);
+        }
+
+        $scope.notes = function () {
+            return buttonClickAction("notes", false);
         }
 
         var buttonClickAction = function (url, isMainPage) {
-            $scope.mainDisplayText = isMainPage == true ? "Consultation" : "Done";
+            $scope.mainButtonText = isMainPage == true ? "Consultation" : "Done";
             $scope.uuid = $route.current.params.patientUuid;
             return $location.url("/patient/" + $scope.uuid + "/" + url);
         }
