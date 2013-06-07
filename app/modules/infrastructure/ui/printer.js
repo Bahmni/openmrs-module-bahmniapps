84a95870258b50a20f3b0185167331929f8192e5
Deepak N <endeep123@gmail.com>
2013-06-07 16:54:28 +0530

    1032 : Fixing print not showing patient image

--- app/modules/infrastructure/ui/printer.js
+++ app/modules/infrastructure/ui/printer.js
@@ -4,6 +4,10 @@ angular.module('infrastructure.printer', [])
     .factory('printer', [function () {
         var print = function (printElementId) {
                         var hiddenFrame = $('<iframe style="display: none" id="printPatientFrame"></iframe>').appendTo('body')[0];
+                        hiddenFrame.contentWindow.printAndRemove = function() {
+                            hiddenFrame.contentWindow.print();
+                            $(hiddenFrame).remove();
+                        }
                         var code = "<!doctype html>"+
                                     "<html>"+
                                         "<head>" +
@@ -12,15 +16,13 @@ angular.module('infrastructure.printer', [])
                                                 'h1, h2, h3 { padding: 0; margin: 0;}' +
                                             '</style>'+
                                        "</head>"+
-                                        "<body>" +
+                                        '<body onload="printAndRemove();">' +
                                              document.getElementById(printElementId).innerHTML +
                                         "</body>"+
                                     "</html>";
                         var doc = hiddenFrame.contentWindow.document.open("text/html", "replace");
                         doc.write(code);
                         doc.close();
-                        hiddenFrame.contentWindow.print();
-                        $(hiddenFrame).remove();
                     };
         return {
             print: print
