02269aa5451192e7b1f04241fd20ea05b0fe7ca1
Pulkit <pulkit.bosco05@gmail.com>
2013-06-12 23:47:18 +0530

    Moving to bower based dependency management. All front end components (except bootstrap and jqueryui) have been moved to bower.

--- test/config/testacular.conf.js
+++ test/config/testacular.conf.js
@@ -3,8 +3,8 @@ basePath = '../';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  '../app/lib/angular-1.0.5/angular.js',
-  'lib/angular-1.0.5/angular-mocks.js',
+  '../app/components/angular/angular.js',
+  '../app/components/angular-mocks/angular-mocks.js',
   '../app/defaults.js',
   '../app/constants.js',
   '../app/modules/**/*.js',
