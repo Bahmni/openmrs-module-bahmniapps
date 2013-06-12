6c54fd5a1404d944c788afb1ee6221110fc51abe
Vinay Venu <vinayv@thoughtworks.com>
2013-06-12 16:02:43 +0530

    Sneha, Vinay | #1012 | Several changes. 1. Integrate with new visit API. 2. Change registration fee to 0 for returning patient 3. Add validation for zero registration fee and no comment
    Conflicts:
    	app/modules/patient/controllers/visitController.js

--- app/modules/patient/controllers/visitController.js
+++ app/modules/patient/controllers/visitController.js
@@ -1,101 +1,152 @@
 'use strict';
 
-angular.module('registration.visitController', ['resources.patientService', 'resources.visitService', 'resources.concept', 'resources.bmi','resources.date', 'infrastructure.spinner', 'infrastructure.printer'])
-    .controller('VisitController', ['$scope', '$location', 'patientService', 'visitService', 'concept', 'bmi','date', '$window', '$route', 'spinner', '$timeout', 'printer',
-                function ($scope, $location, patientService, visitService, conceptService, bmiModule, date, $window, $route, spinner, $timeout, printer) {
-    var registrationConcepts = [];
+angular.module('registration.visitController', ['resources.patientService', 'resources.visitService', 'resources.concept', 'resources.bmi', 'resources.date', 'infrastructure.spinner', 'infrastructure.printer'])
+    .controller('VisitController', ['$scope', '$location', 'patientService', 'visitService', 'concept', 'bmi', 'date', '$window', '$route', 'spinner', '$timeout', '$q', 'printer', function ($scope, $location, patientService, visitService, conceptService, bmiModule, date, $window, $route, spinner, $timeout, $q, printer) {
+        var registrationConcepts = [];
 
-    (function () {
-        $scope.encounter = {};
-        $scope.obs = {};
-        $scope.visit = {};
-        $scope.patient = patientService.getPatient();
-        $scope.obs.registration_fees = defaults.registration_fees($scope.patient.isNew);
+        (function () {
+            $scope.encounter = {};
+            $scope.obs = {};
+            $scope.visit = {};
+            $scope.patient = patientService.getPatient();
+            $scope.obs.registration_fees = defaults.registration_fees($scope.patient.isNew);
 
-        var registrationConceptsPromise = conceptService.getRegistrationConcepts().success(function (data) {
-            var concepts = data.results[0].setMembers;
-            concepts.forEach(function (concept) {
-                registrationConcepts.push({name: concept.name.name, uuid: concept.uuid });
+            var registrationConceptsPromise = conceptService.getRegistrationConcepts().success(function (data) {
+                var concepts = data.results[0].setMembers;
+                concepts.forEach(function (concept) {
+                    registrationConcepts.push({name: concept.name.name, uuid: concept.uuid });
+                });
             });
-        });
-        spinner.forPromise(registrationConceptsPromise);
-    })();
+            spinner.forPromise(registrationConceptsPromise);
+        })();
 
-    $scope.calculateBMI = function () {
-        if($scope.obs.height && $scope.obs.weight){
-            var bmi = bmiModule.calculateBmi($scope.obs.height, $scope.obs.weight);
-            var valid = bmi.valid();
-            $scope.obs.bmi_error = !valid;
-            $scope.obs.bmi = bmi.value;
-            $scope.obs.bmi_status = valid ? bmi.status() : "Invalid";
-        } else {
-            $scope.obs.bmi_error = false;
-            $scope.obs.bmi = null;
-            $scope.obs.bmi_status = null;
-        }
-    };
-
-    $scope.back = function () {
-        $window.history.back();
-    };
-
-    $scope.registrationFeeLabel = $scope.patient.isNew ? "Registration Fee" : "Consultation Fee";
-
-    var save = function(successCallback){
-        var datetime = date.now().toISOString();
-        $scope.visit.patient = $scope.patient.uuid;
-        $scope.visit.startDatetime = datetime;
-        $scope.visit.visitType = $scope.patient.isNew ? constants.visitType.registration : constants.visitType.returningPatient;
-
-        $scope.encounter.patient = $scope.patient.uuid;
-        $scope.encounter.encounterDatetime = datetime;
-        $scope.encounter.encounterType = constants.visitType.registration;
-
-        $scope.encounter.obs = [];
-        registrationConcepts.forEach(function (concept) {
-            var conceptName = concept.name.replace(" ","_").toLowerCase();
-            var value = $scope.obs[conceptName];
-            if(value != null && value!=""){
-                $scope.encounter.obs.push({concept:concept.uuid,value:value});
+        $scope.calculateBMI = function () {
+            if ($scope.obs.height && $scope.obs.weight) {
+                var bmi = bmiModule.calculateBmi($scope.obs.height, $scope.obs.weight);
+                var valid = bmi.valid();
+                $scope.obs.bmi_error = !valid;
+                $scope.obs.bmi = bmi.value;
+                $scope.obs.bmi_status = valid ? bmi.status() : "Invalid";
+            } else {
+                $scope.obs.bmi_error = false;
+                $scope.obs.bmi = null;
+                $scope.obs.bmi_status = null;
             }
-        });
+        };
+
         $scope.visit.encounters = [$scope.encounter];
 
-        var createVisitPromise = visitService.create($scope.visit).success(function(){
-            patientService.clearPatient();
-            if(successCallback) successCallback();
-            var path = $scope.patient.isNew ? "/patient/new" : "/search";
-            $timeout(function(){
+        $scope.back = function () {
+            $window.history.back();
+        };
+
+        $scope.registrationFeeLabel = $scope.patient.isNew ? "Registration Fee" : "Consultation Fee";
+
+        $scope.print = function () {
+            return $timeout(function () {
+                printer.print('registrationCard');
+            }, 0);
+        };
+
+        $scope.save = function () {
+            var datetime = date.now().toISOString();
+            $scope.visit.patient = $scope.patient.uuid;
+            $scope.visit.startDatetime = datetime;
+            $scope.visit.visitType = $scope.patient.isNew ? constants.visitType.registration : constants.visitType.returningPatient;
+
+            $scope.encounter.patient = $scope.patient.uuid;
+            $scope.encounter.encounterDatetime = datetime;
+            $scope.encounter.encounterType = constants.visitType.registration;
+
+            $scope.encounter.obs = [];
+            registrationConcepts.forEach(function (concept) {
+                var conceptName = concept.name.replace(" ", "_").toLowerCase();
+                var value = $scope.obs[conceptName];
+                if (value != null && value != "") {
+                    $scope.encounter.obs.push({concept: concept.uuid, value: value});
+                }
+            });
+            $scope.visit.encounters = [$scope.encounter];
+
+            var createPromise = visitService.create($scope.visit);
+            spinner.forPromise(createPromise);
+            return createPromise;
+        }
+
+        $scope.moveToNextPage = function () {
+            return $timeout(function () {
+                var path = $scope.patient.isNew ? "/patient/new" : "/search";
                 $location.path(path);
-            }, 100)
-        });
-        spinner.forPromise(createVisitPromise);
-    }
+            }, 100);
+        };
 
-    $scope.submit = function(){
-        if($scope.patient.isNew) {
-            $scope.saveAndPrint();
+        $scope.validate = function () {
+            if ($scope.obs.registration_fees === 0 && (!$scope.obs.comments || $scope.obs.comments === '')) {
+                return $scope.confirmDialog.show();
+            } else {
+                var deferred = $q.defer();
+                deferred.resolve();
+                return deferred.promise;
+            }
         }
-        else {
-            save();
+
+        $scope.submit = function () {
+            if ($scope.patient.isNew) {
+                return $scope.saveAndPrint();
+            }
+            else {
+                return $scope.validate().then($scope.save).then(patientService.clearPatient).then($scope.moveToNextPage);
+            }
+        };
+
+        $scope.saveAndPrint = function () {
+            return $scope.validate().then($scope.save).then(patientService.clearPatient).then($scope.print).then($scope.moveToNextPage);
+        };
+
+        $scope.today = function () {
+            return new Date();
+        };
+
+        $scope.printLayout = function () {
+            return $route.routes['/printPatient'].templateUrl;
+        };
+    }])
+
+    .directive('confirmDialog', function ($q) {
+        return function (scope, element, attrs) {
+            var dialogObject = {};
+            dialogObject.dialogElement = $('#dialog-confirm');
+            dialogObject.continue = false;
+            dialogObject.dialogElement.dialog({autoOpen: false,
+                buttons: {
+                    "Continue": function () {
+                        dialogObject.continue = true;
+                        $(this).dialog("close");
+                    },
+                    "Go back": function () {
+                        dialogObject.continue = false;
+                        $(this).dialog("close");
+                    }
+                }
+            });
+
+            dialogObject.show = function () {
+                var deferred = $q.defer();
+
+                var dialogObject = this;
+                this.dialogElement.on("dialogclose", function () {
+                    scope.$apply(function () {
+                        if (dialogObject.continue) {
+                            deferred.resolve();
+                        } else {
+                            deferred.reject();
+                        }
+                    });
+                });
+                this.dialogElement.dialog('open');
+                return deferred.promise;
+            };
+
+            scope[attrs['handle']] = dialogObject;
         }
-    }
-
-    $scope.saveAndPrint = function () {
-        save(function(){
-            $scope.printPatient();
-        });
-    };
-
-    $scope.today = function(){
-        return new Date();
-    };
-
-    $scope.printLayout = function() {
-        return $route.routes['/printPatient'].templateUrl;
-    };
-
-    $scope.printPatient = function () {
-        printer.print('registrationCard');
-    };
- }]);
+    });
--- test/unit/patient/controllers/visitController.spec.js
+++ test/unit/patient/controllers/visitController.spec.js
@@ -12,10 +12,23 @@ describe('VisitController', function () {
     var $location;
     var $window;
     var $timeout;
-    var createPromise;
     var spinner;
+    var stubAllPromise = function () {
+        return {
+            then: function () {
+                return stubAllPromise();
+            }
+        }
+    }
+    var stubOnePromise = function () {
+        return {
+            then: function (callBack) {
+                return callBack();
+            }
+        }
+    }
 
-    var sampleConcepts={
+    var sampleConcepts = {
         "results": [
             {
                 "setMembers": [
@@ -57,9 +70,7 @@ describe('VisitController', function () {
             }
         };
         spinner = jasmine.createSpyObj('spinner', ['forPromise'])
-        visitService = jasmine.createSpyObj('visit', ['create']);
-        createPromise = jasmine.createSpyObj('createPromise', ['success'])
-        visitService.create.andReturn(createPromise)
+        visitService = jasmine.createSpyObj('visitService', ['create', 'clearPatient']);
     }]));
 
     describe('initialization', function () {
@@ -72,8 +83,8 @@ describe('VisitController', function () {
                 patientService: patientService
             });
 
-           expect(scope.obs.registration_fees).toBe(defaults.registration_fees_newPatient);
-           expect(scope.patient).toBe(patient);
+            expect(scope.obs.registration_fees).toBe(defaults.registration_fees_newPatient);
+            expect(scope.patient).toBe(patient);
         });
 
         it('should set the registration fee for returning patient', function () {
@@ -85,13 +96,14 @@ describe('VisitController', function () {
                 patientService: patientService
             });
 
-           expect(scope.obs.registration_fees).toBe(defaults.registration_fees_oldPatient);
-           expect(scope.patient).toBe(patient);
+            expect(scope.obs.registration_fees).toBe(defaults.registration_fees_oldPatient);
+            expect(scope.patient).toBe(patient);
         });
     });
 
-    describe("saveAndPrint", function(){
-        beforeEach(function(){
+    describe("validate", function () {
+        beforeEach(function () {
+            scope = { "$watch": jasmine.createSpy() };
             $controller('VisitController', {
                 $scope: scope,
                 concept: conceptService,
@@ -101,13 +113,101 @@ describe('VisitController', function () {
                 spinner: spinner,
                 date: date
             });
-            visitService.create.andReturn(createPromise)
-            scope.patient = {uuid: "21308498-2502-4495-b604-7b704a55522d", isNew: "true"}
+            spyOn(scope, 'save').andCallFake(stubAllPromise);
         });
-    
-        it("should create visit", function(){
+
+        it("should be called during save and print", function () {
+            scope.validate = jasmine.createSpy('validate').andCallFake(stubOnePromise);
+
+            scope.saveAndPrint();
+
+            expect(scope.validate).toHaveBeenCalled();
+        })
+
+        it("should be called during submit for new patient", function () {
+            scope.patient.isNew = true;
+            scope.validate = jasmine.createSpy('validate').andCallFake(stubOnePromise);
+
+            scope.submit();
+
+            expect(scope.validate).toHaveBeenCalled();
+        });
+
+        it("should be called during submit for existing patient", function () {
+            scope.patient.isNew = false;
+            scope.validate = jasmine.createSpy('validate').andCallFake(stubOnePromise);
+
+            scope.submit();
+
+            expect(scope.validate).toHaveBeenCalled();
+        });
+
+        it("should open popup when consultation fee = 0 and comments are empty", function () {
+            scope.obs.registration_fees = 0;
+            scope.obs.comments = '';
+            scope.confirmDialog = jasmine.createSpyObj('confirmDialog', ['show']);
+            scope.confirmDialog.show.andCallFake(stubOnePromise);
+
+            scope.validate();
+
+            expect(scope.confirmDialog.show).toHaveBeenCalled();
+        });
+
+        it("should not open popup when consultation fee != 0", function () {
+            scope.obs.registration_fees = 10;
+            scope.obs.comments = '';
+            scope.confirmDialog = jasmine.createSpyObj('confirmDialog', ['show']);
+            scope.confirmDialog.show.andCallFake(stubOnePromise);
+
+            scope.validate();
+
+            expect(scope.confirmDialog.show).not.toHaveBeenCalled();
+        });
+
+        it("should not open popup when comments are not empty", function () {
+            scope.obs.registration_fees = 0;
+            scope.obs.comments = 'adfadfs';
+            scope.confirmDialog = jasmine.createSpyObj('confirmDialog', ['show']);
+            scope.confirmDialog.show.andCallFake(stubOnePromise);
+
+            scope.validate();
+
+            expect(scope.confirmDialog.show).not.toHaveBeenCalled();
+        })
+
+        it("should always return a promise", function () {
+            scope.obs.registration_fees = 0;
+            scope.obs.comments = 'adfadfs';
+            scope.confirmDialog = jasmine.createSpyObj('confirmDialog', ['show']);
+            scope.confirmDialog.show.andCallFake(stubOnePromise);
+
+            var result = scope.validate();
+
+            expect(result.then).toBeTruthy();
+        });
+    })
+
+    describe("saveAndPrint", function () {
+        beforeEach(function () {
+            $controller('VisitController', {
+                $scope: scope,
+                concept: conceptService,
+                patientService: patientService,
+                visitService: visitService,
+                $location: $location,
+                spinner: spinner,
+                date: date
+            });
+            visitService.create.andCallFake(stubOnePromise);
+            patientService.clearPatient.andCallFake(stubOnePromise);
+            scope.patient = {uuid: "21308498-2502-4495-b604-7b704a55522d", isNew: "true"};
+            spyOn(scope, 'validate').andCallFake(stubOnePromise);
+        });
+
+        it("should create visit", function () {
             var now = new Date();
-            spyOn(date, "now").andReturn(now)
+            spyOn(date, "now").andReturn(now);
+            scope.print = jasmine.createSpy().andCallFake(stubOnePromise);
             scope.obs.comments = "fine";
             scope.obs.registration_fees = "100";
 
@@ -119,50 +219,62 @@ describe('VisitController', function () {
             expect(scope.encounter.patient).toBe(scope.patient.uuid);
             expect(scope.encounter.encounterDatetime).toBe(now.toISOString());
             expect(scope.encounter.encounterType).toBe(constants.visitType.registration);
-            expect(scope.encounter.obs).toEqual([{concept: "d7c66ef3-1f55-4b83-8f10-009e4acec1e3",value: "fine"},{concept: "8032ad29-0591-4ec6-9776-22e7a3062df8",value: "100"}])
-            expect(scope.visit.encounters).toEqual([scope.encounter]) ;
+            expect(scope.encounter.obs).toEqual([
+                {concept: "d7c66ef3-1f55-4b83-8f10-009e4acec1e3", value: "fine"},
+                {concept: "8032ad29-0591-4ec6-9776-22e7a3062df8", value: "100"}
+            ])
+            expect(scope.visit.encounters).toEqual([scope.encounter]);
             expect(visitService.create).toHaveBeenCalledWith(scope.visit);
         });
 
-        it("should print patient and go to create new page on creation of visit for new patient", function () {
-            spyOn($location, 'path');
-            spyOn(scope, 'printPatient');
-            scope.saveAndPrint();
-            expect(createPromise.success).toHaveBeenCalled();
+        describe("once saved and printed", function () {
+            beforeEach(function () {
+                scope = { "$watch": jasmine.createSpy() };
+                $controller('VisitController', {
+                    $scope: scope,
+                    concept: conceptService,
+                    patientService: patientService,
+                    visitService: visitService,
+                    $location: $location,
+                    spinner: spinner,
+                    date: date
+                });
+                scope.print = jasmine.createSpy().andCallFake(stubOnePromise);
+                scope.save = jasmine.createSpy().andCallFake(stubOnePromise);
+                spyOn(scope, 'validate').andCallFake(stubOnePromise);
+            });
 
-            createPromise.success.mostRecentCall.args[0]();
+            it(" should print patient and go to create new page on creation of visit for new patient", function () {
+                scope.patient.isNew = true;
+                spyOn($location, 'path');
+                scope.saveAndPrint();
 
-            expect(scope.printPatient).toHaveBeenCalled();
-            $timeout.flush();
-            expect($location.path).toHaveBeenCalledWith("/patient/new");
+                expect(scope.print).toHaveBeenCalled();
+                $timeout.flush();
+                expect($location.path).toHaveBeenCalledWith("/patient/new");
+            });
+
+            it("should print patient and go to search on creation of visit for edit of patient", function () {
+                scope.patient.isNew = false;
+                spyOn($location, 'path');
+
+                scope.saveAndPrint();
+
+                expect(scope.print).toHaveBeenCalled();
+                $timeout.flush();
+                expect($location.path).toHaveBeenCalledWith("/search");
+            });
+
+            it("should clear the stored patient on success", function () {
+                scope.saveAndPrint();
+
+                expect(patientService.clearPatient).toHaveBeenCalled();
+            });
         });
-
-        it("should print patient and go to search on creation of visit for edit of patient", function () {
-            scope.patient.isNew=false;
-            spyOn($location, 'path');
-            spyOn(scope, 'printPatient');
-            scope.saveAndPrint();
-            expect(createPromise.success).toHaveBeenCalled();
-
-            createPromise.success.mostRecentCall.args[0]();
-
-            expect(scope.printPatient).toHaveBeenCalled();
-            $timeout.flush();
-            expect($location.path).toHaveBeenCalledWith("/search");
-        });
-
-         it("should clear the stored patient on success", function(){
-             spyOn(scope, 'printPatient');
-             scope.saveAndPrint();
-                
-            createPromise.success.mostRecentCall.args[0]();
-            
-          expect(patientService.clearPatient).toHaveBeenCalled();
-       });
     });
 
-    describe("submit", function(){
-        beforeEach(function(){
+    describe("submit", function () {
+        beforeEach(function () {
             $controller('VisitController', {
                 $scope: scope,
                 concept: conceptService,
@@ -170,52 +282,46 @@ describe('VisitController', function () {
                 spinner: spinner,
                 visitService: visitService
             });
+            visitService.create.andCallFake(stubOnePromise);
+            patientService.clearPatient.andCallFake(stubOnePromise);
             scope.patient = {uuid: "21308498-2502-4495-b604-7b704a55522d"}
-            spyOn(scope, 'printPatient');
+            spyOn(scope, 'print').andCallFake(stubOnePromise);
+            spyOn($location, 'path');
+            spyOn(scope, 'validate').andCallFake(stubOnePromise);
         });
 
-        describe("on successful creation of new patient visit", function(){
-            beforeEach(function(){
+        describe("on successful creation of new patient visit", function () {
+            beforeEach(function () {
                 scope.patient.isNew = true;
             });
 
-            it("should print the card for new patient", function(){
+            it("should print the card for new patient", function () {
                 scope.submit();
 
-                createPromise.success.mostRecentCall.args[0]();
-
-                expect(scope.printPatient).toHaveBeenCalled();
+                expect(scope.print).toHaveBeenCalled();
             });
 
-            it("should go to new patient registration page", function(){
-                spyOn($location, 'path');
-
+            it("should go to new patient registration page", function () {
                 scope.submit();
-                createPromise.success.mostRecentCall.args[0]();
                 $timeout.flush();
 
                 expect($location.path).toHaveBeenCalledWith("/patient/new");
             });
         })
 
-        describe("on successful creation of returning patient visit", function(){
-            beforeEach(function(){
+        describe("on successful creation of returning patient visit", function () {
+            beforeEach(function () {
                 scope.patient.isNew = false;
             });
 
-            it("should not print card for returning patient", function(){
+            it("should not print card for returning patient", function () {
                 scope.submit();
 
-                createPromise.success.mostRecentCall.args[0]();
-
-                expect(scope.printPatient).not.toHaveBeenCalled();
+                expect(scope.print).not.toHaveBeenCalled();
             });
 
-            it("should go to search page", function(){
-                spyOn($location, 'path');
-
+            it("should go to search page", function () {
                 scope.submit();
-                createPromise.success.mostRecentCall.args[0]();
                 $timeout.flush();
 
                 expect($location.path).toHaveBeenCalledWith("/search");
@@ -223,8 +329,8 @@ describe('VisitController', function () {
         })
     });
 
-    describe("calculateBMI", function(){
-        beforeEach(function(){
+    describe("calculateBMI", function () {
+        beforeEach(function () {
             $controller('VisitController', {
                 $scope: scope,
                 concept: conceptService,
@@ -233,7 +339,7 @@ describe('VisitController', function () {
             });
         });
 
-        it("should set bmi, bmi_status and bmi_error when height and weight are present", function(){
+        it("should set bmi, bmi_status and bmi_error when height and weight are present", function () {
             scope.obs.height = 50;
             scope.obs.weight = 100;
 
@@ -245,13 +351,12 @@ describe('VisitController', function () {
 
         });
 
-        it("should clear the bmi, bmi_status when height is not present", function(){
+        it("should clear the bmi, bmi_status when height is not present", function () {
             scope.obs.bmi = 200;
             scope.obs.bmi_status = "Invalid";
             scope.obs.bmi_error = true;
             scope.obs.height = null;
             scope.obs.weight = 100;
-
 
             scope.calculateBMI();
 
@@ -260,13 +365,12 @@ describe('VisitController', function () {
             expect(scope.obs.bmi_status).toBe(null);
         });
 
-        it("should clear the bmi, bmi_status when weight is not present", function(){
+        it("should clear the bmi, bmi_status when weight is not present", function () {
             scope.obs.bmi = 200;
             scope.obs.bmi_status = "Invalid";
             scope.obs.bmi_error = true;
             scope.obs.height = 100;
             scope.obs.weight = null;
-
 
             scope.calculateBMI();
 
