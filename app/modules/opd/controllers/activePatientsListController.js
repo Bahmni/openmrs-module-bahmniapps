ca5f335d78e52a21664f404e3896702317b309fd
indraneelr <indraneel.rr@gmail.com>
2013-06-20 18:15:21 +0530

    IN, RT | #1030 | Infinite scroll implementation

--- app/modules/opd/controllers/activePatientsListController.js
+++ app/modules/opd/controllers/activePatientsListController.js
@@ -10,54 +10,64 @@ angular.module('opd.activePatientsListController', ['opd.patientsListService', '
                 datum.image = patientService.constructImageUrl(datum.identifier);
             });
             $scope.activePatientsList = data;
-                $scope.visiblePatientsList= $scope.activePatientsList.slice(0,2);
+            $scope.visiblePatientsList= $scope.activePatientsList.slice(0,30);
         });
     }
 
-    $scope.getActivePatientList();
 
-        $scope.loadMore = function() {
+
+    $scope.loadMore = function() {
         if($scope.visiblePatientsList !== undefined){
             var last = $scope.visiblePatientsList.length - 1;
-            for(var i = 1; i <= 2; i++) {
+            for(var i = 1; i <=8; i++) {
                 $scope.visiblePatientsList.push($scope.activePatientsList[i+last]);
             }
         }
     };
 
-
+    $scope.getActivePatientList();
 
     $scope.matchesNameOrId = function(patient){
-        if(patient !== undefined && patient.name !== undefined && patient.identifier !== undefined){
-            if(patient.name.search($scope.searchParameter) >=0 || patient.identifier.search($scope.searchParameter) >= 0){
+        if(patient !== undefined && patient.name !== undefined && patient.identifier !== undefined ){
+            if($scope.searchParameter === undefined){
                 return true;
             }
+            if(patient.name.toLowerCase().search($scope.searchParameter.toLowerCase()) >=0
+                || patient.identifier.toLowerCase().search($scope.searchParameter.toLowerCase()) >= 0){
+                return true;
+            }
+
         }
         return false;
     };
 
-    }]).directive('resize', function ($window) {
-    return function (scope, element) {
-        var w = angular.element($window);
-        scope.getWindowDimensions = function () {
-            return { 'h': w.height(), 'w': w.width() };
+
+
+    }])/*.directive('resize', function ($window) {
+        return function (scope,element) {
+            scope.width =  element.get(0).offsetWidth;
+            scope.height = $window.innerHeight;
+
+            scope.tileContainer = element.get(0);
+
+            angular.element($window).bind('resize', function () {
+                scope.$apply(function () {
+                    scope.width = $window.innerWidth;
+                    scope.height = $window.innerHeight;
+                });
+            });
         };
-        scope.$watch(scope.getWindowDimensions, function (newValue, oldValue) {
-            scope.windowHeight = newValue.h;
-            scope.windowWidth = newValue.w;
+    })
 
-            scope.style = function () {
-                return {
-                    'height': (newValue.h - 100) + 'px',
-                    'width': (newValue.w - 100) + 'px'
-                };
-            };
+    /*.directive('recordSize', function ($window) {
+        return function (scope,element,attrs) {
 
-        }, true);
+            var recordSizeConfig = attrs.recordSize;
+            if(scope.recordSize == undefined){
+            scope.recordSize = {width : element.get(0).offsetWidth, height : element.get(0).offsetHeight};
 
-        w.bind('resize', function () {
-            scope.$apply();
-        });
-    }
-})
+            scope.loadMore();
+        }
+        };
+    });*/
 
