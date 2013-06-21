86ba8ac46c551a99e8e253902461864023df24bf
indraneelr <indraneel.rr@gmail.com>
2013-06-21 15:20:35 +0530

    IN,RT | #1030 | build fix

--- test/config/testacular.conf.js
+++ test/config/testacular.conf.js
@@ -3,14 +3,13 @@ basePath = '../';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  '../app/lib/angular-1.0.5/angular.js',
-  'lib/angular-1.0.5/angular-mocks.js',
-  '../app/defaults.js',
-  '../app/constants.js',
-  '../app/modules/**/*.js',
-  'support/**/*.js',
-  'unit/**/*.js',
-  '../app/components/**/*.js'
+    '../app/lib/angular-1.0.5/angular.js',
+    'lib/angular-1.0.5/angular-mocks.js',
+    '../app/constants.js',
+    '../app/components/**/*.js',
+    '../app/modules/**/*.js',
+    'support/**/*.js',
+    'unit/**/*.js'
 ];
 
 singleRun = true;
