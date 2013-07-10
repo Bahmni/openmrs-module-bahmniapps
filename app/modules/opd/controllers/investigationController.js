d6810e142e3a08d8a9f55e01ab615f74bd0e30ce
Vinkesh Banka <vinkesh.banka@gmail.com>
2013-07-10 17:05:23 +0530

    RT, Banka | #1089 | Using treeSelector directive in investigation page to show lab, radiology, endoscopy and others conceptTree.

--- app/modules/opd/controllers/investigationController.js
+++ app/modules/opd/controllers/investigationController.js
@@ -2,4 +2,19 @@
 
 angular.module('opd.investigationController', [])
     .controller('InvestigationController', ['$scope', function ($scope) {
+
+        $scope.tabs = [
+            {name: 'Lab', template: 'LabTreeSelect'},
+            {name: 'Radiology', template: 'RadiologyTreeSelect'},
+            {name: 'Endoscopy', template: 'EndoscopyTreeSelect'},
+            {name: 'Others', template: 'OthersTreeSelect'},
+        ]
+
+        $scope.activateTab = function(tab){
+            $scope.activeTab && ($scope.activeTab.klass="");
+            $scope.activeTab = tab;
+            $scope.activeTab.klass="active";
+        }
+
+        $scope.activateTab($scope.tabs[0]);
     }]);
