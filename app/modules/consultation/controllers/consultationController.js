4dabc7cdb2a24a5eb1311ca6e8dcf38b0100e4e6
Deepak N <endeep123@gmail.com>
2013-07-25 15:39:41 +0530

    Shruthi,d3| #1090 | Showing tests in consultation page

--- app/modules/consultation/controllers/consultationController.js
+++ app/modules/consultation/controllers/consultationController.js
@@ -1,17 +1,20 @@
 'use strict';
 
 angular.module('opd.consultation.controllers')
-    .controller('ConsultationController', ['$scope', '$rootScope', 'consultationService', '$route', function ($scope, $rootScope, consultationService, $route) {
+    .controller('ConsultationController', ['$scope', '$rootScope', 'consultationService', '$route', '$location', function ($scope, $rootScope, consultationService, $route, $location) {
       $scope.patient = $rootScope.currentPatient;
 	  $scope.patient.uuid = $route.current.params.patientUuid; //TODO: Remove this once load patient story is played
 
       $scope.save = function() {
         var encocounterData = {};
-        encocounterData.patientUUID = $scope.patient.uuid; //Theres work
+        encocounterData.patientUUID = $scope.patient.uuid;
         encocounterData.encounterTypeUUID = $rootScope.encounterConfig.getOpdConsultationEncounterUUID();
         encocounterData.testOrders = $rootScope.currentConsultation.tests.map(function(test) {
         	return { conceptUUID: test.uuid }
         });
-        consultationService.create(encocounterData);
+        consultationService.create(encocounterData).success(function(){
+        	$rootScope.currentConsultation = {};
+        	$location.url(Bahmni.Opd.Constants.activePatientsListUrl);
+        });
       };      
 }]);
