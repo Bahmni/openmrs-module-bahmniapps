b47d26660971eb461499dfb919d3d8125a4b1b63
angshu <angshuonline@gmail.com>
2013-09-10 17:44:54 +0530

    angshu,indraneel|925|opd patient modeule. removing patient/module/infrastucture code and using opd/common/infrastructure instead.

--- app/patients/constants.js
+++ app/patients/constants.js
@@ -2,7 +2,6 @@ var Bahmni = Bahmni || {};
 Bahmni.Opd = Bahmni.Opd || {};
 
 Bahmni.Opd.Constants = {
-    bahmniConfigurationUrl: "/openmrs/ws/rest/v1/bahmnicore/conf",
     patientTileHeight:100,
     patientTileWidth:100,
     tileLoadRatio:1/2,
--- app/patients/modules/patient/initialization.js
+++ app/patients/modules/patient/initialization.js
@@ -1,12 +1,11 @@
 'use strict';
 
-angular.module('opd.admission').factory('initialization', ['$rootScope', '$q', 'configurationService',
+angular.module('opd.patient').factory('initialization', ['$rootScope', '$q', 'configurationService',
         function ($rootScope, $q, configurationService) {
             var deferrable = $q.defer();
-            var cfgs = configurationService.getConfigurations(['bahmniConfiguration', 'encounterConfig']);
+            var cfgs = configurationService.getConfigurations(['bahmniConfiguration']);
             cfgs.then(function(configurations) {
                 $rootScope.bahmniConfiguration = configurations.bahmniConfiguration;
-                $rootScope.encounterConfig = configurations.encounterConfig;
                 deferrable.resolve();
             });
             return deferrable.promise;
