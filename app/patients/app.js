5a86ec6d6804ab2adb2e47e241e4888ad9512285
Deepak N <endeep123@gmail.com>
2013-09-12 11:30:09 +0530

    Fixing dist

--- app/patients/app.js
+++ app/patients/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('patients', ['opd.patient', 'bahmni.common.infrastructure'])
+angular.module('patients', ['opd.patient', 'bahmni.common.infrastructure', 'bahmni.common.patient'])
 angular.module('patients').config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when("/", { templateUrl: 'modules/patient/views/activePatientsList.html', controller: 'ActivePatientsListController', resolve: {initialization: 'initialization'}});
 }]);
