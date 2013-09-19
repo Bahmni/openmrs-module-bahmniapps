abb6dc728d3d8f9571ba33d222029e2574b16ef8
kunalch <kunalch@thoughtworks.com>
2013-09-19 14:25:32 +0530

    treatment page added

--- app/consultation/app.js
+++ app/consultation/app.js
@@ -4,7 +4,7 @@ angular.module('consultation', ['opd.consultation', 'bahmni.common.infrastructur
 angular.module('consultation').config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/visit/:visitUuid/', {templateUrl: 'modules/consultation/views/consultation.html', controller: 'ConsultationController', resolve: {initialization: 'initialization'}});
         $routeProvider.when('/visit/:visitUuid/diagnosis', {templateUrl: 'modules/consultation/views/addObservation.html', controller: 'DiagnosisController', resolve: {initialization: 'initialization'}});
-        $routeProvider.when('/visit/:visitUuid/treatment', {templateUrl: 'modules/consultation/views/addObservation.html', controller: 'TreatmentController', resolve: {initialization: 'initialization'}});
+        $routeProvider.when('/visit/:visitUuid/treatment', {templateUrl: 'modules/consultation/views/treatment.html', controller: 'TreatmentController', resolve: {initialization: 'initialization'}});
         $routeProvider.when('/visit/:visitUuid/investigation', {templateUrl: 'modules/consultation/views/investigations.html', controller: 'InvestigationController', resolve: {initialization: 'initialization'}});
         $routeProvider.when('/visit/:visitUuid/notes', {templateUrl: 'modules/consultation/views/notes.html'});
         $routeProvider.when('/visit/:visitUuid/templates', {templateUrl: 'modules/consultation/views/comingSoon.html'});
