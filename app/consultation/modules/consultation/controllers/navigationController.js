bf74766418759a48800aa4d7a27bf892c875d63c
angshu <angshuonline@gmail.com>
2013-09-25 23:15:10 +0530

    nishi, angshu | #1183 | updated navigation controller and index file to show the correct tab selected and also on refresh. refactored code.

--- app/consultation/modules/consultation/controllers/navigationController.js
+++ app/consultation/modules/consultation/controllers/navigationController.js
@@ -2,54 +2,45 @@
 
 angular.module('opd.consultation.controllers')
     .controller('ConsultationNavigationController', ['$scope', '$rootScope', '$location', '$route', function ($scope, $rootScope, $location, $route) {
-    $scope.mainButtonText = "Consultation";
-    $scope.selectedContext = '';
+    //$scope.mainButtonText = "Consultation";
+    $scope.currentBoard = null;
+    $scope.availableBoards = [
+        { name:'Consultation', url: ''},
+        { name:'Investigations', url:'investigation'},
+        { name:'Diagnosis', url:'diagnosis'}, 
+        { name:'Treatment', url:'treatment'}, 
+        { name:'Disposition', url:'disposition'}];
 
-    $scope.blank = function () {
-        return $location.url("/blank");
-    }
-
-    $scope.dashboard = function () {
-        return buttonClickAction("");
-    }
-
-    $scope.treatment = function () {
-        return buttonClickAction("treatment");
-    }
-
-    $scope.investigation = function () {
-        return buttonClickAction("investigation");
-    }
-
-    $scope.diagnosis = function () {
-        return buttonClickAction("diagnosis");
-    }
-
-    $scope.templates = function () {
-        return buttonClickAction("templates");
-    }
-
-    $scope.consultation = function (patient) {
-        return buttonClickAction("consultation");
-    }
-
-    $scope.notes = function () {
-        return buttonClickAction("notes");
-    }
-
-    $scope.disposition = function () {
-        return buttonClickAction("disposition");
+    $scope.showBoard = function(name) {
+        var board = findBoardByname(name);
+        return buttonClickAction(board);
     }
 
     var initialize = function() {
         var currentPath = $location.path();
         var pathInfo = currentPath.substr(currentPath.lastIndexOf('/') + 1);
-        $scope.selectedContext = pathInfo;
+        var board = findBoardByUrl(pathInfo);
+        $scope.currentBoard = board || $scope.availableBoards[0];
+        return;
     }
 
-    var buttonClickAction = function (ctx) {
-        if ($scope.selectedContext) {
-            if ($scope.selectedContext === ctx) {
+    var findBoardByname = function(name) {
+        var boards = $scope.availableBoards.filter(function(board){
+            return board.name === name;
+        });
+        return boards.length > 0 ? boards[0] : null;
+    }
+
+    var findBoardByUrl = function(url) {
+        var boards = $scope.availableBoards.filter(function(board){
+            return board.url === url;
+        });
+        return boards.length > 0 ? boards[0] : null;
+    }
+
+    var buttonClickAction = function (board) {
+        if ($scope.currentBoard) {
+            if ($scope.currentBoard === board) {
                 return;
             }
         }
@@ -61,8 +52,8 @@ angular.module('opd.consultation.controllers')
             }
         }
         $rootScope.beforeContextChange = null;
-        $scope.selectedContext = ctx;
-        return $location.url("/visit/" + $rootScope.visit.uuid + "/" + ctx);
+        $scope.currentBoard = board;
+        return $location.url("/visit/" + $rootScope.visit.uuid + "/" + board.url);
     }
 
     initialize();
