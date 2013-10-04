0cd23a1b7ad5bfa9a80e4a2e0c51b05f206326b4
Mário Areias <mario.s.s.areias@gmail.com>
2013-10-04 13:01:52 +0530

    removing duplicated folders inside script folder

--- registration-ui/scripts/modules/patient/models/preferences.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('registration.patient.models')
-    .factory('Preferences', ['$http', '$rootScope', function() {
-      return {
-          centerID: "GAN",
-          hasOldIdentifier: false
-      };
-}]);
