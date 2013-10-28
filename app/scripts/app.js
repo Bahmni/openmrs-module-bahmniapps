f79c47a09cdfd5f43ff9d3e99c14f74f197d3060
angshu <angshuonline@gmail.com>
2013-10-28 15:04:47 +0530

    angshu | #1085 | artefact created will be named home.zip

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,8 +4,16 @@
 angular
     .module('bahmnihome', ['http-auth-interceptor', 'httpErrorInterceptor', 'infrastructure'])
     .config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
-        $routeProvider.when('/login', {templateUrl: 'modules/auth/views/login.html', controller: 'LoginController'});
-        $routeProvider.when('/dashboard', {templateUrl: 'modules/dashboard/views/dashboard.html', controller: 'DashboardController'});
+        $routeProvider.when('/login', 
+            { 
+                templateUrl: 'modules/auth/views/login.html', 
+                controller: 'LoginController'
+            });
+        $routeProvider.when('/dashboard', 
+            {
+                templateUrl: 'modules/dashboard/views/dashboard.html', 
+                controller: 'DashboardController'
+            });
         $routeProvider.otherwise({redirectTo: '/login'});
         $httpProvider.defaults.headers.common['Disable-WWW-Authenticate'] = true;
     }]).run(function ($rootScope, $templateCache) {
--- scripts/package.sh
+++ scripts/package.sh
@@ -4,8 +4,8 @@ BASE_DIR=`dirname $0`
 ROOT_DIR=$BASE_DIR/..
 
 mkdir -p $ROOT_DIR/target
-rm -rf $ROOT_DIR/target/registration.zip
+rm -rf $ROOT_DIR/target/home.zip
 
 rvm use 1.9.3
 grunt dist
-cd $ROOT_DIR/dist && zip -r ../target/registration.zip *
+cd $ROOT_DIR/dist && zip -r ../target/home.zip *
