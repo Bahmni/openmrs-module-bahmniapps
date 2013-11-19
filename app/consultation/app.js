6a7370403cf7a171c49020507039cb46591f6197
arathyjan <arathyja@thoughtworks.com>
2013-11-19 16:21:38 +0530

    Banka,RT | removing unused files

--- app/consultation/app.js
+++ app/consultation/app.js
@@ -10,8 +10,6 @@ angular.module('consultation').config(['$routeProvider', '$httpProvider', functi
         $routeProvider.when('/visit/:visitUuid/templates', {templateUrl: 'modules/consultation/views/comingSoon.html'});
         $routeProvider.when('/visit/:visitUuid/disposition', {templateUrl: 'modules/consultation/views/disposition.html',controller: 'DispositionController',resolve: {initialization: 'initialization'}});
         $routeProvider.when('/visit/:visitUuid/bed-management', {templateUrl: 'modules/bed-management/views/bedManagement.html',controller: 'BedManagementController',resolve: {initialization: 'initialization'}});
-        //$routeProvider.when('/visit/:visitUuid/bed-management/wardLayout/:wardId', {templateUrl: 'modules/bed-management/views/wardLayout.html',controller: 'WardLayoutController',resolve: {initialization: 'initialization'}});
-        //$routeProvider.when('/visit/:visitUuid/concept-set/:conceptSet', {templateUrl: 'modules/concept-set/views/conceptSetView.html',controller: 'ConceptSetController',resolve: {initialization: 'initialization'}});
         $routeProvider.when('/visit/:visitUuid/concept-set/:conceptSetName', {templateUrl: 'modules/concept-set/views/conceptSetView.html',resolve: {initialization: 'initialization'}});
         $routeProvider.otherwise({redirectTo: Bahmni.Opd.Constants.activePatientsListUrl});
         $httpProvider.defaults.headers.common['Disable-WWW-Authenticate'] = true;
