793d3351d9c02d17f2062128b51e5cdbc7b31ac8
pchandra <pchandra@thoughtworks.com>
2013-11-22 16:50:30 +0530

    praveen,angshu|refactoring consultation dashboard to get rid of nesting

--- app/consultation/modules/consultation/initialization.js
+++ app/consultation/modules/consultation/initialization.js
@@ -27,7 +27,7 @@ angular.module('opd.consultation').factory('initialization',
         };
 
         var getConsultationConfigs = function() {
-            var configurationsPromises = $q.defer();
+            var deferrable = $q.defer();
             var configNames = ['bahmniConfiguration', 'encounterConfig', 'patientConfig', ,'dosageFrequencyConfig','dosageInstructionConfig', 'consultationNoteConfig'];
             configurationService.getConfigurations(configNames).then(function (configurations) {
                 $rootScope.bahmniConfiguration = configurations.bahmniConfiguration;
@@ -36,34 +36,41 @@ angular.module('opd.consultation').factory('initialization',
                 $rootScope.dosageFrequencyConfig = configurations.dosageFrequencyConfig;
                 $rootScope.dosageInstructionConfig = configurations.dosageInstructionConfig;
                 $rootScope.consultationNoteConfig = configurations.consultationNoteConfig;
-
-                return visitService.getVisit($route.current.params.visitUuid).success(function (visit) {
-                    $rootScope.visit = visit;
-                    $rootScope.consultation = new Bahmni.Opd.ConsultationMapper($rootScope.encounterConfig, $rootScope.dosageFrequencyConfig, $rootScope.dosageInstructionConfig, $rootScope.consultationNoteConfig).map(visit);
-                    $rootScope.getBedDetailsForPatient(visit.patient.uuid);
-                    $rootScope.disposition = new Bahmni.Opd.DispositionMapper($rootScope.encounterConfig).map(visit);
-                    $rootScope.disposition.currentActionIndex = 0; // this will be used in case we have multiple encounters with dispositions
-
-
-
-                    return patientService.getPatient(visit.patient.uuid).success(function (openMRSPatient) {
-                        $rootScope.patient = patientMapper.map(openMRSPatient);
-                        configurationsPromises.resolve();
-                    });
-
-                })
+                deferrable.resolve();
             });
-            return configurationsPromises.promise;
-        }        
+            return deferrable.promise;
+        }
+
+        var getVisit = function() {
+            var visitDeferrable = $q.defer();
+            visitService.getVisit($route.current.params.visitUuid).success(function (visit) {
+                $rootScope.visit = visit;
+                $rootScope.consultation = new Bahmni.Opd.ConsultationMapper($rootScope.encounterConfig, $rootScope.dosageFrequencyConfig, $rootScope.dosageInstructionConfig, $rootScope.consultationNoteConfig).map(visit);
+                $rootScope.getBedDetailsForPatient(visit.patient.uuid);
+                $rootScope.disposition = new Bahmni.Opd.DispositionMapper($rootScope.encounterConfig).map(visit);
+                $rootScope.disposition.currentActionIndex = 0; // this will be used in case we have multiple encounters with dispositions
+                visitDeferrable.resolve(visit);
+            });
+            return visitDeferrable.promise;
+        };
+
+        var getPatient = function(visit) {
+            var patientDeferrable = $q.defer();
+            patientService.getPatient(visit.patient.uuid).success(function (openMRSPatient) {
+                $rootScope.patient = patientMapper.map(openMRSPatient);
+                patientDeferrable.resolve($rootScope.patient);
+            });
+            return patientDeferrable.promise;
+        };
 
         authenticator.authenticateUser().then(function () {
             appService.initialize('clinical').then(function() {
                 var configPromise = getConsultationConfigs();
-                configPromise.then(function() {
+                configPromise.then(getVisit).then(getPatient).then(function(patient) {
                     initializationPromise.resolve();
                 });
             });
-         });
+        });
 
         return initializationPromise.promise;
     }]
