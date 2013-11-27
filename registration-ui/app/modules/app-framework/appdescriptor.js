4fe5516ee3808c61bb1bf091ea8c9194d8474bbe
angshu <angshuonline@gmail.com>
2013-11-27 20:16:19 +0530

    angshu | #1524 | minor modification, splitting AppDescriptor to separate file

---
+++ registration-ui/app/modules/app-framework/appdescriptor.js
@@ -0,0 +1,100 @@
+function AppDescriptor(name, retrieveUserCallback) {
+    this.name = name;
+    this.id = null;
+    this.instanceOf = null;
+    this.description = null;
+    this.contextModel = null;
+
+    this.extensionPoints = [];
+    this.extensions = [];
+    this.configs = [];
+
+    var that = this;
+
+    this.setExtensions = function(extensions) {
+        extensions.forEach(function(extn) {
+            var existing = that.extensionPoints.filter(function(ep) {
+                return ep.id == extn.extensionPointId;
+            });
+
+            if (existing.length == 0) {
+                that.extensionPoints.push({
+                    id: extn.extensionPointId,
+                    description: extn.description
+                });
+            }
+        });
+        that.extensions = extensions;
+    };
+
+    this.setTemplate = function(template) {
+        that.instanceOf = template.id;
+        that.description = that.description || template.description;
+        that.contextModel = that.contextModel || template.contextModel;
+        if (template.configOptions) {
+            template.configOptions.forEach(function(opt) {
+                var existing = that.configs.filter(function(cfg) {
+                    return cfg.name == opt.name;
+                });
+                if (existing.length > 0) {
+                    existing[0].description = opt.description;
+                } else {
+                    that.configs.push({
+                        name: opt.name,
+                        description: opt.description,
+                        value: opt.defaultValue
+                    });
+                }
+            });
+        }
+    };
+
+    this.setDefinition = function(instance) {
+        that.instanceOf = instance.instanceOf;
+        that.id = instance.id;
+        that.description = instance.description;
+        if (instance.extensionPoints) {
+            instance.extensionPoints.forEach(function(iep) {
+                var existing = that.extensionPoints.filter(function(ep) {
+                    return ep.id == iep.id;
+                });
+                if (existing.length === 0) {
+                    that.extensionPoints.push(iep);
+                }
+            });
+        }
+
+        if (instance.config) {
+            for (var configName in instance.config) {
+                var existingConfig = that.getConfig(configName);
+                if (existingConfig) {
+                    existingConfig.value = instance.config[configName];
+                }
+            }
+        }
+    };
+
+    this.getExtensions = function (extPointId, type) {
+        var currentUser = retrieveUserCallback();
+        if (currentUser && that.extensions) {
+            var extnType = type || 'all';
+            var userPrivileges = currentUser.privileges.map(function (priv) {
+                return priv.retired ? "" : priv.name;
+            });
+            var appsExtns = that.extensions.filter(function (extn) {
+                return ((extnType==='all') || (extn.type===extnType)) && (extn.extensionPointId === extPointId) && (!extn.requiredPrivilege || (userPrivileges.indexOf(extn.requiredPrivilege) >= 0));
+            });
+            appsExtns.sort(function(extn1, extn2) {
+                return extn1.order - extn2.order;
+            });
+            return appsExtns;
+        }
+    };
+
+    this.getConfig = function(configName) {
+        var cfgList = that.configs.filter(function(cfg) {
+            return cfg.name == configName;
+        });
+        return (cfgList.length > 0) ? cfgList[0] : null;
+    };
+}
