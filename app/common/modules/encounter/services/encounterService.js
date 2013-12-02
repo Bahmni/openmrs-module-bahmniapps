43422a5c97fd89f1af7c6f87ee0ef9d1a6009eac
mujir <mujir@hotmail.com>
2013-12-02 18:13:25 +0530

    Mujir, Banka | Stripping extra information in concept associated with encounter's observations while saving the encounter

--- app/common/modules/encounter/services/encounterService.js
+++ app/common/modules/encounter/services/encounterService.js
@@ -4,9 +4,20 @@ angular.module('bahmni.common.encounter.services')
     .service('encounterService', ['$http', function ($http) {
 
     this.create = function (encounter) {
+        encounter.observations = encounter.observations || [];
+        encounter.observations.forEach(function(obs) {
+            stripExtraConceptInfo(obs);
+        });
         return $http.post(Bahmni.Common.Constants.encounterUrl, encounter, {
             withCredentials:true
         });
     };
 
+    var stripExtraConceptInfo = function(obs) {
+        obs.concept = {uuid: obs.concept.uuid};
+        obs.groupMembers = obs.groupMembers || [];
+        obs.groupMembers.forEach(function(groupMember) {
+            stripExtraConceptInfo(groupMember);
+        });
+    }
 }]);
