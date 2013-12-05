b4f4fc1ae0599210595d027daf90570f53a51717
Deepak N <endeep123@gmail.com>
2013-12-05 12:25:43 +0530

    RT, D3 | #1520 | Show loader on scroll, ensure the order of days is correct

--- app/consultation/modules/consultation/controllers/visitController.js
+++ app/consultation/modules/consultation/controllers/visitController.js
@@ -4,23 +4,32 @@ angular.module('opd.consultation.controllers')
     .controller('VisitController', ['$scope', '$rootScope', 'encounterService', '$route', '$location', function ($scope, $rootScope, encounterService, $route, $location) {
     var visitUuid = $route.current.params["visitUuid"];    
 	$scope.visitDays = [];
+    $scope.hasMoreVisitDays = true;
     var currentEncounterDate;
+    var loading;
+
+    var markLoadingDone = function() {
+        loading = false;
+    }
 
     var loadEncounters = function(encounterDate) {
-    	encounterService.search(visitUuid, encounterDate.toISOString().substring(0, 10)).success(function(encounterTransactions){
+    	if(loading) return;
+        loading = true;
+        encounterService.search(visitUuid, encounterDate.toISOString().substring(0, 10)).success(function(encounterTransactions){
             var dayNumber = Bahmni.Common.Util.DateUtil.diffInDays($rootScope.visitSummary.visitStartDateTime, encounterDate) + 1;
             var visitDay = Bahmni.Opd.Consultation.VisitDay.create(dayNumber, encounterDate, encounterTransactions, $rootScope.consultationNoteConcept, $rootScope.encounterConfig.orderTypes);
             $scope.visitDays.push(visitDay);
-	    });
+	    }).then(markLoadingDone, markLoadingDone);
     	currentEncounterDate = encounterDate;
+        $scope.hasMoreVisitDays = currentEncounterDate >= $rootScope.visitSummary.visitStartDateTime;
     }
 
     loadEncounters($rootScope.visitSummary.mostRecentEncounterDateTime);
 
     $scope.loadEncountersForPreviousDay = function() {    	
-        if(currentEncounterDate >= $rootScope.visitSummary.visitStartDateTime) {
+        if($scope.hasMoreVisitDays) {
             var previousDate = new Date(currentEncounterDate.valueOf() - 60 * 1000 * 60 * 24);
             loadEncounters(previousDate)            
-        }
+        } 
     };        
 }])
