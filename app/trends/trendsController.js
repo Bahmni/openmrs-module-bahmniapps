8b68c58d6cb145ac135d2c1d1fdfeacf07c1c8c3
Chirantan Mitra <chirantan.mitra@gmail.com>
2013-12-11 16:53:15 +0530

    chiku | All observation are available in scope. Display weight & BMI in charts.

--- app/trends/trendsController.js
+++ app/trends/trendsController.js
@@ -2,9 +2,9 @@ angular.module("trends").controller("TrendsController", ["$scope", "$routeParams
     var patientUUID = $routeParams.patientUUID,
         fetchedObservations = observationService.fetch(patientUUID),
 
-        fetch = function(observations, category) {
+        fetch = function(observations, concept) {
             return observations.filter(function(observation) {
-                return observation.conceptName === category;
+                return observation.conceptName === concept;
             }).map(function(observation) {
                 return [observation.observationDate, observation.value];
             });
@@ -13,22 +13,58 @@ angular.module("trends").controller("TrendsController", ["$scope", "$routeParams
         epochToDateString = function(epoch){
             var date = new Date(epoch);
             return d3.time.format("%d/%m/%y")(date);
+        },
+
+        displayableConceptNames = {
+            "BMI": "BMI",
+            "REGISTRATION FEES": "Fees"
+        },
+
+        unique = function(array) {
+            return array.filter(function(item, position) {
+                return array.indexOf(item) === position;
+            });
+        },
+
+        sentenceCase = function(words) {
+            return words[0].toUpperCase() + words.slice(1, words.length).toLowerCase();
+        },
+
+        displayNameFor = function(concept) {
+            if (displayableConceptNames[concept]) {
+                return displayableConceptNames[concept];
+            }
+
+            return sentenceCase(concept);
+        },
+
+        cleanse = function(token) {
+            return token.replace(" ", "_");
+        },
+
+        init = function() {
+            fetchedObservations.success(function(observations) {
+                var allConcepts = observations.map(function(observation) {
+                        return observation.conceptName;
+                    }),
+                    uniqueConcepts = unique(allConcepts);
+
+                $scope.observations = {};
+                uniqueConcepts.forEach(function(concept) {
+                    var displayName = displayNameFor(concept),
+                        values = fetch(observations, concept);
+
+                    $scope.observations[cleanse(concept)] = [{
+                        "key": displayName,
+                        "values": values
+                    }];
+                });
+            });
         };
 
     $scope.xAxisTickFormatFunction = function(){
         return epochToDateString;
     };
 
-    var init = function(key, displayName) {
-        fetchedObservations.success(function(observations) {
-            var values = fetch(observations, key);
-            $scope.observations = [{
-                "key": displayName,
-                "values": values
-            }];
-        });
-    };
-
-    init("WEIGHT", "Weight");
-    // init("HEIGHT", "Heights");
+    init();
 }]);
--- test/unit/trends/trendsControllerTest.js
+++ test/unit/trends/trendsControllerTest.js
@@ -29,22 +29,50 @@ describe("TrendsController", function() {
     });
 
     describe("when initialized", function() {
-        it("pulls weight observation", inject(function() {
+        it("pulls categorized observations", inject(function() {
+            var expectedObservations = {
+                "WEIGHT": [{
+                    "key": "Weight",
+                    "values": [
+                        [1371619826000, 38.7],
+                        [1372227320000, 40.0],
+                        [1372821738000, 40.3],
+                    ]
+                }],
+                "HEIGHT": [{
+                    "key": "Height",
+                    "values": [
+                        [1371619826000, 147.0]
+                    ]
+                }],
+                "BMI": [{
+                    "key": "BMI",
+                    "values": [
+                        [1371619826000, 17.91]
+                    ]
+                }],
+                "REGISTRATION_FEES": [{
+                    "key": "Fees",
+                    "values": [
+                        [1372821738000, 15]
+                    ]
+                }]
+            };
+
             observationFetchPromise.callSuccessCallBack([
                 {"observationDate":1371619826000,"conceptName":"WEIGHT","value":38.7},
                 {"observationDate":1371619826000,"conceptName":"BMI","value":17.91},
                 {"observationDate":1371619826000,"conceptName":"HEIGHT","value":147.0},
                 {"observationDate":1372227320000,"conceptName":"WEIGHT","value":40.0},
-                {"observationDate":1372821738000,"conceptName":"WEIGHT","value":40.3}
+                {"observationDate":1372821738000,"conceptName":"WEIGHT","value":40.3},
+                {"observationDate":1372821738000,"conceptName":"REGISTRATION FEES","value":15}
             ]);
-            expect(scope.observations).toEqual([{
-                "key": "Weight",
-                "values": [
-                    [1371619826000, 38.7],
-                    [1372227320000, 40.0],
-                    [1372821738000, 40.3],
-                ]
-            }]);
+
+            expect(scope.observations.keys).toEqual(expectedObservations.keys);
+            expect(scope.observations.WEIGHT).toEqual(expectedObservations.WEIGHT);
+            expect(scope.observations.HEIGHT).toEqual(expectedObservations.HEIGHT);
+            expect(scope.observations.BMI).toEqual(expectedObservations.BMI);
+            expect(scope.observations.REGISTRATION_FEES).toEqual(expectedObservations.REGISTRATION_FEES);
         }));
     });
 });
