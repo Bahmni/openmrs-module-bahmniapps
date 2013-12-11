b60d4fffd1ae23395af6f57a65d5385730c60430
Vinay Venu <vinayv@thoughtworks.com>
2013-12-11 16:42:50 +0530

    Vinay | No nested callbacks, only elegant promises

--- app/bed-management/modules/bed-management/initialization.js
+++ app/bed-management/modules/bed-management/initialization.js
@@ -1,28 +1,25 @@
 'use strict';
 
 angular.module('opd.bedManagement').factory('initialization',
-    ['$rootScope', '$q', '$route', 'authenticator', 'BedService', 'patientService', 'patientMapper', 'configurationService',
-        function ($rootScope, $q, $route, authenticator, bedService, patientService, patientMapper, configurationService) {
-            var initializationPromise = $q.defer();
-
-            if (!String.prototype.trim) {
-                String.prototype.trim = function () {
-                    return this.replace(/^\s+|\s+$/g, '');
-                };
-            }
-
-            authenticator.authenticateUser().then(function () {
-                configurationService.getConfigurations(['patientConfig', 'encounterConfig']).then(function (configurations) {
+    ['$rootScope', '$route', 'authenticator', 'BedService', 'patientService', 'patientMapper', 'configurationService',
+        function ($rootScope, $route, authenticator, bedService, patientService, patientMapper, configurationService) {
+            var bedDetailsForPatient = function (response) {
+                    var openMRSPatient = response.data;
+                    $rootScope.patient = patientMapper.map(openMRSPatient);
+                    return bedService.getBedDetailsForPatient($route.current.params.patientUuid);
+                },
+                getPatient = function (configurations) {
                     $rootScope.patientConfig = configurations.patientConfig;
                     $rootScope.encounterConfig = configurations.encounterConfig;
-                    return patientService.getPatient($route.current.params.patientUuid).success(function (openMRSPatient) {
-                        $rootScope.patient = patientMapper.map(openMRSPatient);
-                        bedService.getBedDetailsForPatient($route.current.params.patientUuid).then(function () {
-                            initializationPromise.resolve();
-                        });
-                    });
-                });
-            });
+                    return patientService.getPatient($route.current.params.patientUuid);
+                },
+                getConfigurations = function () {
+                    return configurationService.getConfigurations(['patientConfig', 'encounterConfig']);
+                },
+                authenticate = function () {
+                    return authenticator.authenticateUser();
+                };
 
-            return initializationPromise.promise;
-        }]);
+            authenticate().then(getConfigurations).then(getPatient).then(bedDetailsForPatient);
+        }]
+    );
