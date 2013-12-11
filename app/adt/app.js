d5b8d53d897f510da79c9429b1cd90d7da153f3f
Shruthi <shruthidipali@gmail.com>
2013-12-11 14:24:05 +0530

    Revert "Shruthi, Sush | #900 | renamed concept set for admission from 'adt' to 'admission'"
    This reverts commit a5a91be13740783bfb7257f179a3fa2b1fdc97af.

--- app/adt/app.js
+++ app/adt/app.js
@@ -4,7 +4,7 @@ angular.module('adt', ['opd.consultation.services', 'bahmni.common.infrastructur
     'opd.conceptSet', 'authentication', 'appFramework', 'httpErrorInterceptor', 'opd.adt',
     'bahmni.common.encounter', 'bahmni.common.visit', 'opd.bedManagement.services', 'bahmni.common.controllers']);
 angular.module('adt').config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
-    $routeProvider.when('/visit/:visitUuid', {templateUrl:'modules/adt/views/admission.html', controller:'AdmissionController', resolve:{initialization:'initialization'}});
+    $routeProvider.when('/visit/:visitUuid', {templateUrl:'modules/adt/views/adt.html', controller:'AdmissionController', resolve:{initialization:'initialization'}});
     $routeProvider.when('/discharge/visit/:visitUuid', {templateUrl:'modules/adt/views/discharge.html', controller:'DischargeController', resolve:{initialization:'initialization'}});
     $routeProvider.otherwise({templateUrl:'../common/modules/common/error.html'});
     $httpProvider.defaults.headers.common['Disable-WWW-Authenticate'] = true;
