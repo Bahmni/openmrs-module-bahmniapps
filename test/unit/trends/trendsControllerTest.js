f6c94cf6660ef3d8f8375e9dd311d9d8e116936b
Chirantan Mitra <chirantan.mitra@gmail.com>
2013-12-12 17:46:40 +0530

    chiku | Test single concept in one test

--- test/unit/trends/trendsControllerTest.js
+++ test/unit/trends/trendsControllerTest.js
@@ -7,6 +7,16 @@ describe("TrendsController", function() {
         scope,
         sortedKeys = function(array) {
             return Object.keys(array).sort();
+        },
+        serverCallForWeightAndHeight = function() {
+            observationFetchPromise.callSuccessCallBack([
+                {"observationDate":1371619826000,"conceptName":"WEIGHT","value":38.7},
+                {"observationDate":1372227320000,"conceptName":"WEIGHT","value":40.0},
+                {"observationDate":1372821738000,"conceptName":"WEIGHT","value":40.3},
+                {"observationDate":1371619826000,"conceptName":"HEIGHT","value":147.0},
+                {"observationDate":1372227320000,"conceptName":"HEIGHT","value":148.0}
+            ]);
+
         };
 
     beforeEach(function () {
@@ -31,95 +41,104 @@ describe("TrendsController", function() {
         }));
     });
 
-    describe("when initialized", function() {
-        it("pulls categorized observations", inject(function() {
-            observationFetchPromise.callSuccessCallBack([
-                {"observationDate":1371619826000,"conceptName":"WEIGHT","value":38.7},
-                {"observationDate":1372227320000,"conceptName":"WEIGHT","value":40.0},
-                {"observationDate":1372821738000,"conceptName":"WEIGHT","value":40.3},
-                {"observationDate":1371619826000,"conceptName":"HEIGHT","value":147.0},
-                {"observationDate":1372227320000,"conceptName":"HEIGHT","value":148.0}
-            ]);
+    describe("after initialization", function() {
+        describe("for populated categories", function() {
+            beforeEach(function() {
+                serverCallForWeightAndHeight();
+            });
 
-            expect(sortedKeys(scope.observations)).toEqual(["HEIGHT", "WEIGHT"]);
-            expect(scope.observations.WEIGHT).toEqual([{
-                "key": "Weight",
-                "values": [ [1371619826000, 38.7], [1372227320000, 40.0], [1372821738000, 40.3] ]
-            }]);
-            expect(scope.observations.HEIGHT).toEqual([{
-                "key": "Height",
-                "values": [ [1371619826000, 147.0], [1372227320000, 148.0] ]
-            }]);
-            expect(scope.concepts).toEqual({"HEIGHT":"Height", "WEIGHT":"Weight"});
-        }));
+            it("pulls categorized observations", inject(function() {
+                expect(sortedKeys(scope.observations)).toEqual(["HEIGHT", "WEIGHT"]);
+                expect(scope.observations.WEIGHT).toEqual([{
+                    "key": "Weight",
+                    "values": [ [1371619826000, 38.7], [1372227320000, 40.0], [1372821738000, 40.3] ]
+                }]);
+                expect(scope.observations.HEIGHT).toEqual([{
+                    "key": "Height",
+                    "values": [ [1371619826000, 147.0], [1372227320000, 148.0] ]
+                }]);
+            }));
 
-        it("renames special keys like BMI & REGISTRATION FEES", inject(function() {
-            observationFetchPromise.callSuccessCallBack([
-                {"observationDate":1371619826000,"conceptName":"BMI","value":17.91},
-                {"observationDate":1372227320000,"conceptName":"BMI","value":18.27},
-                {"observationDate":1371619826000,"conceptName":"REGISTRATION FEES","value":15},
-                {"observationDate":1372227320000,"conceptName":"REGISTRATION FEES","value":10}
-            ]);
+            it("populates concepts", inject(function() {
+                expect(scope.concepts).toEqual({"HEIGHT":"Height", "WEIGHT":"Weight"});
+            }));
+        });
 
-            expect(sortedKeys(scope.observations)).toEqual(["BMI", "REGISTRATION FEES"]);
-            expect(scope.observations.BMI).toEqual([{
-                "key": "BMI",
-                "values": [ [1371619826000, 17.91], [1372227320000, 18.27] ]
-            }]);
-            expect(scope.observations["REGISTRATION FEES"]).toEqual([{
-                "key": "Fees",
-                "values": [ [1371619826000, 15], [1372227320000, 10] ]
-            }]);
-            expect(scope.concepts).toEqual({"BMI":"BMI", "REGISTRATION FEES":"Fees"});
-        }));
+        describe("for observations that need a different name from the sever send name", function() {
+            beforeEach(function() {
+                observationFetchPromise.callSuccessCallBack([
+                    {"observationDate":1371619826000,"conceptName":"BMI","value":17.91},
+                    {"observationDate":1372227320000,"conceptName":"BMI","value":18.27},
+                    {"observationDate":1371619826000,"conceptName":"REGISTRATION FEES","value":15},
+                    {"observationDate":1372227320000,"conceptName":"REGISTRATION FEES","value":10}
+                ]);
+            });
 
-        it("rejects samples without enough data", inject(function() {
-            observationFetchPromise.callSuccessCallBack([
-                {"observationDate":1371619826000,"conceptName":"BMI","value":17.91}
-            ]);
+            it("renames keys names in the observations", inject(function() {
+                expect(sortedKeys(scope.observations)).toEqual(["BMI", "REGISTRATION FEES"]);
+                expect(scope.observations.BMI).toEqual([{
+                    "key": "BMI",
+                    "values": [ [1371619826000, 17.91], [1372227320000, 18.27] ]
+                }]);
+                expect(scope.observations["REGISTRATION FEES"]).toEqual([{
+                    "key": "Fees",
+                    "values": [ [1371619826000, 15], [1372227320000, 10] ]
+                }]);
+            }));
 
-            expect(sortedKeys(scope.observations)).toEqual([]);
-            expect(scope.concepts).toEqual({});
-        }));
+            it("renames concepts", inject(function() {
+                expect(scope.concepts).toEqual({"BMI":"BMI", "REGISTRATION FEES":"Fees"});
+            }));
+        });
+
+        describe("for observations with insufficient data", function() {
+            beforeEach(function() {
+                observationFetchPromise.callSuccessCallBack([
+                    {"observationDate":1371619826000,"conceptName":"BMI","value":17.91}
+                ]);
+            });
+
+            it("has empty observations", inject(function() {
+                expect(scope.concepts).toEqual({});
+            }));
+
+            it("has empty categories", inject(function() {
+                expect(scope.concepts).toEqual({});
+            }));
+        });
     });
 
     describe("addObservations", function() {
-        it("adds the specified observation to visible observation", inject(function() {
-            observationFetchPromise.callSuccessCallBack([
-                {"observationDate":1371619826000,"conceptName":"WEIGHT","value":38.7},
-                {"observationDate":1372227320000,"conceptName":"WEIGHT","value":40.0},
-                {"observationDate":1371619826000,"conceptName":"HEIGHT","value":147.0},
-                {"observationDate":1372227320000,"conceptName":"HEIGHT","value":148.0}
-            ]);
+        beforeEach(function() {
+            serverCallForWeightAndHeight();
+        });
 
+        it("adds the specified observation to visible observation", inject(function() {
             scope.addObservations("WEIGHT");
 
             expect(scope.visibleObservations).toEqual({
                 "WEIGHT": [{
                     "key": "Weight",
-                    "values": [ [1371619826000, 38.7], [1372227320000, 40.0] ]
+                    "values": [ [1371619826000, 38.7], [1372227320000, 40.0], [1372821738000, 40.3] ]
                 }]
             });
         }));
     });
 
     describe("removeObservations", function() {
-        it("removes the specified observation from visible observation", inject(function() {
-            observationFetchPromise.callSuccessCallBack([
-                {"observationDate":1371619826000,"conceptName":"WEIGHT","value":38.7},
-                {"observationDate":1372227320000,"conceptName":"WEIGHT","value":40.0},
-                {"observationDate":1371619826000,"conceptName":"HEIGHT","value":147.0},
-                {"observationDate":1372227320000,"conceptName":"HEIGHT","value":148.0}
-            ]);
+        beforeEach(function() {
+            serverCallForWeightAndHeight();
             scope.addObservations("WEIGHT");
             scope.addObservations("HEIGHT");
+        });
 
+        it("removes the specified observation from visible observation", inject(function() {
             scope.removeObservations("HEIGHT");
 
             expect(scope.visibleObservations).toEqual({
                 "WEIGHT": [{
                     "key": "Weight",
-                    "values": [ [1371619826000, 38.7], [1372227320000, 40.0] ]
+                    "values": [ [1371619826000, 38.7], [1372227320000, 40.0], [1372821738000, 40.3] ]
                 }]
             });
         }));
