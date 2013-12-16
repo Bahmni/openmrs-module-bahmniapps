140034b30093c1de1b653768cae7602ccb16814d
Vinay Venu <vinayv@thoughtworks.com>
2013-12-16 11:32:56 +0530

    Vinay | Fix dev config

--- test/config/testacular.conf.dev.js
+++ test/config/testacular.conf.dev.js
@@ -9,13 +9,14 @@ files = [
     '../app/components/d3/d3.min.js',
     '../app/components/nvd3/nv.d3.min.js',
     '../app/components/angularjs-nvd3-directives/dist/*.js',
-
+    '../app/components/moment/moment.js',
+    '../app/common/constants.js',
     '../app/**/modules/**/init.js',
     '../app/**/constants.js',
     '../app/**/modules/**/*.js',
     '../app/**/modules/**/constants.js',
     '../app/trends/**/*.js',
-
+    'support/**/*.js',
     'unit/**/*.js'
 ];
 
