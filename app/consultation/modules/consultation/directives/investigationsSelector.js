0b3be27126a2536f0f644f00fb2c73b74399f38f
Deepak N <endeep123@gmail.com>
2013-12-16 16:49:07 +0530

    #1581 | Added ngModel support to new investigations selector

--- app/consultation/modules/consultation/directives/investigationsSelector.js
+++ app/consultation/modules/consultation/directives/investigationsSelector.js
@@ -1,6 +1,6 @@
 angular.module('opd.consultation')
 .directive('investigationsSelector',function(){
-    var Selectable = function(data, selectableChildren) {
+    var Selectable = function(data, selectableChildren, onSelectionChange) {
         angular.extend(this, data);
         var selectionSources = [];
         var children = selectableChildren || [];
@@ -9,10 +9,14 @@ angular.module('opd.consultation')
             return selectionSources.length > 0;
         }
 
+        this.isSelectedFromSelf = function() {
+            return selectionSources.indexOf(this) !== -1;
+        }
+
         this.addChild = function(selectable) {
             children.push(selectable);
         }
-
+        
         this.toggle = function(selectionSource) {
             selectionSource = selectionSource || this;
             this.isSelected() ? this.unselect(selectionSource) : this.select(selectionSource);
@@ -22,8 +26,12 @@ angular.module('opd.consultation')
             selectionSource = selectionSource || this;
             if(selectionSources.indexOf(selectionSource) === -1) {
                 selectionSources.push(selectionSource);  
-                angular.forEach(children, function(child){ child.select(selectionSource); });
-            } 
+                angular.forEach(children, function(child){ 
+                    child.unselect(child); 
+                    child.select(selectionSource); 
+                });
+                onSelectionChange(this); 
+            }
         }        
 
         this.unselect = function(selectionSource) {
@@ -32,6 +40,7 @@ angular.module('opd.consultation')
             if(index !== -1) {
                 selectionSources.splice(index, 1);
                 angular.forEach(children, function(child){ child.unselect(selectionSource); });
+                onSelectionChange(this);
             }
         }        
     }
@@ -49,13 +58,23 @@ angular.module('opd.consultation')
         }
     }
 
-    var controller = function($scope) {
+    var controller = function($scope, $rootScope) {
+        var onSelectionChange = function(selectable) {
+            if(selectable.isSelected()) {
+                if(selectable.isSelectedFromSelf()) addInvestigationForSelectable(selectable);
+            } else {
+                removeInvestigationForSelectable(selectable);
+            }
+        }
+
         var initializeTests = function() {
             var categories = [];
             var selectablePanels = [];
+            var selectableTests = []
             var filters = [];
             angular.forEach($scope.tests, function(test){
-                var selectableTest = new Selectable(test);            
+                var selectableTest = new Selectable(test, [], onSelectionChange);            
+                selectableTests.push(selectableTest);
                 var categoryData = test[$scope.categoryColumn] || {name: "Other"};
                 var category = categories.filter(function(category) { return category.name === categoryData.name })[0];
                 category ? category.tests.push(selectableTest) : categories.push(new Category(categoryData.name, [selectableTest]));
@@ -65,7 +84,7 @@ angular.module('opd.consultation')
                     if(selectablePanel) {
                         selectablePanel.addChild(selectableTest)
                     } else {
-                        selectablePanel = new Selectable(testPanel, [selectableTest]);
+                        selectablePanel = new Selectable(testPanel, [selectableTest], onSelectionChange);
                         selectablePanels.push(selectablePanel)                    
                     }
                 });
@@ -75,11 +94,55 @@ angular.module('opd.consultation')
             });
             $scope.categories = categories;
             $scope.selectablePanels = selectablePanels;
+            $scope.selectableTests = selectableTests;
             $scope.filters = filters;
         };
 
+        var selectSelectablesBasedOnInvestigations = function() {
+            var selectables = $scope.selectablePanels.concat($scope.selectableTests);
+            angular.forEach($scope.investigations, function(investigation){
+                var selectable = selectables.filter(function(selectableConcept){ return selectableConcept.uuid === investigation.concept.uuid; })[0];
+                if(selectable) selectable.select();
+            });
+        }
+        
+        var createInvestigationFromSelectable = function(selectable) {
+            return {
+                concept: {uuid: selectable.uuid, name: selectable.name},
+                orderTypeUuid: $rootScope.encounterConfig.orderTypes['Lab Order']//TODO: Fetch this based on tab    
+            };
+        }
+
+        var addInvestigationForSelectable = function(selectable) {
+            var investigation = findInvestigationForSelectable(selectable);
+            if(investigation) {
+                investigation.voided = false;
+            } else {
+                $scope.investigations.push(createInvestigationFromSelectable(selectable));
+            }         
+        }
+
+        var removeInvestigationForSelectable = function(selectable) {
+            var investigation = findInvestigationForSelectable(selectable);
+            if(investigation) removeInvestigation(investigation);;
+        }
+
+        var removeInvestigation = function(investigation) {
+            if(investigation.uuid) {
+                investigation.voided = true;
+            } else {
+                var index = $scope.investigations.indexOf(investigation);
+                $scope.investigations.splice(index, 1);
+            }
+        }
+
+        var findInvestigationForSelectable = function(selectable) {
+            return $scope.investigations.filter(function(investigation){ return investigation.concept.uuid === selectable.uuid })[0];
+        }
+
         $scope.$watch('tests', function(){
             initializeTests();
+            selectSelectablesBasedOnInvestigations();
             $scope.clearFilter();
         });
 
@@ -112,7 +175,9 @@ angular.module('opd.consultation')
         restrict: 'EA',
         templateUrl: 'modules/consultation/views/investigationsSelector.html',
         controller: controller,
+        require: 'ngModel',
         scope: {
+            investigations: '=ngModel',
             tests: "=",
             filterColumn: "@",
             categoryColumn: "@"
