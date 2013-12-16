7936cbe0cc73145e86f65078cc5f2d8c40c81def
Deepak N <endeep123@gmail.com>
2013-12-16 14:46:58 +0530

    #1518 | Added filter by sample

--- app/consultation/modules/consultation/directives/investigationsSelector.js
+++ app/consultation/modules/consultation/directives/investigationsSelector.js
@@ -13,62 +13,53 @@ angular.module('opd.consultation')
             children.push(selectable);
         }
 
-        this.select = function() {
-            this.selectVia(this);
-            selectChildren();
+        this.toggle = function(selectionSource) {
+            selectionSource = selectionSource || this;
+            this.isSelected() ? this.unselect(selectionSource) : this.select(selectionSource);
         }
 
-        var selectChildren = function() {
-            angular.forEach(children, function(child){
-                child.selectVia(this);
-            });
-        }
-
-        var unselectChildren = function() {
-            angular.forEach(children, function(child){
-                child.unselectVia(this);
-            });
-        }
-
-        this.unselect = function() {
-            selectionSources.length = 0;
-            unselectChildren();
-        }
-
-        this.toggle = function() {
-            this.isSelected() ? this.unselect() : this.select();
-        }
-
-        this.toggleVia = function(selectionSource) {
-            this.isSelected() ? this.unselectVia(selectionSource) : this.selectVia(selectionSource);
-        }
-
-        this.selectVia = function(selectionSource) {
-            if(selectionSources.indexOf(selectionSource) === -1)
-                selectionSources.push(selectionSource);
+        this.select = function(selectionSource) {
+            selectionSource = selectionSource || this;
+            if(selectionSources.indexOf(selectionSource) === -1) {
+                selectionSources.push(selectionSource);  
+                angular.forEach(children, function(child){ child.select(selectionSource); });
+            } 
         }        
 
-        this.unselectVia = function(selectionSource) {
+        this.unselect = function(selectionSource) {
+            selectionSource = selectionSource || this;
             var index = selectionSources.indexOf(selectionSource)
             if(index !== -1) {
                 selectionSources.splice(index, 1);
+                angular.forEach(children, function(child){ child.unselect(selectionSource); });
             }
-            unselectChildren();
         }        
     }
 
+    var Category = function(name, tests) {
+        this.name = name;
+        this.tests = tests;
+        
+        this.filter = function(filterFunction) {
+            this.filteredTests = tests.filter(filterFunction);
+        }
 
+        this.hasTests = function() {
+            return this.filteredTests.length > 0;
+        }
+    }
 
     var controller = function($scope) {
-        var setSelectables = function(tests, categoryColumn) {
+        var initializeTests = function() {
             var categories = [];
             var selectablePanels = [];
             var filters = [];
-            angular.forEach(tests, function(test){
+            angular.forEach($scope.tests, function(test){
                 var selectableTest = new Selectable(test);            
-                var categoryData = test[categoryColumn] || {name: "Other"};
+                var categoryData = test[$scope.categoryColumn] || {name: "Other"};
                 var category = categories.filter(function(category) { return category.name === categoryData.name })[0];
-                category ? category.tests.push(selectableTest) : categories.push({name: categoryData.name, tests: [selectableTest]});
+                category ? category.tests.push(selectableTest) : categories.push(new Category(categoryData.name, [selectableTest]));
+                
                 angular.forEach(test.panels, function(testPanel){
                     var selectablePanel = selectablePanels.filter(function(panel){ return panel.name === testPanel.name })[0];
                     if(selectablePanel) {
@@ -78,24 +69,42 @@ angular.module('opd.consultation')
                         selectablePanels.push(selectablePanel)                    
                     }
                 });
+
                 var filter = test[$scope.filterColumn];
                 if(filters.indexOf(filter) === -1) filters.push(filter);
             });
             $scope.categories = categories;
             $scope.selectablePanels = selectablePanels;
             $scope.filters = filters;
-        }
+        };
 
         $scope.$watch('tests', function(){
-            setSelectables($scope.tests, $scope.categoryColumn);
+            initializeTests();
+            $scope.clearFilter();
         });
 
-        $scope.toggleTest = function(test) {
-            test.toggleVia(test);
+        $scope.clearFilter = function() {
+            $scope.filterBy(null)
         }
 
-        $scope.togglePanel = function(panel) {
-            panel.toggle();
+        var applyCurrentFilterByFilterCoulmn = function(selectable) {
+            return $scope.currentFilter ? selectable[$scope.filterColumn] === $scope.currentFilter : true;
+        }
+
+        $scope.filterBy = function(filter) {
+            $scope.currentFilter = filter;
+            $scope.filteredPanels = $scope.selectablePanels.filter(applyCurrentFilterByFilterCoulmn);
+            angular.forEach($scope.categories, function(category){
+                category.filter(applyCurrentFilterByFilterCoulmn);
+            });
+        }
+        
+        $scope.hasFilter = function() {
+            return !!$scope.currentFilter;
+        }
+
+        $scope.isFilteredBy = function(filter) {
+            return $scope.currentFilter === filter;
         }
     }
 
