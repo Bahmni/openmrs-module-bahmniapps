49b1195bdcf21848d0c14c492122d1283fe99f51
kunalch <kunalch@thoughtworks.com>
2013-12-31 17:08:21 +0530

    visit history right panel slider implemented

--- clinical-ui/app/consultation/modules/consultation/controllers/visitHistoryController.js
+++ clinical-ui/app/consultation/modules/consultation/controllers/visitHistoryController.js
@@ -15,4 +15,14 @@ angular.module('opd.consultation.controllers')
     $scope.isCurrentVisit = function(visit) {
         return visit.uuid === visitUuid;
     }
+
+    $scope.toggleDrawer = function(){
+            if($('.visit-history').hasClass("drawerClose")){
+                $('.visit-history').removeClass("drawerClose");
+                $('.visit-history').animate({width:'200px'},1000);
+            }
+            else{
+                $('.visit-history').animate({width:'0px'},1000,function(){$('.visit-history').addClass("drawerClose");});
+            }
+    }
 }]);
