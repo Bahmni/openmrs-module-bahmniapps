633125fa8a655a2d0041ed48d3ee5f4899f1d9c7
pchandra <pchandra@thoughtworks.com>
2014-01-17 12:06:24 +0530

    praveen,neha|#1690|adding spinner for search results and showing multiple results in list page
    Conflicts:
    
    	clinical-ui/app/document-upload/modules/document-upload/controllers/searchPatientController.js

--- clinical-ui/app/document-upload/modules/document-upload/controllers/searchPatientController.js
+++ clinical-ui/app/document-upload/modules/document-upload/controllers/searchPatientController.js
@@ -1,24 +1,39 @@
 'use strict';
 
 angular.module('opd.documentupload')
-    .controller('SearchPatientController', ['$rootScope', '$scope', '$route', '$location', '$window',  'patientService', 'appService', function ($rootScope, $scope, $route, $location, $window,  patientService, appService) {
+    .controller('SearchPatientController', ['$rootScope', '$scope', '$route', '$location', '$window','spinner',  'patientService', 'appService', function ($rootScope, $scope, $route, $location, $window,spinner,  patientService, appService) {
         $scope.results = [];
 
-        $scope.searchById = function () {
-            if(!$scope.patientIdentifier) return;
-            $scope.results = [];
-            var patientIdentifier = $scope.patientIdentifier
+        $scope.resultsPresent = function () {
+           return $scope.results.length > 1;
+        }
 
-            var searchPromise = patientService.search(patientIdentifier).success(function (data) {
+        $scope.gotoDocumentUpload = function(patientUuid) {
+            $location.path("/patient/" + patientUuid);
+        }
+
+        var searchPromise = function(patientIdentifier) {
+            return patientService.search(patientIdentifier).success(function (data) {
                 $scope.results = data.results;
                 if (data.results.length > 0) {
                     var patient = data.results[0];
                     $rootScope.patient = patient;
                     $location.path("/patient/" + patient.uuid + "/document");
+                    if(data.results.length == 1)
+                        $scope.gotoDocumentUpload(patient.uuid);
+
                 } else {
+                    spinner.hide();
                     $scope.noResultsMessage = "Could not find patient with identifier " + patientIdentifier + ". Please verify the patient ID entered "
                 }
-            }).error();
+            }).error(spinner.hide);
+        }
+
+        $scope.searchById = function () {
+            if(!$scope.patientIdentifier) return;
+            $scope.results = [];
+            var patientIdentifier = $scope.patientIdentifier
+            spinner.forPromise(searchPromise(patientIdentifier));
         };
 
 }]);
