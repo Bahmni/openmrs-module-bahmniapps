cf5fc351143af26ea7e7a618669d29592ad06e36
Deepak N <endeep123@gmail.com>
2014-03-19 14:15:41 +0530

    Fixing test

--- ui/test/support/specUtil.js
+++ ui/test/support/specUtil.js
@@ -4,6 +4,7 @@ var specUtil = {
         servicePromise.then = jasmine.createSpy(name + ' then').andReturn(servicePromise);
         servicePromise.success = jasmine.createSpy(name + ' success').andReturn(servicePromise);
         servicePromise.error = jasmine.createSpy(name + ' error').andReturn(servicePromise);
+        servicePromise['finally'] = jasmine.createSpy(name + ' finally').andReturn(servicePromise);
         servicePromise.callSuccessCallBack = function() {                
             servicePromise.success.mostRecentCall.args[0].apply(servicePromise, arguments);
         }
