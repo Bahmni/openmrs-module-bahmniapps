4d833b6c4e64563e22ed2339ca3209ada01971c7
Deepak N <endeep123@gmail.com>
2014-03-19 11:38:33 +0530

    #1604 | Fixing package script

--- ui/scripts/package.sh
+++ ui/scripts/package.sh
@@ -2,10 +2,11 @@
 
 BASE_DIR=`dirname $0`
 ROOT_DIR=$BASE_DIR/..
+ZIP_FILE_NAME=bahmniapps.zip
 
 mkdir -p $ROOT_DIR/target
-rm -rf $ROOT_DIR/target/bahmni.zip
+rm -rf $ROOT_DIR/target/$ZIP_FILE_NAME
 
 rvm use 1.9.3
 grunt dist
-cd $ROOT_DIR/dist && zip -r ../target/bahmniapps.zip *
+cd $ROOT_DIR/dist && zip -r ../target/$ZIP_FILE_NAME *
