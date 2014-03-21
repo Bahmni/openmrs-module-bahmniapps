1468479870f86c61a7596c1a576e75f73ebee390
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-03-21 14:50:25 +0530

    Gurpreet, Balaji|#1965: Added vagrant database to run the bahmni-core migrations on vagrant

---
+++ scripts/vagrant-database.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -x
+PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $PATH_OF_CURRENT_SCRIPT/vagrant_functions.sh
+
+#invoke migration of openmrs core
+run_in_vagrant -c "sudo su - jss -c 'cd /bahmni_temp/ && ./run-liquibase-openmrs.sh'"
+#invoke migrations of bahmni core omods
+run_in_vagrant -c "sudo su - jss -c 'cd /bahmni_temp/ && ./run-modules-liquibase.sh'"
