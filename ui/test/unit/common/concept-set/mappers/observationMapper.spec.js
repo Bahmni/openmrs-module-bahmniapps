f7b8ebb42a43cfa394f25386ce32c0e04a68b5ee
Deepak N <endeep123@gmail.com>
2014-04-03 12:25:18 +0530

    Neha, D3| #1748 | Added tests for observation mapper

--- ui/test/unit/common/concept-set/mappers/observationMapper.spec.js
+++ ui/test/unit/common/concept-set/mappers/observationMapper.spec.js
@@ -1,48 +1,32 @@
 'use strict';
 
 describe("ObservationMapper", function () {
-    var build = function(conceptData) {
-        var concept = {
-            name: {name: conceptData.name || "conceptName"},
-            datatype: {name: "N/A"},
-            set: conceptData.set,
-            setMembers: conceptData.setMembers || []
-        };
-        return concept;
-    }, 
-    getObservations = function(observations, conceptName) {
-        return observations.filter(function(obs) {
-            return obs.concept.name === conceptName;
-        });
-    },
-    getXObservationByConceptName = function(xObservations, conceptName) {
-        return xObservations.filter(function(xObs) {
-            return xObs.groupMembers.some(function(groupMember) {
-                return groupMember.concept.name === conceptName;
-            });
-        });
-    };
-
-    it('should map new observations with given concept set and ui config', function () {
-        var conceptSet = 
+    var conceptSet, conceptSetUIConfig, compoundObservationConcept, observationMapper;
+    
+    beforeEach(function() {
+        conceptSet = 
             build({name: "VITALS_CONCEPT", set: true,
                 setMembers : [
                     build({name: "Pulse"}),
                     build({name: "BP", set: true, setMembers: [build({name: "Systolic"}), build({name: "Diastolic"})]}),
                     build({name: "Sugar"})
                 ]});
-        var conceptSetUIConfig = {
+
+        conceptSetUIConfig = {
             "Pulse": {},
             "Systolic": {showAbnormalIndicator: true},
             "Diastolic": {showAbnormalIndicator: false}
         };
-        var compoundObservation = build({name: "XCompoundObservation", 
-            set: true, 
-            setMembers: [ build({name: "IS_ABNORMAL"})]
-        });
-        
-        var observationMapper = new Bahmni.ConceptSet.ObservationMapper(conceptSetUIConfig, compoundObservation);
-        var rootNode = observationMapper.map([], conceptSet);
+
+        compoundObservationConcept = build({name: "XCompoundObservation", set: true, setMembers: [ build({name: "IS_ABNORMAL"})]});
+
+        observationMapper = new Bahmni.ConceptSet.ObservationMapper(conceptSetUIConfig, compoundObservationConcept);
+    });
+
+    it('should map new observations with given concept set and ui config', function () {
+        var existingObservations = [];
+
+        var rootNode = observationMapper.map(existingObservations, conceptSet);
 
         expect(rootNode.compoundObservation.concept.name).toBe('XCompoundObservation');
         expect(rootNode.primaryObservation.concept.name).toBe('VITALS_CONCEPT');
@@ -64,7 +48,6 @@ describe("ObservationMapper", function () {
         expect(rootNode.primaryObservation.groupMembers[2].concept.name).toBe('XCompoundObservation');
         expect(rootNode.primaryObservation.groupMembers[2].groupMembers[0].concept.name).toBe('Sugar');
         expect(rootNode.primaryObservation.groupMembers[2].groupMembers[1].concept.name).toBe('IS_ABNORMAL');
-
         var nodeForBP = rootNode.children[1];
         expect(nodeForBP.compoundObservation.concept.name).toBe('XCompoundObservation');
         expect(nodeForBP.primaryObservation.concept.name).toBe('BP');
@@ -75,6 +58,83 @@ describe("ObservationMapper", function () {
         expect(nodeForBP.children[1].primaryObservation.concept.name).toBe('Diastolic');
         expect(nodeForBP.children[1].showAbnormalIndicator).toBe(false);
     });
+
+
+    it('should map existing observations with given concept set and ui config', function () {
+        var systolic = buildObservation({concept: getConcept("Systolic"), value: 10});
+        var diastolic = buildObservation({concept: getConcept("Diastolic"), value: 20});
+        var bp = buildObservation({concept: getConcept("BP"), groupMembers: [buildCompundObservation(systolic, false), buildCompundObservation(diastolic)]});
+        var pulse = buildObservation({concept: getConcept("Pulse"), value: 30});
+        var vitals = buildObservation({concept:  getConcept("VITALS_CONCEPT"), groupMembers: [buildCompundObservation(pulse, true), buildCompundObservation(bp)]});
+        var existingObservations = [buildCompundObservation(vitals)];
+
+        var rootNode = observationMapper.map(existingObservations, conceptSet);
+
+        expect(rootNode.compoundObservation.concept.name).toBe('XCompoundObservation');
+        expect(rootNode.primaryObservation).toBe(vitals);
+        expect(rootNode.abnormalityObservation).toBeDefined();
+        expect(rootNode.children.length).toBe(3);
+        var nodeForPulse = rootNode.children[0];
+        expect(nodeForPulse.primaryObservation).toBe(pulse);
+        expect(nodeForPulse.abnormalityObservation.value).toBe(true);
+        var nodeForBP = rootNode.children[1];
+        expect(nodeForBP.primaryObservation).toBe(bp);
+        expect(nodeForBP.children.length).toBe(2);
+        expect(nodeForBP.children[0].primaryObservation.concept.name).toBe('Systolic');
+        expect(nodeForBP.children[0].abnormalityObservation.value).toBe(false);
+        expect(nodeForBP.children[0].showAbnormalIndicator).toBe(true);
+        expect(nodeForBP.children[1].primaryObservation.concept.name).toBe('Diastolic');
+        expect(nodeForBP.children[1].abnormalityObservation.value).toBe(undefined);
+        expect(nodeForBP.children[1].showAbnormalIndicator).toBe(false);
+    });
+
+    var build = function(conceptData) {
+        var concept = {
+            uuid: conceptData.uuid || genUUID(),
+            name: { name: conceptData.name || "conceptName"},
+            datatype: {name: "N/A"},
+            set: conceptData.set,
+            setMembers: conceptData.setMembers || []
+        };
+        return concept;
+    }; 
+    
+    var genUUID = function() { return (Math.random() * Math.pow(10, 16)).toString(); };
+    
+    var getConceptByName = function(concepts, conceptName) {
+        var foundConcept = concepts.filter(function(concept) {
+            return concept.name.name === conceptName;
+        })[0];
+        if(foundConcept) return foundConcept;
+        concepts.forEach(function(concept){
+            if(!foundConcept) foundConcept = getConceptByName(concept.setMembers, conceptName);
+        });
+        return foundConcept;
+    };
+
+    var getConcept = function(conceptName) {
+        var openMRSConcept = getConceptByName([conceptSet], conceptName);
+        return mapConcept(openMRSConcept);
+    };
+
+    var mapConcept = function(openMRSConcept) {
+        return {uuid: openMRSConcept.uuid, name: openMRSConcept.name.name};
+    };
+
+    var buildObservation = function(observationData) {
+        return {
+            uuid: observationData.uuid || genUUID(),
+            value: observationData.value,
+            concept: observationData.concept,
+            groupMembers: observationData.groupMembers
+        }
+    };
+
+    var buildCompundObservation = function(groupMember, isAbnormal) {
+        var groupMembers = [groupMember];
+        if(isAbnormal !== undefined) groupMembers.push(buildObservation({value: isAbnormal ,concept: mapConcept(build({name: "IS_ABNORMAL"}))}));
+        return buildObservation({concept: mapConcept(compoundObservationConcept), groupMembers: groupMembers})
+    }
 });
 
 
