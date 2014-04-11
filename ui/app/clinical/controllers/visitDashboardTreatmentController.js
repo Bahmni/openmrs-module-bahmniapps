9c41f7a502c31055953e712d99224946331a95a1
mihirk <mihir.khatwani@gmail.com>
2014-04-11 12:30:56 +0530

    Vinay|Navaneeth|Mihir|Shantan|#1770, styled the treatment section

--- ui/app/clinical/controllers/visitDashboardTreatmentController.js
+++ ui/app/clinical/controllers/visitDashboardTreatmentController.js
@@ -1,7 +1,11 @@
 angular.module('bahmni.clinical')
     .controller('VisitDashboardTreatmentController', ['$scope', '$stateParams', 'TreatmentService', function ($scope, $stateParams, treatmentService) {
 
+        var dateCompare = function (drugOrder1, drugOrder2) {
+            return drugOrder1.orderDate > drugOrder2.orderDate? -1: 1;
+        };
         treatmentService.getActiveDrugOrders($stateParams.patientUuid).then(function (drugOrders) {
-            $scope.drugOrders = drugOrders;
+            $scope.drugOrders = drugOrders.sort(dateCompare);
+
         });
     }]);
--- ui/app/clinical/models/drugOrder.js
+++ ui/app/clinical/models/drugOrder.js
@@ -5,12 +5,14 @@ Bahmni.Clinical.DrugOrder = (function () {
         this.orderDate = options.orderDate;
         this.dosage = options.dosage;
         this.days = options.days;
+        this.dose = options.dose;
     };
 
     DrugOrder.createFromOpenMRSRest = function (drugOrder) {
         return {name: drugOrder.drug.name,
             orderDate: drugOrder.startDate,
             dosage: drugOrder.drug.dosageForm.display,
+            dose: drugOrder.dose,
             days: Bahmni.Common.Util.DateUtil.diffInDays(new Date(drugOrder.startDate), new Date(drugOrder.autoExpireDate))}
     };
 
