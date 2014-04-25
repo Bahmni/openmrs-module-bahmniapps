d7e96d83e4a5af8d641b49e3b14a57c4c818e215
Deepak N <endeep123@gmail.com>
2014-04-25 15:18:55 +0530

    Fixing testacular conf

--- ui/test/config/testacular.conf.js
+++ ui/test/config/testacular.conf.js
@@ -6,7 +6,7 @@ files = [
     '../app/components/angular/angular.js',
     '../app/components/angular-route/angular-route.js',
     '../app/components/angular-mocks/angular-mocks.js',
-    '../app/components/ngInfiniteScroll/ng-infinite-scroll.js',
+    '../app/components/ngInfiniteScroll/build/ng-infinite-scroll.js',
     '../app/components/d3/d3.min.js',
     '../app/components/nvd3/nv.d3.min.js',
     '../app/components/angularjs-nvd3-directives/dist/*.js',
