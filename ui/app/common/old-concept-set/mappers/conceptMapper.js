5660bc4314d2079bac0911d2c86c65a81fbd36c1
shruthidipali <shruthidipali@gmail.com>
2014-04-25 12:55:52 +0530

    Mujir, Shruthi | #1785 |  ConceptSet : Swapped the old version with the new version

--- ui/app/common/old-concept-set/mappers/conceptMapper.js
+++ ui/app/common/old-concept-set/mappers/conceptMapper.js
@@ -1,16 +1,12 @@
 Bahmni.ConceptSet.ConceptMapper = function () {
-    this.map = function (openMrsConcept) {
+    this.map = function(openMrsConcept) {
         return {
             uuid: openMrsConcept.uuid,
             name: openMrsConcept.name.name,
             set: openMrsConcept.set,
             dataType: openMrsConcept.datatype.name,
-            hiAbsolute: openMrsConcept.hiAbsolute,
-            lowAbsolute: openMrsConcept.lowAbsolute,
             hiNormal: openMrsConcept.hiNormal,
-            lowNormal: openMrsConcept.lowNormal,
-            conceptClass: openMrsConcept.conceptClass,
-            answers: openMrsConcept.answers
+            lowNormal: openMrsConcept.lowNormal
         }
     };
 };
