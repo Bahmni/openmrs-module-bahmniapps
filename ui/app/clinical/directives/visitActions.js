d94ab5b19ec3a67eb0c0264b385fc680c6691351
Vinay Venu <vinayv@thoughtworks.com>
2014-04-25 12:58:42 +0530

    temp

--- ui/app/clinical/directives/visitActions.js
+++ ui/app/clinical/directives/visitActions.js
@@ -1,18 +1,33 @@
 angular.module('bahmni.clinical')
-.directive('visitActions', [function () {
-    var controller = function($scope, printer) {
-        $scope.printDischargeSummary = function() {
-            var dischargeSummary = new Bahmni.Clinical.DischargeSummary($scope.patient, $scope.visit);
-            printer.print('views/dischargeSummary.html', {dischargeSummary: dischargeSummary, visit: $scope.visit, patient: $scope.patient});
+    .directive('visitActions', [function () {
+        var controller = function ($scope, printer) {
+            var actions = {
+                printDischargeSummary: function () {
+                    $scope.title = "Print Discharge Summary";
+                    $scope.act = function () {
+                        var dischargeSummary = new Bahmni.Clinical.DischargeSummary($scope.patient, $scope.visit);
+                        printer.print('views/dischargeSummary.html', {dischargeSummary: dischargeSummary, visit: $scope.visit, patient: $scope.patient});
+                    };
+                },
+                printVisitSummary: function () {
+                    $scope.title = "Print Visit Summary";
+                    $scope.act = function () {
+                        printer.print('views/visitSummaryPrint.html', {visit: $scope.visit, patient: $scope.patient});
+                    };
+                }
+            };
+
+            actions[$scope.action]();
+        };
+        return {
+            restrict: 'EA',
+            scope: {
+                action: "=",
+                visit: "=",
+                patient: "="
+            },
+            controller: controller,
+            template: '<button type="button" ng-if="visit.hasAdmissionEncounter()" ng-click="act()">{{title}}</button>'
         }
-    };
-    return {
-        restrict:'EA',
-        scope:{
-            visit:"=",
-            patient:"="
-        },
-        controller: controller,
-        template:'<button type="button" ng-if="visit.hasAdmissionEncounter()" ng-click="printDischargeSummary()">Print Discharge Summary</button>'
-    }
-}]);
+    }])
+;
