c2d34ffb149b43e291f526a2cff43c612c7a708e
Deepak N <endeep123@gmail.com>
2014-05-02 23:11:04 +0530

    Fix observation mapper, small refactoring on spinner

--- ui/app/common/concept-set/mappers/observationMapper.js
+++ ui/app/common/concept-set/mappers/observationMapper.js
@@ -8,7 +8,7 @@ Bahmni.ConceptSet.ObservationMapper = function () {
     };
 
     var findInSavedObservation = function (concept, observations) {
-        return observations.filter(function (obs) {
+        return _.filter(observations, function (obs) {
             return obs && concept.uuid === obs.concept.uuid;
         });
     };
@@ -20,39 +20,39 @@ Bahmni.ConceptSet.ObservationMapper = function () {
             var configForConcept = conceptSetConfig[memberConcept.name.name] || {};
             var numberOfNodes = configForConcept.multiple || 1; 
             for(var i =0; i < savedObservations.length; i++) {
-                observationGroupMembers.push(mapObservation(memberConcept, savedObservations[i], conceptSetConfig || {}))
+                observationGroupMembers.push(mapObservation(memberConcept, savedObservations[i], conceptSetConfig))
             }
             for(var i =0; i < numberOfNodes - savedObservations.length; i++) {
-                observationGroupMembers.push(mapObservation(memberConcept, null, conceptSetConfig || {}))
+                observationGroupMembers.push(mapObservation(memberConcept, null, conceptSetConfig))
             }
         });
         return observationGroupMembers;
     };
 
-    var newObservationNode = function (concept, savedObsNode, conceptSetUIConfig) {
+    var newObservationNode = function (concept, savedObsNode, conceptSetConfig) {
         var observation = { concept: conceptMapper.map(concept), units: concept.units, label: concept.display, possibleAnswers: concept.answers, groupMembers: []};
-        return new Bahmni.ConceptSet.ObservationNode(observation, savedObsNode, conceptSetUIConfig);
+        return new Bahmni.ConceptSet.ObservationNode(observation, savedObsNode, conceptSetConfig);
     };
 
-    var mapObservation = function (concept, savedObs, conceptSetUIConfig) {
+    var mapObservation = function (concept, savedObs, conceptSetConfig) {
         if (savedObs && (savedObs.isObservation || savedObs.isObservationNode)) 
             return savedObs;
 
         var observation;
         if (concept.conceptClass.name === Bahmni.Common.Constants.conceptDetailsClassName) {
-            observation = newObservationNode(concept, savedObs, conceptSetUIConfig);
+            observation = newObservationNode(concept, savedObs, conceptSetConfig);
         } else {
             observation = newObservation(concept, savedObs);
         }
 
         var savedObsGroupMembers = savedObs ? savedObs.groupMembers : [];
-        observation.groupMembers = concept.set ? mapObservationGroupMembers(savedObsGroupMembers, concept.setMembers, conceptSetUIConfig) : [];
+        observation.groupMembers = concept.set ? mapObservationGroupMembers(savedObsGroupMembers, concept.setMembers, conceptSetConfig) : [];
 
         return observation;
     };
 
     this.map = function (observations, rootConcept, conceptSetConfig) {
         var savedObs = findInSavedObservation(rootConcept, observations)[0];
-        return mapObservation(rootConcept, savedObs || [], conceptSetConfig);
+        return mapObservation(rootConcept, savedObs, conceptSetConfig || {});
     };
 };
--- ui/app/common/ui-helper/spinner.js
+++ ui/app/common/ui-helper/spinner.js
@@ -2,18 +2,17 @@
 
 angular.module('bahmni.common.uiHelper')
     .factory('spinner', ['$q', function ($q) {
-        var arrayUtil = Bahmni.Common.Util.ArrayUtil
         var tokens = [];
 
-
-
-        var show = function (token) {
+        var show = function () {
+           var token = Math.random();
            tokens.push(token);
            if($('#overlay').length == 0) {
                 $('body').prepend('<div id="overlay"><div></div></div>');
            }
            $('#view-content').hide();
            $('#overlay').show();
+           return token;
         }
 
         var hide = function (token) {
@@ -24,19 +23,10 @@ angular.module('bahmni.common.uiHelper')
             }
         }
 
-        var forPromise = function (promise, options) {
-            options = options || {}
-            var token = Math.random();
-            show(token);
-            return promise.then(function (response) {
-                if(!options.doNotHideOnSuccess) {
-                    hide(token);
-                }
-                return response;
-            }, function (response) {
-                hide(token);
-                return $q.reject(response);
-            });
+        var forPromise = function(promise) {
+            var token = show();
+            promise['finally'](function() { hide(token); });
+            return promise;
         };
 
         return {
