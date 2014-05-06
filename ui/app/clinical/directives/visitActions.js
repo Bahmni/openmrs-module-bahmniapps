92a8234bc7a2e4ba7bd8340cd0a656669e311e0f
Navaneethakrishna Ramanathan <nramana@thoughtworks.com>
2014-05-06 11:59:50 +0530

    #1911 - Print Vist - Navaneeth

--- ui/app/clinical/directives/visitActions.js
+++ ui/app/clinical/directives/visitActions.js
@@ -3,17 +3,24 @@ angular.module('bahmni.clinical')
         var controller = function ($scope, printer) {
             var actions = {
                 printDischargeSummary: function () {
-                    $scope.title = "Print Discharge Summary";
+                    $scope.title = "Discharge Summary";
                     $scope.act = function () {
                         var dischargeSummary = new Bahmni.Clinical.DischargeSummary($scope.patient, $scope.visit);
                         printer.print('views/dischargeSummary.html', {dischargeSummary: dischargeSummary, visit: $scope.visit, patient: $scope.patient});
                     };
                 },
                 printVisitSummary: function () {
-                    $scope.title = "Print Visit Summary";
+                    $scope.title = "Visit Summary";
                     $scope.act = function () {
                         var showLabInvestigations = $scope.visit.admissionDate ? false: true;
                         printer.print('views/visitSummaryPrint.html', {visit: $scope.visit, patient: $scope.patient, showLabInvestigations: showLabInvestigations, visitDate: $scope.visitDate});
+                    };
+                },
+                printVisit: function () {
+                    $scope.title = "Visit";
+                    $scope.act = function () {
+                        var showLabInvestigations = $scope.visit.admissionDate ? false: true;
+                        printer.print('views/visitPrint.html', {visit: $scope.visit, patient: $scope.patient, showLabInvestigations: showLabInvestigations});
                     };
                 }
             };
