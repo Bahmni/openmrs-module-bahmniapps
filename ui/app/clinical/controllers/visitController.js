127c680c2bfc6c3fe8edf4858dc399ad14b6241e
Vinay Venu <vinayv@thoughtworks.com>
2014-05-07 18:32:13 +0530

    Revert "Hemanth, Vinay | #2170 | Bring back obsIgnoreList functionality in visit page"
    This reverts commit 4f9e803cc1e0b851c05ee2b452fb848e31f20da9.

--- ui/app/clinical/controllers/visitController.js
+++ ui/app/clinical/controllers/visitController.js
@@ -1,20 +1,15 @@
 'use strict';
 
 angular.module('bahmni.clinical')
-    .controller('VisitController', ['$scope', 'encounterService', 'visitService', 'spinner', '$stateParams', '$rootScope', 'appService',
-        function ($scope, encounterService, visitService, spinner, $stateParams, $rootScope, appService) {
-
-            var obsIgnoreList = appService.getAppDescriptor().getConfig("obsIgnoreList").value || [];
+    .controller('VisitController', ['$scope', 'encounterService', 'visitService', 'spinner', '$stateParams', '$rootScope',
+        function ($scope, encounterService, visitService, spinner, $stateParams, $rootScope) {
+            var visitUuid = $stateParams.visitUuid;
             $scope.visit = $rootScope.visit;
             $scope.patientUuid = $stateParams.patientUuid;
             $scope.showTrends = true;
 
             $scope.isNumeric = function (value) {
                 return $.isNumeric(value);
-            };
-
-            $scope.shouldShowObservation = function(observation) {
-                return obsIgnoreList.indexOf(observation.concept.name) === -1;
             };
 
             $scope.toggle = function (item) {
