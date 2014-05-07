4f9e803cc1e0b851c05ee2b452fb848e31f20da9
Vinay Venu <vinayv@thoughtworks.com>
2014-05-07 17:27:43 +0530

    Hemanth, Vinay | #2170 | Bring back obsIgnoreList functionality in visit page

--- ui/app/clinical/controllers/visitController.js
+++ ui/app/clinical/controllers/visitController.js
@@ -1,15 +1,20 @@
 'use strict';
 
 angular.module('bahmni.clinical')
-    .controller('VisitController', ['$scope', 'encounterService', 'visitService', 'spinner', '$stateParams', '$rootScope',
-        function ($scope, encounterService, visitService, spinner, $stateParams, $rootScope) {
-            var visitUuid = $stateParams.visitUuid;
+    .controller('VisitController', ['$scope', 'encounterService', 'visitService', 'spinner', '$stateParams', '$rootScope', 'appService',
+        function ($scope, encounterService, visitService, spinner, $stateParams, $rootScope, appService) {
+
+            var obsIgnoreList = appService.getAppDescriptor().getConfig("obsIgnoreList").value || [];
             $scope.visit = $rootScope.visit;
             $scope.patientUuid = $stateParams.patientUuid;
             $scope.showTrends = true;
 
             $scope.isNumeric = function (value) {
                 return $.isNumeric(value);
+            };
+
+            $scope.shouldShowObservation = function(observation) {
+                return obsIgnoreList.indexOf(observation.concept.name) === -1;
             };
 
             $scope.toggle = function (item) {
