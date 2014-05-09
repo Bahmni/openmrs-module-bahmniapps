a0c47b1193104729e227dc3e168114c200f2a426
Miss Beens <bhartin@thoughtworks.com>
2014-05-09 06:16:39 +0530

    Bharti, Mujir | #2191: Grouping lab orders on patient dashboard by visits

--- ui/app/clinical/controllers/patientDashboardLabOrdersController.js
+++ ui/app/clinical/controllers/patientDashboardLabOrdersController.js
@@ -2,6 +2,7 @@ angular.module('bahmni.clinical')
     .controller('PatientDashboardLabOrdersController', ['$scope', '$rootScope', '$stateParams', 'patientVisitHistoryService', 'encounterService', '$q',
         function ($scope, $rootScope, $stateParams, patientVisitHistoryService, encounterService, $q) {
             $scope.patientUuid = $stateParams.patientUuid;
+            var lastTwoVisitUuids;
             var createObservationsObject = function (encounterTransactions) {
                 return new Bahmni.Clinical.EncounterTransactionToObsMapper().map(encounterTransactions);
             };
@@ -40,17 +41,34 @@ angular.module('bahmni.clinical')
 
 
                 if ($scope.visits) {
-                    var lastTwoVisitUuids = getLastNVisitUuids($scope.visits, 2);
+                    lastTwoVisitUuids = getLastNVisitUuids($scope.visits, 2);
                     encounterService.searchForVisits(lastTwoVisitUuids).success(function (encounterTransactions) {
+                        var grouppedVisits = [];
 
-                        var orderGroupWithResults = new Bahmni.Clinical.OrdersMapper().create(encounterTransactions, 'testOrders', isLabTests, "visitUuid", $rootScope.allTestsAndPanelsConcept);
-                        var displayList = [];
-                        if (orderGroupWithResults.length != 0) {
-                            var uniqueTests = getUniqueOrdersWithinAVisit(orderGroupWithResults);
-                            displayList = getDisplayListForUniqueOrders(uniqueTests);
-                        }
-
-                        $scope.patientSummary.data = displayList;
+                        encounterTransactions.forEach(function(encounter){
+                            existingGrouppedVisit = grouppedVisits.filter(function(visit) {return visit.visitUuid == encounter.visitUuid})[0]
+                            if(existingGrouppedVisit){
+                                existingGrouppedVisit.records.push(encounter);
+                            }
+                            else{
+                                if(getLastNVisitUuids($scope.visits, 1)[0] == encounter.visitUuid){
+                                    grouppedVisits.push({records: [], name: "Latest", visitUuid: encounter.visitUuid});
+                                }
+                                else{
+                                    grouppedVisits.push({records: [], name: "Previous", visitUuid: encounter.visitUuid});
+                                }
+                            }
+                        });
+                        grouppedVisits.forEach(function(grouppedVisit){
+                            var orderGroupWithResults = new Bahmni.Clinical.OrdersMapper().create(grouppedVisit.records, 'testOrders', isLabTests, "visitUuid", $rootScope.allTestsAndPanelsConcept);
+                            var displayList = [];
+                            if (orderGroupWithResults.length != 0) {
+                                var uniqueTests = getUniqueOrdersWithinAVisit(orderGroupWithResults);
+                                displayList = getDisplayListForUniqueOrders(uniqueTests);
+                            }
+                            grouppedVisit.records = displayList;
+                        });
+                        $scope.patientSummary.data = grouppedVisits;
                         if ($scope.patientSummary.data.length == 0) {
                             $scope.patientSummary.message = Bahmni.Clinical.Constants.messageForNoLabOrders;
                         }
