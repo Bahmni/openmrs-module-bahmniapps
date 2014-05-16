48e5c3010a8e955a207168b708c18519a819ccb1
Nehashri <nehashrl@thoughtworks.com>
2014-05-16 17:11:16 +0530

    Neha | #2310 | voiding chief complaint tree if no value is present.

--- ui/app/common/domain/observationFilter.js
+++ ui/app/common/domain/observationFilter.js
@@ -20,8 +20,8 @@
 
 		self.filter = function(observations) {
 			var wrappedObservations = observations.map(Observation.wrap);
-			voidExistingObservationWithOutValue(wrappedObservations);
-			var filteredObservations = removeNewObservationsWithoutValue(wrappedObservations);
+            var filteredObservations = removeNewObservationsWithoutValue(wrappedObservations);
+			voidExistingObservationWithOutValue(filteredObservations);
 			return filteredObservations;
 		}
 	}
