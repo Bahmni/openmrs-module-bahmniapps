d206c2532b73113ace4050fc6be8edd4e88caf70
rohanpoddar <rohan.poddar@gmail.com>
2014-05-21 18:08:23 +0530

    #2342 | Rohan | Group observations by concept set in consultation tab

--- ui/app/clinical/controllers/consultationController.js
+++ ui/app/clinical/controllers/consultationController.js
@@ -12,11 +12,26 @@ angular.module('bahmni.clinical')
             return editedDiagnosesFromPastEncounters;
         };
         $scope.editedDiagnosesFromPastEncounters = geEditedDiagnosesFromPastEncounters();
-        var observationsToDisplay = new Bahmni.ConceptSet.ObservationMapper().getObservationsForView($scope.consultation.observations);
-        $scope.observationsToDisplay = observationsToDisplay;
 
         $scope.onNoteChanged = function () {
             $scope.consultation.consultationNote.observationDateTime = new Date();
         };
+
+        var groupedObservations = function(){
+            var groupedObservationsArray = [];
+            $scope.consultation.observations.forEach(function(observation){
+                var temp =[];
+                temp[0]=observation;
+                var observationsByGroup={
+                    "conceptSetName": observation.concept.name,
+                    "groupMembers": new Bahmni.ConceptSet.ObservationMapper().getObservationsForView(temp)
+                };
+                if(observationsByGroup.groupMembers.length){
+                    groupedObservationsArray.push(observationsByGroup);
+                }
+            });
+            return groupedObservationsArray;
+        };
+        $scope.groupedObservations = groupedObservations();
     }]);
 
