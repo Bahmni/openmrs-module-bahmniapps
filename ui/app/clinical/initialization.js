ca1acb9ce8c56250889241b5faa7c00a92c67467
Vinay Venu <vinayv@thoughtworks.com>
2014-06-02 15:48:54 +0530

    Vinay | #0 | Ensure treatment tab works for Nepal Demo

--- ui/app/clinical/initialization.js
+++ ui/app/clinical/initialization.js
@@ -4,16 +4,16 @@ angular.module('bahmni.clinical').factory('initialization',
     ['$rootScope', '$q', 'configurationService', 'authenticator', 'appService', 'spinner',
     function ($rootScope, $q, configurationService, authenticator, appService, spinner) {
         var getConfigs = function() {
-            // var configNames = ['encounterConfig', 'patientConfig', 'dosageFrequencyConfig','dosageInstructionConfig', 'consultationNoteConfig','labOrderNotesConfig', 'ruledOutDiagnosisConfig', 'allTestsAndPanelsConcept'];
-            var configNames = ['encounterConfig', 'consultationNoteConfig', 'labOrderNotesConfig', 'allTestsAndPanelsConcept'];
+            var configNames = ['encounterConfig', 'patientConfig', 'dosageFrequencyConfig','dosageInstructionConfig', 'consultationNoteConfig','labOrderNotesConfig', 'ruledOutDiagnosisConfig', 'allTestsAndPanelsConcept'];
+//            var configNames = ['encounterConfig', 'consultationNoteConfig', 'labOrderNotesConfig', 'allTestsAndPanelsConcept', 'dosageFrequencyConfig', 'dosageInstructionConfig'];
             return configurationService.getConfigurations(configNames).then(function (configurations) {
                 $rootScope.encounterConfig = angular.extend(new EncounterConfig(), configurations.encounterConfig);
                 $rootScope.consultationNoteConcept = configurations.consultationNoteConfig.results[0];
                 $rootScope.labOrderNotesConcept = configurations.labOrderNotesConfig.results[0];
                 $rootScope.allTestsAndPanelsConcept = configurations.allTestsAndPanelsConcept.results[0];
-                // $rootScope.patientConfig = configurations.patientConfig;
-                // $rootScope.dosageFrequencyConfig = configurations.dosageFrequencyConfig;
-                // $rootScope.dosageInstructionConfig = configurations.dosageInstructionConfig;
+                $rootScope.patientConfig = configurations.patientConfig;
+                $rootScope.dosageFrequencyConfig = configurations.dosageFrequencyConfig;
+                $rootScope.dosageInstructionConfig = configurations.dosageInstructionConfig;
                 // $rootScope.ruledOutDiagnosisConcept = configurations.ruledOutDiagnosisConfig.results[0];
             });
         };
