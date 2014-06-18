ff690e46c1f6631b496a3d082b55d2812a9879df
arathyja <arathyja@INarathyja.local>
2014-06-18 17:59:03 +0530

    RT | #108 | grouping the lab results by visits and loading encounter details only on the click event

--- ui/app/clinical/controllers/patientDashboardLabSummaryController.js
+++ ui/app/clinical/controllers/patientDashboardLabSummaryController.js
@@ -2,42 +2,24 @@ angular.module('bahmni.clinical')
     .controller('PatientDashboardLabSummaryController', ['$scope', '$rootScope', 'encounterService', '$q', 'spinner',
         function ($scope, $rootScope, encounterService, $q, spinner) {
 
-            $scope.allVisits = [];
-            var getAllVisitUuids = function () {
-                var visits = $rootScope.visits;
-                visits.sort(function (visit1, visit2) {
-                    return visit1.startDatetime - visit2.startDatetime;
-                });
-
-                return visits.map(function (visit) {
-                    return visit.uuid;
-                });
-            };
-
-
-            var init = function () {
-                if ($rootScope.visits) {
-                    var visits = getAllVisitUuids();
-
-                    spinner.forPromise(encounterService.searchForVisits(visits).success(function (encounterTransactions) {
-                        var groupedEncounterTransactions = _.groupBy(encounterTransactions, function (encounterTransaction) {
-                            return encounterTransaction.visitUuid;
-                        });
-                        for (var key in groupedEncounterTransactions) {
-                            var visit = Bahmni.Clinical.Visit.create(groupedEncounterTransactions[key], $rootScope.consultationNoteConcept, $rootScope.labOrderNotesConcept, $rootScope.encounterConfig, $rootScope.allTestsAndPanelsConcept, [], undefined);
-                            $scope.allVisits.push(visit);
-                        }
+            var initVisit = function (visit) {
+                if (visit) {
+                    spinner.forPromise(encounterService.searchForVisits(visit.uuid).success(function (encounterTransactions) {
+                        $scope.visit = Bahmni.Clinical.Visit.create(encounterTransactions, $rootScope.consultationNoteConcept, $rootScope.labOrderNotesConcept, $rootScope.encounterConfig, $rootScope.allTestsAndPanelsConcept, [], visit.uuid);
                     }));
                 }
             };
 
-//            $scope.resetCurrentVisit = function (visit) {
-//                $scope.currentVisit = ($scope.isCurrentVisit(visit)) ? $scope.newVisit : visit;
-//            };
-//
-//            $scope.isCurrentVisit = function (visit) {
-//                return $scope.currentVisit && $scope.currentVisit.uuid === visit.uuid;
-//            };
+            $scope.resetCurrentVisit = function (visit) {
+                $scope.currentVisit = ($scope.isCurrentVisit(visit)) ? $scope.newVisit : visit;
+                initVisit(visit);
+            };
 
-            init();
+            $scope.isCurrentVisit = function (visit) {
+                return $scope.currentVisit && $scope.currentVisit.uuid === visit.uuid;
+            };
+
+            $scope.currentVisit = $rootScope.visits[0];
+            initVisit($scope.currentVisit);
+
         }]);
