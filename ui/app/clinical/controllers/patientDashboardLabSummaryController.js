e24c1f50b8b55cba07eaba0effb0eb48c54d86bd
Vinkesh Banka <vinkesh.banka@gmail.com>
2014-07-21 18:07:15 +0530

    Banka | Fixing bug where notes was not shown for panel test orders.

--- ui/app/clinical/controllers/patientDashboardLabSummaryController.js
+++ ui/app/clinical/controllers/patientDashboardLabSummaryController.js
@@ -10,9 +10,17 @@ angular.module('bahmni.clinical')
                 line.showNotes = !line.showNotes;
             }
 
+            var flattened = function(accessions) {
+                return accessions.map(function(results) {
+                    return _.flatten(results, function(result) {
+                        return result.isPanel == true ? [result, result.tests] : result;
+                    });
+                });
+            }
+
             spinner.forPromise(labOrderResultService.getAllForPatient($rootScope.patient.uuid).then(function(results) {
                 var sortedConceptSet = new Bahmni.Clinical.SortedConceptSet($rootScope.allTestsAndPanelsConcept);
-                $scope.labAccessions = results.accessions.map(sortedConceptSet.sortTestResults);
+                $scope.labAccessions = flattened(results.accessions.map(sortedConceptSet.sortTestResults));
                 $scope.tabular = results.tabularResult;
                 $scope.tabular.tabularResult.orders = sortedConceptSet.sortTestResults($scope.tabular.tabularResult.orders);
             }));
