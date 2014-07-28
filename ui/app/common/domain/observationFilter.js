272bd812db0548a3b26bda5b15ecfc6157b0df0f
mihirk <mihir.khatwani@gmail.com>
2014-07-28 21:00:13 +0530

    Mihir | #143 | Removing sending of datetime of diagnosis and disposition to the server side

--- ui/app/common/domain/observationFilter.js
+++ ui/app/common/domain/observationFilter.js
@@ -10,7 +10,7 @@
                 if (observation.voided)
                     voidAllChildren(observation);
 			});
-		}
+		};
 
         var voidAllChildren = function(voidedObservation) {
             voidedObservation.groupMembers.forEach(function(childWithVoidedParent) {
@@ -18,7 +18,7 @@
 
                 voidAllChildren(childWithVoidedParent);
             })
-        }
+        };
 
 		var removeNewObservationsWithoutValue = function(observations) {
 			observations.forEach(function(observation){
@@ -27,7 +27,7 @@
 			return observations.filter(function(observation) {
 				return observation.isExisting() || observation.hasValue() || observation.hasMemberWithValue();
 			});
-		}
+		};
 
 		self.filter = function(observations) {
 			var wrappedObservations = observations.map(Observation.wrap);
@@ -35,54 +35,55 @@
 			voidExistingObservationWithOutValue(filteredObservations);
 			return filteredObservations;
 		}
-	}
+	};
 
 	var Observation = function(observationData) {
 		angular.extend(this, observationData);
 
 		this.isNew = function() {
 			return !this.uuid;
-		}
+		};
 
 		this.isExisting = function() {
 			return !this.isNew();
-		}
+		};
 
 		this.hasValue = function() {
 			return this.value !== undefined && this.value !== null && this.value !== '';
-		}
+		};
 
 		this.hasMemberWithValue = function() {
 			return this.groupMembers.some(function(groupMember){
 				return groupMember.hasValue() || groupMember.hasMemberWithValue();
 			})
-		}
+		};
 
 		this.isGroup = function() {
 			return this.groupMembers.length > 0;
-		}
+		};
 
 		this.isLeaf = function() {
 			return !this.isGroup();
-		}
+		};
 
 		this.isGroupWithOnlyVoidedMembers = function() {
 			return this.isGroup() && this.groupMembers.every(function(groupMember) {
 				return groupMember.voided;
 			})
-		}
+		};
 
 		this.isLeafNodeWithOutValue = function() {
 			return this.isLeaf() && !this.hasValue();
-		}
+		};
 
 		this.canBeVoided = function() {
 			return this.isExisting() && (this.isLeafNodeWithOutValue() || this.isGroupWithOnlyVoidedMembers());
 		}
-	}
+	};
 
 	Observation.wrap = function(observationData) {
 		var observation = new Observation(observationData);
+        observation.observationDateTime = null;
 		observation.groupMembers = observation.groupMembers ? observation.groupMembers.map(Observation.wrap) : [];
 		return observation;
 	}
