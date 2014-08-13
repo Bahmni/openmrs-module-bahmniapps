78851e2ab8605c0b74d01ba73325a7d789320bc3
rohanpoddar <rohan.poddar@gmail.com>
2014-08-13 12:17:04 +0530

    #523 | Rohan | Fix the date in dashboard to be of a specific format

--- ui/app/clinical/filters/observationValue.js
+++ ui/app/clinical/filters/observationValue.js
@@ -1,10 +1,11 @@
 angular.module('bahmni.clinical')
 .filter('observationValue', function ($filter) {
     return function(obs) {
-        if(obs.concept.dataType === 'Date') {
+        var type = obs.concept.dataType ? obs.concept.dataType : obs.type;
+        if(type === 'Date') {
             return $filter('date')(obs.value, 'd-MMM-yyyy');
         } 
-        if(obs.concept.dataType === 'Coded') {
+        if(type === 'Coded') {
             return obs.value ? (obs.value.name ? obs.value.name : obs.value) : "";
         } 
         return obs.value;
