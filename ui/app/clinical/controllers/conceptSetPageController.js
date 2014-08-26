cefe3634113764fbd54f5eb825226ecdd40d2718
hemanths <hemanths@inaniketsr.corporate.thoughtworks.com>
2014-08-26 11:25:20 +0530

    Chethan, Hemanth | #625 | fix default expandable template issue for different users.

--- ui/app/clinical/controllers/conceptSetPageController.js
+++ ui/app/clinical/controllers/conceptSetPageController.js
@@ -12,6 +12,16 @@ angular.module('bahmni.clinical')
     var fields = ['uuid','name'];
     var customRepresentation = Bahmni.ConceptSet.CustomRepresentationBuilder.build(fields, 'setMembers', numberOfLevels);
 
+            var showFirstTemplate = function () {
+                for (var index in $rootScope.consultation.selectedObsTemplate) {
+                    var template = $rootScope.consultation.selectedObsTemplate[index];
+                    if (template.isAdded) {
+                        template.show();
+                        break;
+                    }
+                }
+            };
+
     if($rootScope.consultation.selectedObsTemplate.length == 0){
         conceptSetService.getConceptSetMembers({name:"All Observation Templates",v:"custom:"+customRepresentation}).success(function(response){
             var allTemplates = response.results[0].setMembers;
@@ -22,7 +32,7 @@ angular.module('bahmni.clinical')
                 return new Bahmni.ConceptSet.ConceptSetSection(conceptSetConfig, $scope.consultation.observations,template);
             });
             $rootScope.consultation.selectedObsTemplate= allConceptSections.filter(function(conceptSet){ return conceptSet.isAvailable($scope.context); });
-            if ($rootScope.consultation.selectedObsTemplate.length) { $rootScope.consultation.selectedObsTemplate[0].show(); };
+            showFirstTemplate();
         });
     }
 
@@ -35,10 +45,10 @@ angular.module('bahmni.clinical')
     		messagingService.showMessage("info",template.conceptName+" Added successfully");
         } else if(!template.isAdded){
     		messagingService.showMessage("info",template.conceptName+" Removed successfully");
-        };
+        }
 
         $scope.showTemplates = !$scope.showTemplates;
-    }
+    };
 
     $scope.closeTemplatesPopup = function(){
         $scope.showTemplates = false;
