78db587bb375998c6161cc17d1fe189360f96605
Navaneeth <navaneethakrishna.r@gmail.com>
2014-10-06 16:16:07 +0530

    #12 - textarea height stays when there is atleast one text - Rohan / Navaneeth

--- ui/app/common/util/iosFixedElement.js
+++ ui/app/common/util/iosFixedElement.js
@@ -6,4 +6,14 @@ $(document)
     })
     .on('blur', 'input[type="text"], input[type="number"], textarea, select', function(e) {
         $body.removeClass('fix-fixed');
-});
+	})
+	.on('focus', 'textarea', function(e) {
+		$(this).css("height", "75px");
+	})
+	.on('blur', 'textarea', function(e) {
+		if ($(this).val()) {
+		    $(this).css("height", "75px");
+		} else {
+			$(this).css("height", "25px");
+		}
+	});
