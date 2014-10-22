912741ab9096316611ee8386aef72103c34f968b
Deepak N <endeep123@gmail.com>
2014-10-22 14:51:13 +0530

    Removed unwanted code

--- ui/app/clinical/helpers/diseaseTemplateSectionHelper.js
+++ ui/app/clinical/helpers/diseaseTemplateSectionHelper.js
@@ -1,23 +1,7 @@
 Bahmni.Clinical.DiseaseTemplateSectionHelper = {
     populateDiseaseTemplateSections: function (patientDashboardSections, diseaseTemplates) {
-        var templateExists = function (diseaseTemplateName, patientDashboardSections) {
-            return _.find(patientDashboardSections, function (section) {
-                return section.data.diseaseTemplateName === diseaseTemplateName;
-            });
-        };
-
-        var cleanUpOldTemplates = function () {
-            _.remove(patientDashboardSections, function (patientDashboardSection) {
-                return _.findIndex(diseaseTemplates, function (diseaseTemplate) {
-                    return diseaseTemplate.name === patientDashboardSection.data.diseaseTemplateName;
-                }) > -1;
-            })
-        };
-
-        cleanUpOldTemplates();
-
         diseaseTemplates.forEach(function (diseaseTemplate) {
-            if (!templateExists(diseaseTemplate.name, patientDashboardSections) && diseaseTemplate.notEmpty()) {
+            if (diseaseTemplate.notEmpty()) {
                 patientDashboardSections.push(Bahmni.Clinical.PatientDashboardSection.create({
                     data: {diseaseTemplateName: diseaseTemplate.name},
                     title: diseaseTemplate.label,
