3219415332debbe488203cbb07e80dc84edff812
rohanpoddar <rohan.poddar@gmail.com>
2014-11-04 18:13:03 +0530

    Rohan | Adding keycode for numpad decimal

--- ui/app/common/concept-set/directives/integer.js
+++ ui/app/common/concept-set/directives/integer.js
@@ -4,7 +4,7 @@ angular.module('bahmni.common.conceptSet')
     .directive('integer', function () {
         var link = function (scope, element, attrs) {
             var keyCode = [8, 9, 13, 46, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102, 103,
-                104, 105, 167, 168, 169, 190];
+                104, 105, 167, 168, 169, 190, 110];
             element.bind("keydown", function (event) {
                 if ($.inArray(event.which, keyCode) == -1) {
                     scope.$apply(function () {
