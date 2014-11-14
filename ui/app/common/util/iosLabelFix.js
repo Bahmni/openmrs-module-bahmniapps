6c4fb2428b39ecc67e675209c8b128629b12fa69
chethanTw <chethandec22@gmail.com>
2014-11-14 13:38:41 +0530

    D3, Chethan, Hemanth | #1228 | fixed consultation image upload from ipad.

--- ui/app/common/util/iosLabelFix.js
+++ ui/app/common/util/iosLabelFix.js
@@ -2,8 +2,7 @@ if (navigator.userAgent.match(/iPhone|iPod|iPad/i)) {
 	$(function(){
 		//This fix is needed when we use fastclick.js on ipad
 		$(document).on("click", "label[for]", function(event) {
-			event.preventDefault();
-			var $inputElement = $('#' + $(this).attr('for'));
+			var $inputElement = $('input#' + $(this).attr('for'));
 			var elementType = $inputElement.attr('type')
 			if(elementType === 'radio') {
 				$inputElement.prop('checked', true)
