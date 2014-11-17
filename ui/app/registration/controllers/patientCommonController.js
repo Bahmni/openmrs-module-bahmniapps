a7fce89c01afcb614b1f249112c341f73010838e
Swathi <swathiv@thoughtworks.com>
2014-11-17 10:48:41 +0530

    Banka, Swathi | 1153 | Configuring "Caste same as Last Name"

--- ui/app/registration/controllers/patientCommonController.js
+++ ui/app/registration/controllers/patientCommonController.js
@@ -1,13 +1,20 @@
 'use strict';
 
 angular.module('bahmni.registration')
-    .controller('PatientCommonController', ['$scope', '$http', 'patientAttributeService', 'addressAttributeService', 'appService',
-        function ($scope, $http, patientAttributeService, addressAttributeService, appService) {
+    .controller('PatientCommonController', ['$scope', '$rootScope', '$http', 'patientAttributeService', 'addressAttributeService', 'appService',
+        function ($scope, $rootScope, $http, patientAttributeService, addressAttributeService, appService) {
 
             var autoCompleteFields = appService.getAppDescriptor().getConfigValue("autoCompleteFields", []);
+            var showCasteSameAsLastNameCheckbox = appService.getAppDescriptor().getConfigValue("showCasteSameAsLastNameCheckbox");
             $scope.showMiddleName = appService.getAppDescriptor().getConfigValue("showMiddleName");
+
             $scope.isAutoComplete = function (fieldName) {
                 return autoCompleteFields.indexOf(fieldName) > -1;
+            };
+
+            $scope.showCasteSameAsLastName = function() {
+                var personAttributeHasCaste = (_.find($rootScope.patientConfiguration.personAttributeTypes, {name: 'caste'}) != null);
+                return showCasteSameAsLastNameCheckbox && personAttributeHasCaste;
             };
 
             $scope.setCasteAsLastName = function () {
