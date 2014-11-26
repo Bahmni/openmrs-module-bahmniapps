1c997ca933315321dcad49e128d4ca2c72cf682b
sravanthi <sravanns@thoughtworks.com>
2014-11-26 18:44:30 +0530

    Vikash, Sravanthi | #818 | Showing only abnormal lab result values on dashboard

--- ui/app/clinical/controllers/patientDashboardLabOrdersController.js
+++ ui/app/clinical/controllers/patientDashboardLabOrdersController.js
@@ -5,7 +5,7 @@ angular.module('bahmni.clinical')
             $scope.patientSummary = {message: "No Lab Orders for this patient."};
 
             var init = function () {
-                spinner.forPromise(labOrderResultService.getAllForPatient($scope.patientUuid, 1).then(function(results) {
+                spinner.forPromise(labOrderResultService.getAllForPatient($scope.patientUuid, 1).then(function (results) {
                     var sortedConceptSet = new Bahmni.Clinical.SortedConceptSet($rootScope.allTestsAndPanelsConcept);
                     $scope.labAccessions = results.accessions.map(sortedConceptSet.sortTestResults);
                 }));
@@ -13,11 +13,25 @@ angular.module('bahmni.clinical')
 
             init();
 
-            $scope.getUploadedFileUrl = function(uploadedFileName){
+            $scope.getUploadedFileUrl = function (uploadedFileName) {
                 return Bahmni.Common.Constants.labResultUploadedFileNameUrl + uploadedFileName;
             };
 
             $scope.hasLabOrders = function () {
                 return $scope.labAccessions && $scope.labAccessions.length > 0;
             };
+            $scope.hasAbnormalTests= function (labOrderResult) {
+                if(labOrderResult.isPanel){
+                    var tests = labOrderResult.tests;
+                    var hasAbnormal = false;
+                    tests.forEach(function (test){
+                        if(test.abnormal){
+                           hasAbnormal = true;
+                            return;
+                        }
+                    });
+                    return hasAbnormal;
+                }
+                return labOrderResult.abnormal;
+            }
         }]);
