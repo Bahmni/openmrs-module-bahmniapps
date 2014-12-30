c8f70af836f090b4228b7d9f9a9293bba86c1592
shruthidipali <shruthidipali@gmail.com>
2014-12-30 16:10:41 +0530

    Chetan, Shruthi | Removing unused rootScope from latestObs

--- ui/app/common/concept-set/directives/latestObs.js
+++ ui/app/common/concept-set/directives/latestObs.js
@@ -1,19 +1,12 @@
 angular.module('bahmni.common.conceptSet')
     .directive('latestObs', function () {
-        var controller = function ($scope, observationsService, $q, spinner, $rootScope) {
+        var controller = function ($scope, observationsService, $q, spinner) {
             var init = function () {
-                $scope.visits = $rootScope.visits;
-                $scope.activeVisit = $rootScope.activeVisit;
-                createObservationSectionView();
-            };
-
-            var createObservationSectionView = function () {
                 spinner.forPromise(observationsService.fetch($scope.patientUuid, $scope.conceptNames, "latest").then(function (response) {
                     var observations = new Bahmni.Common.Obs.ObservationMapper().map(response.data, []);
                     $scope.observations = _.sortBy(observations, 'sortWeight');
                 }));
             };
-
             init();
         };
 
