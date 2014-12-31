5f0bae3988899dd0bd7a1bacd222f34541612584
Vinkesh Banka <vinkesh.banka@gmail.com>
2014-12-31 10:43:18 +0530

    Hemanth, Banka | Fixed datetimepicker which was failing after angular 1.3 update.

--- ui/app/common/ui-helper/directives/datetimepicker.js
+++ ui/app/common/ui-helper/directives/datetimepicker.js
@@ -1,20 +1,27 @@
 angular.module('bahmni.common.uiHelper')
     .directive('datetimepicker', function () {
         var link = function ($scope) {
-            $scope.selectedTime = "00:00";
+            $scope.selectedTime = moment("00:00", "HH:mm").toDate();
+            var getSelectedDateStr = function() {
+                return moment($scope.selectedDate).format("YYYY-MM-DD");
+            }
+
+            var getSelectedTimeStr = function() {
+                return $scope.selectedTime != null ? moment($scope.selectedTime).format("HH:mm") : "";
+            }
+
             $scope.updateModel = function() {
                 if($scope.selectedDate != null) {
-                    $scope.model = $scope.selectedDate + ($scope.selectedTime != null ? " " + $scope.selectedTime : "");
+                    $scope.model =  getSelectedDateStr() + " " + getSelectedTimeStr();
                 }
             }
 
             if($scope.model != null) {
                 var date = moment($scope.model, "MMMM DD, YYYY HH:mm:ss a");
-                $scope.selectedDate = date.format("YYYY-MM-DD");
-                $scope.selectedTime = date.format("HH:mm");
+                $scope.selectedDate = date.toDate();
+                $scope.selectedTime = date.toDate();
                 $scope.updateModel();
             }
-
         }
 
         return {
