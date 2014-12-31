187cbdc04f55a5fb3fbc01abfc36ae8e21d239e4
Vinkesh Banka <vinkesh.banka@gmail.com>
2014-12-31 11:01:34 +0530

    Hemanth, Banka | Fixing initialization promise issue. This was causing failure in consultationInitialization sometimes.

--- ui/app/clinical/initialization.js
+++ ui/app/clinical/initialization.js
@@ -4,14 +4,24 @@ angular.module('bahmni.clinical').factory('initialization',
     ['authenticator', 'appService', 'spinner', 'configurations',
         function (authenticator, appService, spinner, configurations) {
 
-            var loadConfigPromise = configurations.load(['patientConfig', 'encounterConfig', 'consultationNoteConfig', 'labOrderNotesConfig',
-                'radiologyImpressionConfig', 'allTestsAndPanelsConcept', 'dosageFrequencyConfig', 'dosageInstructionConfig']);
+            var loadConfigPromise = function() {
+                return configurations.load([
+                    'patientConfig',
+                    'encounterConfig',
+                    'consultationNoteConfig',
+                    'labOrderNotesConfig',
+                    'radiologyImpressionConfig',
+                    'allTestsAndPanelsConcept',
+                    'dosageFrequencyConfig',
+                    'dosageInstructionConfig'
+                ]);
+            }
 
             var initApp = function () {
                 return appService.initApp('clinical', {'app': true, 'extension': true });
             };
 
             return spinner.forPromise(authenticator.authenticateUser().then(initApp).then(loadConfigPromise));
-
-        }]
+        }
+    ]
 );
