80005173de1c17bddcbf7aa6887674716d18fc54
indraneelr <indraneel.rr@gmail.com>
2015-01-15 17:50:56 +0530

    indraneel | refactoring grid observations to not have dependency on conceptSetObs mapper

--- ui/app/common/obs/models/gridObservation.js
+++ ui/app/common/obs/models/gridObservation.js
@@ -1,9 +1,23 @@
 Bahmni.Common.Obs.GridObservation = (function () {
 
+    var conceptMapper = new Bahmni.Common.Domain.ConceptMapper();
+
     var GridObservation = function (obs, conceptConfig) {
         angular.extend(this, obs);
         this.type = "grid";
         this.conceptConfig = conceptConfig;
+    };
+
+    var getObservationDisplayValue = function (observation) {
+        if (observation.isBoolean || observation.type === "Boolean") {
+            return observation.value === true ? "Yes" : "No";
+        }
+        if(!observation.value) return "";
+        if(typeof observation.value.name === 'object') {
+            var valueConcept = conceptMapper.map(observation.value);
+            return valueConcept.shortName || valueConcept.name;
+        }
+        return observation.value.shortName || observation.value.name || observation.value ;
     };
 
     GridObservation.prototype = {
@@ -13,11 +27,14 @@ Bahmni.Common.Obs.GridObservation = (function () {
         },
 
         getDisplayValue: function () {
-            var gridObservationDisplayValue = new Bahmni.ConceptSet.ObservationMapper().getGridObservationDisplayValue(this);
+            var gridObservationDisplayValue = _.compact(_.map(this.groupMembers, function (member) {
+                return getObservationDisplayValue(member);
+            })).join(', ');
             return gridObservationDisplayValue || this.value;
         }
-
     };
+
+
 
     return GridObservation;
 
