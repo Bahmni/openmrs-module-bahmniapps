b23589dc8767e4c2d27b2be0f01065e53d198b02
Vinkesh Banka <vinkesh.banka@gmail.com>
2015-01-21 16:49:45 +0530

    Charles, Banka | Adding more tests

---
+++ ui/test/unit/clinical/common/mappers/diagnosisMapper.spec.js
@@ -0,0 +1,31 @@
+describe("Diagnosis Mapper", function () {
+    it("should map simple diagnosis", function() {
+        var mappedDiagnosis = new Bahmni.DiagnosisMapper().mapDiagnosis({});
+        expect(mappedDiagnosis).toEqual(jasmine.any(Bahmni.Clinical.Diagnosis));
+        expect(mappedDiagnosis.codedAnswer).toEqual({name: undefined, uuid: undefined});
+    });
+
+    it("should map diagnosis status", function() {
+        var mappedDiagnosis = new Bahmni.DiagnosisMapper().mapDiagnosis({diagnosisStatusConcept: {name: "Ruled Out Diagnosis"}});
+        expect(mappedDiagnosis.diagnosisStatus).toEqual("RULED OUT");
+    });
+
+    it("should map first diagnosis", function() {
+        var mappedDiagnosis = new Bahmni.DiagnosisMapper().mapDiagnosis({firstDiagnosis: {}});
+        expect(mappedDiagnosis.firstDiagnosis).toEqual(jasmine.any(Bahmni.Clinical.Diagnosis));
+    });
+
+    it("should map saved diagnoses from current encounter", function() {
+        var mappedDiagnosis = new Bahmni.DiagnosisMapper().mapDiagnosis({firstDiagnosis: {}});
+        expect(mappedDiagnosis.firstDiagnosis).toEqual(jasmine.any(Bahmni.Clinical.Diagnosis));
+    });
+
+    it("should map simple diagnoses", function() {
+        var mappedDiagnoses = new Bahmni.DiagnosisMapper().mapDiagnoses([{}, {}]);
+        expect(mappedDiagnoses.length).toBe(2);
+        expect(mappedDiagnoses[0]).toEqual(jasmine.any(Bahmni.Clinical.Diagnosis));
+        expect(mappedDiagnoses[1]).toEqual(jasmine.any(Bahmni.Clinical.Diagnosis));
+    });
+
+
+});
