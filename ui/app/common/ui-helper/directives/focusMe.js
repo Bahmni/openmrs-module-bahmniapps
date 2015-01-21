5e54a57aa748de5af5dfea8d38a36022958de1f2
Hemanth S.R <hemanthfeb25@gmail.com>
2015-01-21 16:46:59 +0530

    Hemanth | #1551 | autofocus on first name field on load.

---
+++ ui/app/common/ui-helper/directives/focusMe.js
@@ -0,0 +1,18 @@
+angular.module('bahmni.common.uiHelper')
+    .directive('focusMe',
+
+    function ($timeout, $parse) {
+        return {
+            link: function (scope, element, attrs) {
+                var model = $parse(attrs.focusMe);
+                scope.$watch(model, function(value) {
+                    if (value === true) {
+                        $timeout(function () {
+                            element[0].focus();
+                        });
+                    }
+                });
+            }
+        };
+    }
+);
