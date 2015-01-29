637beae82d2a3af07086da40e22046f3510bcdf5
Vinkesh Banka <vinkesh.banka@gmail.com>
2015-01-29 18:08:53 +0530

    Banka | #1326 | Using observation display control in registration visit page

--- ui/app/registration/app.js
+++ ui/app/registration/app.js
@@ -2,7 +2,7 @@
 
 angular
     .module('registration', ['ui.router', 'bahmni.registration', 'authentication', 'bahmni.common.config',
-        'bahmni.common.appFramework', 'httpErrorInterceptor', 'bahmni.common.photoCapture', 'bahmni.common.obs'])
+        'bahmni.common.appFramework', 'httpErrorInterceptor', 'bahmni.common.photoCapture', 'bahmni.common.obs', 'bahmni.common.displaycontrol'])
     .config(['$urlRouterProvider', '$stateProvider', '$httpProvider', function ($urlRouterProvider, $stateProvider, $httpProvider) {
         $httpProvider.defaults.headers.common['Disable-WWW-Authenticate'] = true;
         $urlRouterProvider.otherwise('/search');
