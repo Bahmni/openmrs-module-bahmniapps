d8c26fc89a20ce311bd799170a275247dcb113af
SwathiVarkala <swathivarkala@gmail.com>
2015-01-30 12:36:18 +0530

    Swathi | Adding more tests

--- ui/test/unit/clinical/services/clinicalConfigService.spec.js
+++ ui/test/unit/clinical/services/clinicalConfigService.spec.js
@@ -14,6 +14,16 @@ describe("clinicalAppConfigService", function () {
                     "Radiology": "Radiology Order",
                     "Endoscopy": "Endoscopy Order"
                 },
+                "visitPage": {
+                    "investigationResultParams": {
+                        "title": "Investigations"
+                    }
+                },
+                "printConfig": {
+                    "visitSummaryPrint": {
+                        "showChart": false
+                    }
+                },
                 "conceptSetUI": {
                     "Receptor Status": {
                         "grid": true
@@ -21,7 +31,33 @@ describe("clinicalAppConfigService", function () {
                     "Pathologic Diagnosis": {
                         "multiSelect": true
                     }
+                },
+                "drugOrder": {
+                    "defaultDurationUnit": "Day(s)",
+                    "defaultInstructions": "As directed",
+                    "frequencyDefaultDurationUnitsMap": [
+                        {
+                            "minFrequency": "1/7",
+                            "maxFrequency": 5,
+                            "defaultDurationUnit": "Day(s)"
+                        }
+                    ],
+                    "drugFormDefaults": {
+                        "Ayurvedic": {
+                            "doseUnits": "Teaspoon",
+                            "route": "Oral"
+                        },
+                        "Capsule": {
+                            "doseUnits": "Capsule(s)",
+                            "route": "Oral"
+                        },
+                        "Tablet": {
+                            "doseUnits": "Tablet(s)",
+                            "route": "Oral"
+                        }
+                    }
                 }
+
             }
 
         }
@@ -70,6 +106,16 @@ describe("clinicalAppConfigService", function () {
             },
             "order": 2,
             "requiredPrivilege": "app:clinical:history"
+        },
+
+        {
+            "id": "bahmni.clinical.treatment.links",
+            "extensionPointId": "org.bahmni.clinical.treatment.links",
+            "type": "link",
+            "label": "Open <u>Q</u>uotation",
+            "url": "http://localhost:8069/quotations/latest?patient_ref={{patient_ref}}",
+            "shortcutKey": "q",
+            "requiredPrivilege": "app:billing"
         }
     ]
     };
@@ -90,7 +136,8 @@ describe("clinicalAppConfigService", function () {
         _sessionService.loadCredentials.and.callFake(function () {
             return  specUtil.respondWith({"privileges": [
                 {"name": "app:clinical:observationTab"},
-                {"name": "app:clinical:history"}
+                {"name": "app:clinical:history"},
+                {"name": "app:billing"},
             ]});
         });
         _sessionService.loadProviders.and.callFake(function () {
@@ -114,6 +161,14 @@ describe("clinicalAppConfigService", function () {
 
 
     describe("should fetch app config", function () {
+        it('should fetch drugorder config', function (done) {
+            appService.initApp('clinical', {'app': true}).then(function () {
+                var result = clinicalAppConfigService.getDrugOrderConfig();
+                expect(result.defaultDurationUnit).toBe("Day(s)");
+                done();
+            });
+        });
+
         it('should fetch concept config', function (done) {
             appService.initApp('clinical', {'app': true}).then(function () {
                 var result1 = clinicalAppConfigService.getConceptConfig("Receptor Status");
@@ -147,12 +202,44 @@ describe("clinicalAppConfigService", function () {
                 done();
             });
         });
+
+        it('should fetch visitPage config', function (done) {
+            appService.initApp('clinical', {'app': true}).then(function () {
+                var result = clinicalAppConfigService.getVisitPageConfig();
+                expect(result.investigationResultParams.title).toBe("Investigations");
+                done();
+            });
+        });
+
+        it('should fetch printConfig', function (done) {
+            appService.initApp('clinical', {'app': true}).then(function () {
+                var result = clinicalAppConfigService.getPrintConfig();
+                expect(result.visitSummaryPrint.showChart).toBe(false);
+                done();
+            });
+        });
     });
 
     describe("should fetch extension config", function () {
         it('should fetch consultation boards', function (done) {
             appService.initApp('clinical', {'extension': true}).then(function () {
                 var result = clinicalAppConfigService.getAllConsultationBoards();
+                expect(result.length).toBe(1);
+                done();
+            });
+        });
+
+        it('should fetch consultation board link', function (done) {
+            appService.initApp('clinical', {'extension': true}).then(function () {
+                var result = clinicalAppConfigService.getConsultationBoardLink();
+                expect(result).toBe("/patient/undefined/concept-set-group/observations");
+                done();
+            });
+        });
+
+        it('should fetch treatment action link', function (done) {
+            appService.initApp('clinical', {'extension': true}).then(function () {
+                var result = clinicalAppConfigService.getTreatmentActionLink();
                 expect(result.length).toBe(1);
                 done();
             });
