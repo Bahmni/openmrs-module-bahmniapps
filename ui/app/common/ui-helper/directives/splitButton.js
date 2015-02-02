69b9d06d71f13db76ca54800bd3905f55cde6843
Vikashg <vikashgupta2000@gmail.com>
2015-02-02 15:37:40 +0530

    Indraneel, Vikash | #1145 | Clicking on visit type, focus on the dropdown menu.

--- ui/app/common/ui-helper/directives/splitButton.js
+++ ui/app/common/ui-helper/directives/splitButton.js
@@ -1,28 +1,47 @@
 angular.module('bahmni.common.uiHelper')
-    .directive('splitButton', function ($parse) {
+    .directive('splitButton',['$timeout', '$parse', function ($timeout, $parse) {
         var controller = function($scope) {
             $scope.primaryOption = $scope.primaryOption || $scope.options[0];
             $scope.secondaryOptions = _.without($scope.options, $scope.primaryOption);
-
             $scope.hasMultipleOptions = function() {
                 return $scope.secondaryOptions.length > 0;
             }
+
         }
 
+        var link = function(scope,element,attrs){
+            var shouldScroll = function(elementPosition, elementHeight){
+                var windowHeight = window.innerHeight + $(window).scrollTop();
+                return windowHeight < (elementHeight+elementPosition);
+            };
+
+            scope.scrollToBottom = function(){
+                var timeout = $timeout(function(){
+                    var scrollHeight = $(element)[0].scrollHeight;
+                    if(shouldScroll(element.position().top,scrollHeight)) {
+                        window.scrollBy(0, scrollHeight);
+                        $timeout.cancel(timeout);
+                    }
+                });
+            }
+        };
         return {
             restrict: 'A',
             template: '<div class="split-button" bm-pop-over>'+
-                        '<button bm-pop-over-trigger class="toggle-button icon-caret-down" ng-show="hasMultipleOptions()" type="button"></button>' +
+                        '<button bm-pop-over-trigger class="toggle-button icon-caret-down" ng-show="hasMultipleOptions()" ng-click="scrollToBottom()" type="button"></button>' +
                         '<ul class="options">' +
                             '<li class="primaryOption">' +
                                 '<button ng-class="buttonClass" ng-click="optionClick()(primaryOption)" accesskey="{{primaryOption.shortcutKey}}" ng-bind-html="optionText()(primaryOption)"></button>' +
                             '</li>' +
+                            '<ul class="hidden-options">' +
                             '<li bm-pop-over-target ng-repeat="option in secondaryOptions" class="secondaryOption">' +
                                 '<button ng-class="buttonClass" ng-click="optionClick()(option)" accesskey="{{option.shortcutKey}}" ng-bind-html="optionText()(option)"></button>' +
                             '</li>' +
+                            '</ul>'+
                         '</ul>' +
                       '</div>',
             controller: controller,
+            link:link,
             scope: {
                 options: '=',
                 primaryOption: '=',
@@ -30,4 +49,4 @@ angular.module('bahmni.common.uiHelper')
                 optionClick: '&'
             }
         };
-    });
+    }])
