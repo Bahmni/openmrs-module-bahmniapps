5efcdce6b5e6e331c93f6e9fbf451ef122378507
chethanTw <chethandec22@gmail.com>
2015-02-02 15:02:31 +0530

    1178 | Chethan, Sravanthi | Ability to display notes on observation control and making it configurable

--- ui/app/common/obs/directives/showObservation.js
+++ ui/app/common/obs/directives/showObservation.js
@@ -1,13 +1,19 @@
 angular.module('bahmni.common.obs')
     .directive('showObservation', function () {
+        var controller = function($scope){
+            $scope.toggle = function(observation) {
+                observation.showDetails = !observation.showDetails
+            };
+        };
         return {
             restrict: 'E',
             scope: {
                 observation: "=",
                 patient: "=",
                 showDate: "@",
-                showComments: "="
+                showDetailsButton: "="
             },
+            controller: controller,
             template: '<ng-include src="\'../common/obs/views/showObservation.html\'" />'
         };
     });
