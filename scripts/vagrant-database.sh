e2b1c51c39700b10b78cec07cc8507567918f857
Vinay Venu <vinayv@thoughtworks.com>
2015-02-06 08:07:06 +0530

    Use bahmni user by default

--- scripts/vagrant-database.sh
+++ scripts/vagrant-database.sh
@@ -3,6 +3,6 @@ PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $PATH_OF_CURRENT_SCRIPT/vagrant_functions.sh
 
 #invoke migration of openmrs core
-run_in_vagrant -c "sudo su - jss -c 'cd /bahmni_temp/ && ./run-liquibase-openmrs.sh'"
+run_in_vagrant -c "sudo su - bahmni -c 'cd /bahmni_temp/ && ./run-liquibase-openmrs.sh'"
 #invoke migrations of bahmni core omods
-run_in_vagrant -c "sudo su - jss -c 'cd /bahmni_temp/ && ./run-modules-liquibase.sh'"
+run_in_vagrant -c "sudo su - bahmni -c 'cd /bahmni_temp/ && ./run-modules-liquibase.sh'"
--- scripts/vagrant-link.sh
+++ scripts/vagrant-link.sh
@@ -2,6 +2,8 @@
 
 PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $PATH_OF_CURRENT_SCRIPT/vagrant_functions.sh
+#USER=jss
+USER=bahmni
 
 if [ "$#" == "0" ]; then
 	FOLDER="app"
@@ -11,11 +13,11 @@ fi
 
 run_in_vagrant -c "sudo rm -rf /var/www/bahmniapps"
 run_in_vagrant -c "sudo ln -s /openmrs-module-bahmniapps/ui/$FOLDER/ /var/www/bahmniapps"
-run_in_vagrant -c "sudo chown -h jss:jss /var/www/bahmniapps"
+run_in_vagrant -c "sudo chown -h ${USER}:${USER} /var/www/bahmniapps"
 
 run_in_vagrant -c "sudo rm -rf /var/www/style-guide"
 run_in_vagrant -c "sudo ln -s /openmrs-module-bahmniapps/ui/style-guide/ /var/www/style-guide"
-run_in_vagrant -c "sudo chown -h jss:jss /var/www/style-guide"
+run_in_vagrant -c "sudo chown -h ${USER}:${USER} /var/www/style-guide"
 
 
 
