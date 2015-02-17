68671aeb6eb1de5c15f93315b902857e79168966
SwathiVarkala <swathivarkala@gmail.com>
2015-02-17 15:32:45 +0530

    Swathi | Fixed Coverage issues

--- ui/test/unit/common/obs/multiSelectObservation.spec.js
+++ ui/test/unit/common/obs/multiSelectObservation.spec.js
@@ -2,6 +2,19 @@
 
 describe("Observation", function () {
     var MultiSelectObservation = Bahmni.Common.Obs.MultiSelectObservation;
+    var ConceptSetMultiSelectObservations = Bahmni.ConceptSet.MultiSelectObservations;
+    var ConceptSetMultiSelectObservation = Bahmni.ConceptSet.MultiSelectObservation;
+    var conceptSetUI = {
+        "Menstrual History": {
+            "allowAddMore": true
+        },
+        "M/C Days" : {
+            "multiSelect": true
+        },
+        "LMP": {
+            "allowAddMore": true
+        }
+    };
 
     describe("display Value", function () {
 
@@ -13,7 +26,59 @@ describe("Observation", function () {
             expect(observation.getDisplayValue()).toBe("Invasive Ductal Carcinoma, Invasive Lobular Carcinoma");
             expect(observation.encounterDateTime).toBe(1414486007000);
         });
+    });
 
-    })
+    describe("Concept Set MultiSelectObservations", function () {
+
+        it("should test MultiSelectObservations", function () {
+            var observation = new ConceptSetMultiSelectObservations(conceptSetUI);
+            observation.map(groupMembers);
+            expect(observation.multiSelectObservationsMap["M/C Days"].label).toBe("M/C Days");
+            
+        });
+    });
+
+    describe("Concept Set MultiSelectObservation", function () {
+
+        it("should test MultiSelectObservation", function () {
+            var observation = new ConceptSetMultiSelectObservation(groupMembers[0]["concept"],groupMembers,conceptSetUI);
+            expect(observation.cloneNew().label).toBe("M/C Days");
+            expect(observation.isFormElement()).toBe(true);
+            expect(observation.getControlType()).toBe("buttonselect");
+            expect(observation.atLeastOneValueSet()).toBe(true);
+            expect(observation.canHaveComment()).toBe(false);
+        });
+    });
+
+    var groupMembers = [ 
+        {
+            "concept": {
+              "name": "M/C Days",
+              "shortName": null,
+              "conceptClass": "Misc",
+              "answers": []
+            },
+            "label": "M/C Days",
+            "possibleAnswers": [],
+            "groupMembers": [],
+            "isObservation": true,
+            "uniqueId": "observation_55",
+            "conceptUIConfig": {"multiSelect": true}
+        },
+        {
+            "concept": {
+              "name": "LMP",
+              "shortName": null,
+              "conceptClass": "Misc",
+              "answers": []
+            },
+            "label": "LMP",
+            "possibleAnswers": [],
+            "groupMembers": [],
+            "isObservation": true,
+            "uniqueId": "observation_56",
+            "conceptUIConfig": {"multiSelect": true}
+        }
+    ]
 
 });
