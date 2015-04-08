2434a11af9c12b41272ef88f95b64919f4b63f16
Pankaj <pankajl@thoughtworks.com>
2015-04-08 11:58:04 +0530

    Pankaj | #699 | Navigate to dashboard of previous patients

--- ui/app/common/ui-helper/directives/backLinks.js
+++ ui/app/common/ui-helper/directives/backLinks.js
@@ -8,11 +8,9 @@ angular.module('bahmni.common.uiHelper')
 					        '<a class="back-btn" ng-if="backLink.action" accesskey="{{backLink.accessKey}}" ng-click="backLink.action()" id="{{backLink.id}}"> <span ng-bind-html="backLink.label"></span> </a>' +
 					        '<a class="back-btn" ng-class="{\'dashboard-link\':backLink.image}" ng-if="backLink.url" accesskey="{{backLink.accessKey}}" ng-href="{{backLink.url}}" id="{{backLink.id}}"  title="{{backLink.title}}"> ' +
                                 '<img ng-if="backLink.image" src="{{backLink.image}}" onerror="this.onerror=null; this.src=\'../images/blank-user.gif\'"/>' +
-                                '<i ng-if="backLink.icon && !backLink.image" class="{{backLink.icon}}"></i>' +
-                                '<span ng-if="!backLink.image" ng-bind-html="backLink.label"></span> </a>' +
+                                '<i ng-if="backLink.icon && !backLink.image" class="{{backLink.icon}}"></i></a>' +
 					        '<a class="back-btn" ng-if="backLink.state" accesskey="{{backLink.accessKey}}" ui-sref="{{backLink.state}}" id="{{backLink.id}}">' + 
 					        	'<i ng-if="backLink.icon" class="{{backLink.icon}}"></i>' +
-					        	'<span ng-bind-html="backLink.label"></span> </a>' +
 					    '</li>' +
 					'</ul>',
 		controller: function ($scope, backlinkService) {
