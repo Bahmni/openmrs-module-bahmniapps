8686d8939171701e23ffff80a73843fa7e9e2eab
Charles Kimpolo <clmkimpo@thoughtworks.com>
2015-04-15 12:09:52 +0530

    Soumya, Charles | #2031 | Adding tests

---
+++ ui/test/unit/registration/services/visitService.spec.js
@@ -0,0 +1,52 @@
+'use strict';
+
+describe('Registration Visit Service', function () {
+    var visitService;
+    var openmrsUrl = "http://searchVisit";
+    var endVisitUrl = "http://endVisit";
+    var uuid = "9e23a1bb-0615-4066-97b6-db309c9c6447";
+   
+    var mockHttp = {
+        get: jasmine.createSpy('Http get').and.returnValue({'results': [{'uuid': uuid}]}),
+        post: jasmine.createSpy('Http post').and.returnValue({
+            'success': function (onSuccess) {
+                return {
+                    'then': function (thenMethod) {
+                        thenMethod()
+                    },
+                    'error': function (onError) {
+                        onError()
+                    }
+                }
+            }})
+    };
+
+    beforeEach(function () {
+        module('bahmni.registration');
+        module(function ($provide) {
+            Bahmni.Common.Constants.endVisitUrl = endVisitUrl;
+            Bahmni.Registration.Constants.webServiceRestBaseURL = openmrsUrl;
+            $provide.value('$http', mockHttp);
+        });
+
+        inject(['visitService', function (visitServiceInjectted) {
+            visitService = visitServiceInjectted;
+        }]);
+    });
+
+    it('Should call search url in registration visit service', function () {
+        var parameters = {patient: uuid, includeInactive: false, v: "custom:(uuid)"}
+        var results = visitService.search(parameters);
+        expect(mockHttp.get).toHaveBeenCalled();
+        expect(mockHttp.get.calls.mostRecent().args[0]).toBe(openmrsUrl + '/visit');
+        expect(mockHttp.get.calls.mostRecent().args[1].params.patient).toBe(uuid);
+        expect(mockHttp.get.calls.mostRecent().args[1].params.includeInactive).toBeFalsy();
+    });
+
+    it('Should call end visit url in registration visit service', function () {
+        var results = visitService.endVisit(uuid);
+        expect(mockHttp.post).toHaveBeenCalled();
+        expect(mockHttp.post.calls.mostRecent().args[0]).toBe(endVisitUrl + '?visitUuid=' + uuid);
+        expect(mockHttp.post.calls.mostRecent().args[1].withCredentials).toBeTruthy();
+    });
+});
