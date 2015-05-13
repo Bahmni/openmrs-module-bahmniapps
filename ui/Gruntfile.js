934fbbb99d6330497b43efce6a4d839b82508032
sravanthi17 <sravanns@thoughtworks.com>
2015-05-13 11:32:21 +0530

    Sravanthi | #1998 | orders tab ui changes

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -55,10 +55,10 @@ module.exports = function (grunt) {
         coverage: {
             options: {
                 thresholds: {
-                    'statements': 54.06,
-                    'branches': 45.68,
-                    'functions': 45.02,
-                    'lines': 54.21
+                    'statements': 54.1,
+                    'branches': 45.72,
+                    'functions': 45.06,
+                    'lines': 54.27
                 },
                 dir: 'coverage',
                 root: '.'
