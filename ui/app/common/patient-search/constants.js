7dc3a0c9586d8323ada86eddce7318a42d692073
Buddha <sidthunagarajan@gmail.com>
2015-05-15 17:37:54 +0530

    Sandeep, Gautam | 2244 | Extra columns visible on ADT list view |

--- ui/app/common/patient-search/constants.js
+++ ui/app/common/patient-search/constants.js
@@ -6,7 +6,7 @@ Bahmni.Common.PatientSearch.Constants = {
     searchExtensionTileViewType:"tile",
     searchExtensionTabularViewType:"tabular",
     tabularViewIgnoreHeadingsList: ["display","uuid","image","$$hashKey","activeVisitUuid"],
-    identifierHeading: ["ID","Id","id"],
+    identifierHeading: ["ID","Id","id","identifier"],
     patientTileHeight:100,
     patientTileWidth:100,
     tileLoadRatio:1 / 2
