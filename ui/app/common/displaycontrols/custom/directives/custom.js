d7e6ad76570e02b57c5d0b2a2b0edaf10cfdcd7f
Chase Yarbrough <vergil@gmail.com>
2015-06-07 22:38:33 +0545

    first working commit

--- ui/app/common/displaycontrols/custom/directives/custom.js
+++ ui/app/common/displaycontrols/custom/directives/custom.js
@@ -1,37 +1,68 @@
 'use strict';
 
 angular.module('bahmni.common.displaycontrol.custom',[])
-    .directive('custom',['observationsService',function(observationsService){
+    .directive('custom',['observationsService','$compile','$http',function(observationsService,$compile,$http){
 	    
 	    var controller = function($scope){
-		var observationsPromise = observationsService.getObservations($scope.patient.uuid,$scope.config.conceptNames,"latest",$scope.visitUuid);
-		var observations = [];
-		var successFunction = function (data){
-		    observations=data;
-		};
-		var failureFunction = function(data){
-		    window.alert("failure");
-		};
-		observationsPromise.then(successFunction, failureFunction);
-		$scope.observations = observations;
+		/*       		$scope.getContentUrl = function() {
+		    return "../../bahmni_config/openmrs/customHTMLTemplates/" + $scope.templateurl;
+		    }*/
 	    };
 
 	    var linkFunction = function(scope,elem,attr) {
-		scope.getContentUrl = function() {
-		    return "../../bahmni_config/openmrs/customHTMLTemplates/" + attr.templateurl;
-		}
+
+		var getObservationsAndCompileTemplate = function() {
+		    var observationsPromise = observationsService.getObservations(scope.patient.uuid,scope.config.conceptNames,"latest",scope.visitUuid);
+
+		    var compileTemplate = function(){
+			var compileFn = $compile(scope.template);
+			var content = compileFn(scope);
+			elem.append(content);
+		    };
+
+		    var successFunction = function (data){
+			var observations=data.data;
+			scope.observations = observations;
+			compileTemplate();
+		    };
+		    var failureFunction = function(data){
+			var observations=[];
+			scope.observations = observations;
+			compileTemplate();
+		    };
+
+		    observationsPromise.then(successFunction,failureFunction);
+		};
+
+		var templateUrl = "../../bahmni_config/openmrs/customHTMLTemplates/" + scope.templateurl;
+		var templatePromise = $http.get(templateUrl);
+		
+		var templateSuccessFn = function(data){
+		    scope.template = data.data;
+		    getObservationsAndCompileTemplate();
+		};
+		
+		var templateFailureFn = function(data){
+		    elem.append("Failed to load custom template.");
+		};
+
+		templatePromise.then(templateSuccessFn, templateFailureFn);
+
+
+
 	    };
 
 	    return {
 		restrict: 'E',
 		controller: controller,
 		link: linkFunction,
-		template: '<div ng-include="getContentUrl()"></div>',
+		    //template: '<div ng-include="getContentUrl()"></div>',
 		scope: {
 		    patient:"=",
                     visitUuid:"@",
                     section:"=",
-                    config:"="
+		    config:"=",
+		    templateurl:"@"
                 }
 	    }
      }]);
