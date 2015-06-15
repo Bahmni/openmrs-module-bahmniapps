08d5bdec9fffbe532925736d5bbd0b04f230e658
chethanTw <chethandec22@gmail.com>
2015-06-15 12:11:16 +0530

    Shan, Chethan, Buddha | 2391 | Link from adt to clinical and vice-versa.

--- ui/app/common/ui-helper/directives/backLinks.js
+++ ui/app/common/ui-helper/directives/backLinks.js
@@ -9,7 +9,7 @@ angular.module('bahmni.common.uiHelper')
 					        '<a class="back-btn" ng-class="{\'dashboard-link\':backLink.image}" ng-if="backLink.url" accesskey="{{backLink.accessKey}}" ng-href="{{backLink.url}}" id="{{backLink.id}}"  title="{{backLink.title}}"> ' +
                                 '<img ng-if="backLink.image" ng-src="{{backLink.image}}" onerror="this.onerror=null; this.src=\'../images/blank-user.gif\'"/>' +
                                 '<i ng-if="backLink.icon && !backLink.image" class="fa {{backLink.icon}}"></i></a>' +
-					        '<a class="back-btn" ng-if="backLink.state" accesskey="{{backLink.accessKey}}" ui-sref="{{backLink.state}}" id="{{backLink.id}}">' + 
+					        '<a class="back-btn" ng-if="backLink.state" accesskey="{{backLink.accessKey}}" ui-sref="{{backLink.state}}" id="{{backLink.id}}">' +
 					        	'<i ng-if="backLink.icon" class="fa {{backLink.icon}}"></i>' +
 					    '</li>' +
 					'</ul>',
