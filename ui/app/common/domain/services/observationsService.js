4775ee38e12c4f52ea70db7d20e551e0e88c05ff
Charles Kimpolo <clmkimpo@thoughtworks.com>
2015-06-19 14:33:08 +0530

    JP, Charles | #2428 | Adding a filter to allow a fetch of lab observations to be displayed on graphs.

--- ui/app/common/domain/services/observationsService.js
+++ ui/app/common/domain/services/observationsService.js
@@ -3,10 +3,13 @@
 angular.module('bahmni.common.domain')
     .service('observationsService', ['$http', function ($http) {
 
-        this.fetch = function (patientUuid, conceptNames, scope, numberOfVisits, visitUuid, obsIgnoreList) {
+        this.fetch = function (patientUuid, conceptNames, scope, numberOfVisits, visitUuid, obsIgnoreList, filterObsWithOrders) {
             var params = {concept: conceptNames};
             if(obsIgnoreList) {
                 params.obsIgnoreList = obsIgnoreList
+            }
+            if(filterObsWithOrders) {
+                params.filterObsWithOrders = filterObsWithOrders;
             }
 
             if(visitUuid){
