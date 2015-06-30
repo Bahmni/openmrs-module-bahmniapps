29a8b901db87330542d616b323841d9b89bf0368
SwathiVarkala <swathivarkala@gmail.com>
2015-06-30 17:50:41 +0530

    Mahesh, Swathi | Temporary fix for Possible preprod deployment

---
+++ ui/scripts/package_dist.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export LANG=en_US.UTF-8
+set -e
+
+BASE_DIR=`dirname $0`
+ROOT_DIR=$BASE_DIR/..
+ZIP_FILE_NAME=bahmniapps.zip
+
+mkdir -p $ROOT_DIR/target
+rm -rf $ROOT_DIR/target/$ZIP_FILE_NAME
+
+npm install
+bower install
+grunt dist
+cd $ROOT_DIR/dist && zip -r ../target/$ZIP_FILE_NAME *
