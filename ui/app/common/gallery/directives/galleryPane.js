d316df111b51028bc73c85c01a01419224bc4982
hemanths <hemanths@INhemanths.local>
2015-07-24 12:24:54 +0530

    PADMA Hemanth | Fixed undefined KeyboardJS error.

--- ui/app/common/gallery/directives/galleryPane.js
+++ ui/app/common/gallery/directives/galleryPane.js
@@ -2,167 +2,167 @@ angular.module('bahmni.common.gallery')
     .directive('bmGalleryPane', ['$rootScope', '$document', 'observationsService', 'encounterService', 'spinner', 'configurations',
         function ($rootScope, $document, observationsService, encounterService, spinner, configurations) {
 
-        var $body = $document.find('body');
+            var $body = $document.find('body');
 
-        $rootScope.$on('$stateChangeStart', function () {
-            close();
-        });
-
-        var link = function ($scope, element) {
-            $scope.galleryElement = element;
-            $body.prepend($scope.galleryElement).addClass('gallery-open');
-
-            KeyboardJS.on('right', function () {
-                $scope.$apply(function () {
-                    $scope.showNext();
-                });
+            $rootScope.$on('$stateChangeStart', function () {
+                close();
             });
-            KeyboardJS.on('left', function () {
-                $scope.$apply(function () {
-                    $scope.showPrev();
+
+            var link = function ($scope, element) {
+                $scope.galleryElement = element;
+                $body.prepend($scope.galleryElement).addClass('gallery-open');
+
+                keyboardJS.on('right', function () {
+                    $scope.$apply(function () {
+                        $scope.showNext();
+                    });
                 });
-            });
-        };
-
-        function close() {
-            $('body #gallery-pane').remove();
-            $body.removeClass('gallery-open');
-            KeyboardJS.clear('right');
-            KeyboardJS.clear('left');
-        }
-
-        var controller = function ($scope) {
-            $scope.imageIndex = $scope.imagePosition.index ? $scope.imagePosition.index : 0;
-            $scope.albumTag = $scope.imagePosition.tag ? $scope.imagePosition.tag : 'defaultTag';
-            $scope.showImpression = false;
-
-            $scope.isActive = function (index, tag) {
-                return $scope.imageIndex == index && $scope.albumTag == tag;
-            };
-
-            var getAlbumIndex = function () {
-                return _.findIndex($scope.albums, function (album) {
-                    return album.tag == $scope.albumTag;
+                keyboardJS.on('left', function () {
+                    $scope.$apply(function () {
+                        $scope.showPrev();
+                    });
                 });
             };
 
-            $scope.showPrev = function () {
-                var albumIndex = getAlbumIndex();
-                if ($scope.imageIndex > 0) {
-                    --$scope.imageIndex;
-                }
-                else {
-                    if (albumIndex == 0) {
-                        albumIndex = $scope.albums.length;
+            function close() {
+                $('body #gallery-pane').remove();
+                $body.removeClass('gallery-open');
+                keyboardJS.releaseKey('right');
+                keyboardJS.releaseKey('left');
+            }
+
+            var controller = function ($scope) {
+                $scope.imageIndex = $scope.imagePosition.index ? $scope.imagePosition.index : 0;
+                $scope.albumTag = $scope.imagePosition.tag ? $scope.imagePosition.tag : 'defaultTag';
+                $scope.showImpression = false;
+
+                $scope.isActive = function (index, tag) {
+                    return $scope.imageIndex == index && $scope.albumTag == tag;
+                };
+
+                var getAlbumIndex = function () {
+                    return _.findIndex($scope.albums, function (album) {
+                        return album.tag == $scope.albumTag;
+                    });
+                };
+
+                $scope.showPrev = function () {
+                    var albumIndex = getAlbumIndex();
+                    if ($scope.imageIndex > 0) {
+                        --$scope.imageIndex;
                     }
-                    var previousAlbum = $scope.albums[albumIndex - 1];
-                    if (previousAlbum.images.length == 0) {
-                        $scope.showPrev(albumIndex - 1);
-                    }
-                    $scope.albumTag = previousAlbum.tag;
-                    $scope.imageIndex = previousAlbum.images.length - 1;
-                }
-            };
-
-            $scope.showNext = function () {
-                var albumIndex = getAlbumIndex();
-                if ($scope.imageIndex < $scope.albums[albumIndex].images.length - 1) {
-                    ++$scope.imageIndex;
-                } else {
-                    if (albumIndex == $scope.albums.length - 1) {
-                        albumIndex = -1;
-                    }
-                    var nextAlbum = $scope.albums[albumIndex + 1];
-                    if (nextAlbum.images.length == 0) {
-                        $scope.showNext(albumIndex + 1);
-                    }
-                    $scope.albumTag = nextAlbum.tag;
-                    $scope.imageIndex = 0;
-                }
-            };
-
-            $scope.getTotalLength = function () {
-                var totalLength = 0;
-                angular.forEach($scope.albums, function (album) {
-                    totalLength += album.images.length;
-                });
-                return totalLength;
-            };
-
-            $scope.getCurrentIndex = function () {
-                var currentIndex = 1;
-                for (var i = 0; i < getAlbumIndex(); i++) {
-                    currentIndex += $scope.albums[i].images.length;
-                }
-                return currentIndex + parseInt($scope.imageIndex);
-            };
-
-            $scope.close = function () {
-                close($scope);
-            };
-
-            $scope.toggleImpression = function () {
-                $scope.showImpression = !$scope.showImpression;
-            };
-
-            $scope.hasObsRelationship = function (image) {
-                return image.sourceObs && image.sourceObs.length > 0;
-            };
-
-            $scope.saveImpression = function (image) {
-                var bahmniEncounterTransaction = mapBahmniEncounterTransaction(image);
-                spinner.forPromise(encounterService.create(bahmniEncounterTransaction).then(function () {
-                    constructNewSourceObs(image);
-                    fetchObsRelationship(image);
-                }));
-            };
-
-            var init = function () {
-                if ($scope.accessImpression) {
-                    $scope.albums.forEach(function (album) {
-                        album.images.forEach(function (image) {
-                            fetchObsRelationship(image);
-                            constructNewSourceObs(image);
-                        })
-                    })
-                }
-            };
-
-            var fetchObsRelationship = function (image) {
-                observationsService.getObsRelationship(image.uuid).then(function (response) {
-                    image.sourceObs = response.data;
-                });
-            };
-
-            var constructNewSourceObs = function (image) {
-                image.newSourceObs = $scope.newSourceObs && $scope.newSourceObs.targetObsRelation.targetObs.uuid === image.uuid ? $scope.targetObs : {
-                    value: "",
-                    concept: {
-                        uuid: configurations.impressionConcept().uuid
-                    },
-                    targetObsRelation: {
-                        relationshipType: Bahmni.Common.Constants.qualifiedByRelationshipType,
-                        targetObs: {
-                            uuid: image.uuid
+                    else {
+                        if (albumIndex == 0) {
+                            albumIndex = $scope.albums.length;
                         }
+                        var previousAlbum = $scope.albums[albumIndex - 1];
+                        if (previousAlbum.images.length == 0) {
+                            $scope.showPrev(albumIndex - 1);
+                        }
+                        $scope.albumTag = previousAlbum.tag;
+                        $scope.imageIndex = previousAlbum.images.length - 1;
                     }
                 };
-            };
 
-            var mapBahmniEncounterTransaction = function (image) {
-                return {
-                    patientUuid: $scope.patient.uuid,
-                    encounterTypeUuid: configurations.encounterConfig().getConsultationEncounterTypeUuid(),
-                    observations: [image.newSourceObs]
+                $scope.showNext = function () {
+                    var albumIndex = getAlbumIndex();
+                    if ($scope.imageIndex < $scope.albums[albumIndex].images.length - 1) {
+                        ++$scope.imageIndex;
+                    } else {
+                        if (albumIndex == $scope.albums.length - 1) {
+                            albumIndex = -1;
+                        }
+                        var nextAlbum = $scope.albums[albumIndex + 1];
+                        if (nextAlbum.images.length == 0) {
+                            $scope.showNext(albumIndex + 1);
+                        }
+                        $scope.albumTag = nextAlbum.tag;
+                        $scope.imageIndex = 0;
+                    }
                 };
+
+                $scope.getTotalLength = function () {
+                    var totalLength = 0;
+                    angular.forEach($scope.albums, function (album) {
+                        totalLength += album.images.length;
+                    });
+                    return totalLength;
+                };
+
+                $scope.getCurrentIndex = function () {
+                    var currentIndex = 1;
+                    for (var i = 0; i < getAlbumIndex(); i++) {
+                        currentIndex += $scope.albums[i].images.length;
+                    }
+                    return currentIndex + parseInt($scope.imageIndex);
+                };
+
+                $scope.close = function () {
+                    close($scope);
+                };
+
+                $scope.toggleImpression = function () {
+                    $scope.showImpression = !$scope.showImpression;
+                };
+
+                $scope.hasObsRelationship = function (image) {
+                    return image.sourceObs && image.sourceObs.length > 0;
+                };
+
+                $scope.saveImpression = function (image) {
+                    var bahmniEncounterTransaction = mapBahmniEncounterTransaction(image);
+                    spinner.forPromise(encounterService.create(bahmniEncounterTransaction).then(function () {
+                        constructNewSourceObs(image);
+                        fetchObsRelationship(image);
+                    }));
+                };
+
+                var init = function () {
+                    if ($scope.accessImpression) {
+                        $scope.albums.forEach(function (album) {
+                            album.images.forEach(function (image) {
+                                fetchObsRelationship(image);
+                                constructNewSourceObs(image);
+                            })
+                        })
+                    }
+                };
+
+                var fetchObsRelationship = function (image) {
+                    observationsService.getObsRelationship(image.uuid).then(function (response) {
+                        image.sourceObs = response.data;
+                    });
+                };
+
+                var constructNewSourceObs = function (image) {
+                    image.newSourceObs = $scope.newSourceObs && $scope.newSourceObs.targetObsRelation.targetObs.uuid === image.uuid ? $scope.targetObs : {
+                        value: "",
+                        concept: {
+                            uuid: configurations.impressionConcept().uuid
+                        },
+                        targetObsRelation: {
+                            relationshipType: Bahmni.Common.Constants.qualifiedByRelationshipType,
+                            targetObs: {
+                                uuid: image.uuid
+                            }
+                        }
+                    };
+                };
+
+                var mapBahmniEncounterTransaction = function (image) {
+                    return {
+                        patientUuid: $scope.patient.uuid,
+                        encounterTypeUuid: configurations.encounterConfig().getConsultationEncounterTypeUuid(),
+                        observations: [image.newSourceObs]
+                    };
+                };
+
+                init();
             };
 
-            init();
-        };
-
-        return {
-            link: link,
-            controller: controller,
-            templateUrl: '../common/gallery/views/gallery.html'
-        }
-    }]);
+            return {
+                link: link,
+                controller: controller,
+                templateUrl: '../common/gallery/views/gallery.html'
+            }
+        }]);
