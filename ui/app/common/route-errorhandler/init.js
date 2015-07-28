765c34a3858ad0a45febe84f57e9c39684464a55
hemanths <hemanths@INhemanths.local>
2015-07-28 11:28:02 +0530

    Padma, Hemanth | fixing commonerror issue.

--- ui/app/common/route-errorhandler/init.js
+++ ui/app/common/route-errorhandler/init.js
@@ -1,12 +1,6 @@
 angular.module('bahmni.common.routeErrorHandler', ['ui.router'])
-    .config(['$stateProvider', function ($stateProvider) {
-        $stateProvider
-        .state('commonerror', {
-            url: '/commonerror',
-            templateUrl: "../common/route-errorhandler/views/error.html"
-        });
-    }]).run(function ($rootScope, $state) {
-        $rootScope.$on('$stateChangeError', function () {
-            $state.go("commonerror");
+    .run(function ($rootScope) {
+        $rootScope.$on('$stateChangeError', function (event) {
+            event.preventDefault();
         });
     });
