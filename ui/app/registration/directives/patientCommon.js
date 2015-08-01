8951eda000ca7f13350ffa27abd1976b380618bc
hemanths <hemanths@INhemanths.local>
2015-08-01 14:49:53 +0530

    Hemanth | Moving patient common to directive.

---
+++ ui/app/registration/directives/patientCommon.js
@@ -0,0 +1,54 @@
+'use strict';
+
+angular.module('bahmni.registration')
+    .directive('patientCommon', ['$rootScope', '$http', 'patientAttributeService', 'addressAttributeService', 'appService',
+        function ($rootScope, $http, patientAttributeService, addressAttributeService, appService) {
+            var controller = function ($scope) {
+                var autoCompleteFields = appService.getAppDescriptor().getConfigValue("autoCompleteFields", []);
+                var showCasteSameAsLastNameCheckbox = appService.getAppDescriptor().getConfigValue("showCasteSameAsLastNameCheckbox");
+                $scope.showMiddleName = appService.getAppDescriptor().getConfigValue("showMiddleName");
+                $scope.genderCodes = Object.keys($rootScope.genderMap);
+
+                $scope.isAutoComplete = function (fieldName) {
+                    return autoCompleteFields.indexOf(fieldName) > -1;
+                };
+
+                $scope.showCasteSameAsLastName = function () {
+                    var personAttributeHasCaste = (_.find($rootScope.patientConfiguration.personAttributeTypes, {name: 'caste'}) != null);
+                    return showCasteSameAsLastNameCheckbox && personAttributeHasCaste;
+                };
+
+                $scope.setCasteAsLastName = function () {
+                    if ($scope.patient.sameAsLastName) {
+                        $scope.patient.caste = $scope.patient.familyName;
+                    }
+                };
+
+                $scope.getAutoCompleteList = function (attributeName, query, type) {
+                    return patientAttributeService.search(attributeName, query, type);
+                };
+
+                $scope.getDataResults = function (data) {
+                    return data.resultList.results;
+                };
+
+                $scope.$watch('patient.familyName', function () {
+                    if ($scope.patient.sameAsLastName) {
+                        $scope.patient.caste = $scope.patient.familyName;
+                    }
+                });
+
+                $scope.$watch('patient.caste', function () {
+                    if ($scope.patient.sameAsLastName && ($scope.patient.familyName !== $scope.patient.caste)) {
+                        $scope.patient.sameAsLastName = false;
+                    }
+                });
+            };
+            return {
+                restrict: 'E',
+                templateUrl: 'views/patientCommon.html',
+                controller: controller
+            }
+        }
+    ]);
+
