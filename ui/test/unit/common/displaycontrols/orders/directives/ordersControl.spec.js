bfc8236cab96e20f2e31103a96cdf7d29a960ee9
Buddha <sidthunagarajan@gmail.com>
2015-08-03 12:42:57 +0530

    Gautam | 2452 | Refactored Unit Tests. Orders control spec. Added few ui tests.

--- ui/test/unit/common/displaycontrols/orders/directives/ordersControl.spec.js
+++ ui/test/unit/common/displaycontrols/orders/directives/ordersControl.spec.js
@@ -1,66 +1,59 @@
 'use strict';
 
-describe('Orders DisplayControl', function () {
-    var q,
-        compile,
-        mockBackend,
-        rootScope,
-        deferred,
-        simpleHtml = '<orders-control config="section" patient="patient" order-uuid="orderUuid"></orders-control>';
-    var _orderService;
-    var orders = [
-        {
+describe("OrdersDisplayControl", function () {
+    var compile, scope, orderService, orderTypeService, spinner, orders;
+
+    beforeEach(function () {
+        module('bahmni.common.uiHelper');
+        module('bahmni.common.displaycontrol.orders');
+        module('ngHtml2JsPreprocessor');
+
+        module(function ($provide) {
+            orderService = jasmine.createSpyObj('orderService', ['getOrders']);
+            orderTypeService = jasmine.createSpyObj('orderTypeService', ['getOrderTypeUuid']);
+            spinner = jasmine.createSpyObj('spinner', ['forPromise']);
+            $provide.value('orderService', orderService);
+            $provide.value('orderTypeService', orderTypeService);
+            $provide.value('spinner', spinner);
+        });
+        inject(function ($compile, $rootScope) {
+            compile = $compile;
+            scope = $rootScope.$new();
+        });
+    });
+
+    var generateElement = function () {
+        var unCompiledHtml =
+            '<orders-control ' +
+            'patient="patient" ' +
+            'config="config" ' +
+            'section="section" ' +
+            'order-type="orderType" ' +
+            'order-uuid="orderUuid" ' +
+            'is-on-dashboard="isOnDashboard">' +
+            '</orders-control>';
+
+        var element = compile(angular.element(unCompiledHtml))(scope);
+        scope.$digest();
+        return element;
+    };
+    beforeEach(function () {
+        scope.section = {};
+        scope.config = {};
+        scope.patient = {uuid: "patientUuid"};
+        orders = [{
             "conceptName": "Absconding",
             "orderDate": "2014-12-16T16:06:49.000+0530",
             "provider": "Surajkumar Surajkumar Surajkumar",
             "bahmniObservations": []
-        }
-    ];
-
-    beforeEach(function () {
-        module('bahmni.common.domain');
-        module('bahmni.common.orders');
-        module('bahmni.common.uiHelper');
-        module('bahmni.common.displaycontrol.orders');
-
-        module(function ($provide) {
-            var _spinner = jasmine.createSpyObj('spinner', ['forPromise', 'then']);
-            _orderService = jasmine.createSpyObj('orderService', ['getOrders', 'then']);
-            _spinner.forPromise.and.callFake(function () {
-                deferred = q.defer();
-                deferred.resolve({data: orders});
-                return deferred.promise;
-            });
-
-            _spinner.then.and.callThrough({data: orders});
-
-            $provide.value('spinner', _spinner);
-        });
-
-        inject(function ($compile, $httpBackend, $rootScope, $q) {
-            compile = $compile;
-            mockBackend = $httpBackend;
-            rootScope = $rootScope;
-            q = $q;
-        });
+        }];
     });
-
     it('should call using order uuid if present', function () {
-        var scope = rootScope.$new();
-
-        scope.section = {
-            numberOfVisits: 1
-        };
-        scope.patient = {uuid: "patientUuid"};
+        scope.config.numberOfVisits = 1;
         scope.orderUuid = "someOrderUuid";
 
-        mockBackend.expectGET('../common/displaycontrols/orders/views/ordersControl.html').respond("<div>dummy</div>");
-        mockBackend.expectGET('/openmrs/ws/rest/v1/bahmnicore/orders?includeObs=true&numberOfVisits=1&orderUuid=someOrderUuid&patientUuid=patientUuid').respond(orders);
-
-        var element = compile(simpleHtml)(scope);
-
-        scope.$digest();
-        mockBackend.flush();
+        orderService.getOrders.and.returnValue(specUtil.createFakePromise(orders));
+        var element = generateElement();
 
         var compiledElementScope = element.isolateScope();
         scope.$digest();
@@ -77,24 +70,16 @@ describe('Orders DisplayControl', function () {
         ];
 
         expect(compiledElementScope.bahmniOrders).not.toBeUndefined();
-        expect(compiledElementScope.bahmniOrders).toEqual(expectedOrders);
+        expect(compiledElementScope.bahmniOrders.length).toBe(1);
+        expect(compiledElementScope.bahmniOrders[0]).toEqual(jasmine.objectContaining(expectedOrders[0]));
     });
 
     it('should return title when show-title flag is true', function () {
-        var scope = rootScope.$new();
-        scope.section = {
-            numberOfVisits: 1
-        };
-        scope.patient = {uuid: "patientUuid"};
+        scope.config.numberOfVisits = 1;
         scope.orderUuid = "someOrderUuid";
 
-        mockBackend.expectGET('../common/displaycontrols/orders/views/ordersControl.html').respond("<div>dummy</div>");
-        mockBackend.expectGET('/openmrs/ws/rest/v1/bahmnicore/orders?includeObs=true&numberOfVisits=1&orderUuid=someOrderUuid&patientUuid=patientUuid').respond(orders);
-
-        var element = compile(simpleHtml)(scope);
-
-        scope.$digest();
-        mockBackend.flush();
+        orderService.getOrders.and.returnValue(specUtil.createFakePromise(orders));
+        var element = generateElement();
 
         var compiledElementScope = element.isolateScope();
         scope.$digest();
@@ -104,20 +89,11 @@ describe('Orders DisplayControl', function () {
     });
 
     it('should set hideIfEmpty flag if the orders observations are empty', function () {
-        var scope = rootScope.$new();
-        scope.section = {
-            numberOfVisits: 1
-        };
-        scope.patient = {uuid: "patientUuid"};
+        scope.config.numberOfVisits = 1;
         scope.orderUuid = "someOrderUuid";
 
-        mockBackend.expectGET('../common/displaycontrols/orders/views/ordersControl.html').respond("<div>dummy</div>");
-        mockBackend.expectGET('/openmrs/ws/rest/v1/bahmnicore/orders?includeObs=true&numberOfVisits=1&orderUuid=someOrderUuid&patientUuid=patientUuid').respond(orders);
-
-        var element = compile(simpleHtml)(scope);
-
-        scope.$digest();
-        mockBackend.flush();
+        orderService.getOrders.and.returnValue(specUtil.createFakePromise(orders));
+        var element = generateElement();
 
         var compiledElementScope = element.isolateScope();
         scope.$digest();
@@ -125,21 +101,12 @@ describe('Orders DisplayControl', function () {
         expect(compiledElementScope.bahmniOrders[0].hideIfEmpty).toBe(true)
     });
 
-    it('should set showHeader flag if the its not already set', function () {
-        var scope = rootScope.$new();
-        scope.section = {
-            numberOfVisits: 1
-        };
-        scope.patient = {uuid: "patientUuid"};
+    it('should set showHeader flag if it is not present', function () {
+        scope.config.numberOfVisits = 1;
         scope.orderUuid = "someOrderUuid";
 
-        mockBackend.expectGET('../common/displaycontrols/orders/views/ordersControl.html').respond("<div>dummy</div>");
-        mockBackend.expectGET('/openmrs/ws/rest/v1/bahmnicore/orders?includeObs=true&numberOfVisits=1&orderUuid=someOrderUuid&patientUuid=patientUuid').respond([]);
-
-        var element = compile(simpleHtml)(scope);
-
-        scope.$digest();
-        mockBackend.flush();
+        orderService.getOrders.and.returnValue(specUtil.createFakePromise(orders));
+        var element = generateElement();
 
         var compiledElementScope = element.isolateScope();
         scope.$digest();
@@ -147,25 +114,13 @@ describe('Orders DisplayControl', function () {
         expect(compiledElementScope.config.showHeader).toBe(true);
     });
 
-    it('should not set showHeader flag if the its already set', function () {
-        var scope = rootScope.$new();
-        scope.section = {
-            numberOfVisits: 1,
-            showHeader: false
-        };
-        scope.config = {
-            showHeader: false
-        };
-        scope.patient = {uuid: "patientUuid"};
+    it('should not set showHeader flag if it is present', function () {
+        scope.config.numberOfVisits = 1;
         scope.orderUuid = "someOrderUuid";
+        scope.config.showHeader = false;
 
-        mockBackend.expectGET('../common/displaycontrols/orders/views/ordersControl.html').respond("<div>dummy</div>");
-        mockBackend.expectGET('/openmrs/ws/rest/v1/bahmnicore/orders?includeObs=true&numberOfVisits=1&orderUuid=someOrderUuid&patientUuid=patientUuid').respond([]);
-
-        var element = compile(simpleHtml)(scope);
-
-        scope.$digest();
-        mockBackend.flush();
+        orderService.getOrders.and.returnValue(specUtil.createFakePromise(orders));
+        var element = generateElement();
 
         var compiledElementScope = element.isolateScope();
         scope.$digest();
@@ -173,14 +128,48 @@ describe('Orders DisplayControl', function () {
         expect(compiledElementScope.config.showHeader).toBe(false);
     });
 
-    it('should set isOpen flag for the first order', function () {
-        var scope = rootScope.$new();
+    it('should have children 1 section', function () {
 
-        scope.section = {
-            numberOfVisits: 1
-        };
-        scope.patient = {uuid: "patientUuid"};
+        orderService.getOrders.and.returnValue(specUtil.createFakePromise(orders));
+        var element = generateElement();
+
+        expect(element.children()[0].localName).toBe('section');
+    });
+
+    it('1 section child should have children 1 section and 1 div', function () {
+
+        orderService.getOrders.and.returnValue(specUtil.createFakePromise(orders));
+        var element = generateElement();
+
+        expect(element.children()[0].localName).toBe('section');
+
+        var section = $(element.children()[0]);
+
+        expect(section.children()[0].localName).toBe('section');
+        expect(section.children()[1].localName).toBe('div');
+    });
+
+    it('1 section child should have children 1 h2, 1 section and 1 div', function () {
+        scope.section.title = "testTitle";
+
+        orderService.getOrders.and.returnValue(specUtil.createFakePromise(orders));
+        var element = generateElement();
+
+        expect(element.children()[0].localName).toBe('section');
+        var section = $(element.children()[0]);
+
+        expect(section.children()[0].localName).toBe('h2');
+        expect(section.children()[0].innerText).toContain('testTitle');
+        expect(section.children()[1].localName).toBe('section');
+        expect(section.children()[2].localName).toBe('div');
+    });
+
+    it('should open the first and close the rest', function () {
+        scope.config.showHeader = true;
+        scope.config.numberOfVisits = 1
+        scope.config.title = "testTitle";
         scope.orderUuid = "someOrderUuid";
+
         var orders = [
             {
                 "conceptName": "Absconding",
@@ -196,37 +185,70 @@ describe('Orders DisplayControl', function () {
             }
         ];
 
-        mockBackend.expectGET('../common/displaycontrols/orders/views/ordersControl.html').respond("<div>dummy</div>");
-        mockBackend.expectGET('/openmrs/ws/rest/v1/bahmnicore/orders?includeObs=true&numberOfVisits=1&orderUuid=someOrderUuid&patientUuid=patientUuid').respond(orders);
+        orderService.getOrders.and.returnValue(specUtil.createFakePromise(orders));
+        var element = generateElement();
 
-        var element = compile(simpleHtml)(scope);
+        var section = $(element.find('section')[0]).find('section')[0];
 
-        scope.$digest();
-        mockBackend.flush();
+        var bahmniObservations = $($(section).find('ul')[0]).children();
 
-        var compiledElementScope = element.isolateScope();
-        scope.$digest();
+        var firstOrderITags = $(bahmniObservations[0]).find('i');
+        var firstOrderFulfillments = $(bahmniObservations[0]).find('ul')[0];
 
-        var expectedOrders = [
-            {
-                "conceptName": "Absconding",
-                "orderDate": "2014-12-16T16:06:49.000+0530",
-                "provider": "Surajkumar Surajkumar Surajkumar",
-                "bahmniObservations": [],
-                "hideIfEmpty": true,
-                "isOpen": true
-            },
-            {
-                "conceptName": "Pulse",
-                "orderDate": "2014-12-16T16:06:49.000+0530",
-                "provider": "Surajkumar Surajkumar Surajkumar",
-                "bahmniObservations": [],
-                "hideIfEmpty": true
-            }
-        ];
+        var secondOrderITags = $(bahmniObservations[1]).find('i');
+        var secondOrderFulfillments = $(bahmniObservations[1]).find('ul')[0];
 
-        expect(compiledElementScope.bahmniOrders).not.toBeUndefined();
-        expect(compiledElementScope.bahmniOrders).toEqual(expectedOrders);
-        expect(compiledElementScope.bahmniOrders[1].isOpen).toEqual(undefined);
+        expect(element.children()[0].localName).toBe('section');
+
+        //firstOrder isOPen
+        expect(firstOrderITags[0].className).toContain('fa-caret-right');
+        expect(firstOrderITags[0].className).toContain('ng-hide');
+        expect(firstOrderITags[1].className).toContain('fa-caret-down');
+        expect(firstOrderITags[1].className).not.toContain('ng-hide');
+        expect(firstOrderFulfillments.className).not.toContain('ng-hide');
+
+        //secondOrder isOPen
+        expect(secondOrderITags[0].className).toContain('fa-caret-right');
+        expect(secondOrderITags[0].className).not.toContain('ng-hide');
+        expect(secondOrderITags[1].className).toContain('fa-caret-down');
+        expect(secondOrderITags[1].className).toContain('ng-hide');
+        expect(secondOrderFulfillments.className).toContain('ng-hide');
+    });
+
+    describe("noOrdersMessage", function () {
+
+        beforeEach(function () {
+            scope.config = {};
+            scope.patient = {};
+            scope.section = {
+                title: "testTitle"
+            };
+            scope.orderType = "testOrder";
+        });
+
+        it('should show the noOrdersMessage when there is no order', function () {
+
+            orderService.getOrders.and.returnValue(specUtil.createFakePromise([]));
+            var element = generateElement();
+
+            expect(element.children()[0].localName).toBe('section');
+
+            var section = $(element.children()[0]);
+
+            expect(section.children()[2].localName).toBe('div');
+            expect($(section.children()[2]).text()).toContain("No testOrder for this patient.");
+        });
+
+        it('should not show the noOrdersMessage when there are orders', function () {
+
+            orderService.getOrders.and.returnValue(specUtil.createFakePromise(orders));
+            var element = generateElement();
+
+            expect(element.children()[0].localName).toBe('section');
+
+            var section = $(element.children()[0]);
+
+            expect($(section.children()[2]).text()).not.toContain("No testOrder for this patient.");
+        });
     });
 });
