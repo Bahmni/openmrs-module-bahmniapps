06d9d1c921e5bb9d6ddfd3cdf5a4d325d9a6e8c5
jayasuganthi <jayasug@thoughtworks.com>
2015-08-10 18:00:22 +0530

    [Nathan, Jaya] | 2674 | Navigational links display control issues

--- ui/test/unit/common/displaycontrols/navigationLinks/directives/navigationLinks.spec.js
+++ ui/test/unit/common/displaycontrols/navigationLinks/directives/navigationLinks.spec.js
@@ -1,16 +1,24 @@
 'use strict';
 
 describe('NavigationalLinks DisplayControl', function () {
-    var scope, rootScope, filter, httpBackend, compile, q, compiledScope;
+    var scope, rootScope, filter, httpBackend, compile, q, compiledScope, appService;
     var html = '<navigation-links params="section" link-params="{patientUuid: patient.uuid, visitUuid: visitSummary.uuid}"></navigation-links>';
+    var mandatoryConfig = { sections: [{type: "General"}, {type: "Discharge Summary"}] };
 
     beforeEach(module('bahmni.common.displaycontrol.navigationlinks'));
+
+    beforeEach(module(function ($provide) {
+        $provide.value('appService', appService);
+    }));
 
     beforeEach(inject(function($filter, $compile, $httpBackend, $rootScope, $q){
         filter = $filter;
         compile = $compile;
         httpBackend = $httpBackend;
         rootScope = $rootScope;
+        appService = jasmine.createSpyObj('appService', ['loadConfig', 'loadMandatoryConfig', 'getAppDescriptor']);
+        appService.loadMandatoryConfig.and.returnValue(specUtil.respondWith({data: mandatoryConfig}));
+        appService.getAppDescriptor.and.returnValue(appDescriptor);
         q = $q;
 
         scope = rootScope.$new();
@@ -32,10 +40,6 @@ describe('NavigationalLinks DisplayControl', function () {
         expect(compiledScope).not.toBeUndefined();
     });
 
-    it('should get dashboard url', function () {
-        expect(compiledScope.getUrl(links[0])).toBe("../home/#/dashboard");
-    });
-
     it('should show url for existing parameters', function () {
         expect(compiledScope.showUrl(links[0])).toBeTruthy();
     });
@@ -43,7 +47,7 @@ describe('NavigationalLinks DisplayControl', function () {
     it('should not show url for non-existing parameters', function () {
         expect(compiledScope.showUrl({
             "title": "Patient ADT Page",
-            "url": "../adt/#/patient/:patientUuid/visit/:visit/"
+            "url": "../adt/#/patient/{{patientUuid}}/visit/{{visit}}/"
         })).toBeFalsy();
     });
 });
@@ -55,26 +59,29 @@ var links = [
     },
     {
         "title": "Patient Visit Page",
-        "url": "../clinical/#/patient/:patientUuid/dashboard/visit/:visitUuid"
+        "url": "../clinical/#/patient/{{patientUuid}}/dashboard/visit/{{visitUuid}}"
     },
     {
         "title": "Patient ADT Page",
-        "url": "../adt/#/patient/:patientUuid/visit/:visitUuid/"
+        "url": "../adt/#/patient/{{patientUuid}}/visit/{{visitUuid}}/"
     },
     {
         "title": "Patient Dashboard",
-        "url": "../clinical/#/patient/:patientUuid/dashboard"
+        "url": "../clinical/#/patient/{{patientUuid}}/dashboard"
     },
     {
         "title": "Discharge Summary Page",
-        "url": "../clinical/#/patient/:patientUuid/dashboard/visit/:visitUuid"
+        "url": "../clinical/#/patient/{{patientUuid}}/dashboard/visit/{{visitUuid}}"
     },
     {
         "title": "Program Management Page",
-        "url": "../clinical/#/patient/:patientUuid/consultationContext"
+        "url": "../clinical/#/patient/{{patientUuid}}/consultationContext"
     },
     {
         "title": "Consultation",
-        "url": "../clinical/#/patient/:patientUuid/concept-set-group/observations"
+        "url": "../clinical/#/patient/{{patientUuid}}/concept-set-group/observations"
     }
 ];
+
+var appDescriptor = {};
+appDescriptor.formatUrl = function(){};
