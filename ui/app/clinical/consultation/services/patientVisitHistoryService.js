3ae7cb02b5662850fdb67c1191f8a1dc79681177
Vikashg <vikashgupta2000@gmail.com>
2015-08-19 15:19:38 +0530

    Vikash | #2755 | Visit Display Control: Enhancements

--- ui/app/clinical/consultation/services/patientVisitHistoryService.js
+++ ui/app/clinical/consultation/services/patientVisitHistoryService.js
@@ -3,7 +3,7 @@
 angular.module('bahmni.clinical')
     .service('patientVisitHistoryService', ['visitService', function (visitService) {
         this.getVisitHistory = function (patientUuid) {
-            return visitService.search({patient: patientUuid, v: 'custom:(uuid,visitType,startDatetime,stopDatetime,encounters:(uuid,display,encounterDatetime,orders,obs))', includeInactive: true})
+            return visitService.search({patient: patientUuid, v: 'custom:(uuid,visitType,startDatetime,stopDatetime,encounters:(uuid,display,encounterDatetime,orders,obs,provider:(display)))', includeInactive: true})
                 .then(function (data) {
                     var visits = data.data.results.map(function (visitData) {
                         return new Bahmni.Clinical.VisitHistoryEntry(visitData);
