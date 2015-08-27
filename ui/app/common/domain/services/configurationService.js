32cd89413adc084f8519f7b7b2c77280794f77e5
padma <padmavb@thoughtworks.com>
2015-08-27 11:37:49 +0530

    Jaya, Padma - #	2752 -Fixing Bug, not displaying text box if the relationship globalproperty is not set and fixing failure js test

--- ui/app/common/domain/services/configurationService.js
+++ ui/app/common/domain/services/configurationService.js
@@ -119,7 +119,7 @@ angular.module('bahmni.common.domain')
             return $http.get(Bahmni.Common.Constants.globalPropertyUrl, {
                 method: "GET",
                 params: {
-                    property: 'bahmni.relationshipTypeMap'
+                    property: 'bahmnicore.relationshipTypeMap'
                 },
                 withCredentials: true
             });
