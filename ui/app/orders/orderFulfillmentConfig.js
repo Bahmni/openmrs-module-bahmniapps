c3e4ecf9482004d5e8da7b55c900f8b244d16b72
Buddha <sidthunagarajan@gmail.com>
2015-08-29 15:22:46 +0530

    Gautam | Refactored. OrderFulfillmentConfig Initialization.

--- ui/app/orders/orderFulfillmentConfig.js
+++ ui/app/orders/orderFulfillmentConfig.js
@@ -1,25 +1,32 @@
 'use strict';
 
-angular.module('bahmni.orders').factory('orderFulfillmentConfig',
-    ['conceptSetService', 'spinner', function(conceptSetService, spinner){
-        return function(formName){
-            var prepareConfig = spinner.forPromise(conceptSetService.getConceptSetMembers({name : formName, v :"custom:(uuid,name,names,conceptClass,setMembers:(uuid,name,names,conceptClass,setMembers:(uuid,name,names,conceptClass,setMembers:(uuid,name,names,conceptClass))))"})).then(function (response) {
-                var config = {};
-                config.conceptNames = [];
-                _.forEach(response.data.results[0].setMembers, function(obsConcept){
-                    config.conceptNames.push(obsConcept.name.name);
-                });
-                config.isObservation = true;
-                config.numberOfVisits = 2;
-                config.showDetailsButton = true;
-                config.hideIfEmpty = false;
-                config.showHeader = false;
-                config.scope = "latest";
-                return config;
-            });
+angular.module('bahmni.orders')
+    .factory('orderFulfillmentConfig', ['conceptSetService', 'spinner',
+        function (conceptSetService, spinner) {
+            var representation = "custom:(uuid,name,names,conceptClass," +
+                "setMembers:(uuid,name,names,conceptClass," +
+                "setMembers:(uuid,name,names,conceptClass," +
+                "setMembers:(uuid,name,names,conceptClass))))";
 
-            return prepareConfig;
+            return function (formName) {
+                return spinner.forPromise(conceptSetService.getConceptSetMembers({
+                    name: formName,
+                    v: representation
+                }).then(function (response) {
+                    var config = {};
+                    var formMembers = response.data.results[0].setMembers;
+                    config.conceptNames = _.map(formMembers,function(concept){
+                        return concept.name.name;
+                    });
+                    config.isObservation = true;
+                    config.numberOfVisits = 2;
+                    config.showDetailsButton = true;
+                    config.hideIfEmpty = false;
+                    config.showHeader = false;
+                    config.scope = "latest";
+                    return config;
+                }));
+            }
         }
-    }
     ]
 );
