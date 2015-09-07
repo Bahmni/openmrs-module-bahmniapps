6ac7041b802bb9ae2b2242b3fced0c44aa0ded7a
Gourav Nema <mail@gourav.info>
2015-09-07 16:51:21 +0530

    Gourav & Jaya | 2874 visit is filtered based on observation templates

--- ui/app/clinical/displaycontrols/allvisits/directives/visitsTable.js
+++ ui/app/clinical/displaycontrols/allvisits/directives/visitsTable.js
@@ -1,13 +1,21 @@
 'use strict';
 
 angular.module('bahmni.clinical')
-    .directive('visitsTable', ['patientVisitHistoryService', 'spinner', '$state', function (patientVisitHistoryService, spinner, $state) {
+    .directive('visitsTable', ['patientVisitHistoryService', 'conceptSetService', 'spinner', '$state', '$q', function (patientVisitHistoryService, conceptSetService, spinner, $state, $q) {
         var controller = function ($scope) {
-            spinner.forPromise(patientVisitHistoryService.getVisitHistory($scope.patientUuid).then(function (visitHistory) {
-                $scope.visits = visitHistory.visits;
-            }));
-            $scope.openVisit = function(visit) {
-                if($scope.$parent.closeThisDialog){
+            var getVisits = function () {
+                return patientVisitHistoryService.getVisitHistory($scope.patientUuid);
+            };
+
+            var getAllObservationTemplates = function () {
+                return conceptSetService.getConceptSetMembers({
+                    name: "All Observation Templates",
+                    v: "custom:(setMembers:(display))"
+                })
+            };
+
+            $scope.openVisit = function (visit) {
+                if ($scope.$parent.closeThisDialog) {
                     $scope.$parent.closeThisDialog("closing modal");
                 }
                 $state.go('patient.visit', {visitUuid: visit.uuid});
@@ -24,9 +32,34 @@ angular.module('bahmni.clinical')
 
             $scope.noVisitsMessage = "No Visits for this patient.";
 
-            $scope.toggle = function(visit) {
+            $scope.toggle = function (visit) {
                 visit.isOpen = !visit.isOpen;
             };
+
+            $scope.filteredObservations = function (observation, observationTemplates) {
+                var observationTemplateArray = [];
+                for (var observationTemplateIndex in observationTemplates) {
+                    observationTemplateArray.push(observationTemplates[observationTemplateIndex].display);
+                }
+
+                var obsArrayFiltered = [];
+                for (var ob in observation) {
+                    if (_.contains(observationTemplateArray, observation[ob].concept.display)) {
+                        obsArrayFiltered.push(observation[ob])
+                    }
+                }
+                return obsArrayFiltered;
+            };
+
+            var init = function () {
+                return $q.all([getVisits(), getAllObservationTemplates()]).then(function (results) {
+                    $scope.visits = results[0].visits;
+                    $scope.observationTemplates = results[1].data.results[0].setMembers;
+                });
+            };
+
+            spinner.forPromise(init());
+
         };
         return {
             restrict: 'E',
@@ -38,4 +71,6 @@ angular.module('bahmni.clinical')
                 showObservationData: "="
             }
         };
-    }]);
+    }
+    ])
+;
