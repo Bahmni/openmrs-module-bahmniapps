606b8d7074c699b59de5fb6f2ec00a87f8c50021
padma <padmavb@thoughtworks.com>
2015-09-07 10:38:29 +0530

    Revert "Ranganathan | Padma - #2628 - Fixing login page js error"

--- ui/app/common/util/httpErrorInterceptor.js
+++ ui/app/common/util/httpErrorInterceptor.js
@@ -51,10 +51,9 @@ angular.module('httpErrorInterceptor',[])
                     showError("Could not connect to the server. Please check your connection and try again");
                 } else if (response.status === 403) {
                     var errorMessage = data.error && data.error.message ? stringAfter(data.error.message, ':') : unexpectedError;
+                    showError(errorMessage);
                     if(shouldRedirectToLogin(response)){
                             $rootScope.$broadcast('event:auth-loginRequired');
-                    }else{
-                      showError(errorMessage);
                     }
                 } else if (response.status === 404) {
                     if(!_.contains(response.config.url, "implementation_config")) {
