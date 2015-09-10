e5367c8127643c51f6c046e58c786b74ff03b340
shiresha <shiresha@INshiresha.local>
2015-09-10 11:35:38 +0530

    Shireesha,Bharat|Removing orders from patientVisitHistory API

--- ui/app/clinical/consultation/services/patientVisitHistoryService.js
+++ ui/app/clinical/consultation/services/patientVisitHistoryService.js
@@ -4,7 +4,7 @@ angular.module('bahmni.clinical')
     .service('patientVisitHistoryService', ['visitService', function (visitService) {
         this.getVisitHistory = function (patientUuid) {
             return visitService.search({patient: patientUuid, v: 'custom:(uuid,visitType:(uuid,name,display),startDatetime,stopDatetime,' +
-            'encounters:(uuid,display,encounterDatetime,orders,obs,encounterProviders:(provider)))', includeInactive: true})
+            'encounters:(uuid,display,encounterDatetime,obs,encounterProviders:(provider)))', includeInactive: true})
                 .then(function (data) {
                     var visits = data.data.results.map(function (visitData) {
                         return new Bahmni.Clinical.VisitHistoryEntry(visitData);
