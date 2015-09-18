1ad01cd23547a4ed048db6f694d05fd2295b9931
padma <padmavb@thoughtworks.com>
2015-09-18 11:54:08 +0530

    Padma, jaya | #2218 - Fixing the bug

--- ui/app/clinical/common/controllers/consultationContextController.js
+++ ui/app/clinical/common/controllers/consultationContextController.js
@@ -1,16 +1,14 @@
 'use strict';
 
 angular.module('bahmni.clinical')
-    .controller('consultationContextController', ['$scope','appService','$q',
-        function ($scope, appService,$q) {
-
-            var init = function(){
-                return appService.loadConfig('dashboard.json').then(function (response) {
-                    var patientConfig = response.general.sections.patientInformation;
-                    if(patientConfig.hasOwnProperty("ageLimit") && $scope.patient.age >= patientConfig.ageLimit){
-                        $scope.patient.ageText = $scope.patient.age.toString()+ " <span> years </span>";
-                    }
-                });
+    .controller('consultationContextController', ['$scope', 'appService',
+        function ($scope, appService) {
+            var init = function () {
+                var programConfig = appService.getAppDescriptor().getConfigValue('program');
+                var patientConfig = !_.isUndefined(programConfig) ? programConfig.patientInformation : {};
+                if (patientConfig.hasOwnProperty("ageLimit") && $scope.patient.age >= patientConfig.ageLimit) {
+                    $scope.patient.ageText = $scope.patient.age.toString() + " <span> years </span>";
+                }
             };
             init();
         }]);
