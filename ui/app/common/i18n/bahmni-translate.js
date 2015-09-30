ef1a15d255181c02d9d0708142726a9f6cce72ab
Venkata Jaswanth <jaswanth@thoughtworks.com>
2015-09-30 15:19:52 +0530

    Bharath, Jaswanth | Added titleTranslate filter for backward compatibility of internationalization with other configs

--- ui/app/common/i18n/bahmni-translate.js
+++ ui/app/common/i18n/bahmni-translate.js
@@ -1,7 +1,27 @@
 'use strict';
 
 angular.module('bahmni.common.i18n',['pascalprecht.translate'])
-    .provider('$bahmniTranslate', $bahmniTranslateProvider);
+    .provider('$bahmniTranslate', $bahmniTranslateProvider).
+    filter('titleTranslate', ['$translate', function ($translate) {
+        return function (input) {
+            if (!input) {
+                return input;
+            }
+            if (input.translationKey) {
+                return $translate.instant(input.translationKey);
+            }
+            if (input.dashboardName) {
+                return input.dashboardName;
+            }
+            if (input.title) {
+                return input.title;
+            }
+            if (input.label) {
+                return input.label;
+            }
+            return null;
+        }
+    }]);
 
 function $bahmniTranslateProvider($translateProvider){
     this.init = function(options){
