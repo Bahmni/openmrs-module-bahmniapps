0365bef8de4ed91372132a3cf6b889dd5bded89e
Ranganathan Balashanmugam <ranganab@thoughtworks.com>
2015-10-15 14:45:54 +0530

    Ranganathan, Abishek | Base offline chromium app

---
+++ offline/background.js
@@ -0,0 +1,20 @@
+/**
+ * Global properties.
+ */
+var CONSTANTS = {
+    APP_WIDTH: 1024,
+    APP_HEIGHT: 800
+};
+
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
+    chrome.app.window.create('index.html', {
+        id: 'main',
+        bounds: { width: CONSTANTS.APP_WIDTH, height: CONSTANTS.APP_HEIGHT },
+        resizable: true
+    });
+});
