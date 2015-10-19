6cbfc11f36e26042465b4eac69f4608441b170ff
Ranganathan Balashanmugam <ranganab@thoughtworks.com>
2015-10-19 16:12:43 +0530

    Ranganathan, Abishek | offline chrome app clean up

--- offline/background.js
+++ offline/background.js
@@ -12,11 +12,23 @@ var CONSTANTS = {
  * @see http://developer.chrome.com/apps/app.window.html
  */
 chrome.app.runtime.onLaunched.addListener(function() {
+    runApp();
+});
+
+/**
+ * Listens for the app restarting then re-creates the window.
+ *
+ * @see http://developer.chrome.com/apps/app.runtime.html
+ */
+chrome.app.runtime.onRestarted.addListener(function() {
+    runApp();
+});
+
+var runApp = function () {
     chrome.app.window.create('index.html', {
         id: 'main',
         bounds: { width: CONSTANTS.APP_WIDTH, height: CONSTANTS.APP_HEIGHT },
         resizable: true
     });
-
-});
+};
 
