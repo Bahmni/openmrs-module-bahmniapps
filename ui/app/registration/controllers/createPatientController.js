71362d89f465c52ae351444d4fc822569de44c83
Sourav <sourava@thoughtworks.com>
2015-10-21 10:53:39 +0530

    Sourav| vikas #3029 fixed the issue if defaults are not specified in config

--- ui/app/registration/controllers/createPatientController.js
+++ ui/app/registration/controllers/createPatientController.js
@@ -13,7 +13,9 @@ angular.module('bahmni.registration')
             var prepopulateDefaultsInFields = function () {
 
                 var personAttributeTypes = getPersonAttributeTypes();
-                var defaults = appService.getAppDescriptor().getConfigValue("patientInformation").defaults || {};
+                var defaults = {}
+                if(appService.getAppDescriptor().getConfigValue("patientInformation"))
+                    defaults = appService.getAppDescriptor().getConfigValue("patientInformation").defaults;
 
                 _.each(personAttributeTypes, function (personAttributeType) {
                     var present = _.contains(Object.keys(defaults), personAttributeType.name);
