789b38f468ccd3188cbc53d70ee30046282efc18
Vinkesh Banka <vinkesh.banka@gmail.com>
2015-11-03 13:20:13 +0530

    Banka | #3092 | Caching disease template calls in dashboard.                 Adding some extra tests.

---
+++ ui/test/unit/clinical/controllers/patientDashboardLabSummaryController.spec.js
@@ -0,0 +1,29 @@
+'use strict';
+
+describe("PatientDashboardLabSummaryController", function () {
+
+    beforeEach(module('bahmni.clinical'));
+
+    var scope;
+    var stateParams;
+
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        scope.ngDialogData = {
+            allLabDetails: {a: 1, b: 2},
+            patient: {uuid: "some uuid"}
+        }
+        stateParams = {
+            patientUuid: "some uuid"
+        };
+        $controller('PatientDashboardLabSummaryController', {
+            $scope: scope,
+            $stateParams: stateParams,
+        });
+    }));
+
+    it("should initialize", function () {
+        expect(scope.allLabDetails).toEqual({a: 1, b: 2, patientUuid: "some uuid"});
+        expect(scope.patient).toEqual({uuid: "some uuid"});
+    });
+});
