eba2d54e64c9d4b4e78e13692d8ec6ab315b0ba8
Vinay Venu <vinayv@thoughtworks.com>
2015-11-05 19:53:46 +0530

    Remove omod packaging and liquibase scripts from bahmniapps
    Conflicts:
    	api/pom.xml
    	omod/pom.xml
    	omod/src/main/resources/config.xml
    	pom.xml

--- omod/src/main/resources/V1_22__AddConceptWordsForRegistrationConceptSet.sql
+++
@@ -1,4 +0,0 @@
-select concept.concept_id, concept_name.concept_name_id from concept, concept_name where concept_name.concept_id = concept.concept_id and
-concept_name.name = 'REGISTRATION_CONCEPTS' into @concept_id, @concept_name_id;
-insert into concept_word (concept_id, word, locale, concept_name_id, weight) values (@concept_id, 'REGISTRATION', 'en', @concept_name_id, 2.1764705882352944);
-insert into concept_word (concept_id, word, locale, concept_name_id, weight) values (@concept_id, 'CONCEPTS', 'en', @concept_name_id, 9.402777777777779);
