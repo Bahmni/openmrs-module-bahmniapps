24dd5bc88068d3fe6973a88ce115c5080ac55d6a
bharatak <bharatak@thoughtworks.com>
2015-11-15 14:41:47 +0530

    Bharat,Santhosh|#3082 - Support for month in Treatment Regimen

--- ui/app/common/displaycontrols/chronicTreatmentChart/directives/chronicTreatmentChart.js
+++ ui/app/common/displaycontrols/chronicTreatmentChart/directives/chronicTreatmentChart.js
@@ -12,6 +12,11 @@ angular.module('bahmni.common.displaycontrol.chronicTreatmentChart').directive('
                 });
             };
 
+            $scope.isMonthNumberRequired = function(){
+                var month = $scope.regimen && $scope.regimen.rows && $scope.regimen.rows[0] && $scope.regimen.rows[0].month;
+                return month;
+            };
+
             $scope.isClickable = function () {
                 return $scope.isOnDashboard && $scope.section.allDetailsParams;
             };
