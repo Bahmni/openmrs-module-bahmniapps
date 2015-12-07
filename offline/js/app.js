1b4d18f06741b2aee60f940d0c1948c6dcbfb544
chethanTw <chethandec22@gmail.com>
2015-12-07 18:12:25 +0530

    Abishek/Chetan | Removing unused files

--- offline/js/app.js
+++
@@ -1,8 +0,0 @@
-function maximizeWebviews() {
-    var webview = document.querySelector("webview");
-    webview.style.height = document.documentElement.clientHeight + "px";
-    webview.style.width = document.documentElement.clientWidth + "px";
-};
-
-onload = maximizeWebviews;
-window.onresize = maximizeWebviews;
