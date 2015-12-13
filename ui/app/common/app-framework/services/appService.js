22518b7a5ef04df8aec17b61c1fca10e4653d8a7
Venkata Jaswanth <jaswanth@thoughtworks.com>
2015-12-13 14:49:34 +0530

    Jaswanth | Fixed a typo

--- ui/app/common/app-framework/services/appService.js
+++ ui/app/common/app-framework/services/appService.js
@@ -12,7 +12,7 @@ angular.module('bahmni.common.appFramework')
             return $http.get(url, {withCredentials: true});
         };
 
-        var loadTemplate = function (appDescriptor) {S
+        var loadTemplate = function (appDescriptor) {
             var deferrable = $q.defer();
             loadConfig(baseUrl + appDescriptor.contextPath + "/appTemplate.json").then(
                 function (result) {
