ba3f65f9866ccac1b435e0e028d1c86031e51ff7
sravanthi17 <sravanns@thoughtworks.com>
2015-12-16 15:28:38 +0530

    Santhosh, sravanthi, |#3487| Merging 292 story and Adding column headers for treatment details control

--- ui/app/common/displaycontrols/drugOrderDetails/directives/drugOrderDetails.js
+++ ui/app/common/displaycontrols/drugOrderDetails/directives/drugOrderDetails.js
@@ -10,6 +10,9 @@ angular.module('bahmni.common.displaycontrol.drugOrderDetails')
                 })
             };
 
+            $scope.columnHeaders = ["DRUG_DETAILS_DRUG_NAME", "DRUG_DETAILS_DOSE_INFO", "DRUG_DETAILS_ROUTE", "DRUG_DETAILS_FREQUENCY",
+                "DRUG_DETAILS_START_DATE", "DRUG_DETAILS_STOP_DATE", "DRUG_DETAILS_ORDER_REASON_CODED", "DRUG_DETAILS_ORDER_REASON_TEXT"];
+
             $scope.showDetails = false;
             $scope.toggle = function (drugOrder) {
                 drugOrder.showDetails = !drugOrder.showDetails;
