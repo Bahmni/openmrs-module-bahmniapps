96c18927935e9299162e49065f42d6e7a28b5ec2
sravanthi17 <sravanns@thoughtworks.com>
2015-12-16 15:29:04 +0530

    Santhosh, sravanthi | 3485 | Fixing the order issue

--- ui/app/common/displaycontrols/drugOrderDetails/directives/drugOrderDetails.js
+++ ui/app/common/displaycontrols/drugOrderDetails/directives/drugOrderDetails.js
@@ -5,9 +5,9 @@ angular.module('bahmni.common.displaycontrol.drugOrderDetails')
         var controller = function ($scope) {
 
             var init = function () {
-               return treatmentService.getAllDrugOrdersFor($scope.patient.uuid, $scope.section.dashboardParams.drugNames).then(function(response){
-                    $scope.drugOrders = response;
-                })
+                return treatmentService.getAllDrugOrdersFor($scope.patient.uuid, $scope.section.dashboardParams.drugNames).then(function (response) {
+                    $scope.drugOrders = sortOrders(response);
+                });
             };
 
             $scope.columnHeaders = ["DRUG_DETAILS_DRUG_NAME", "DRUG_DETAILS_DOSE_INFO", "DRUG_DETAILS_ROUTE", "DRUG_DETAILS_FREQUENCY",
@@ -16,6 +16,30 @@ angular.module('bahmni.common.displaycontrol.drugOrderDetails')
             $scope.showDetails = false;
             $scope.toggle = function (drugOrder) {
                 drugOrder.showDetails = !drugOrder.showDetails;
+            };
+
+            var sortOrders = function(response){
+                var drugOrderUtil = Bahmni.Clinical.DrugOrder.Util;
+                var now = new Date();
+                var activeAndScheduled = _.filter(response, function (order) {
+                    return order.isActive() || order.isScheduled();
+                });
+                var autoExpiredDrugs = _.filter(response, function(order){
+                    return order.effectiveStopDate && order.effectiveStopDate <= now && order.dateStopped === null;
+                });
+                var inActiveOrders = _.filter(response, function (order) {
+                    return order.isDiscontinuedOrStopped();
+                });
+
+                var partitionedDrugOrders = _.groupBy(activeAndScheduled, function (drugOrder) {
+                    return (drugOrder.effectiveStartDate > now) ? "scheduled" : "active";
+                });
+                var sortedDrugOrders = [];
+                sortedDrugOrders.push(drugOrderUtil.sortDrugOrders(partitionedDrugOrders.scheduled));
+                sortedDrugOrders.push(drugOrderUtil.sortDrugOrders(partitionedDrugOrders.active));
+                sortedDrugOrders.push(drugOrderUtil.sortDrugOrders(autoExpiredDrugs));
+                sortedDrugOrders.push(drugOrderUtil.sortDrugOrders(inActiveOrders));
+                return _.flatten(sortedDrugOrders);
             };
 
             spinner.forPromise(init());
