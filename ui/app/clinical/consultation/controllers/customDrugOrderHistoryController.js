019d0ef711e8bc67d9a3a122d1eb9398330aca4d
Preethi <preethi.ms34@gmail.com>
2015-12-31 10:49:13 +0530

    Preethi, Chethan | #459 | Maintained order of sections specified in config medications.json

--- ui/app/clinical/consultation/controllers/customDrugOrderHistoryController.js
+++ ui/app/clinical/consultation/controllers/customDrugOrderHistoryController.js
@@ -1,5 +1,5 @@
 angular.module('bahmni.clinical')
     .controller('CustomDrugOrderHistoryController', ['$scope', 'clinicalAppConfigService', function ($scope, clinicalAppConfigService) {
         var drugOrderHistoryConfig = clinicalAppConfigService.getMedicationConfig().drugOrderHistoryConfig || {};
-        $scope.drugOrderHistorySections = drugOrderHistoryConfig.sections;
+        $scope.drugOrderHistorySections = _.values(drugOrderHistoryConfig.sections);
     }]);
