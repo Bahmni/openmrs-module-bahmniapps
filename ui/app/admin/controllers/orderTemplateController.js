23103c3d6a51dda3da77b49844c3cf2b00a621b0
ys-achinta <achintar@thoughtworks.com>
2016-01-11 12:34:58 +0530

    Pankaj, Achinta, Gautam, | CSS changes and UI fixes for displaying orderTemplate for orderSet

--- ui/app/admin/controllers/orderTemplateController.js
+++ ui/app/admin/controllers/orderTemplateController.js
@@ -3,6 +3,7 @@
 angular.module('bahmni.common.domain')
     .controller('OrderTemplateController', ['$scope', '$state', '$http', 'orderSetService', 'spinner', function ($scope, $state, $http, orderSetService, spinner) {
 
+        console.log($scope);
         $scope.getDrugResults = function () {
             return function (results) {
                 return results.map(function (drug) {
