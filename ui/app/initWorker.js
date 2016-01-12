acaefccf0bbf39280440d8005d177b65cfc6cd9a
Ranganathan Balashanmugam <ranganab@thoughtworks.com>
2016-01-12 15:15:59 +0530

    Ranganathan | Change the alert text for service worker not working.

--- ui/app/initWorker.js
+++ ui/app/initWorker.js
@@ -6,7 +6,7 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('worker.js').then(function (bahmni) {
         console.log('Yo! service worker working.', bahmni.scope);
     }).catch(function (err) {
-        alert('No Yo! service worker not working');
+        alert('Sorry, offline cannot be setup.');
         console.log('No Yo! service worker not working', err);
     });
 }
