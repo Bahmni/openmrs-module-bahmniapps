19dec697458e32545f69912bb3a4f3cd3aea69f8
jayasuganthi <jayasug@thoughtworks.com>
2016-01-19 15:07:48 +0530

    Hemanth | fixed offline icon in registration

--- ui/app/registration/controllers/navigationController.js
+++ ui/app/registration/controllers/navigationController.js
@@ -1,30 +1,33 @@
 'use strict';
 
 angular.module('bahmni.registration')
-    .controller('NavigationController', ['$scope', '$rootScope', '$location', 'sessionService', '$window', 'appService', '$sce',
-        function ($scope, $rootScope, $location, sessionService, $window, appService, $sce) {
+    .controller('NavigationController', ['$scope', '$rootScope', '$location', 'sessionService', '$window', 'appService', '$sce', 'offlineService',
+        function ($scope, $rootScope, $location, sessionService, $window, appService, $sce, offlineService) {
 
-        $scope.extensions = appService.getAppDescriptor().getExtensions("org.bahmni.registration.navigation", "link");
+            $scope.extensions = appService.getAppDescriptor().getExtensions("org.bahmni.registration.navigation", "link");
 
-        $scope.goTo = function(url) {
-            $location.url(url);
-        };
+            $scope.goTo = function (url) {
+                $location.url(url);
+            };
 
-        $scope.htmlLabel = function(label){
-            return $sce.trustAsHtml(label);
-        }
+            $scope.htmlLabel = function (label) {
+                return $sce.trustAsHtml(label);
+            };
 
-        $scope.logout = function () {
-            $rootScope.errorMessage = null;
-            sessionService.destroy().then(
-                function() {
-                    $window.location = "../home/";
-                }
-            );
-        };
+            $scope.logout = function () {
+                $rootScope.errorMessage = null;
+                sessionService.destroy().then(
+                    function () {
+                        $window.location = "../home/";
+                    }
+                );
+            };
 
-        $rootScope.$on('offline', function () {
-            $scope.$apply();
-        });
+            $rootScope.$on('offline', function () {
+                $scope.$apply();
+            });
 
-    }]);
+            $scope.isOffline = function () {
+                return offlineService.offline();
+            };
+        }]);
