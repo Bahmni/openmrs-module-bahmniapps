dea31bdd718b6188299b275f459db9d85341251a
Vinay Venu <vinayv@thoughtworks.com>
2016-02-05 17:31:03 +0530

    Prevent duplicate encounter creation caused by double click on admit button

--- ui/app/adt/controllers/adtController.js
+++ ui/app/adt/controllers/adtController.js
@@ -43,14 +43,6 @@ angular.module('bahmni.adt')
                 return mappingCode;
             };
 
-            var getAdtActionForEncounterType = function (encounterTypeUuid) {
-                var adtActionsForType = $scope.dispositionActions.filter(function (dispositionAction) {
-                    var actionConfig = actionConfigs[getActionCode(dispositionAction)];
-                    return actionConfig ? actionConfig.encounterTypeUuid === encounterTypeUuid : false;
-                });
-                return adtActionsForType.length > 0 ? adtActionsForType[0] : null;
-            };
-
             var initializeActionConfig = function () {
                 var admitActions = appService.getAppDescriptor().getExtensions("org.bahmni.adt.admit.action", "config");
                 var transferActions = appService.getAppDescriptor().getExtensions("org.bahmni.adt.transfer.action", "config");
@@ -159,6 +151,7 @@ angular.module('bahmni.adt')
 
             $scope.call = function (functionName) {
                 if (functionName) {
+                    $scope.submitButtonDisabled = false;
                     return $scope[functionName]();
                 } else {
                     return true;
@@ -198,7 +191,7 @@ angular.module('bahmni.adt')
             };
 
 
-            $scope.admit = function (visitTypeUuid) {
+            $scope.admit = function () {
                 if ($scope.visitSummary && $scope.visitSummary.visitType != 'IPD') {
                     var confirmed = $window.confirm("Patient Visit Type is "+$scope.visitSummary.visitType+", Do you want to close the Visit and start new IPD Visit?");
                     if (confirmed) {
