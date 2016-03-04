a27f38490818c02a31d592c23defe3e8e6147918
Pankaj <ladharpankaj@gmail.com>
2016-03-04 10:15:49 +0530

    Pankaj, Preethi | Removed  function in toggle.js

--- ui/app/common/ui-helper/directives/toggle.js
+++ ui/app/common/ui-helper/directives/toggle.js
@@ -3,14 +3,16 @@
 angular.module('bahmni.common.uiHelper')
     .directive('toggle', function () {
         var link = function ($scope, element) {
-            $scope.$watch("toggle", function (value) {
-                if(value!= undefined) {
-                    $(element).toggleClass('active', value)
-                }
-            });
+            //$scope.$watch("toggle", function (value) {
+            //    if(value!= undefined) {
+            //        $(element).toggleClass('active', value)
+            //    }
+            //});
+            $scope.toggle && $(element).addClass('active',$scope.toggle);
             $(element).click(function () {
                 $scope.$apply(function () {
                     $scope.toggle = !$scope.toggle
+                    $(element).toggleClass('active', $scope.toggle)
                 });
             });
         };
