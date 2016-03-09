7a912c03b95ae698d30fcca151aa4a45e6a2ccad
Dé <rahul080327@gmail.com>
2016-03-09 12:15:14 +0530

    Rahul, Jaswanth | #1333 | Clear errors on reload of module

--- ui/app/common/ui-helper/spinner.js
+++ ui/app/common/ui-helper/spinner.js
@@ -1,35 +1,40 @@
 'use strict';
 
 angular.module('bahmni.common.uiHelper')
-    .factory('spinner', [ function () {
+    .factory('spinner', ['messagingService', function (messagingService) {
         var tokens = [];
 
         var show = function () {
-           var token = Math.random();
-           tokens.push(token);
-           if($('#overlay').length == 0) {
+            messagingService.hideMessages("error");
+            var token = Math.random();
+            tokens.push(token);
+            if ($('#overlay').length == 0) {
                 $('body').prepend('<div id="overlay"><div></div></div>');
-           }
-           $('#overlay').stop().show();
-           return token;
+            }
+            $('#overlay').stop().show();
+            return token;
         };
 
         var hide = function (token) {
             _.pull(tokens, token);
-            if(tokens.length === 0) {
+            if (tokens.length === 0) {
                 $('#overlay').fadeOut(300);
             }
         };
 
-        var forPromise = function(promise) {
+        var forPromise = function (promise) {
             var token = show();
-            promise['finally'](function() { hide(token); });
+            promise['finally'](function () {
+                hide(token);
+            });
             return promise;
         };
 
-        var forAjaxPromise = function(promise) {
+        var forAjaxPromise = function (promise) {
             var token = show();
-            promise.always(function() { hide(token); });
+            promise.always(function () {
+                hide(token);
+            });
             return promise;
         };
 
