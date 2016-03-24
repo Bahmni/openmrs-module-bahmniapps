87d6d75fde5d4a1ab0e3f31c4c7ef7d186a9981c
Buddha <sidthunagarajan@gmail.com>
2016-03-24 14:45:01 +0530

    Chethan, Gautam | #Refactoring. | dashboardSection.js
    Simplified logics for getting viewUrl for sections.
    Enclosed with IIFE.

--- ui/app/common/displaycontrols/dashboard/models/dashboardSection.js
+++ ui/app/common/displaycontrols/dashboard/models/dashboardSection.js
@@ -1,11 +1,9 @@
 'use strict';
 
-Bahmni.Common.DisplayControl.Dashboard.Section = function (section) {
-    angular.extend(this, section);
-    this.displayOrder = section.displayOrder;
-    this.data = section.data || {};
-    this.isObservation = section.isObservation || false;
-    this.patientAttributes = section.patientAttributes || [];
+(function () {
+    var OBSERVATION_SECTION_URL = "../common/displaycontrols/dashboard/views/sections/observationSection.html";
+    var COMMON_DISPLAY_CONTROL_URL = "../common/displaycontrols/dashboard/views/sections/SECTION_NAME.html";
+    var CLINICAL_DISPLAY_CONTROL_URL = "../clinical/dashboard/views/dashboardSections/SECTION_NAME.html";
     var commonDisplayControlNames = [
         "admissionDetails",
         "bacteriologyResultsControl",
@@ -20,17 +18,29 @@ Bahmni.Common.DisplayControl.Dashboard.Section = function (section) {
         "pacsOrders",
         "patientInformation"
     ];
-    if (this.isObservation === true) {
-        this.viewName = "../common/displaycontrols/dashboard/views/sections/observationSection.html";
-    } else if (commonDisplayControlNames.some(function (name) {
-            return name == section.type
-        })) {
-        this.viewName = "../common/displaycontrols/dashboard/views/sections/" + this.type + ".html";
-    } else {
-        this.viewName = "../clinical/dashboard/views/dashboardSections/" + this.type + ".html";
-    }
-};
 
-Bahmni.Common.DisplayControl.Dashboard.Section.create = function (section) {
-    return new Bahmni.Common.DisplayControl.Dashboard.Section(section);
-};
+    var getViewUrl = function (section) {
+        if (section.isObservation) {
+            return OBSERVATION_SECTION_URL;
+        }
+        var isCommonDisplayControl = _.includes(commonDisplayControlNames, section.type);
+        if (isCommonDisplayControl) {
+            return COMMON_DISPLAY_CONTROL_URL.replace('SECTION_NAME', section.type);
+        }
+        return CLINICAL_DISPLAY_CONTROL_URL.replace('SECTION_NAME', section.type);
+    };
+
+    Bahmni.Common.DisplayControl.Dashboard.Section = function (section) {
+        angular.extend(this, section);
+        this.displayOrder = section.displayOrder;
+        this.data = section.data || {};
+        this.isObservation = !!section.isObservation;
+        this.patientAttributes = section.patientAttributes || [];
+        this.viewName = getViewUrl(this);
+    };
+
+    Bahmni.Common.DisplayControl.Dashboard.Section.create = function (section) {
+        return new Bahmni.Common.DisplayControl.Dashboard.Section(section);
+    };
+
+})();
