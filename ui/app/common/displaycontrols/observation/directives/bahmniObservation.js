47890cd4717d68d7ba75b85c8bb2b2e36856dc5d
Dé <rahul080327@gmail.com>
2016-04-12 11:51:11 +0530

    Revert "De, JP | #1115 | Short Names should not be accepted in the config."
    This reverts commit c9dcb3e0b1ea913c0736eda3e635da2e35685065.

--- ui/app/common/displaycontrols/observation/directives/bahmniObservation.js
+++ ui/app/common/displaycontrols/observation/directives/bahmniObservation.js
@@ -14,10 +14,6 @@ angular.module('bahmni.common.displaycontrol.observation')
                     var conceptsConfig = appService.getAppDescriptor().getConfigValue("conceptSetUI") || {};
                     observations = new Bahmni.Common.Obs.ObservationMapper().map(observations, conceptsConfig);
 
-                    observations = _.filter(observations, function(obs) {
-                        return ($scope.config.conceptNames.includes(obs.concept.name));
-                    });
-
                     $scope.bahmniObservations = new Bahmni.Common.DisplayControl.Observation.GroupingFunctions().groupByEncounterDate(observations);
                     if (_.isEmpty($scope.bahmniObservations)) {
                         $scope.noObsMessage = Bahmni.Common.Constants.messageForNoObservation;
