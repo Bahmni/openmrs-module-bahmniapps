c0316a6a1011f369fded4d607c2722c1232b9b17
Dé <rahul080327@gmail.com>
2016-04-18 17:08:09 +0530

    Rahul, Hemanth | #1461 | Add support for custom drug frequency

--- ui/app/clinical/common/services/visitTabConfig.js
+++ ui/app/clinical/common/services/visitTabConfig.js
@@ -16,21 +16,33 @@ angular.module('bahmni.clinical')
                 results[0].data.sections = _.sortBy(results[0].data.sections, function (section) {
                     return section.displayOrder;
                 });
+
                 for (var tab in results[1]) {
                     var sortedSections = _.sortBy(results[1][tab].sections, function (section) {
                         return section.displayOrder;
                     });
+
                     if (sortedSections.length > 0) {
                         results[1][tab].sections = sortedSections;
                     }
                 }
+
                 var mandatoryConfig = results[0].data;
                 var tabs = _.values(results[1]);
                 var firstTabWithDefaultSection = _.find(tabs, function(tab) {return tab.defaultSections});
+
+                // TODO: Patch for #1461 lokbiradari-config: It's bad. But to err is Human, to forgive is Divine!
+                if (_.find(mandatoryConfig.sections, {title: "Treatments"}) && _.find(firstTabWithDefaultSection.sections, {title: "Treatments"})) {
+                    mandatoryConfig.sections = _.filter(mandatoryConfig.sections, function(section) {
+                        return section.title !== "Treatments";
+                    });
+                }
+
                 firstTabWithDefaultSection.sections = _.union(_.values(mandatoryConfig.sections), _.values(firstTabWithDefaultSection.sections));
                 firstTabWithDefaultSection.sections = _.sortBy( firstTabWithDefaultSection.sections, function(section) {
                     return section.displayOrder;
                 });
+
                 return new Bahmni.Clinical.VisitTabConfig(tabs);
             });
         }
