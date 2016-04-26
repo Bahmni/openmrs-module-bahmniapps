c78cf9cd460df6522f405ed1aaa8281c33874617
Shireesha <bongaralashireesha@gmail.com>
2016-04-26 10:50:16 +0530

    Shireesha, Banka | #1570 | Ability to have multiple apps for reports.

--- ui/app/reports/initialization.js
+++ ui/app/reports/initialization.js
@@ -3,16 +3,18 @@
 angular.module('bahmni.reports').factory('initialization',
     ['authenticator', 'appService', 'spinner', 'configurations',
         function (authenticator, appService, spinner, configurations) {
+            return function(appName) {
+                var loadConfigPromise = function () {
+                    return configurations.load([]);
+                };
+                var initApp = function () {
+                    return appService.initApp(appName || 'reports', {'app': true, 'extension': true }, null, ["reports"]);
+                };
 
-            var loadConfigPromise = function () {
-                return configurations.load([]);
-            };
-
-            var initApp = function () {
-                return appService.initApp('reports', {'app': true, 'extension': true }, null, ["reports"]);
-            };
-
-            return spinner.forPromise(authenticator.authenticateUser().then(initApp).then(loadConfigPromise));
+                return spinner.forPromise(authenticator.authenticateUser()
+                    .then(initApp)
+                    .then(loadConfigPromise));
+            }
         }
     ]
 );
