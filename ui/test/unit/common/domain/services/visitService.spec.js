ab51ba21b83d35dbcd241917978c58d90ff5e74b
Venkata Jaswanth <aj.jaswanth@gmail.com>
2016-06-13 17:06:55 +0530

    Jaswanth | #1886 | Do not send location uuid while starting a visit

--- ui/test/unit/common/domain/services/visitService.spec.js
+++ ui/test/unit/common/domain/services/visitService.spec.js
@@ -2,14 +2,14 @@
 
 describe('Registration Visit Service', function () {
     var visitService;
-    var openmrsUrl = "/openmrs/ws/rest/v1/visit";
+    var openmrsVisitUrl = "/openmrs/ws/rest/v1/visit";
     var endVisitUrl = "/openmrs/ws/rest/v1/endVisit";
     var uuid = "9e23a1bb-0615-4066-97b6-db309c9c6447";
 
     var mockHttp = {
         get: jasmine.createSpy('Http get').and.returnValue({'results': [{'uuid': uuid}]}),
         post: jasmine.createSpy('Http post').and.returnValue({
-            'success': function (onSuccess) {
+            'success': function () {
                 return {
                     'then': function (thenMethod) {
                         thenMethod()
@@ -18,15 +18,15 @@ describe('Registration Visit Service', function () {
                         onError()
                     }
                 }
-            }})
+            }
+        })
     };
-
 
     beforeEach(function () {
         module('bahmni.common.domain');
         module(function ($provide) {
             Bahmni.Common.Constants.endVisitUrl = endVisitUrl;
-            Bahmni.Common.Constants.visitUrl = openmrsUrl;
+            Bahmni.Common.Constants.visitUrl = openmrsVisitUrl;
             $provide.value('$http', mockHttp);
         });
 
@@ -36,19 +36,33 @@ describe('Registration Visit Service', function () {
     });
 
     it('Should call search url in registration visit service', function () {
-        var parameters = {patient: uuid, includeInactive: false, v: "custom:(uuid)"}
-        var results = visitService.search(parameters);
+        var parameters = {patient: uuid, includeInactive: false, v: "custom:(uuid)"};
+
+        visitService.search(parameters);
+
         expect(mockHttp.get).toHaveBeenCalled();
-        expect(mockHttp.get.calls.mostRecent().args[0]).toBe(openmrsUrl);
+        expect(mockHttp.get.calls.mostRecent().args[0]).toBe(openmrsVisitUrl);
         expect(mockHttp.get.calls.mostRecent().args[1].params.patient).toBe(uuid);
         expect(mockHttp.get.calls.mostRecent().args[1].params.includeInactive).toBeFalsy();
     });
 
     it('Should call end visit url in registration visit service', function () {
-        var results = visitService.endVisit(uuid);
+        visitService.endVisit(uuid);
+
         expect(mockHttp.post).toHaveBeenCalled();
         expect(mockHttp.post.calls.mostRecent().args[0]).toBe(endVisitUrl + '?visitUuid=' + uuid);
         expect(mockHttp.post.calls.mostRecent().args[1].withCredentials).toBeTruthy();
+    });
+
+    it("Should post visit details to create visit url", function () {
+        var visitDetails = {patientUuid: "uuid"};
+
+        visitService.createVisit(visitDetails);
+
+        expect(mockHttp.post).toHaveBeenCalled();
+        expect(mockHttp.post.calls.mostRecent().args[0]).toBe(openmrsVisitUrl);
+        expect(mockHttp.post.calls.mostRecent().args[1]).toBe(visitDetails);
+        expect(mockHttp.post.calls.mostRecent().args[2].withCredentials).toBeTruthy();
     });
 
 });
