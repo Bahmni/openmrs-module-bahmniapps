917b6813d1c0a0e121d3bda3e0dd8fb1d902bcda
hanishar <hanisha946@gmail.com>
2016-06-15 10:35:30 +0530

    #1891 | Pankaj, Hanisha | Added toggle.js into index.html of ADT module

--- ui/app/common/ui-helper/directives/toggle.js
+++ ui/app/common/ui-helper/directives/toggle.js
@@ -3,12 +3,8 @@
 angular.module('bahmni.common.uiHelper')
     .directive('toggle', function () {
         var link = function ($scope, element) {
-            //$scope.$watch("toggle", function (value) {
-            //    if(value!= undefined) {
-            //        $(element).toggleClass('active', value)
-            //    }
-            //});
             $scope.toggle && $(element).addClass('active',$scope.toggle);
+
             $(element).click(function () {
                 $scope.$apply(function () {
                     $scope.toggle = !$scope.toggle
