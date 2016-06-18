00b408685f39acc393c61d731e239e781f8bd93f
mogoodrich <mgoodrich@pih.org>
2016-06-18 15:14:33 +0200

    #1755: Ability to configure forward-url on click of Save on Registration visit page

--- ui/app/registration/controllers/visitController.js
+++ ui/app/registration/controllers/visitController.js
@@ -166,7 +166,7 @@ angular.module('bahmni.registration')
             var afterSave = function () {
                 var forwardUrl = appService.getAppDescriptor().getConfigValue("afterVisitSaveForwardUrl");
                 if (forwardUrl != null) {
-                    $location.url(appService.getAppDescriptor().formatUrl(forwardUrl, {'patientUuid': patientUuid}));
+                    $window.location.href = appService.getAppDescriptor().formatUrl(forwardUrl, {'patientUuid': patientUuid});
                 }
                 else {
                     $state.transitionTo($state.current, $state.params, {
