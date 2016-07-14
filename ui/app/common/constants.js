36858721b5263209507bd245c33786af2329c5e6
Shruthip <shruthi.pitta@gmail.com>
2016-07-14 11:35:55 +0530

    Shruthi|Changing the url as sometimes it is getting redirected to a get call

--- ui/app/common/constants.js
+++ ui/app/common/constants.js
@@ -113,7 +113,7 @@ Bahmni.Common = Bahmni.Common || {};
         visitSummaryUrl: BAHMNI_CORE + "/visit/summary",
         encounterModifierUrl: BAHMNI_CORE + "/bahmniencountermodifier",
         openmrsUrl: hostUrl + "/openmrs",
-        loggingUrl: hostUrl + "/log",
+        loggingUrl: hostUrl + "/log/",
         idgenConfigurationURL: RESTWS_V1 + "/idgen/identifiersources",
         bahmniRESTBaseURL: BAHMNI_CORE + "",
         observationsUrl: BAHMNI_CORE + "/observations",
