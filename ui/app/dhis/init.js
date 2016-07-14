08431801d021d958f459b0ee833a571ddf0c6f16
padma <padmavb@thoughtworks.com>
2016-07-14 10:18:29 +0530

    Padma, Bharath | Deleting all the code related to dhis.

--- ui/app/dhis/init.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var Bahmni = Bahmni || {};
-Bahmni.Dhis = Bahmni.Dhis || {};
-
-angular.module('bahmni.dhis', ['bahmni.common.uiHelper', 'bahmni.common.util', 'bahmni.common.appFramework', 'bahmni.common.logging', 'ui.router']);
