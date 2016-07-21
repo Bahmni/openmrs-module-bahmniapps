3b6a2da9331c189219b7c76787336b2004221264
chethandeshpande <chethandec22@gmail.com>
2016-07-21 17:44:57 +0530

    Chethan | #1851 | Filtering the visit based on location when patient dashboard is accessed.

--- ui/app/clinical/consultation/services/patientVisitHistoryService.js
+++ ui/app/clinical/consultation/services/patientVisitHistoryService.js
@@ -2,14 +2,14 @@
 
 angular.module('bahmni.clinical')
     .service('patientVisitHistoryService', ['visitService', function (visitService) {
-        this.getVisitHistory = function (patientUuid) {
-            return visitService.search({patient: patientUuid, v: 'custom:(uuid,visitType,startDatetime,stopDatetime,encounters:(uuid))', includeInactive: true})
+        this.getVisitHistory = function (patientUuid, currentVisitLocation) {
+            return visitService.search({patient: patientUuid, v: 'custom:(uuid,visitType,startDatetime,stopDatetime,location,encounters:(uuid))', includeInactive: true})
                 .then(function (data) {
                     var visits = _.map(data.data.results, function(visitData) {
                         return new Bahmni.Clinical.VisitHistoryEntry(visitData);
                     });
                     var activeVisit = visits.filter(function (visit) {
-                        return visit.isActive();
+                        return visit.isActive() && visit.isFromCurrentLocation(currentVisitLocation);
                     })[0];
 
                     return {"visits": visits, "activeVisit": activeVisit};
