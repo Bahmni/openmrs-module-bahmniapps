474df0d76a19697b4cc743e5cbdb3397a3f0d799
Shireesha <bongaralashireesha@gmail.com>
2016-07-22 19:02:24 +0530

    Swathi, Shireesha | #2033 | Added state for displaying form templates in a new window

--- ui/app/common/patient-search/constants.js
+++ ui/app/common/patient-search/constants.js
@@ -5,10 +5,11 @@ Bahmni.Common.PatientSearch = Bahmni.Common.PatientSearch || {};
 Bahmni.Common.PatientSearch.Constants = {
     searchExtensionTileViewType:"tile",
     searchExtensionTabularViewType:"tabular",
-    tabularViewIgnoreHeadingsList: ["display","uuid","image","$$hashKey","activeVisitUuid", "hasBeenAdmitted"],
-    identifierHeading: ["ID","Id","id","identifier"],
+    tabularViewIgnoreHeadingsList: ["display","uuid","image","$$hashKey","activeVisitUuid", "hasBeenAdmitted","forwardUrl", "programUuid", "enrollment"],
+    identifierHeading: ["ID","Id","id","identifier", "DQ_COLUMN_TITLE_ACTION"],
     nameHeading: ["NAME","Name","name"],
     patientTileHeight:100,
     patientTileWidth:100,
+    printIgnoreHeadingsList: ["DQ_COLUMN_TITLE_ACTION"],
     tileLoadRatio:1 / 2
 };
