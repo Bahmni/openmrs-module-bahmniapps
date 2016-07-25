364c438d39636c69e34c2f5e9b094d116b38ac63
sourava <212sourav@gmail.com>
2016-07-25 11:34:24 +0530

    Revert "Sourav, Vikash | #1938 | Adding the functionality to override existing admin imports through configs."
    This reverts commit 748296927485236ae51666ecaf264e7ba22fdb73.

--- ui/app/admin/controllers/csvUploadController.js
+++ ui/app/admin/controllers/csvUploadController.js
@@ -6,15 +6,15 @@ angular.module('bahmni.admin')
             var adminCSVExtension = appService.getAppDescriptor().getExtensionById("bahmni.admin.csv");
             var patientMatchingAlgorithm = adminCSVExtension.extensionParams.patientMatchingAlgorithm || "";
             var urlMap = {
-                "concept": {name: "Concept", url: Bahmni.Common.Constants.conceptImportUrl},
-                "conceptset": {name: "Concept Set", url: Bahmni.Common.Constants.conceptSetImportUrl},
-                "program": {name: "Program", url: Bahmni.Common.Constants.programImportUrl},
-                "patient": {name: "Patient", url: Bahmni.Common.Constants.patientImportUrl},
-                "encounter": {name: "Encounter", url: Bahmni.Common.Constants.encounterImportUrl},
-                "drug": {name: "Drug", url: Bahmni.Common.Constants.drugImportUrl},
-                "labResults": {name: "Lab Results", url: Bahmni.Common.Constants.labResultsImportUrl},
-                "referenceterms": {name: "Reference Terms", url: Bahmni.Common.Constants.referenceTermsImportUrl},
-                "relationship": {name: "Relationship Information", url: Bahmni.Common.Constants.relationshipImportUrl}
+                "program": Bahmni.Common.Constants.programImportUrl,
+                "encounter": Bahmni.Common.Constants.encounterImportUrl,
+                "concept": Bahmni.Common.Constants.conceptImportUrl,
+                "conceptset": Bahmni.Common.Constants.conceptSetImportUrl,
+                "patient": Bahmni.Common.Constants.patientImportUrl,
+                "drug": Bahmni.Common.Constants.drugImportUrl,
+                "labResults": Bahmni.Common.Constants.labResultsImportUrl,
+                "referenceterms": Bahmni.Common.Constants.referenceTermsImportUrl,
+                "relationship": Bahmni.Common.Constants.relationshipImportUrl
             };
             var fileUploaderOptions = {
                 removeAfterUpload: true,
@@ -30,19 +30,13 @@ angular.module('bahmni.admin')
                     });
                 }));
             };
-            var init = function () {
-                var configUrlMap = adminCSVExtension.urlMap;
-                if (!_.isEmpty(configUrlMap)) {
-                    urlMap = configUrlMap;
-                }
-                $scope.urlMaps = urlMap;
-            };
-            $scope.option = {selected: "encounter"};
+
+            $scope.option = "encounter";
             $scope.uploader = new FileUploader(fileUploaderOptions);
             $scope.uploader.onBeforeUploadItem = function (item) {
-                item.url = urlMap[$scope.option.selected].url;
+                item.url = urlMap[$scope.option];
             };
             $scope.uploader.onCompleteAll = $scope.loadImportedItems;
+
             $scope.loadImportedItems();
-            init();
         }]);
