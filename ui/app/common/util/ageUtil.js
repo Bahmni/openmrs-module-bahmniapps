fa494583cc6514035b5d3fe652a6dda476701be4
sumanmaity112 <suman.maity112@gmail.com>
2016-08-16 17:00:49 +0530

    Suman | Fix some of jshint errors

--- ui/app/common/util/ageUtil.js
+++ ui/app/common/util/ageUtil.js
@@ -9,8 +9,8 @@ Bahmni.Common.Util.AgeUtil = (function() {
 
     var fromBirthDateTillReferenceDate = function (birthDate, referenceDate) {
         var DateUtil = Bahmni.Common.Util.DateUtil;
-        var referenceDate = referenceDate || DateUtil.now();
-        var period = DateUtil.diffInYearsMonthsDays(birthDate, referenceDate)
+        referenceDate = referenceDate || DateUtil.now();
+        var period = DateUtil.diffInYearsMonthsDays(birthDate, referenceDate);
         return {years: period.years, months: period.months, days: period.days};
     };
 
