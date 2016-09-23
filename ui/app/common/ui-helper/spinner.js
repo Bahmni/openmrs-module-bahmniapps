d2879b128eab8db766b684455b3c0b11e2ce6003
Bijayini <bijayinp@thoughtworks.com>
2016-09-23 14:56:48 +0530

    Bijayini | #2330 | UI fix for Delinking Action Buttons on Patient Dashboard Form Display Controls

--- ui/app/common/ui-helper/spinner.js
+++ ui/app/common/ui-helper/spinner.js
@@ -9,7 +9,7 @@ angular.module('bahmni.common.uiHelper')
             var token = Math.random();
             tokens.push(token);
             if(element !== undefined)
-                $(element).append('<div class="section-title-loader"><img src="../images/spinner.gif" /></div>');
+                $(element).prepend('<div class="dashboard-section-loader"></div>');
             return token;
         };
 
