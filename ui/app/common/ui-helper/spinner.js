e141103ca5d7b3a26e163cf625b75c497920fe5f
sumanmaity112 <suman.maity112@gmail.com>
2016-09-23 17:02:47 +0530

    Suman, Kaustav | #2330 | Added spinner overlay for non display control

--- ui/app/common/ui-helper/spinner.js
+++ ui/app/common/ui-helper/spinner.js
@@ -3,20 +3,40 @@
 angular.module('bahmni.common.uiHelper')
     .factory('spinner', ['messagingService', function (messagingService) {
         var tokens = [];
+        var overlayTokens = [];
+
+        var showSpinnerForElement = function (token, element) {
+            $('#overlay').hide();
+            tokens.push(token);
+            $(element).append('<div class="dashboard-section-loader"></div>');
+            return token;
+        };
+
+        var showSpinnerForOverlay = function (token) {
+            overlayTokens.push(token);
+            $('body').prepend('<div id="overlay"><div></div></div>');
+            $('#overlay').stop().show();
+            return token;
+        };
 
         var show = function (element) {
             messagingService.hideMessages("error");
             var token = Math.random();
-            tokens.push(token);
-            if(element !== undefined)
-                $(element).prepend('<div class="dashboard-section-loader"></div>');
-            return token;
+            if (element !== undefined)
+                return showSpinnerForElement(token, element);
+
+            if ($('#overlay').length == 0)
+                return showSpinnerForOverlay(token);
         };
 
         var hide = function (token) {
             _.pull(tokens, token);
+            _.pull(overlayTokens, token);
             if (tokens.length === 0) {
                 $('.dashboard-section-loader').fadeOut(300);
+            }
+            if (overlayTokens.length === 0) {
+                $('#overlay').fadeOut(300);
             }
         };
 
