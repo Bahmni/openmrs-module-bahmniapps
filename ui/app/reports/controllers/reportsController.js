49eb65c6957abc08aada3dd60d2e24c3401a1d3c
sourava <212sourav@gmail.com>
2016-10-10 11:26:35 +0530

    Sourav | #2557 | Renaming the config of reports and adding button to reports page

--- ui/app/reports/controllers/reportsController.js
+++ ui/app/reports/controllers/reportsController.js
@@ -14,6 +14,7 @@ angular.module('bahmni.reports')
 
         $rootScope.default = _.isUndefined($rootScope.default) ? {reportsRequiringDateRange: {}, reportsNotRequiringDateRange: {}} : $rootScope.default;
         $scope.reportsDefined = true;
+        $scope.enableReportQueue = appService.getAppDescriptor().getConfigValue("enableReportQueue");
 
         $scope.setDefault = function (item, header) {
             var setToChange = header === 'reportsRequiringDateRange' ? $rootScope.reportsRequiringDateRange : $rootScope.reportsNotRequiringDateRange;
@@ -56,14 +57,6 @@ angular.module('bahmni.reports')
                 return false;
             }
             return true;
-        };
-
-        $scope.run = function (report) {
-            if(appService.getAppDescriptor().getConfigValue("scheduledReports")) {
-                $scope.scheduleReport(report);
-            } else {
-                $scope.downloadReport(report);
-            }
         };
 
         $scope.downloadReport = function (report) {
