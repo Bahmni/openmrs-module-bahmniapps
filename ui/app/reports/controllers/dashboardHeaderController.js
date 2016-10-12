c67faa294d48ed27ed9e5a527b3ff7bbec01d55e
Vikash <vikashgupta2000@gmail.com>
2016-10-12 12:42:30 +0530

    Revert "Sourav | #2557 | Renaming the config of reports and adding button to reports page"
    This reverts commit 4396f92a92da68219b403eebd372e7e50c7edcc5.

--- ui/app/reports/controllers/dashboardHeaderController.js
+++ ui/app/reports/controllers/dashboardHeaderController.js
@@ -6,7 +6,7 @@ angular.module('bahmni.reports')
 
             var getBackLinks = function () {
                 var backLinks = [{label: "Home", url: "../home/", accessKey: "h", icon: "fa-home"}]
-                if(appService.getAppDescriptor().getConfigValue("enableReportQueue")) {
+                if(appService.getAppDescriptor().getConfigValue("scheduledReports")) {
                     backLinks.push({text: "REPORTS_HEADER_REPORTS", state: "dashboard.reports", accessKey: "d"});
                     backLinks.push({text: "REPORTS_HEADER_MY_REPORTS", state: "dashboard.myReports", accessKey: "m"});
                 }
