fc977e3b796fe689401b8be94bd9820ef6a8819a
Venkata Jaswanth <aj.jaswanth@gmail.com>
2016-10-13 12:31:08 +0530

    Jaswanth | #2603 | Fix false check for numeric observations

--- ui/app/registration/controllers/visitController.js
+++ ui/app/registration/controllers/visitController.js
@@ -167,6 +167,9 @@ angular.module('bahmni.registration')
             };
             var isValid = function (mandatoryConcepts){
                 var concept = mandatoryConcepts.filter(function(mandatoryConcept){
+                    if (mandatoryConcept.isNumeric() && mandatoryConcept.value === 0) {
+                        return false;
+                    }
                     return !mandatoryConcept.value;
                 });
                 return _.isEmpty(concept);
