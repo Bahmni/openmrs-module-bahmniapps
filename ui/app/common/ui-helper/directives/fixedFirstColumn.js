7d0afab9585c5ca22d0fef8164fd7e7e21698ba1
SwathiVarkala <swathivarkala@gmail.com>
2016-10-20 17:12:41 +0530

    Bijayini, Swathi | #2540 | Fixing the Patient Summay section table fixed column height.
    Conflicts:
    	ui/app/common/ui-helper/directives/fixedFirstColumn.js

--- ui/app/common/ui-helper/directives/fixedFirstColumn.js
+++ ui/app/common/ui-helper/directives/fixedFirstColumn.js
@@ -7,42 +7,42 @@ angular.module('bahmni.common.uiHelper')
         template: "<div class='table-responsive'><div ng-transclude  class='table-responsive-fixedColumn' ></div></div>",
         transclude: true,
         link: function ($scope, $element) {
-            var interval = setTimeout(function () {
-                var tr = $element.find("tr");
+        var checkIfTableLoaded = setInterval(function() {
+                if($element.find("table").length > 0) {
+                    var tr = $element.find("tr");
+                    angular.forEach(tr, function (i) {
+                        var columns = angular.element(i).children();
 
-                angular.forEach(tr, function (i) {
-                    var columns = angular.element(i).children();
+                        if (columns.length < 1) {
+                            // Row with no columns? Ignore it.
+                            return;
+                        }
+                        var column0 = columns[0];
+                        var column1 = columns[1];
 
-                    if (columns.length < 1) {
-                        // Row with no columns? Ignore it.
-                        return;
-                    }
+                        // Calculate heights of each <td>.
+                        var height0 = (column0).offsetHeight;
+                        var height1 = column1 ? column1.offsetHeight : 0;
 
-                    var column0 = columns[0];
-                    var column1 = columns[1];
+                        // Calculate final height.
+                        var height = Math.max(height0, height1);
 
-                    // Calculate heights of each <td>.
-                    var height0 = (column0).offsetHeight;
-                    var height1 = column1 ? column1.offsetHeight : 0;
+                        // Set heights of <td> and <tr>.
+                        columns[0].style.height = height + "px";
+                        i.style.height = height + "px";
 
-                    // Calculate final height.
-                    var height = Math.max(height0, height1);
+                        if (column1) {
+                            column1.style.height = height + "px";
+                        }
 
-                    // Set heights of <td> and <tr>.
-                    columns[0].style.height = height + "px";
-                    i.style.height = height + "px";
-
-                    if (column1) {
-                        column1.style.height = height + "px";
-                    }
-
-                    // If <td> heights have stabilized.
-                    if (height0 !== 0 && height0 === height1) {
-                        clearInterval(interval);
-                    }
-                });
-            }, 5000);
-
+                        // If <td> heights have stabilized.
+                        if (height0 !== 0 && height0 === height1) {
+                            clearInterval(interval);
+                        }
+                    });
+                    clearInterval(checkIfTableLoaded);
+                }
+            }, 100);
         }
     };
 }]);
