3b7be5407c97332e7471f63f7c0787021ca04945
SwathiVarkala <swathivarkala@gmail.com>
2016-11-03 14:49:53 +0530

    Swathi | #2672 | Fixed the issue with scrolling in popup on dashboard
    Conflicts:
    	ui/app/common/ui-helper/directives/focusOnInputErrors.js

--- ui/app/common/ui-helper/directives/focusOnInputErrors.js
+++ ui/app/common/ui-helper/directives/focusOnInputErrors.js
@@ -5,15 +5,15 @@ angular.module('bahmni.common.uiHelper')
         return function (scope, elem) {
             var cleanUpListenerErrorsOnForm = scope.$on("event:errorsOnForm", function () {
                 var isTopElement = true;
-                $timeout(function () {
-                    $("*", elem).each(function () {
-                        if ($(this).hasClass('illegalValue') && isTopElement) {
-                            $(this).focus();
-                            var scrollPosition = $(this).offset().top - window.innerHeight / 2;
-                            $(window).scrollTop(scrollPosition);
-                            isTopElement = false;
-                        }
-                    });
+                $timeout(function() {
+                    if($('.concept-set-container').length > 0 ) {
+                        var scrollPosition = $('.illegalValue:first').offset().top + $('.illegalValue:first').innerHeight() - $('.concept-set-container').offset().top - $('.concept-set-container').innerHeight()/2;
+                        $('.concept-set-container').scrollTop(scrollPosition);
+                    }
+                    else {
+                        var scrollPosition = $('.illegalValue:first').offset().top - window.innerHeight/2;
+                        $(window).scrollTop(scrollPosition);
+                    }
                 }, 10, false);
                 if (isTopElement) {
                     $(window).scrollTop(0);
