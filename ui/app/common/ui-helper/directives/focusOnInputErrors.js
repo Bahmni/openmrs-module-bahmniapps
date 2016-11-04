83b333f4bb7a9bbf6a64d7bf866b3b96d9f2a495
SwathiVarkala <swathivarkala@gmail.com>
2016-11-04 12:00:49 +0530

    Swathi | #2672 | Fixing the issue with FocusOnInputErrors

--- ui/app/common/ui-helper/directives/focusOnInputErrors.js
+++ ui/app/common/ui-helper/directives/focusOnInputErrors.js
@@ -1,23 +1,11 @@
 'use strict';
 
 angular.module('bahmni.common.uiHelper')
-    .directive('focusOnInputErrors', function ($timeout) {
-        return function (scope, elem) {
+    .directive('focusOnInputErrors', function () {
+        return function (scope) {
             var cleanUpListenerErrorsOnForm = scope.$on("event:errorsOnForm", function () {
-                var isTopElement = true;
-                $timeout(function () {
-                    $("*", elem).each(function () {
-                        if ($(this).hasClass('illegalValue') && isTopElement) {
-                            $(this).focus();
-                            var scrollPosition = $(this).offset().top - window.innerHeight / 2;
-                            $(window).scrollTop(scrollPosition);
-                            isTopElement = false;
-                        }
-                    });
-                }, 10, false);
-                if (isTopElement) {
-                    $(window).scrollTop(0);
-                }
+                $('.illegalValue:first button').focus();
+                $('.illegalValue:first').focus();
             });
 
             scope.$on("$destroy", cleanUpListenerErrorsOnForm);
