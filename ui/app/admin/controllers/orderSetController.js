6bebf3ac32dfa3bd80ab1806e33a90b24ed47098
shashikanthgadgay <shashikanthgadgay@gmail.com>
2016-11-14 12:34:36 +0530

    Shashi, Jaswanth | #0 | Fix eslint issues

--- ui/app/admin/controllers/orderSetController.js
+++ ui/app/admin/controllers/orderSetController.js
@@ -136,7 +136,7 @@
                 };
 
                 var validationSuccess = function () {
-                    if(!validateForm()){
+                    if (!validateForm()) {
                         return false;
                     }
 
@@ -154,10 +154,10 @@
                     };
                 };
 
-                var validateForm = function() {
+                var validateForm = function () {
                     var requiredFields = angular.element($("[required]"));
                     for (var i = 0; i < requiredFields.length; i++) {
-                        if (!requiredFields[i].disabled && !requiredFields[i].value ) {
+                        if (!requiredFields[i].disabled && !requiredFields[i].value) {
                             messagingService.showMessage('error', 'Please fill all mandatory fields');
                             return false;
                         }
