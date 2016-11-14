7775f2e70bcf09535aad20e93b103059d662da67
shashikanthgadgay <shashikanthgadgay@gmail.com>
2016-11-14 12:34:36 +0530

    Shashi, Jaswanth | #0 | Fix eslint issues

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -95,7 +95,7 @@ module.exports = function (grunt) {
         },
         eslint: {
             options: {
-                fix: true,
+                fix: false,
                 quiet: true
             },
             target: [
