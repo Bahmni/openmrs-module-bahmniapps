90647929060a1eb2feea9b55452820503d4eb03f
gauravd <gauravd@thoughtworks.com>
2016-11-24 16:50:06 +0530

    Gaurav| #2843 | added manping for server exception message , this exception message has change in openmrs-core

--- ui/app/common/constants.js
+++ ui/app/common/constants.js
@@ -23,6 +23,10 @@ Bahmni.Common = Bahmni.Common || {};
         {
             serverMessage: "Cannot have more than one active order for the same orderable and care setting at same time",
             clientMessage: "One or more drugs you are trying to order are already active. Please change the start date of the conflicting drug or remove them from the new prescription."
+        },
+        {
+            serverMessage: "[Order.cannot.have.more.than.one]",
+            clientMessage: "One or more drugs you are trying to order are already active. Please change the start date of the conflicting drug or remove them from the new prescription."
         }
     ];
 
