7884407e327d8141f57046ebfb8c343df81a000c
Venkata Jaswanth <aj.jaswanth@gmail.com>
2016-12-29 17:00:34 +0530

    Jaswanth, Shashi | #2770 | Set form version in form JSON

--- ui/app/clinical/consultation/directives/formControls.js
+++ ui/app/clinical/consultation/directives/formControls.js
@@ -15,6 +15,7 @@ angular.module('bahmni.common.conceptSet')
                             var formDetailsAsString = _.get(response, 'data.resources[0].valueReference');
                             if (formDetailsAsString) {
                                 var formDetails = JSON.parse(formDetailsAsString);
+                                formDetails.version = $scope.form.formVersion;
                                 loadedFormDetails[formUuid] = formDetails;
                                 $scope.form.component = renderWithControls(formDetails, formObservations, formUuid);
                             }
