dcfd2290a3cde8a00c3a91a64f04cf16e3e3ae46
shireesha <bongaralashireesha@gmail.com>
2017-01-20 14:39:48 +0530

    Revert "Shireesha, Shruthi | #2923 | Added Medication controller for new medication tab"
    This reverts commit 606e2a3d97e42123b20e419d97b8a2d7b813767b.

--- ui/app/clinical/consultation/controllers/medicationController.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('bahmni.clinical')
-    .controller('MedicationController', ['$scope', 'treatmentConfig', function ($scope, treatmentConfig) {
-        $scope.props = {
-            isDropDown: treatmentConfig.isDropDown,
-            drugConceptSet: treatmentConfig.drugConceptSet
-        };
-    }]);
