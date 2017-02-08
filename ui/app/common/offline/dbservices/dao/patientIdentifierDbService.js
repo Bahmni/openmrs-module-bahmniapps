902f4b3cfa7f9982f877bd2df81f9e520dcf6e9f
sumanmaity112 <suman.maity112@gmail.com>
2017-02-08 16:51:32 +0530

    Suman | #3256 | Fix initial sync fails, when secondary identifier is not unique

--- ui/app/common/offline/dbservices/dao/patientIdentifierDbService.js
+++ ui/app/common/offline/dbservices/dao/patientIdentifierDbService.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('bahmni.common.offline')
-    .service('patientIdentifierDbService', function () {
+    .service('patientIdentifierDbService', ['$q', function ($q) {
         var getPatientIdentifiersByPatientUuid = function (db, patientUuid) {
             var pi = db.getSchema().table('patient_identifier');
             return db.select(pi.identifierJson)
@@ -12,30 +12,55 @@ angular.module('bahmni.common.offline')
                 });
         };
 
+        var insertQueries = function (identifier, patientIdentifierTable, patientUuid, queries, db) {
+            var identifierTypeUuid = identifier.identifierType && identifier.identifierType.uuid || identifier.identifierType;
+            var isPrimaryIdentifier = identifier.identifierType && identifier.identifierType.primary;
+            var row = patientIdentifierTable.createRow({
+                'typeUuid': identifierTypeUuid,
+                'identifier': identifier.identifier || null,
+                'primaryIdentifier': identifier.primaryIdentifier,
+                'extraIdentifiers': identifier.extraIdentifiers,
+                'primary': isPrimaryIdentifier,
+                'patientUuid': patientUuid,
+                'identifierJson': identifier
+            });
+            queries.push(db.insertOrReplace().into(patientIdentifierTable).values([row]));
+            return queries;
+        };
+
         var insertPatientIdentifiers = function (db, patientUuid, identifiers) {
             var patientIdentifierTable = db.getSchema().table('patient_identifier');
-            var queries = [];
+            var defer = $q.defer();
+            var promises = [], queries = [];
             _.each(identifiers, function (identifier) {
-                var identifierTypeUuid = identifier.identifierType && identifier.identifierType.uuid || identifier.identifierType;
-                var isPrimaryIdentifier = identifier.identifierType && identifier.identifierType.primary;
-                var row = patientIdentifierTable.createRow({
-                    'typeUuid': identifierTypeUuid,
-                    'identifier': identifier.identifier || null,
-                    'primaryIdentifier': identifier.primaryIdentifier,
-                    'extraIdentifiers': identifier.extraIdentifiers,
-                    'primary': isPrimaryIdentifier,
-                    'patientUuid': patientUuid,
-                    'identifierJson': identifier
-                });
-                queries.push(db.insertOrReplace().into(patientIdentifierTable).values([row]));
+                if (!identifier.primaryIdentifier) {
+                    queries = insertQueries(identifier, patientIdentifierTable, patientUuid, queries, db);
+                }
+                else {
+                    var promise = db.select(patientIdentifierTable.primaryIdentifier)
+                        .from(patientIdentifierTable)
+                        .where(lf.op.and(patientIdentifierTable.primaryIdentifier.eq(identifier.primaryIdentifier), patientIdentifierTable.patientUuid.neq(patientUuid))).exec()
+                        .then(function (results) {
+                            if (results.length > 0) {
+                                return defer.reject({code: 201});
+                            } else {
+                                queries = insertQueries(identifier, patientIdentifierTable, patientUuid, queries, db);
+                            }
+                        });
+                    promises.push(promise);
+                }
             });
-
-            var tx = db.createTransaction();
-            return tx.exec(queries);
+            $q.all(promises).then(function () {
+                var tx = db.createTransaction();
+                return tx.exec(queries).then(function () {
+                    return defer.resolve();
+                });
+            });
+            return defer.promise;
         };
 
         return {
             insertPatientIdentifiers: insertPatientIdentifiers,
             getPatientIdentifiersByPatientUuid: getPatientIdentifiersByPatientUuid
         };
-    });
+    }]);
