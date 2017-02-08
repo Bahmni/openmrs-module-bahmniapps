d1736809b475472530c2adbadbbae627be5bb5c0
sumanmaity112 <suman.maity112@gmail.com>
2017-02-08 12:33:40 +0530

    Suman | #3217 | Fix app does not redirect to login page when you open the app in offline mode and initial sync already done

--- ui/app/offline/app.js
+++ ui/app/offline/app.js
@@ -13,7 +13,22 @@ angular.module('bahmni.offline', ['ui.router', 'httpErrorInterceptor', 'bahmni.c
                         offlineDb: function (offlineDbInitialization) {
                             return offlineDbInitialization();
                         },
-                        offlineReferenceDataInitialization: function (offlineReferenceDataInitialization, offlineDbService, offlineService, androidDbService, $state, offlineDb) {
+                        offlineConfigInitialization: function (offlineConfigInitialization, offlineDb, offlineService, offlineDbService, androidDbService, $q) {
+                            var checkConfig = function () {
+                                var allowMultipleLoginLocation = offlineService.getItem("allowMultipleLoginLocation");
+                                return allowMultipleLoginLocation !== null && !allowMultipleLoginLocation;
+                            };
+                            if (offlineService.isAndroidApp()) {
+                                offlineDbService = androidDbService;
+                            }
+                            return offlineDbService.getConfig("dbNameCondition").then(function (result) {
+                                if (result || checkConfig()) {
+                                    return $q.when();
+                                }
+                                else return offlineConfigInitialization();
+                            });
+                        },
+                        offlineReferenceDataInitialization: function (offlineReferenceDataInitialization, offlineDbService, offlineService, androidDbService, $state, offlineConfigInitialization) {
                             if (offlineService.isAndroidApp()) {
                                 offlineDbService = androidDbService;
                             }
