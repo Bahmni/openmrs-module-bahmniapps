54ede96562f39550ff9109c90c2c1a6c958521c9
Krishna Kumar Ravi <kravi@thoughtworks.com>
2017-02-14 15:26:42 +0530

    Krishna | #0000 | Fixing the camel-case class names

--- ui/app/ipd/initialization.js
+++ ui/app/ipd/initialization.js
@@ -45,11 +45,11 @@ angular.module('bahmni.ipd').factory('initialization', ['$rootScope', '$q', '$ba
             });
         };
         window.onscroll = function () {
-            var element = document.getElementsByClassName('bedLegends');
+            var element = document.getElementsByClassName('bed-legends');
             if (window.scrollY >= 140) {
-                element[0].className = 'bedLegends scrolled';
+                element[0].className = 'bed-legends scrolled';
             }else {
-                element[0].className = 'bedLegends';
+                element[0].className = 'bed-legends';
             }
         };
         return spinner.forPromise(authenticator.authenticateUser().then(initApp).then(getConfigs));
