ce5b37d26d7e3b4993e451d0e462cb350af9a33f
Santhosh Bairam <bairamsa@thoughtworks.com>
2017-02-22 16:58:23 +0530

    add disabled while building the observation

--- ui/app/common/concept-set/mappers/observationMapper.js
+++ ui/app/common/concept-set/mappers/observationMapper.js
@@ -147,6 +147,7 @@ Bahmni.ConceptSet.ObservationMapper = function () {
 
     function buildObservation (concept, savedObs, mappedGroupMembers) {
         var comment = savedObs ? savedObs.comment : null;
+        var disabled = savedObs ? savedObs.disabled : null;
         return {
             concept: conceptMapper.map(concept),
             units: concept.units,
@@ -154,6 +155,7 @@ Bahmni.ConceptSet.ObservationMapper = function () {
             possibleAnswers: concept.answers,
             groupMembers: mappedGroupMembers,
             comment: comment,
+            disabled: disabled,
             showAddMoreButton: showAddMoreButton
         };
     }
