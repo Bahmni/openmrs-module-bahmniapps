d9f3bfa90037424dc827c140a192b931c3d2153a
yaeli <yaeli@thoughtworks.com>
2017-03-06 17:11:47 +0800

    Yae & Sicong | #3255 | Can show form name from form created by form builder within multiple observations in the patient visit page

--- ui/app/common/displaycontrols/observation/helpers/constructFunctions.js
+++ ui/app/common/displaycontrols/observation/helpers/constructFunctions.js
@@ -4,19 +4,32 @@
 Bahmni.Common.DisplayControl.Observation.ConstructFunctions = function () {
     var self = this;
 
-    self.createDummyObsGroupForObservationsForForm = function(observations) {
-        var dummyObsGroup = {
-            "groupMembers": [],
-            "concept": {
-                "shortName": "Sickling Test"
-            }
-        };
+    self.createDummyObsGroupForObservationsForForm = function (observations) {
+        _.forEach(observations, function (obs) {
+            var newValues = []
+            _.forEach(obs.value, function(value){
+                if(!value.formFieldPath){
+                    newValues.push(value);
+                    return;
+                }
 
-        dummyObsGroup.concept.shortName = observations[0].value[0].formFieldPath.split('.')[0];
-        dummyObsGroup.groupMembers.push(observations[0].value[0]);
+                var dummyObsGroup = {
+                    "groupMembers": [],
+                    "concept": {
+                        "shortName": ""
+                    }
+                };
 
-        observations[0].value[0] =  dummyObsGroup;
-        return observations;
+                dummyObsGroup.concept.shortName = value.formFieldPath.split('.')[0];
+                dummyObsGroup.groupMembers.push(value);
+
+                newValues.push(dummyObsGroup);
+            });
+
+            obs.value = newValues;
+        });
+
+        return observations
     };
 
     return self;
--- ui/test/unit/common/displaycontrols/observation/helpers/constructFunction.spec.js
+++ ui/test/unit/common/displaycontrols/observation/helpers/constructFunction.spec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 ddescribe("Construct Functions", function () {
-    it("should construct dummy obs group for form", function () {
+    it("should construct dummy obs group for single observation from form", function () {
         var observations = [{
             "key": "1488782460000",
             "value": [{
@@ -33,6 +33,50 @@ ddescribe("Construct Functions", function () {
         expect(dummyObsGroup[0].value[0].concept.shortName).toBe("myForm");
         expect(dummyObsGroup[0].value[0].groupMembers.length).toBe(1);
         expect(dummyObsGroup[0].value[0].groupMembers[0].concept.shortName).toBe("Sickling Test");
+    });
+
+    it('should construct dummy obs group for single observation from form within multiple observations', function () {
+        var observations = [{
+            "value": [{
+                "targetObsRelation": null,
+                "groupMembers": [{
+                    "formNamespace": null,
+                    "formFieldPath": null,
+                    "concept": {
+                        "name": "SPO2 Data",
+                        "shortName": "SPO2",
+                    },
+                    "valueAsString": "100.0",
+                    "conceptNameToDisplay": "SPO2",
+                }],
+                "formNamespace": null,
+                "formFieldPath": null,
+                "concept": {
+                    "name": "Vitals",
+                    "shortName": "Vitals",
+                },
+            }, {
+                "groupMembers": [],
+                "formNamespace": "Bahmni",
+                "formFieldPath": "test.1/1-0",
+                "concept": {
+                    "name": "HEAD Nose lateral",
+                    "shortName": "head nose lateral",
+                }
+            }]
+        }]
+
+        var dummyObsGroup = new Bahmni.Common.DisplayControl.Observation.ConstructFunctions().createDummyObsGroupForObservationsForForm(observations);
+
+        expect(dummyObsGroup[0].value[0].concept.shortName).toBe('Vitals');
+        expect(dummyObsGroup[0].value[0].groupMembers.length).toBe(1);
+        expect(dummyObsGroup[0].value[0].groupMembers[0].concept.shortName).toBe("SPO2");
+
+        expect(dummyObsGroup[0].value[1].concept.shortName).toBe('test');
+        expect(dummyObsGroup[0].value[1].groupMembers.length).toBe(1);
+        expect(dummyObsGroup[0].value[1].groupMembers[0].concept.shortName).toBe("head nose lateral");
+
+
     })
 
 });
