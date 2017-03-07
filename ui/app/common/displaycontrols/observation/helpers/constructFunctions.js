41e70de97c5787a4dfb185e0f3d8d05a7a0998f7
yaeli <yaeli@thoughtworks.com>
2017-03-07 10:53:35 +0800

    Yae & Sicong | #3255 | Can show multiple obs and obsGroup in same form

--- ui/app/common/displaycontrols/observation/helpers/constructFunctions.js
+++ ui/app/common/displaycontrols/observation/helpers/constructFunctions.js
@@ -1,14 +1,13 @@
 'use strict';
 
-
 Bahmni.Common.DisplayControl.Observation.ConstructFunctions = function () {
     var self = this;
 
     self.createDummyObsGroupForObservationsForForm = function (observations) {
         _.forEach(observations, function (obs) {
-            var newValues = []
-            _.forEach(obs.value, function(value){
-                if(!value.formFieldPath){
+            var newValues = [];
+            _.forEach(obs.value, function (value) {
+                if (!value.formFieldPath) {
                     newValues.push(value);
                     return;
                 }
@@ -21,15 +20,26 @@ Bahmni.Common.DisplayControl.Observation.ConstructFunctions = function () {
                 };
 
                 dummyObsGroup.concept.shortName = value.formFieldPath.split('.')[0];
-                dummyObsGroup.groupMembers.push(value);
 
+                var flag;
+                _.forEach(newValues, function (newValue) {
+                    flag = (dummyObsGroup.concept.shortName == newValue.concept.shortName);
+                    if (flag) {
+                        newValue.groupMembers.push(value);
+                        return;
+                    }
+                });
+                if (flag) {
+                    return;
+                }
+                dummyObsGroup.groupMembers.push(value);
                 newValues.push(dummyObsGroup);
             });
 
             obs.value = newValues;
         });
 
-        return observations
+        return observations;
     };
 
     return self;
