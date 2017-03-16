ed09b22ab711d3326d0fe7c0a36b957f06b25cb1
yaeli <yaeli@thoughtworks.com>
2017-03-16 15:36:07 +0800

    Yae & Sicong | #3340 | Can show section name with obs group

--- ui/app/common/displaycontrols/observation/helpers/constructSectionIntoFormFunctions.js
+++ ui/app/common/displaycontrols/observation/helpers/constructSectionIntoFormFunctions.js
@@ -3,10 +3,26 @@
 Bahmni.Common.DisplayControl.Observation.ConstructSectionIntoFormFunctions = function () {
     var self = this;
 
-    self.getMemberFromValueByFormFieldPath = function (members, id) {
+    self.getFormVersion = function (members) {
+        var formVersion;
+        _.forEach(members, function (member) {
+            if(member.formFieldPath){
+               formVersion = member.formFieldPath.split('.')[1].split('/')[0];
+               return false;
+            }
+        })
+        return formVersion;
+    }
+
+    self.getMemberFromFormByFormFieldPath = function (members, id) {
         return _.find(members, function (member) {
             return member.formFieldPath.split('.')[1].split('/')[1].split('-')[0] == id;
         });
+    };
+    self.getFormByFormName = function (formList, formName, formVersion) {
+        return _.find(formList,function (form) {
+            return form.name == formName && form.version == formVersion;
+        })
     }
 
     self.parseSection = function (members, controls, value) {
@@ -23,19 +39,19 @@ Bahmni.Common.DisplayControl.Observation.ConstructSectionIntoFormFunctions = fun
                 dummyObsGroup.concept.shortName = control.label.value;
                 value.groupMembers.push(dummyObsGroup);
                 sectionIsEmpty = false;
-                if (!self.parseSection(members, control.controls, dummyObsGroup)) {
+                if (!self.parseSection(members,control.controls, dummyObsGroup)){
                     value.groupMembers.pop();
                 }
             } else {
-                var member = self.getMemberFromValueByFormFieldPath(members, control.id)
-                if (member) {
+                var member = self.getMemberFromFormByFormFieldPath(members, control.id)
+                if(member){
                     value.groupMembers.push(member);
                     sectionIsEmpty = false;
                     return;
                 }
             }
         });
-        if (sectionIsEmpty) {
+        if (sectionIsEmpty){
             return null;
         }
         return value;
@@ -44,30 +60,32 @@ Bahmni.Common.DisplayControl.Observation.ConstructSectionIntoFormFunctions = fun
     self.createSectionForSingleForm = function (obsFromSameForm, formDetails) {
         var members = obsFromSameForm.groupMembers.slice();
         obsFromSameForm.groupMembers.splice(0, obsFromSameForm.groupMembers.length);
+
         return self.parseSection(members, formDetails.controls, obsFromSameForm);
     };
 
-    self.createDummyObsGroupForSectionsForForm = function (bahmniObservations, observationFormService) {
+    self.createDummyObsGroupForSectionsForForm = function (bahmniObservations, observationFormService){
         _.forEach(bahmniObservations, function (observation) {
             var forms = [];
             _.forEach(observation.value, function (form) {
+
                 if (form.concept.conceptClass) {
                     forms.push(form);
                     return;
                 }
-
-                observationFormService.getFormDetail("", {
-                    v: "custom:(resources:(value))",
-                    q: form.concept.shortName
+                observationFormService.getAllForms().then(function (response) {
+                    return observationFormService.getFormDetail(self.getFormByFormName(response.data.results,form.concept.shortName,self.getFormVersion(form.groupMembers)).uuid, {
+                        v: "custom:(resources:(value))"
+                    })
                 }).then(function (response) {
-                    var formDetailsAsString = _.get(response, 'data.results[0].resources[0].value');
+                        var formDetailsAsString = _.get(response, 'data.resources[0].value');
+                        if (formDetailsAsString) {
+                            var formDetails = JSON.parse(formDetailsAsString);
+                            forms.push(self.createSectionForSingleForm(form, formDetails));
+                        }
+                        observation.value = forms;
+                    })
 
-                    if (formDetailsAsString) {
-                        var formDetails = JSON.parse(formDetailsAsString);
-                        forms.push(self.createSectionForSingleForm(form, formDetails));
-                    }
-                    observation.value = forms;
-                })
             });
         });
     };
