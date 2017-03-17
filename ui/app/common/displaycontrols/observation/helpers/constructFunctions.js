b972316fce5dd03e77737c091a2a598fb227333a
yaeli <yaeli@thoughtworks.com>
2017-03-17 15:08:23 +0800

    Yae & Sicong | #3340 | Refactor to show form name & show section name functions to formHierarchyService

--- ui/app/common/displaycontrols/observation/helpers/constructFunctions.js
+++ ui/app/common/displaycontrols/observation/helpers/constructFunctions.js
@@ -15,47 +15,5 @@ Bahmni.Common.DisplayControl.Observation.ConstructFunctions = function () {
         return observations;
     };
 
-    self.createDummyObsGroupForObservationsForForm = function (observations) {
-        _.forEach(observations, function (obs) {
-            var newValues = [];
-            _.forEach(obs.value, function (value) {
-                if (!value.formFieldPath) {
-                    newValues.push(value);
-                    return;
-                }
-
-                var dummyObsGroup = {
-                    "groupMembers": [],
-                    "concept": {
-                        "shortName": "",
-                        "conceptClass": null
-                    },
-                    "encounterUuid": ""
-                };
-
-                dummyObsGroup.concept.shortName = value.formFieldPath.split('.')[0];
-                dummyObsGroup.encounterUuid = value.encounterUuid;
-                var previousDummyObsGroupFound;
-                _.forEach(newValues, function (newValue) {
-                    if (dummyObsGroup.concept.shortName == newValue.concept.shortName) {
-                        newValue.groupMembers.push(value);
-                        previousDummyObsGroupFound = true;
-                    }
-                });
-
-                if (previousDummyObsGroupFound) {
-                    return;
-                }
-
-                dummyObsGroup.groupMembers.push(value);
-                newValues.push(dummyObsGroup);
-            });
-
-            obs.value = newValues;
-        });
-
-        return observations;
-    };
-
     return self;
 };
