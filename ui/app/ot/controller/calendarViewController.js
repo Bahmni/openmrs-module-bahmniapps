d3127f3206505b19c5504c87b91634aff9e44727
pramidatumma <pramidat@thoughtworks.com>
2017-05-31 16:54:34 +0530

    Shireesha | #413 | Ffixing eslint errors

--- ui/app/ot/controller/calendarViewController.js
+++ ui/app/ot/controller/calendarViewController.js
@@ -8,24 +8,21 @@ angular.module('bahmni.ot')
                 options['dashboardCachebuster'] = Math.random();
                 $state.go("newSurgicalAppointment", options);
             };
-            $scope.viewDate = $state.viewDate ||(moment().startOf('day')).toDate();
+            $scope.viewDate = $state.viewDate || (moment().startOf('day')).toDate();
             $state.viewDate = $scope.viewDate;
             $scope.calendarConfig = appService.getAppDescriptor().getConfigValue("calendarView");
             $scope.goToPreviousDate = function (date) {
                 $scope.viewDate = Bahmni.Common.Util.DateUtil.subtractDays(date, 1);
                 $state.viewDate = $scope.viewDate;
-
             };
 
             $scope.goToCurrentDate = function () {
                 $scope.viewDate = new Date(moment().startOf('day'));
                 $state.viewDate = $scope.viewDate;
-
             };
 
             $scope.goToNextDate = function (date) {
                 $scope.viewDate = Bahmni.Common.Util.DateUtil.addDays(date, 1);
                 $state.viewDate = $scope.viewDate;
-
             };
         }]);
