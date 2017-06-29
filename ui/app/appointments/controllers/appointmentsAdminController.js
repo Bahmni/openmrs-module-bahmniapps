b42f6427eac908c70fc623f512b1b18b36ae1cfc
Deepak Sharma <deepaucksharma@gmail.com>
2017-06-29 11:14:44 +0530

    Deepak | #3641 | Add appointments test and redirection.

--- ui/app/appointments/controllers/appointmentsAdminController.js
+++ ui/app/appointments/controllers/appointmentsAdminController.js
@@ -1,8 +1,16 @@
 'use strict';
 
 angular.module('bahmni.appointments')
-    .controller('AppointmentsAdminController', ['$scope', '$translate',
-        function ($scope, $translate) {
+    .controller('AppointmentsAdminController', ['$scope', '$translate', '$location',
+        function ($scope, $translate, $location) {
+            $scope.openServiceView = function (uuid) {
+                if (!uuid) {
+                    uuid = "new";
+                }
+                var url = "/home/service/" + uuid;
+                $location.url(url);
+            };
+
             var init = function () {
             };
             return init();
