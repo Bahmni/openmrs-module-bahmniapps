91f2582634e6932e6fed83f12186cd9f6b5f68e4
pushpa446 <pdumpala@thoughtworks.com>
2017-07-14 19:09:12 +0530

    Shruthi, Pushpa | #3650 | define service availability for service

---
+++ ui/app/appointments/models/appointmentServiceViewModel.js
@@ -0,0 +1,32 @@
+'use strict';
+
+Bahmni.Appointments.AppointmentServiceViewModel = (function () {
+    var Service = function (serviceDetails) {
+        angular.extend(this, serviceDetails);
+    };
+
+    Service.createFromResponse = function (serviceDetails) {
+        var getDateTime = function (time) {
+            return time ? new Date('1970-01-01 ' + time) : undefined;
+        };
+
+        var parse = function (availabilities) {
+            return undefined;
+        };
+        var service = new Service({
+            name: serviceDetails.name,
+            description: serviceDetails.description,
+            durationMins: serviceDetails.durationMins,
+            maxAppointmentsLimit: serviceDetails.maxAppointmentsLimit,
+            startTime: getDateTime(serviceDetails.startTime),
+            endTime: getDateTime(serviceDetails.endTime),
+            specialityUuid: serviceDetails.specialityUuid,
+            locationUuid: serviceDetails.locationUuid,
+            weeklyAvailability: parse(serviceDetails.weeklyAvailability)
+        });
+        return service;
+    };
+
+    return Service;
+})();
+
