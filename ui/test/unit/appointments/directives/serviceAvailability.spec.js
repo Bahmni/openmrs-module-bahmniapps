ee61d10006c7f0e4b1309a33457378e134494a4a
pushpa446 <pdumpala@thoughtworks.com>
2017-07-19 14:42:45 +0530

    Shruthi, Pushpa | #3650 | Fix issue with weekstartsindex in daySelector

--- ui/test/unit/appointments/directives/serviceAvailability.spec.js
+++ ui/test/unit/appointments/directives/serviceAvailability.spec.js
@@ -1,9 +1,16 @@
 'use strict';
 
 describe('ServiceAvailability', function () {
-    var compile, scope, httpBackend;
+    var compile, scope, httpBackend, appService, appDescriptor;
 
-    beforeEach(module('bahmni.appointments'));
+    beforeEach(module('bahmni.appointments', function ($provide) {
+        appService = jasmine.createSpyObj('appService', ['getAppDescriptor']);
+        appDescriptor = jasmine.createSpyObj('appDescriptor', ['getConfigValue']);
+        appService.getAppDescriptor.and.returnValue(appDescriptor);
+        appDescriptor.getConfigValue.and.returnValue(1);
+
+        $provide.value('appService', appService);
+    }));
 
     beforeEach(inject(function ($compile, $httpBackend, $rootScope) {
         compile = $compile;
@@ -15,7 +22,7 @@ describe('ServiceAvailability', function () {
     }));
 
     var createElement = function () {
-        var html = '<service-availability availability="availability" availability-list="availabilityList" state="state"></service-availability>';
+        var html = '<service-availability availability="availability" availability-list="availabilityList" state="state" on-add-availability="toggleAdded()"></service-availability>';
 
         var element = compile(angular.element(html))(scope);
         scope.$digest();
@@ -23,23 +30,110 @@ describe('ServiceAvailability', function () {
         return element;
     };
 
-    it('should init index of the availability', function () {
-        scope.state = 2;
-        scope.availability = {
-            startTime: new Date().toString(),
-            endTime: new Date().toString(),
-            days: [{name: 'MONDAY', isSelected: true}]
-        };
+    it('should init availability and startOfWeek', function () {
         scope.availabilityList = [];
-        scope.availabilityList.push(scope.availability);
-
+        expect(scope.availability).toBeUndefined();
+        appDescriptor.getConfigValue.and.returnValue(2);
         var element = createElement();
         var compiledElementScope = element.isolateScope();
 
-        expect(compiledElementScope.index).toBe(0);
+        expect(compiledElementScope.startOfWeek).toBe(2);
+        expect(scope.availability).toEqual({});
     });
 
-    iit('should add availability to weeklyAvailability list', function () {
+    describe('checkState', function () {
+        it('isNew should return true if state is 0', function () {
+            scope.state = 0;
+            var element = createElement();
+            var compiledElementScope = element.isolateScope();
+            expect(compiledElementScope.isNew()).toBeTruthy();
+            expect(compiledElementScope.isReadOnly()).toBeFalsy();
+            expect(compiledElementScope.isEdit()).toBeFalsy();
+        });
+
+        it('isEdit should return true if state is 1', function () {
+            scope.state = 1;
+            var element = createElement();
+            var compiledElementScope = element.isolateScope();
+            expect(compiledElementScope.isEdit()).toBeTruthy();
+            expect(compiledElementScope.isNew()).toBeFalsy();
+            expect(compiledElementScope.isReadOnly()).toBeFalsy();
+        });
+
+        it('isReadOnly should return true if state is 2', function () {
+            scope.state = 2;
+            var element = createElement();
+            var compiledElementScope = element.isolateScope();
+            expect(compiledElementScope.isReadOnly()).toBeTruthy();
+            expect(compiledElementScope.isNew()).toBeFalsy();
+            expect(compiledElementScope.isEdit()).toBeFalsy();
+        });
+    });
+
+    describe('validateAvailability', function () {
+        it('should return true if all fields are valid', function () {
+            scope.availability = {
+                startTime: new Date().toString(),
+                endTime: new Date().toString(),
+                days: [{name: 'MONDAY', isSelected: true}]
+            };
+            scope.availabilityList = [];
+            var element = createElement();
+            var compiledElementScope = element.isolateScope();
+
+            expect(compiledElementScope.isValid()).toBeTruthy();
+        });
+
+        it('should return false if all fields are empty', function () {
+            scope.availabilityList = [];
+            var element = createElement();
+            var compiledElementScope = element.isolateScope();
+
+            expect(scope.availability).toEqual({});
+            expect(compiledElementScope.isValid()).toBeFalsy();
+        });
+
+        it('should return false if startTime is not filled', function () {
+            scope.availability = {
+                startTime: undefined,
+                endTime: new Date().toString(),
+                days: [{name: 'MONDAY', isSelected: true}]
+            };
+            scope.availabilityList = [];
+            var element = createElement();
+            var compiledElementScope = element.isolateScope();
+
+            expect(compiledElementScope.isValid()).toBeFalsy();
+        });
+
+        it('should return false if startTime is not filled', function () {
+            scope.availability = {
+                startTime: new Date().toString(),
+                endTime: undefined,
+                days: [{name: 'MONDAY', isSelected: true}]
+            };
+            scope.availabilityList = [];
+            var element = createElement();
+            var compiledElementScope = element.isolateScope();
+
+            expect(compiledElementScope.isValid()).toBeFalsy();
+        });
+
+        it('should return false if not even one day is selected', function () {
+            scope.availability = {
+                startTime: new Date().toString(),
+                endTime: new Date().toString(),
+                days: [{name: 'MONDAY', isSelected: false}]
+            };
+            scope.availabilityList = [];
+            var element = createElement();
+            var compiledElementScope = element.isolateScope();
+
+            expect(compiledElementScope.isValid()).toBeFalsy();
+        });
+    });
+
+    it('should add availability to weeklyAvailability list', function () {
         scope.availability = {
             startTime: new Date().toString(),
             endTime: new Date().toString(),
@@ -48,14 +142,25 @@ describe('ServiceAvailability', function () {
         scope.availabilityList = [];
         var element = createElement();
         var compiledElementScope = element.isolateScope();
+
         expect(compiledElementScope.availabilityList.length).toBe(0);
         compiledElementScope.add();
         expect(compiledElementScope.availabilityList.length).toBe(1);
         expect(compiledElementScope.availabilityList[0]).toEqual(scope.availability);
-        expect(scope.availability).toBe({});
+        expect(compiledElementScope.availability).toEqual({});
     });
 
-    iit('should delete availability from weeklyAvailability list', function () {
+    it('should invoke function assigned to on-add-availability on add', function () {
+        scope.toggleAdded = jasmine.createSpy('toggleAdded');
+        scope.availabilityList = [];
+        var element = createElement();
+        var compiledElementScope = element.isolateScope();
+
+        compiledElementScope.add();
+        expect(scope.toggleAdded).toHaveBeenCalled();
+    });
+
+    it('should delete availability from weeklyAvailability list', function () {
         var availability1 = {
             startTime: new Date().toString(),
             endTime: new Date().toString(),
@@ -66,20 +171,87 @@ describe('ServiceAvailability', function () {
             endTime: new Date().toString(),
             days: [{name: 'TUESDAY', isSelected: true}]
         };
-
         scope.availability = availability1;
         scope.availabilityList = [];
         scope.availabilityList.push(scope.availability);
         scope.availabilityList.push(availability2);
-
         var element = createElement();
         var compiledElementScope = element.isolateScope();
+
         expect(compiledElementScope.availabilityList.length).toBe(2);
-        expect(compiledElementScope.availabilityList[0]).toEqual(scope.availability);
+        expect(compiledElementScope.availabilityList[0]).toEqual(availability1);
         expect(compiledElementScope.availabilityList[1]).toEqual(availability2);
         compiledElementScope.delete();
         expect(compiledElementScope.availabilityList.length).toBe(1);
         expect(compiledElementScope.availabilityList[0]).toEqual(availability2);
+    });
+
+    it('should change state to edit and take backup availability on enableEdit', function () {
+        scope.state = 2;
+        scope.availability = {
+            startTime: new Date().toString(),
+            endTime: new Date().toString(),
+            days: [{name: 'MONDAY', isSelected: true}]
+        };
+        scope.availabilityList = [scope.availability];
+
+        var element = createElement();
+        var compiledElementScope = element.isolateScope();
+
+        expect(compiledElementScope.state).toBe(2);
+        compiledElementScope.enableEdit();
+        expect(compiledElementScope.backUpAvailability).toBe(scope.availability);
+        expect(compiledElementScope.availability).not.toBe(scope.availability);
+        expect(compiledElementScope.availability).toEqual(scope.availability);
+        expect(compiledElementScope.state).toBe(1);
+    });
+
+    it('should change state to readonly and restore availability on cancel', function () {
+        scope.state = 1;
+        scope.availability = {
+            startTime: new Date().toString(),
+            endTime: new Date().toString(),
+            days: [{name: 'MONDAY', isSelected: true}]
+        };
+        scope.availabilityList = [scope.availability];
+
+        var element = createElement();
+        var compiledElementScope = element.isolateScope();
+
+        expect(compiledElementScope.state).toBe(1);
+        compiledElementScope.cancel();
+        expect(compiledElementScope.availability).toBe(compiledElementScope.backUpAvailability);
+        expect(compiledElementScope.state).toBe(2);
+    });
+
+    it('should update availability on weeklyAvailability list and set its state to read only', function () {
+        var availability1 = {
+            startTime: new Date().toString(),
+            endTime: new Date().toString(),
+            days: [{name: 'MONDAY', isSelected: true}]
+        };
+        var availability2 = {
+            startTime: new Date().toString(),
+            endTime: new Date().toString(),
+            days: [{name: 'TUESDAY', isSelected: true}]
+        };
+        scope.availability = availability1;
+        scope.availabilityList = [availability1, availability2];
+        var element = createElement();
+        var compiledElementScope = element.isolateScope();
+
+        expect(compiledElementScope.availabilityList.length).toBe(2);
+        expect(compiledElementScope.availabilityList[0]).toEqual(availability1);
+        expect(compiledElementScope.availabilityList[1]).toEqual(availability2);
+        compiledElementScope.backUpAvailability = availability1;
+        compiledElementScope.availability = {
+            days: [{name: 'SUNDAY', isSelected: true}]
+        };
+        compiledElementScope.update();
+        expect(compiledElementScope.availabilityList.length).toBe(2);
+        expect(compiledElementScope.availabilityList[0]).toEqual(compiledElementScope.availability);
+        expect(compiledElementScope.availabilityList[1]).toEqual(availability2);
+        expect(compiledElementScope.state).toBe(2);
     });
 });
 
--- ui/test/unit/appointments/models/appointmentService.spec.js
+++ ui/test/unit/appointments/models/appointmentService.spec.js
@@ -49,4 +49,44 @@ describe('AppointmentService', function () {
         expect(appointmentService.weeklyAvailability[0]).toEqual({dayOfWeek: 'MONDAY', startTime: startTime, endTime: endTime});
         expect(appointmentService.weeklyAvailability[1]).toEqual({dayOfWeek: 'TUESDAY', startTime: startTime, endTime: endTime});
     });
+
+    it('should construct weekly availability by day for multiple availabilities', function () {
+        var startDateTime = new Date().toString();
+        var endDateTime = new Date().toString();
+
+        var startDateTime2 = new Date("2015-10-01T18:30:00.000Z").toString();
+        var endDateTime2 = new Date("2015-10-01T18:30:00.000Z").toString();
+        var service = {
+            name: 'Chemotherapy',
+            description: 'For cancer',
+            weeklyAvailability: [
+                {
+                    startTime: startDateTime,
+                    endTime: endDateTime,
+                    days: [{name: 'MONDAY', isSelected: true},
+                           {name: 'TUESDAY', isSelected: true},
+                           {name: 'SUNDAY', isSelected: false}]
+                },
+                {
+                    startTime: startDateTime2,
+                    endTime: endDateTime2,
+                    days: [{name: 'MONDAY', isSelected: true},
+                           {name: 'TUESDAY', isSelected: true},
+                           {name: 'SATURDAY', isSelected: true}]
+                }
+            ]
+        };
+        var appointmentService = Bahmni.Appointments.AppointmentService.createFromUIObject(service);
+
+        expect(appointmentService.weeklyAvailability.length).toBe(5);
+        var startTime = dateUtil.getDateTimeInSpecifiedFormat(startDateTime, timeFormat);
+        var endTime = dateUtil.getDateTimeInSpecifiedFormat(endDateTime, timeFormat);
+        var startTime2 = dateUtil.getDateTimeInSpecifiedFormat(startDateTime2, timeFormat);
+        var endTime2 = dateUtil.getDateTimeInSpecifiedFormat(endDateTime2, timeFormat);
+        expect(appointmentService.weeklyAvailability).toEqual([{dayOfWeek: 'MONDAY', startTime: startTime, endTime: endTime},
+        {dayOfWeek: 'TUESDAY', startTime: startTime, endTime: endTime},
+        {dayOfWeek: 'MONDAY', startTime: startTime2, endTime: endTime2},
+        {dayOfWeek: 'TUESDAY', startTime: startTime2, endTime: endTime2},
+        {dayOfWeek: 'SATURDAY', startTime: startTime2, endTime: endTime2}]);
+    });
 });
