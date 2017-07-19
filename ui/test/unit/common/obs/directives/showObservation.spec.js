daac396db8d6e0c5bedf8150ea8199ecb44ee594
shireesha <bongaralashireesha@gmail.com>
2017-07-19 12:20:35 +0530

    Shireesha, KK | #504 | Showing videos on dashboard on pop up

--- ui/test/unit/common/obs/directives/showObservation.spec.js
+++ ui/test/unit/common/obs/directives/showObservation.spec.js
@@ -2,7 +2,7 @@
 
 describe("ensure that the directive show-observation works properly", function () {
 
-    var scope, rootScope, filter, httpBackend, compile, q;
+    var scope, rootScope, filter, httpBackend, compile, q, ngDialog;
     var html = '<show-observation  show-details-button="showDetailsButton" patient="patient" observation="observation" show-date="showDate" show-time="showTime"></show-observation>';
 
     var obsDate = new Date();
@@ -21,7 +21,9 @@ describe("ensure that the directive show-observation works properly", function (
 
     beforeEach(module(function($provide) {
         var conceptSetService = jasmine.createSpyObj('conceptSetService', ['getConceptSetMembers']);
+        ngDialog = jasmine.createSpyObj('ngDialog', ['open']);
         $provide.value('conceptSetService', conceptSetService);
+        $provide.value('ngDialog', ngDialog);
     }));
 
     beforeEach(inject(function($filter, $compile, $httpBackend, $rootScope, $q){
@@ -102,5 +104,36 @@ describe("ensure that the directive show-observation works properly", function (
 
         expect(compiledScope).not.toBeUndefined();
         expect(compiledScope.dateString(observation)).toBe(moment(obsDate).format("DD MMM YY h:mm a"));
+    });
+
+    it("should open video in a new dialog", function () {
+        scope = rootScope.$new();
+        scope.showDetailsButton = true;
+        scope.patient = patient;
+        scope.observation = observation;
+        scope.showDate = true;
+        scope.showTime = true;
+
+
+        httpBackend.expectGET("../common/obs/views/showObservation.html").respond("<div>dummy</div>");
+
+        var compiledEle = compile(html)(scope);
+
+        scope.$digest();
+        httpBackend.flush();
+
+        var compiledScope = compiledEle.isolateScope();
+        scope.$digest();
+
+        compiledScope.openVideoInPopup(scope.observation);
+        expect(ngDialog.open).toHaveBeenCalledWith(
+            {
+                template: '../common/obs/views/showVideo.html',
+                closeByDocument: false,
+                className: 'ngdialog-theme-default',
+                showClose: true,
+                data: {observation: scope.observation}
+            }
+        )
     });
 });
