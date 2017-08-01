741e22f4a83d3e1d18266e48f1fbec1199e8be27
Santhosh Bairam <bairamsa@thoughtworks.com>
2017-08-01 12:25:15 +0530

    Santhosh, Maharjun, Pramida | #3690 | Added cancel button, closing ngDialog on error and internationlized labels

--- ui/test/unit/appointments/controllers/deleteAppointmentServiceController.spec.js
+++ ui/test/unit/appointments/controllers/deleteAppointmentServiceController.spec.js
@@ -1,17 +1,19 @@
 'use strict';
 
 describe("deleteAppointmentServiceController", function () {
-    var controller, scope, ngDialog, appointmentsServiceService, messagingService, state;
+    var controller, scope, ngDialog, appointmentsServiceService, messagingService, state, mockPromise;
 
     beforeEach(function () {
         module('bahmni.appointments');
         inject(function($controller, $rootScope) {
             scope = $rootScope.$new();
             controller = $controller;
-        })});
+        });
+
+        appointmentsServiceService = jasmine.createSpyObj('appointmentsServiceService', ['deleteAppointmentService']);
+    });
 
     ngDialog = jasmine.createSpyObj('ngDialog', ['close']);
-    appointmentsServiceService = jasmine.createSpyObj('appointmentsServiceService', ['deleteAppointmentService']);
     messagingService = jasmine.createSpyObj('messagingService', ['showMessage']);
     state = jasmine.createSpyObj('$state', ['reload']);
 
@@ -25,6 +27,28 @@ describe("deleteAppointmentServiceController", function () {
         });
     };
 
+    it('should close the dialog when appointment service throws an error', function () {
+        mockPromise = {
+            then: function (successFn, errorFn) {
+                errorFn();
+            }
+        };
+        appointmentsServiceService = {
+            deleteAppointmentService: function () {
+                return mockPromise;
+            }
+        };
+        var service = {name: "service name", uuid: "serviceUuid"};
+        scope.ngDialogData = {service: service};
+        createController();
+
+        scope.deleteServiceConfirmation(service);
+
+        expect(messagingService.showMessage).not.toHaveBeenCalled();
+        expect(ngDialog.close).toHaveBeenCalled();
+        expect(state.reload).not.toHaveBeenCalled();
+    });
+
     it('should delete the appointment service on confirmation', function () {
         appointmentsServiceService.deleteAppointmentService.and.returnValue(specUtil.simplePromise({"data": "service"}));
         var service = {name: "service name", uuid: "serviceUuid"};
@@ -37,4 +61,13 @@ describe("deleteAppointmentServiceController", function () {
         expect(state.reload).toHaveBeenCalled();
     });
 
+    it('should close the dialog on cancel', function () {
+        var service = {name: "service name", uuid: "serviceUuid"};
+        scope.ngDialogData = {service: service};
+        createController();
+
+        scope.cancelDeleteService();
+
+        expect(ngDialog.close).toHaveBeenCalled();
+    });
 });
