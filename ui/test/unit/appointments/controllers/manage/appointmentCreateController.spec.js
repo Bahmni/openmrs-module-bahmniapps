2f001a8957d772cc72921096d493d55aac928ba7
pramidatumma <pramidat@thoughtworks.com>
2017-08-08 17:10:08 +0530

    Shireesha, Pramida|#3661|Removing dependency from startimes[] and depending on  allowedStartTime and allowedEndTime

--- ui/test/unit/appointments/controllers/manage/appointmentCreateController.spec.js
+++ ui/test/unit/appointments/controllers/manage/appointmentCreateController.spec.js
@@ -114,43 +114,42 @@ describe("AppointmentsCreateController", function () {
     });
 
     describe('availabilityValidations', function () {
-        it('should not set the warning message when start time is inside the service availability', function () {
+        it('should not set the warning message when start time is same as the start time of service availability', function () {
             createController();
-            $scope.startTimes = ["09:00 am", "09:30 am"];
+            $scope.allowedStartTime = "09:00 am";
+            $scope.allowedEndTime = "10:00 am";
             var data = {value: "09:00 am"};
-            $scope.warning.startTime = true;
-            $scope.onSelectStartTime(data);
-            expect($scope.warning.startTime).toBeFalsy();
-            var data = {value: "09:11 am"};
             $scope.warning.startTime = true;
             $scope.onSelectStartTime(data);
             expect($scope.warning.startTime).toBeFalsy();
         });
 
-        it('should not set the warning message when start time is after the last start time slot and before the service end time', function () {
+        it('should not set the warning message when start time is before the service end time', function () {
             createController();
-            $scope.startTimes = ["09:00 am", "09:30 am"];
-            $scope.minDuration = 30;
+            $scope.allowedStartTime = "09:00 am";
+            $scope.allowedEndTime = "10:00 am";
             var data = {value: "09:41 am"};
             $scope.onSelectStartTime(data);
             expect($scope.warning.startTime).toBeFalsy();
         });
 
-        it('should set the warning message when start time is outside the service available start time', function () {
+        it('should set the warning message when start time is outside the service available time', function () {
             createController();
-            $scope.startTimes = ["09:00 am", "09:30 am"];
+            $scope.allowedStartTime = "09:00 am";
+            $scope.allowedEndTime = "10:00 am";
             var data = {value: "08:00 am"};
             $scope.onSelectStartTime(data);
             expect($scope.warning.startTime).toBeTruthy();
-            data.value = "10:00 am";
+            data.value = "11:00 am";
             $scope.onSelectStartTime(data);
             expect($scope.warning.startTime).toBeTruthy();
         });
 
         it('should not set warning on start time when the service availability is not defined for a given day', function () {
             createController();
-            $scope.startTimes = [];
             var data = undefined;
+            $scope.allowedStartTime = undefined;
+            $scope.allowedEndTime = undefined;
             $scope.appointment.startTime = "06:00 am";
             $scope.onSelectStartTime(data);
             expect($scope.warning.startTime).toBeFalsy();
@@ -159,7 +158,8 @@ describe("AppointmentsCreateController", function () {
         it('should reset the warning on start time to false when start time is undefined', function () {
             createController();
             $scope.warning.startTime = true;
-            $scope.startTimes = ["09:00 am", "09:30 am"];
+            $scope.allowedStartTime = "09:00 am";
+            $scope.allowedEndTime = "10:00 am";
             var data = {value: "09:00 am"};
             $scope.onSelectStartTime();
             expect($scope.warning.startTime).toBeFalsy();
@@ -167,7 +167,8 @@ describe("AppointmentsCreateController", function () {
 
         it('should not set the warning message when end time is inside the service availability', function () {
             createController();
-            $scope.startTimes = ["09:00 am", "09:30 am"];
+            $scope.allowedStartTime = "09:00 am";
+            $scope.allowedEndTime = "10:00 am";
             var data = {value: "09:00 am"};
             $scope.warning.endTime = true;
             $scope.onSelectEndTime(data);
@@ -176,8 +177,8 @@ describe("AppointmentsCreateController", function () {
 
         it('should set the warning on end time when it is outside the service availability', function () {
             createController();
-            $scope.startTimes = ["09:00 am", "09:30 am"];
-            $scope.minDuration = 15;
+            $scope.allowedStartTime = "09:00 am";
+            $scope.allowedEndTime = "09:45 am";
             $scope.appointment.endTime = "10:00 am";
             $scope.onSelectEndTime();
             expect($scope.warning.endTime).toBeTruthy();
@@ -197,7 +198,7 @@ describe("AppointmentsCreateController", function () {
         });
 
 
-        it('should not set warning on a day when the service availability is not defined for a given day', function () {
+        it('should not set warning on a day and set default start and end times when the service weekly availability is not defined for a given day', function () {
             createController();
             $scope.appointment.date = moment().toDate();
             $scope.selectedService = {
@@ -205,12 +206,12 @@ describe("AppointmentsCreateController", function () {
                 startTime: undefined,
                 endTime: undefined
             };
-            $scope.minDuration = 30;
             $scope.OnDateChange();
             expect($scope.warning.appointmentDate).toBeFalsy();
+            expect($scope.allowedStartTime).toEqual('08:00 am');
         });
 
-        it('should set warning on a day when the service availability is not defined for a given day', function () {
+        it('should set warning on a day and allowedStartTime and allowedEndTime to undefined when the service availability is not defined for a given day', function () {
             createController();
             $scope.appointment.date = moment('2017-08-04').toDate();
             $scope.selectedService = {
@@ -218,25 +219,25 @@ describe("AppointmentsCreateController", function () {
                 startTime: undefined,
                 endTime: undefined
             };
-            $scope.minDuration = 30;
-            $scope.warning.startTime = true;
-            $scope.warning.endTime = true;
             $scope.OnDateChange();
             expect($scope.warning.appointmentDate).toBeTruthy();
+            expect($scope.allowedStartTime).toBeUndefined();
+            expect($scope.allowedEndTime).toBeUndefined();
         });
 
-        it('should remove warning on a day when the service availability is defined for a given day', function () {
+        it('should remove warning on a day and set allowedStartTime and allowedEndTime when the service availability is defined for a given day', function () {
             createController();
             $scope.selectedService = {
                 weeklyAvailability: [{dayOfWeek: "MONDAY", startTime: "09:00:00", endTime: "12:00:00"}],
                 startTime: undefined,
                 endTime: undefined
             };
-            $scope.minDuration = 30;
             $scope.warning.appointmentDate = true;
             $scope.appointment.date = moment('2017-08-07').toDate();
             $scope.OnDateChange();
             expect($scope.warning.appointmentDate).toBeFalsy();
+            expect($scope.allowedStartTime).toEqual('09:00:00');
+            expect($scope.allowedEndTime).toEqual('12:00:00');
         });
 
         it('should calculate warning messages for start time and end time when appointment date is changed', function () {
@@ -246,7 +247,6 @@ describe("AppointmentsCreateController", function () {
                 startTime: undefined,
                 endTime: undefined
             };
-            $scope.minDuration = 30;
             $scope.warning.appointmentDate = false;
             $scope.warning.startTime = true;
             $scope.warning.endTime = true;
@@ -264,7 +264,22 @@ describe("AppointmentsCreateController", function () {
             expect($scope.warning.endTime).toBeTruthy();
         });
 
-        it('should reset availibilty warnings when a service is selected', function () {
+        it('should validate and set allowedStartTime and allowedEndTime ' +
+            'when service weeklyAvailability is not defined but service start time and end time are defined', function () {
+            createController();
+            $scope.selectedService = {
+                weeklyAvailability: [],
+                startTime: '09:00 am',
+                endTime: '11:00 am'
+            };
+            $scope.appointment.date = moment().toDate();
+            $scope.OnDateChange();
+            expect($scope.warning.appointmentDate).toBeFalsy();
+            expect($scope.allowedStartTime).toEqual('09:00 am');
+            expect($scope.allowedEndTime).toEqual('11:00 am');
+        });
+
+        it('should reset availibility warnings when a service is selected', function () {
             createController();
             $scope.warning.appointmentDate = true;
             $scope.warning.startTime = true;
