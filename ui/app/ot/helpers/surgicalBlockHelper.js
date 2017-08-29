2a8cb4321934cfa59cbebecfd0775876481f754c
pramidatumma <pramidat@thoughtworks.com>
2017-08-29 11:47:08 +0530

    Santhosh, Pramida|#586|Changing the logic such that appointments with status POSTPONED and CANCELLED are not considered while calculating the available block duration

--- ui/app/ot/helpers/surgicalBlockHelper.js
+++ ui/app/ot/helpers/surgicalBlockHelper.js
@@ -4,7 +4,9 @@ angular.module('bahmni.ot')
     .service('surgicalBlockHelper', ['surgicalAppointmentHelper', function (surgicalAppointmentHelper) {
         this.getAvailableBlockDuration = function (surgicalBlock) {
             var blockDuration = Bahmni.Common.Util.DateUtil.diffInMinutes(surgicalBlock.startDatetime, surgicalBlock.endDatetime);
-            var appointmentsDuration = _.sumBy(_.reject(surgicalBlock.surgicalAppointments, ['sortWeight', null]), function (surgicalAppointment) {
+            var appointmentsDuration = _.sumBy(_.reject(surgicalBlock.surgicalAppointments, function (appointment) {
+                return (appointment.status == "POSTPONED" || appointment.status == "CANCELLED");
+            }), function (surgicalAppointment) {
                 return surgicalAppointmentHelper.getAppointmentDuration(surgicalAppointment.surgicalAppointmentAttributes.estTimeHours.value, surgicalAppointment.surgicalAppointmentAttributes.estTimeMinutes.value, surgicalAppointment.surgicalAppointmentAttributes.cleaningTime.value);
             });
             return blockDuration - appointmentsDuration;
