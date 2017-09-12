0a7ee0ea72823c139a1305503ad40251fb59c46c
Camilo Rivera <camilo@riverapineda.com>
2017-09-12 14:01:29 -0600

    test with firefox

--- ui/test/config/karma.conf.js
+++ ui/test/config/karma.conf.js
@@ -2,7 +2,7 @@ module.exports = function (config) {
     config.set({
         basePath: '../..',
         frameworks: ['jasmine'],
-        browsers: ['PhantomJS'],
+        browsers: ['Firefox'],
         browserNoActivityTimeout: 100000,
         autoWatch: false,
         singleRun: true,
