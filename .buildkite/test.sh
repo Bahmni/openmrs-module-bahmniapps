0ab246363dcebd3759fa9837a6c3b9ff5ea8b2de
Camilo Rivera <camilo@riverapineda.com>
2017-09-12 13:59:20 -0600

    fix test replace

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -8,5 +8,5 @@ cd ui/
 echo "--- Install Dependencies" | tee -a bk-pipeline.log
 npm install | tee -a bk-pipeline.log
 echo "--- Test" | tee -a bk-pipeline.log
-sed -i -e 's/"test": "karma start/"e2e": "xvfb-run -a karma start/' package.json | tee -a bk-pipeline.log
+sed -i -e 's/"test": "karma start/"test": "xvfb-run -a karma start/' package.json | tee -a bk-pipeline.log
 npm run test | tee -a bk-pipeline.log
