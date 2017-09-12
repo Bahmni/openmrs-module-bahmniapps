96983044eacdf4416106b2bac5c0402ef84a7c9f
Eliseo García <eliseo@acklenavenue.com>
2017-09-12 10:33:12 -0600

    Adding sudo to run tests

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -8,4 +8,4 @@ cd ui/
 echo "--- Install Dependencies" | tee -a bk-pipeline.log
 npm install | tee -a bk-pipeline.log
 echo "--- Test" | tee -a bk-pipeline.log
-grunt test | tee -a bk-pipeline.log
+sudo grunt test | tee -a bk-pipeline.log
