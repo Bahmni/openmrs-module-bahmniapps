be0af61bb02f0edb7b90e856938421c12df2189c
Maharjun <maharjun123@gmail.com>
2018-05-17 05:11:45 +0530

    BAH-529 | Sending date without time for fixing program enrollement issue (#160)

--- ui/app/common/domain/mappers/patientProgramMapper.js
+++ ui/app/common/domain/mappers/patientProgramMapper.js
@@ -4,7 +4,7 @@ Bahmni.Common.Domain.PatientProgramMapper = function () {
     this.map = function (patientProgram, programAttributeTypes, dateCompleted) {
         var attributeFormatter = new Bahmni.Common.Domain.AttributeFormatter();
         return {
-            dateEnrolled: moment(patientProgram.dateEnrolled).format(Bahmni.Common.Constants.ServerDateTimeFormat),
+            dateEnrolled: moment(Bahmni.Common.Util.DateUtil.getDateWithoutTime(patientProgram.dateEnrolled)).format(Bahmni.Common.Constants.ServerDateTimeFormat),
             states: patientProgram.states,
             uuid: patientProgram.uuid,
             dateCompleted: dateCompleted ? moment(dateCompleted).format(Bahmni.Common.Constants.ServerDateTimeFormat) : null,
