1f0c7b33b35785eeeddae54ec3f27b44d1a281f5
Saikumar <saikv@thoughtworks.com>
2018-05-21 15:13:04 +0530

    BAH-528 | [Fix] Add undefined checks for rootObservation and it's groupMembers

--- ui/app/common/concept-set/directives/conceptSet.js
+++ ui/app/common/concept-set/directives/conceptSet.js
@@ -238,6 +238,7 @@ angular.module('bahmni.common.conceptSet')
                 };
 
                 var runFormConditionForAllObsRecursively = function (formName, rootObservation) {
+                    rootObservation && rootObservation.groupMembers &&
                     rootObservation.groupMembers.forEach(function (observation) {
                         var conditionFn = Bahmni.ConceptSet.FormConditions.rules && Bahmni.ConceptSet.FormConditions.rules[observation.concept.name];
                         if (conditionFn != null) {
