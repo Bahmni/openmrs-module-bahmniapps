148e6769081a60400b64316d310b6d26633c8226
Krishnan Menon <krishnanm@satvix.com>
2018-05-24 11:03:32 +0530

    BAH-462: Made changes to appointmentCalendarViewController and now all the tests are passing.

--- ui/app/appointments/controllers/manage/calendar/appointmentsCalendarViewController.js
+++ ui/app/appointments/controllers/manage/calendar/appointmentsCalendarViewController.js
@@ -12,61 +12,63 @@ angular.module('bahmni.appointments')
             };
 
             var parseAppointments = function (allAppointments) {
-                var appointments = allAppointments.filter(function (appointment) {
-                    return appointment.status !== 'Cancelled';
-                });
-                var resources = _.chain(appointments)
-                    .filter(function (appointment) {
-                        return !_.isEmpty(appointment.provider);
-                    }).map(function (appointment) {
-                        return appointment.provider;
-                    }).uniqBy('name')
-                    .map(function (provider) {
-                        return {id: provider.name, title: provider.name, provider: provider};
-                    }).sortBy(function (provider) {
-                        return provider.id && provider.id.toLowerCase();
-                    })
-                    .value();
-
-                var hasAppointmentsWithNoProvidersSpecified = _.find(appointments, function (appointment) {
-                    return _.isEmpty(appointment.provider);
-                });
-
-                if (hasAppointmentsWithNoProvidersSpecified) {
-                    resources.push({
-                        id: $translate.instant("NO_PROVIDER_COLUMN_KEY"),
-                        title: $translate.instant("NO_PROVIDER_COLUMN_KEY"),
-                        provider: {name: $translate.instant("NO_PROVIDER_COLUMN_KEY"), display: $translate.instant("NO_PROVIDER_COLUMN_KEY"), uuid: 'no-provider-uuid'}
+                if (allAppointments) {
+                    var appointments = allAppointments.filter(function (appointment) {
+                        return appointment.status !== 'Cancelled';
                     });
-                }
+                    var resources = _.chain(appointments)
+                        .filter(function (appointment) {
+                            return !_.isEmpty(appointment.provider);
+                        }).map(function (appointment) {
+                            return appointment.provider;
+                        }).uniqBy('name')
+                        .map(function (provider) {
+                            return {id: provider.name, title: provider.name, provider: provider};
+                        }).sortBy(function (provider) {
+                            return provider.id && provider.id.toLowerCase();
+                        })
+                        .value();
 
-                var events = [];
-                appointments.reduce(function (result, appointment) {
-                    var event = {};
-                    event.resourceId = appointment.provider ? appointment.provider.name : $translate.instant("NO_PROVIDER_COLUMN_KEY");
-                    event.start = appointment.startDateTime;
-                    event.end = appointment.endDateTime;
-                    event.color = appointment.service.color;
-                    event.serviceName = appointment.service.name;
-                    var existingEvent = _.find(result, event);
-                    var patientName = appointment.patient.name + " (" + appointment.patient.identifier + ")";
-                    var isBedAssigned = appointment.additionalInfo && appointment.additionalInfo.BED_NUMBER_KEY;
-                    if (existingEvent) {
-                        existingEvent.title = [existingEvent.title, patientName].join(', ');
-                        existingEvent.className = 'appointmentIcons multiplePatients' + (isBedAssigned ? ' bed-accom' : '');
-                        existingEvent.appointments.push(appointment);
-                    } else {
-                        event.title = patientName;
-                        event.className = 'appointmentIcons ' + appointment.status + (isBedAssigned ? ' bed-accom' : '');
-                        event.appointments = [];
-                        event.appointments.push(appointment);
-                        result.push(event);
+                    var hasAppointmentsWithNoProvidersSpecified = _.find(appointments, function (appointment) {
+                        return _.isEmpty(appointment.provider);
+                    });
+
+                    if (hasAppointmentsWithNoProvidersSpecified) {
+                        resources.push({
+                            id: $translate.instant("NO_PROVIDER_COLUMN_KEY"),
+                            title: $translate.instant("NO_PROVIDER_COLUMN_KEY"),
+                            provider: {name: $translate.instant("NO_PROVIDER_COLUMN_KEY"), display: $translate.instant("NO_PROVIDER_COLUMN_KEY"), uuid: 'no-provider-uuid'}
+                        });
                     }
-                    return result;
-                }, events);
 
-                $scope.providerAppointments = {resources: resources, events: events};
-                $scope.shouldReload = true;
+                    var events = [];
+                    appointments.reduce(function (result, appointment) {
+                        var event = {};
+                        event.resourceId = appointment.provider ? appointment.provider.name : $translate.instant("NO_PROVIDER_COLUMN_KEY");
+                        event.start = appointment.startDateTime;
+                        event.end = appointment.endDateTime;
+                        event.color = appointment.service.color;
+                        event.serviceName = appointment.service.name;
+                        var existingEvent = _.find(result, event);
+                        var patientName = appointment.patient.name + " (" + appointment.patient.identifier + ")";
+                        var isBedAssigned = appointment.additionalInfo && appointment.additionalInfo.BED_NUMBER_KEY;
+                        if (existingEvent) {
+                            existingEvent.title = [existingEvent.title, patientName].join(', ');
+                            existingEvent.className = 'appointmentIcons multiplePatients' + (isBedAssigned ? ' bed-accom' : '');
+                            existingEvent.appointments.push(appointment);
+                        } else {
+                            event.title = patientName;
+                            event.className = 'appointmentIcons ' + appointment.status + (isBedAssigned ? ' bed-accom' : '');
+                            event.appointments = [];
+                            event.appointments.push(appointment);
+                            result.push(event);
+                        }
+                        return result;
+                    }, events);
+
+                    $scope.providerAppointments = {resources: resources, events: events};
+                    $scope.shouldReload = true;
+                }
             };
 
             $scope.$watch(function () {
@@ -74,7 +76,9 @@ angular.module('bahmni.appointments')
             }, function (newValue, oldValue) {
                 if (newValue !== oldValue) {
                     var filteredAppointments = appointmentsFilter($scope.allAppointmentsForDay || $state.params.appointmentsData, $state.params.filterParams);
-                    parseAppointments(filteredAppointments);
+                    if (filteredAppointments) {
+                        parseAppointments(filteredAppointments);
+                    }
                 }
             }, true);
 
