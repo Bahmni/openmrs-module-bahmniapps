f29c79595aa8beea8a43c4fdd6b19e2879f21891
Saikumar <saikv@thoughtworks.com>
2018-05-28 13:03:19 +0530

    BAH-510 | Reformat commit

--- ui/test/unit/clinical/patientcontrolpanel/directives/patientControlPanel.spec.js
+++ ui/test/unit/clinical/patientcontrolpanel/directives/patientControlPanel.spec.js
@@ -15,9 +15,9 @@ describe('patientControlPanelTest', function () {
         $bahmniCookieStore,
         simpleHtml = '<patient-control-panel patient="patient" visit-history="visitHistory" visit="visit" show="showControlPanel"/>';
 
-    beforeEach(module('bahmni.common.patient','bahmni.clinical','bahmni.common.appFramework','bahmni.common.util','bahmni.common.uiHelper'));
+    beforeEach(module('bahmni.common.patient', 'bahmni.clinical', 'bahmni.common.appFramework', 'bahmni.common.util', 'bahmni.common.uiHelper'));
 
-    beforeEach(module(function($provide){
+    beforeEach(module(function ($provide) {
         $translate = jasmine.createSpyObj('$translate', ['instant']);
         $translate.instant.and.callFake(function (value) {
             return value;
@@ -25,27 +25,31 @@ describe('patientControlPanelTest', function () {
         _provide = $provide;
 
         _configurations = {
-            encounterConfig: function(){return _configurations;},
-            getPatientDocumentEncounterTypeUuid: function(){return "patientDocumentEncounterTypeUuid";}
+            encounterConfig: function () {
+                return _configurations;
+            },
+            getPatientDocumentEncounterTypeUuid: function () {
+                return "patientDocumentEncounterTypeUuid";
+            }
         };
 
-        _encounterService = jasmine.createSpyObj('encounterService',['getEncountersForEncounterType','then']);
-        _encounterService.getEncountersForEncounterType.and.callFake(function(){
+        _encounterService = jasmine.createSpyObj('encounterService', ['getEncountersForEncounterType', 'then']);
+        _encounterService.getEncountersForEncounterType.and.callFake(function () {
             deferred = q.defer();
             deferred.resolve({data: {results: []}});
             return deferred.promise;
         });
 
-        $bahmniCookieStore = jasmine.createSpyObj('$bahmniCookieStore',['get']);
+        $bahmniCookieStore = jasmine.createSpyObj('$bahmniCookieStore', ['get']);
 
         _encounterService.then.and.returnValue({data: {results: []}});
-        $provide.value('configurations',_configurations);
-        $provide.value('encounterService',_encounterService);
+        $provide.value('configurations', _configurations);
+        $provide.value('encounterService', _encounterService);
         $provide.value('$bahmniCookieStore', $bahmniCookieStore);
         $provide.value('$translate', $translate);
     }));
 
-    beforeEach(inject(function ($compile, $httpBackend, $rootScope,$q) {
+    beforeEach(inject(function ($compile, $httpBackend, $rootScope, $q) {
         compile = $compile;
         mockBackend = $httpBackend;
         rootScope = $rootScope;
@@ -54,21 +58,21 @@ describe('patientControlPanelTest', function () {
 
     it('ensure links are correctly populated on patient consultation page', function () {
         stateParams = {visitUuid: "12345", configName: "default"};
-        state = {current: {name : "patient.consultation"}};
+        state = {current: {name: "patient.consultation"}};
 
-        _provide.value('$state',state);
+        _provide.value('$state', state);
         _provide.value('$stateParams', stateParams);
 
         var scope = rootScope.$new();
 
-        scope.visitHistory={
+        scope.visitHistory = {
             activeVisit: true
         };
         scope.section = {
-            numberOfVisits:1
+            numberOfVisits: 1
         };
         scope.patient = {uuid: "patientUuid"};
-        scope.visitUuid= "1234";
+        scope.visitUuid = "1234";
 
         mockBackend.expectGET('patientcontrolpanel/views/controlPanel.html').respond("<div>dummy</div>");
 
@@ -81,26 +85,30 @@ describe('patientControlPanelTest', function () {
         scope.$digest();
 
         expect(compiledElementScope.links).not.toBeUndefined();
-        expect(compiledElementScope.links).toEqual([{text: "CONTROL_PANEL_DASHBOARD_TEXT", icon: "btn-summary dashboard-btn", href: "#/default/patient/patientUuid/dashboard"}]);
+        expect(compiledElementScope.links).toEqual([{
+            text: "CONTROL_PANEL_DASHBOARD_TEXT",
+            icon: "btn-summary dashboard-btn",
+            href: "#/default/patient/patientUuid/dashboard"
+        }]);
     });
 
     it('ensure links are correctly populated on patient visit page and without an active visit', function () {
         stateParams = {visitUuid: "12345", configName: "default"};
-        state = {current: {name : "patient.visit"}};
+        state = {current: {name: "patient.visit"}};
 
-        _provide.value('$state',state);
+        _provide.value('$state', state);
         _provide.value('$stateParams', stateParams);
 
         var scope = rootScope.$new();
 
-        scope.visitHistory={
+        scope.visitHistory = {
             activeVisit: false
         };
         scope.section = {
-            numberOfVisits:1
+            numberOfVisits: 1
         };
         scope.patient = {uuid: "patientUuid"};
-        scope.visitUuid= "1234";
+        scope.visitUuid = "1234";
 
         mockBackend.expectGET('patientcontrolpanel/views/controlPanel.html').respond("<div>dummy</div>");
 
@@ -113,30 +121,34 @@ describe('patientControlPanelTest', function () {
         scope.$digest();
 
         expect(compiledElementScope.links).not.toBeUndefined();
-        expect(compiledElementScope.links).toEqual([{text: "CONTROL_PANEL_DASHBOARD_TEXT", icon: "btn-summary dashboard-btn", href: "#/default/patient/patientUuid/dashboard"}]);
+        expect(compiledElementScope.links).toEqual([{
+            text: "CONTROL_PANEL_DASHBOARD_TEXT",
+            icon: "btn-summary dashboard-btn",
+            href: "#/default/patient/patientUuid/dashboard"
+        }]);
     });
 
     it('ensure links are correctly populated on patient dashboard page without an active visit', function () {
         stateParams = {visitUuid: "12345"};
-        state = {current: {name : "patient.dashboard"}};
+        state = {current: {name: "patient.dashboard"}};
 
-        var _clinicalAppConfigService = jasmine.createSpyObj('clinicalAppConfigService',['getConsultationBoardLink']);
+        var _clinicalAppConfigService = jasmine.createSpyObj('clinicalAppConfigService', ['getConsultationBoardLink']);
         _clinicalAppConfigService.getConsultationBoardLink.and.returnValue("test");
 
-        _provide.value('$state',state);
+        _provide.value('$state', state);
         _provide.value('$stateParams', stateParams);
-        _provide.value('clinicalAppConfigService',_clinicalAppConfigService);
+        _provide.value('clinicalAppConfigService', _clinicalAppConfigService);
 
         var scope = rootScope.$new();
 
-        scope.visitHistory={
+        scope.visitHistory = {
             activeVisit: false
         };
         scope.section = {
-            numberOfVisits:1
+            numberOfVisits: 1
         };
         scope.patient = {uuid: "patientUuid"};
-        scope.visitUuid= "1234";
+        scope.visitUuid = "1234";
 
         mockBackend.expectGET('patientcontrolpanel/views/controlPanel.html').respond("<div>dummy</div>");
 
@@ -153,26 +165,26 @@ describe('patientControlPanelTest', function () {
 
     it('ensure links are correctly populated for an active visit', function () {
         stateParams = {visitUuid: "12345"};
-        state = {current: {name : "patient.dashboard"}};
+        state = {current: {name: "patient.dashboard"}};
 
-        var _clinicalAppConfigService = jasmine.createSpyObj('clinicalAppConfigService',['getConsultationBoardLink']);
+        var _clinicalAppConfigService = jasmine.createSpyObj('clinicalAppConfigService', ['getConsultationBoardLink']);
         _clinicalAppConfigService.getConsultationBoardLink.and.returnValue("test");
 
-        _provide.value('$state',state);
+        _provide.value('$state', state);
         _provide.value('$stateParams', stateParams);
-        _provide.value('clinicalAppConfigService',_clinicalAppConfigService);
+        _provide.value('clinicalAppConfigService', _clinicalAppConfigService);
 
 
         var scope = rootScope.$new();
 
-        scope.visitHistory={
+        scope.visitHistory = {
             activeVisit: true
         };
         scope.section = {
-            numberOfVisits:1
+            numberOfVisits: 1
         };
         scope.patient = {uuid: "patientUuid"};
-        scope.visitUuid= "1234";
+        scope.visitUuid = "1234";
 
         mockBackend.expectGET('patientcontrolpanel/views/controlPanel.html').respond("<div>dummy</div>");
 
@@ -185,29 +197,33 @@ describe('patientControlPanelTest', function () {
         scope.$digest();
 
         expect(compiledElementScope.links).not.toBeUndefined();
-        expect(compiledElementScope.links).toEqual([{text: "CONTROL_PANEL_CONSULTATION_TEXT", icon: "btn-consultation dashboard-btn", href: "#test"}]);
+        expect(compiledElementScope.links).toEqual([{
+            text: "CONTROL_PANEL_CONSULTATION_TEXT",
+            icon: "btn-consultation dashboard-btn",
+            href: "#test"
+        }]);
     });
 
-    it("isInEditEncounterMode() should return false for active encounter",function(){
+    it("isInEditEncounterMode() should return false for active encounter", function () {
         stateParams = {encounterUuid: "active"};
         mockBackend.expectGET('patientcontrolpanel/views/controlPanel.html').respond("<div>dummy</div>");
 
 
-        var _clinicalAppConfigService = jasmine.createSpyObj('clinicalAppConfigService',['getConsultationBoardLink']);
+        var _clinicalAppConfigService = jasmine.createSpyObj('clinicalAppConfigService', ['getConsultationBoardLink']);
         _clinicalAppConfigService.getConsultationBoardLink.and.returnValue("test");
 
-        _provide.value('$state',state);
+        _provide.value('$state', state);
         _provide.value('$stateParams', stateParams);
-        _provide.value('clinicalAppConfigService',_clinicalAppConfigService);
+        _provide.value('clinicalAppConfigService', _clinicalAppConfigService);
         var scope = rootScope.$new();
-        scope.visitHistory={
+        scope.visitHistory = {
             activeVisit: true
         };
         scope.section = {
-            numberOfVisits:1
+            numberOfVisits: 1
         };
         scope.patient = {uuid: "patientUuid"};
-        scope.visitUuid= "1234";
+        scope.visitUuid = "1234";
 
 
         var element = compile(simpleHtml)(scope);
@@ -222,26 +238,26 @@ describe('patientControlPanelTest', function () {
 
     });
 
-    it("isInEditEncounterMode() should return true on editing encounter",function(){
+    it("isInEditEncounterMode() should return true on editing encounter", function () {
         stateParams = {encounterUuid: "abcd-1234-efagksjk"};
         mockBackend.expectGET('patientcontrolpanel/views/controlPanel.html').respond("<div>dummy</div>");
 
 
-        var _clinicalAppConfigService = jasmine.createSpyObj('clinicalAppConfigService',['getConsultationBoardLink']);
+        var _clinicalAppConfigService = jasmine.createSpyObj('clinicalAppConfigService', ['getConsultationBoardLink']);
         _clinicalAppConfigService.getConsultationBoardLink.and.returnValue("test");
 
-        _provide.value('$state',state);
+        _provide.value('$state', state);
         _provide.value('$stateParams', stateParams);
-        _provide.value('clinicalAppConfigService',_clinicalAppConfigService);
+        _provide.value('clinicalAppConfigService', _clinicalAppConfigService);
         var scope = rootScope.$new();
-        scope.visitHistory={
+        scope.visitHistory = {
             activeVisit: true
         };
         scope.section = {
-            numberOfVisits:1
+            numberOfVisits: 1
         };
         scope.patient = {uuid: "patientUuid"};
-        scope.visitUuid= "1234";
+        scope.visitUuid = "1234";
 
 
         var element = compile(simpleHtml)(scope);
@@ -255,7 +271,7 @@ describe('patientControlPanelTest', function () {
         expect(compiledElementScope.isInEditEncounterMode()).toBeTruthy();
     });
 
-    it("goToDashBoard should return the dashboardUrl with given program stateParams", function() {
+    it("goToDashBoard should return the dashboardUrl with given program stateParams", function () {
         stateParams = {
             configName: "programs",
             patientUuid: "patientUuid",
@@ -264,19 +280,19 @@ describe('patientControlPanelTest', function () {
             enrollment: "patientProgramUuid",
             dateEnrolled: "dateEnrolled"
         };
-        state = {current: {name : "patient.dashboard"}};
+        state = {current: {name: "patient.dashboard"}};
         mockBackend.expectGET('patientcontrolpanel/views/controlPanel.html').respond("<div>dummy</div>");
-        var _clinicalAppConfigService = jasmine.createSpyObj('clinicalAppConfigService',['getConsultationBoardLink']);
+        var _clinicalAppConfigService = jasmine.createSpyObj('clinicalAppConfigService', ['getConsultationBoardLink']);
         _clinicalAppConfigService.getConsultationBoardLink.and.returnValue("test");
-        _provide.value('$state',state);
+        _provide.value('$state', state);
         _provide.value('$stateParams', stateParams);
-        _provide.value('clinicalAppConfigService',_clinicalAppConfigService);
+        _provide.value('clinicalAppConfigService', _clinicalAppConfigService);
         var scope = rootScope.$new();
-        scope.visitHistory={
+        scope.visitHistory = {
             activeVisit: true
         };
         scope.section = {
-            numberOfVisits:1
+            numberOfVisits: 1
         };
         scope.patient = {uuid: "patientUuid"};
         var element = compile(simpleHtml)(scope);
@@ -287,26 +303,26 @@ describe('patientControlPanelTest', function () {
         expect(compiledElementScope.getDashboardLink()).toEqual("#/programs/patient/patientUuid/dashboard?programUuid=programUuid&enrollment=patientProgramUuid&dateEnrolled=dateEnrolled");
     });
 
-    it("goToDashBoard() should return the dashboardUrl with given default stateParams ",function(){
+    it("goToDashBoard() should return the dashboardUrl with given default stateParams ", function () {
         stateParams = {
             configName: "default",
             patientUuid: "patientUuid",
             encounterUuid: "encounterUuid"
         };
-        state = {current: {name : "patient.dashboard"}};
+        state = {current: {name: "patient.dashboard"}};
         mockBackend.expectGET('patientcontrolpanel/views/controlPanel.html').respond("<div>dummy</div>");
-        var _clinicalAppConfigService = jasmine.createSpyObj('clinicalAppConfigService',['getConsultationBoardLink']);
+        var _clinicalAppConfigService = jasmine.createSpyObj('clinicalAppConfigService', ['getConsultationBoardLink']);
         _clinicalAppConfigService.getConsultationBoardLink.and.returnValue("test");
-        _provide.value('$state',state);
+        _provide.value('$state', state);
         _provide.value('$stateParams', stateParams);
-        _provide.value('clinicalAppConfigService',_clinicalAppConfigService);
+        _provide.value('clinicalAppConfigService', _clinicalAppConfigService);
 
         var scope = rootScope.$new();
-        scope.visitHistory={
+        scope.visitHistory = {
             activeVisit: true
         };
         scope.section = {
-            numberOfVisits:1
+            numberOfVisits: 1
         };
         scope.patient = {uuid: "patientUuid"};
         var element = compile(simpleHtml)(scope);
