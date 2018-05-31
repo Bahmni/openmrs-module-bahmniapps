48e5a75143d39d8102b44c357470a7f33f70b1c7
Pasayadaan <rjoshi@iplit.in>
2018-05-30 23:55:40 -0700

    BAH-415 | Mandatory Boolean Concept on Visit Page  (#163)
    * BAH-415 Mandatory Boolean Concept on Visit Page
    
    Added condition for Mandatory Boolean Concept on Visit Page as explained
    https://talk.openmrs.org/t/error-while-saving-visit-page-with-mandatory-boolean-concept/14967
    
    * BAH-415 Mandatory Boolean Concept on Visit Page
    
    Added condition for Mandatory Boolean Concept on Visit Page as explained
    https://talk.openmrs.org/t/error-while-saving-visit-page-with-mandatory-boolean-concept/14967
    
    * BAH-415 - Common condition for Mandatory concept
    
    Checking common condition for mandatory concepts other than multi select which needs special treatment

--- ui/app/registration/controllers/visitController.js
+++ ui/app/registration/controllers/visitController.js
@@ -229,7 +229,7 @@ angular.module('bahmni.registration')
             };
             var isValid = function (mandatoryConcepts) {
                 var concept = mandatoryConcepts.filter(function (mandatoryConcept) {
-                    if (mandatoryConcept.isNumeric() && mandatoryConcept.value === 0) {
+                    if (mandatoryConcept.hasValue()) {
                         return false;
                     }
                     if (mandatoryConcept instanceof Bahmni.ConceptSet.Observation &&
