292887f7ec29360d6f1caaaba50904bf5af19c20
Saikumar <saikv@thoughtworks.com>
2018-06-07 10:51:02 +0530

    Remove unnecessary library

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -36,7 +36,6 @@ module.exports = function (grunt) {
         'components/angular-recursion/angular-recursion.min.js',
         'components/ngInfiniteScroll/build/ng-infinite-scroll.min.js',
         'components/moment/min/moment-with-locales.min.js',
-        'components/moment/min/moment.min.js',
         'components/angular-drag-and-drop-lists/angular-drag-and-drop-lists.min.js',
         'components/select2/select2.min.js',
         'components/angular-ui-select2/src/select2.js',
