39de1ca7a38c89db2db44e1e937a47a156b573c7
krishnanspace <34261449+krishnanspace@users.noreply.github.com>
2018-06-22 15:03:47 +0530

    BAH-497: Prompt to save if home or search clicked in registration screen (#179)
    * BAH-497: Prompt to save if home or search clicked in registration screen (only on the first page of registration)
    
    * BAH-497: Added a unit test case
    
    * BAH-497: Made some more changes to the unit test

--- ui/test/unit/registration/controllers/patientCommonController.spec.js
+++ ui/test/unit/registration/controllers/patientCommonController.spec.js
@@ -2,7 +2,7 @@
 
 describe('PatientCommonController', function () {
 
-    var $aController, $httpBackend, scope, appService, rootScope, patientAttributeService;
+    var $aController, $httpBackend, scope, appService, rootScope, patientAttributeService, $state;
     var spinner = jasmine.createSpyObj('spinner', ['forPromise']);
     var $compile;
 
@@ -13,10 +13,11 @@ describe('PatientCommonController', function () {
     }));
 
     beforeEach(
-        inject(function ($controller, _$httpBackend_, $rootScope, _$compile_) {
+        inject(function ($controller, _$httpBackend_, $rootScope, _$compile_, _$state_) {
             $aController = $controller;
             $httpBackend = _$httpBackend_;
             scope = $rootScope.$new();
+            $state = _$state_;
             rootScope = $rootScope;
             $compile = _$compile_;
         })
@@ -25,7 +26,6 @@ describe('PatientCommonController', function () {
 
     beforeEach(function () {
         appService = jasmine.createSpyObj('appService', ['getAppDescriptor']);
-
         rootScope.genderMap = {};
 
         scope.patient = {};
@@ -51,8 +51,9 @@ describe('PatientCommonController', function () {
 
     });
 
-    it('checks that the confirmation popup is prompted when the home button is clicked and the config is enabled', function () {
+    it('checks that the confirmation popup is prompted when the home button is clicked and the config is enabled and current Page is not the Registration Second Page', function () {
         scope.showSaveConfirmDialogConfig = true;
+        $state.current.name = "patient.new";
         scope.onHomeNavigate = jasmine.createSpy("onHomeNavigate");
         scope.confirmationPrompt = jasmine.createSpy("confirmationPrompt");
         var element = angular.element("<a ng-click='onHomeNavigate()'>");
@@ -60,6 +61,7 @@ describe('PatientCommonController', function () {
         compiled.triggerHandler('click');
         expect(scope.onHomeNavigate).toHaveBeenCalled();
         expect(scope.showSaveConfirmDialogConfig).toBe(true);
+        expect($state.current.name).not.toBe("patient.visit");
         scope.confirmationPrompt();
         expect(scope.confirmationPrompt).toHaveBeenCalled();
     });
@@ -73,6 +75,20 @@ describe('PatientCommonController', function () {
         compiled.triggerHandler('click');
         expect(scope.onHomeNavigate).toHaveBeenCalled();
         expect(scope.showSaveConfirmDialogConfig).not.toBe(true);
+        expect(scope.confirmationPrompt).not.toHaveBeenCalled();
+    });
+
+it('checks that the confirmation popup is not prompted on the Registration second page when the home button is clicked and the config is enabled', function () {
+        scope.showSaveConfirmDialogConfig = true;
+        $state.current.name = "patient.visit";
+        scope.onHomeNavigate = jasmine.createSpy("onHomeNavigate");
+        scope.confirmationPrompt = jasmine.createSpy("confirmationPrompt");
+        var element = angular.element("<a ng-click='onHomeNavigate()'>");
+        var compiled = $compile(element)(scope);
+        compiled.triggerHandler('click');
+        expect(scope.onHomeNavigate).toHaveBeenCalled();
+        expect(scope.showSaveConfirmDialogConfig).toBe(true);
+        expect($state.current.name).toBe("patient.visit");
         expect(scope.confirmationPrompt).not.toHaveBeenCalled();
     });
 
