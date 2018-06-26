0ccc9850957c5b31bd1c124a63a4904bc815bcac
tbindu <tbindu@thoughtworks.com>
2018-06-26 18:02:46 +0530

    Swathi, Bindu | BAH-511 | Handled "undefined" visitUuid paramater

--- ui/app/clinical/visitSummaryInitialization.js
+++ ui/app/clinical/visitSummaryInitialization.js
@@ -4,9 +4,9 @@ angular.module('bahmni.clinical').factory('visitSummaryInitialization',
     ['visitService',
         function (visitService) {
             return function (visitUuid) {
-                return visitService.getVisitSummary(visitUuid).then(function (visitSummaryResponse) {
+                return visitUuid ? visitService.getVisitSummary(visitUuid).then(function (visitSummaryResponse) {
                     return new Bahmni.Common.VisitSummary(visitSummaryResponse.data);
-                });
+                }) : null;
             };
         }]
 );
