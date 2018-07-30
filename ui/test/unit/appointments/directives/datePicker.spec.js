7791f7c2512503f538df0a1a323dec470341e075
shilpa-iplit <38578773+shilpa-iplit@users.noreply.github.com>
2018-07-30 15:22:38 +0530

    https://bahmni.atlassian.net/browse/BAH-435 (#185)
    * https://bahmni.atlassian.net/browse/BAH-435
    
    * https://bahmni.atlassian.net/browse/BAH-435

--- ui/test/unit/appointments/directives/datePicker.spec.js
+++ ui/test/unit/appointments/directives/datePicker.spec.js
@@ -38,4 +38,41 @@ describe('DatePicker', function () {
         compileElementScope.goToNext();
         expect(scope.toggleChanged).toHaveBeenCalled();
     });
+
+    it('should call function provided to ngChange when data is changed and enter key is pressed', function () {
+        scope.toggleChanged = jasmine.createSpy('toggleChanged');
+        var element = createElement();
+        var compileElementScope = element.isolateScope();
+
+        compileElementScope.goToNext();
+        var e = $.Event("keydown");
+        e.which = 13;
+        e.keyCode = 13;
+        element.triggerHandler(e);
+        expect(scope.toggleChanged).toHaveBeenCalled();
+    });
+
+    it('should not call function provided to ngChange when data is not changed', function () {
+        scope.toggleChanged = jasmine.createSpy('toggleChanged');
+        var element = createElement();
+
+        var e1 = $.Event("focus");
+        element.triggerHandler(e1);
+        var e2 = $.Event("blur");
+        element.triggerHandler(e2);
+
+        expect(scope.viewDate).toEqual(scope.lastValidDate);
+    });
+
+    it('should call function provided to ngChange when data is changed and mouse is pressed outside', function () {
+        scope.toggleChanged = jasmine.createSpy('toggleChanged');
+        var element = createElement();
+        var compileElementScope = element.isolateScope();
+
+        compileElementScope.goToNext();
+        var e = $.Event("blur");
+        element.triggerHandler(e);
+        expect(scope.toggleChanged).toHaveBeenCalled();
+    });
+
 });
