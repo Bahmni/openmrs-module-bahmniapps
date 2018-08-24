ec238ba20789b2719035f3b547af9d334e334913
Dipak Thapa <dipakthapaofficial@gmail.com>
2018-08-24 11:44:50 +0545

    Adds nepali date time filter

--- ui/app/common/ui-helper/filters/dateFilters.js
+++ ui/app/common/ui-helper/filters/dateFilters.js
@@ -30,4 +30,13 @@ angular.module('bahmni.common.uiHelper')
         } 
         return date;
     };
+}).filter('npDateTime', function () {
+    return function (date) {
+        if (date !== null && date !== undefined && date !== '' && date != NaN && Bahmni.Common.Util.DateUtil.isValid(date)) {
+            var adDate = Bahmni.Common.Util.DateUtil.getDateWithoutTime(date).split("-");
+            var bsDate = calendarFunctions.getBsDateByAdDate(parseInt(adDate[0]), parseInt(adDate[1]), parseInt(adDate[2]));
+            return calendarFunctions.bsDateFormat("%y %M, %d", bsDate.bsYear, bsDate.bsMonth, bsDate.bsDate) +" "+ Bahmni.Common.Util.DateUtil.formatTime(date);
+        } 
+        return date;
+    };
 });
