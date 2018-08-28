47cc7ac27f135d4152a46ea0d5818c3c0f5a8d28
Dipak Thapa <dipakthapaofficial@gmail.com>
2018-08-28 14:06:22 +0545

    nepali date field in drugOrderSection

--- ui/app/common/displaycontrols/drugOrdersSection/directives/drugOrdersSection.js
+++ ui/app/common/displaycontrols/drugOrdersSection/directives/drugOrdersSection.js
@@ -4,6 +4,7 @@ angular.module('bahmni.common.displaycontrol.drugOrdersSection')
     .directive('drugOrdersSection', ['treatmentService', 'spinner', '$rootScope', 'appService',
         function (treatmentService, spinner, $rootScope) {
             var controller = function ($scope) {
+                $scope.displayNepaliDates = appService.getAppDescriptor().getConfigValue('displayNepaliDates');
                 var DateUtil = Bahmni.Common.Util.DateUtil;
                 $scope.showAdditionalInstructions = true;
                 $scope.toggle = true;
