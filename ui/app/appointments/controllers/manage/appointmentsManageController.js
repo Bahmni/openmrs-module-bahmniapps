1669f1276f4f92dfdd5d86b9373347c408ad7960
nehagupta2123 <nehagupta.dig@gmail.com>
2018-10-15 11:20:51 +0530

    Neha | [Product Fix] Clicking on AppointmentList Tab changing the view to calendar day view always

--- ui/app/appointments/controllers/manage/appointmentsManageController.js
+++ ui/app/appointments/controllers/manage/appointmentsManageController.js
@@ -4,9 +4,13 @@ angular.module('bahmni.appointments')
     .controller('AppointmentsManageController', ['$scope', '$state', 'appService',
         function ($scope, $state, appService) {
             $scope.enableCalendarView = appService.getAppDescriptor().getConfigValue('enableCalendarView');
-
+            $scope.currentViewName = 'summary';
             $scope.navigateTo = function (viewName) {
+                if ($scope.currentViewName === 'appointments' && viewName === 'appointments') {
+                    return;
+                }
                 var stateName = 'home.manage.' + ((viewName === 'appointments') ? getAppointmentsTab() : viewName);
+                $scope.currentViewName = viewName;
                 $state.go(stateName, $state.params, {reload: false});
             };
             var getAppointmentsTab = function () {
