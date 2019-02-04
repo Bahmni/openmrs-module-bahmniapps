888c3800a4e625c8ce374bae0e950a78b98e13fa
sumanmaity112 <suman.maity112@gmail.com>
2019-02-04 14:44:47 +0530

    BAH-752 | Suman | Remove grunt-regarde package. Used grunt-contrib-watch instead

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -534,8 +534,6 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.renameTask('regarde', 'watch');
-
     grunt.registerTask('test', ['karma:unit', 'coverage']);
 
     grunt.registerTask('bundle', [
