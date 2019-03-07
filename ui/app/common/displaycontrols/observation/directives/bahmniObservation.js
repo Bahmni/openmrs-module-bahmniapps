19991cc23c02e2e119626679abc981fb945de175
Alekhya <alekhyayalla23@gmail.com>
2019-03-07 17:23:38 +0530

    Alekhya | MOBN-38 | Change formsV2 constant variable name

--- ui/app/common/displaycontrols/observation/directives/bahmniObservation.js
+++ ui/app/common/displaycontrols/observation/directives/bahmniObservation.js
@@ -11,7 +11,7 @@ angular.module('bahmni.common.displaycontrol.observation')
                 $scope.showGroupDateTime = $scope.config.showGroupDateTime !== false;
 
                 var mapObservation = function (observations) {
-                    var conceptsConfig = $scope.config.formType === Bahmni.Common.Constants.formBuilderDisplayControlType ? {} :
+                    var conceptsConfig = $scope.config.formType === Bahmni.Common.Constants.forms2Type ? {} :
                         appService.getAppDescriptor().getConfigValue("conceptSetUI") || {};
 
                     observations = new Bahmni.Common.Obs.ObservationMapper().map(observations, conceptsConfig);
