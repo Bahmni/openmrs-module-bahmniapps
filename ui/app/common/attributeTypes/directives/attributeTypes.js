50ce01cad3fb2f4de0597294ae4e31b8eb636521
Edgêncio da Calista <edgencio@gmail.com>
2019-03-23 12:13:39 +0200

    Adds auto-suggest field

--- ui/app/common/attributeTypes/directives/attributeTypes.js
+++ ui/app/common/attributeTypes/directives/attributeTypes.js
@@ -7,8 +7,6 @@ angular.module('bahmni.common.attributeTypes', []).directive('attributeTypes', [
             attribute: '=',
             fieldValidation: '=',
             isAutoComplete: '&',
-            handleLocationChange: '&',
-            handleSectorChange: '&',
             getAutoCompleteList: '&',
             getDataResults: '&',
             handleUpdate: '&',
@@ -24,8 +22,10 @@ angular.module('bahmni.common.attributeTypes', []).directive('attributeTypes', [
             $scope.isAutoComplete = $scope.isAutoComplete() || function () { return false; };
             $scope.isReadOnly = $scope.isReadOnly() || function () { return false; };
             $scope.handleUpdate = $scope.handleUpdate() || function () { return false; };
-            $scope.handleLocationChange = $scope.handleLocationChange() || function () { return false; };
-            $scope.handleSectorChange = $scope.handleSectorChange() || function () { return false; };
+
+            $scope.suggestions = $scope.attribute.answers;
+
+            $scope.showTag = false;
 
             $scope.appendConceptNameToModel = function (attribute) {
                 var attributeValueConceptType = $scope.targetModel[attribute.name];
@@ -33,6 +33,25 @@ angular.module('bahmni.common.attributeTypes', []).directive('attributeTypes', [
                     return answer.conceptId === attributeValueConceptType.conceptUuid;
                 });
                 attributeValueConceptType.value = concept && concept.fullySpecifiedName;
+            };
+
+            $scope.suggest = function (string) {
+                $scope.hideList = false;
+                $scope.showTag = true;
+                var output = [];
+                angular.forEach($scope.suggestions, function (suggestion) {
+                    if (suggestion.description.toLowerCase().indexOf(string.value.toLowerCase()) >= 0) {
+                        output.push(suggestion);
+                    }
+                });
+                $scope.filterOcuppation = output;
+            };
+
+            $scope.hideSuggestions = function (object) {
+                $scope.targetModel[$scope.attribute.name] = object;
+                $scope.targetModel[$scope.attribute.name].value = object.description;
+                $scope.targetModel[$scope.attribute.name].conceptUuid = object.conceptId;
+                $scope.hideList = true;
             };
         }
     };
