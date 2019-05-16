d13a800e69d03f27c5cec49459875678a224362d
amosh101 <amosh101@outlook.com>
2019-05-16 15:06:41 +0200

    Amos | MPOC 221 fix grunt clean

--- ui/app/registration/controllers/editPatientController.js
+++ ui/app/registration/controllers/editPatientController.js
@@ -37,12 +37,12 @@ angular.module('bahmni.registration')
                         if ($scope.nationalityChoice == 'Mocambicano' || $scope.nationalityChoice == 'Mozambican') {
                             $scope.nationalityDocs = mozAttributes;
                             $scope.existDocs = $scope.nationalityDocs;
-                            $scope.nationalityDocs=[];
+                            $scope.nationalityDocs = [];
                         }
                         else if ($scope.nationalityChoice == 'Estrangeiro' || $scope.nationalityChoice == 'Foreigner') {
                             $scope.nationalityDocs = foreignAttributes;
                             $scope.existDocs = $scope.nationalityDocs;
-                            $scope.nationalityDocs=[];
+                            $scope.nationalityDocs = [];
                         }
                     }
                 };
@@ -58,13 +58,13 @@ angular.module('bahmni.registration')
                             var mozAttributes = ['BI', 'Cartao_de_Eleitor', 'Cedula_de_Nascimento', 'NUIT', 'NUIC', 'Passaporte_Mocambicano'];
                             $scope.nationalityDocs = mozAttributes;
                             $scope.existDocs = $scope.nationalityDocs;
-                            $scope.nationalityDocs=[];
+                            $scope.nationalityDocs = [];
                         }
                         else if ($scope.nationalityChoice == 'Estrangeiro' || $scope.nationalityChoice == 'Foreigner') {
                             var foreignAttributes = ['DIRE', 'NUIT', 'Passaporte_Estrangeiro'];
                             $scope.nationalityDocs = foreignAttributes;
                             $scope.existDocs = $scope.nationalityDocs;
-                            $scope.nationalityDocs=[];
+                            $scope.nationalityDocs = [];
                         }
                     }
                 };
