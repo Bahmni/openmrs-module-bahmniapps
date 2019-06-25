3a4e35d3bd7fb3bb544fc0849f0f734d66477660
swethab <swethab@thoughtworks.com>
2019-06-25 14:23:05 +0530

    Swetha | BAH-273 | Changed the enpoint url

--- ui/app/registration/services/defaultPatientServiceStrategy.js
+++ ui/app/registration/services/defaultPatientServiceStrategy.js
@@ -7,7 +7,7 @@ angular.module('bahmni.registration')
 
         var search = function (config) {
             var defer = $q.defer();
-            var patientSearchUrl = Bahmni.Common.Constants.bahmniSearchUrl + "/patient/luceneHibernate";
+            var patientSearchUrl = Bahmni.Common.Constants.bahmniSearchUrl + "/patient/quick-search";
             var onResults = function (result) {
                 defer.resolve(result);
             };
