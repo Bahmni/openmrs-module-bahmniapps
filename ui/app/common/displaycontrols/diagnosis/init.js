c9b3863437c406dcddea677db1c28329c13a064e
MD Dubey <mddubey409@gmail.com>
2019-07-08 12:48:37 +0530

    Dubey | Move displaycontrols/diagnosis to bahmniapps-commons and include as library

--- ui/app/common/displaycontrols/diagnosis/init.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var Bahmni = Bahmni || {};
-Bahmni.Common = Bahmni.Common || {};
-Bahmni.Common.DisplayControl = Bahmni.Common.DisplayControl || {};
-Bahmni.Common.DisplayControl.Diagnosis = Bahmni.Common.DisplayControl.Diagnosis || {};
-
-angular.module('bahmni.common.displaycontrol.diagnosis', []);
