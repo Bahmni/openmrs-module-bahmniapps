3021c5bf187d8bc84f893928012c58d246f7d8d8
MD Dubey <mddubey409@gmail.com>
2019-07-08 11:45:24 +0530

    Dubey | Move displaycontrols/observation to bahmniapps-commons and include as library

--- ui/app/common/displaycontrols/observation/controllers/allObservationDetailsController.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-angular.module('bahmni.common.displaycontrol.observation')
-    .controller('AllObservationDetailsController', ['$scope',
-        function ($scope) {
-            $scope.patient = $scope.ngDialogData.patient;
-            $scope.section = $scope.ngDialogData.section;
-            $scope.config = $scope.ngDialogData.section ? $scope.ngDialogData.section.expandedViewConfig : {};
-        }]);
--- ui/app/common/displaycontrols/observation/init.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var Bahmni = Bahmni || {};
-Bahmni.Common = Bahmni.Common || {};
-Bahmni.Common.DisplayControl = Bahmni.Common.DisplayControl || {};
-Bahmni.Common.DisplayControl.Observation = Bahmni.Common.DisplayControl.Observation || {};
-
-angular.module('bahmni.common.displaycontrol.observation', ['bahmni.common.conceptSet', 'pascalprecht.translate']);
