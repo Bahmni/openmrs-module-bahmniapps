fd18391dbbd86b65f449270d856e33c9d111a926
nehagupta2123 <33066070+nehagupta2123@users.noreply.github.com>
2019-09-13 10:35:18 +0530

    Neha | BAH-745 | Make service filter collapsable by default by introducing a config (#226)
    * Neha | BAH-745 | Make service filter collapsable by default by introducing a configuration
    
    * Neha | BAH-745 | Rename variable for better readability
    
    * Neha | BAH-745 | Rename variable for better readability

--- ui/app/appointments/controllers/manage/appointmentsFilterController.js
+++ ui/app/appointments/controllers/manage/appointmentsFilterController.js
@@ -7,6 +7,8 @@ angular.module('bahmni.appointments')
                 $scope.isSpecialityEnabled = appService.getAppDescriptor().getConfigValue('enableSpecialities');
                 $scope.isServiceTypeEnabled = appService.getAppDescriptor().getConfigValue('enableServiceTypes');
                 $scope.isFilterOpen = $state.params.isFilterOpen;
+                $scope.expandToDepth = appService.getAppDescriptor().getConfigValue('collapseServiceFilter') == true ?
+                    Bahmni.Appointments.Constants.collapseServiceFilter : Bahmni.Appointments.Constants.defaultExpandServiceFilter;
                 $scope.isSearchEnabled = $state.params.isSearchEnabled;
                 $scope.statusList = _.map(Bahmni.Appointments.Constants.appointmentStatusList, function (status) {
                     return {name: status, value: status};
@@ -20,6 +22,7 @@ angular.module('bahmni.appointments')
                         return status.name !== "Cancelled";
                     });
                 }
+
                 var params = {v: "custom:(display,person,uuid,retired,attributes:(attributeType:(display),value,voided))"};
 
                 $scope.$watch("selectedProviders", function (selectedProviders) {
@@ -94,6 +97,30 @@ angular.module('bahmni.appointments')
                 }));
             };
 
+            $scope.$watch("searchText", function () {
+                if ($scope.expandToDepth == Bahmni.Appointments.Constants.collapseServiceFilter) {
+                    expandCollapsedFilter();
+                }
+            });
+
+            var collapseExpandedFilter = function () {
+                if (!_.isUndefined($scope.selectedSpecialities)) {
+                    var collapsableNodes = $scope.selectedSpecialities.filter(function (selectedSpeciality) {
+                        return _.isUndefined(selectedSpeciality.selected);
+                    });
+                    ivhTreeviewMgr.collapseRecursive($scope.selectedSpecialities, collapsableNodes);
+                }
+            };
+
+            var expandCollapsedFilter = function () {
+                if (!_.isUndefined($scope.searchText) && $scope.searchText.length > 0) {
+                    ivhTreeviewMgr.expandRecursive($scope.selectedSpecialities);
+                }
+                else {
+                    collapseExpandedFilter();
+                }
+            };
+
             $scope.filterSelected = function () {
                 $scope.filterSelectedValues = $scope.showSelected ? {selected: true} : undefined;
             };
@@ -127,9 +154,16 @@ angular.module('bahmni.appointments')
                 return $state.current.tabName;
             };
 
+            var collapseExpandedFilterOnFilterReset = function () {
+                if ($scope.expandToDepth == Bahmni.Appointments.Constants.collapseServiceFilter) {
+                    ivhTreeviewMgr.collapseRecursive($scope.selectedSpecialities, $scope.selectedSpecialities);
+                }
+            };
+
             $scope.resetFilter = function () {
                 if ($scope.selectedSpecialities) {
                     ivhTreeviewMgr.deselectAll($scope.selectedSpecialities, false);
+                    collapseExpandedFilterOnFilterReset();
                 }
                 $scope.selectedProviders = [];
                 $scope.selectedStatusList = [];
