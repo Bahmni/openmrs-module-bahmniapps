5e9090b2c3466dcb679f545538f89d680a19740b
vinay-ThoughtWorks <49633725+vinay-ThoughtWorks@users.noreply.github.com>
2020-02-06 15:29:58 +0530

    #BAH-973 | Vinay | Print the List View in Bed Management module (#273)
    * Vinay, Siva | MOBN-610 | Print bed management list view
    
    * Siva | MOBN-610 | Remove empty rows to be printed in bed management list view
    
    * Siva | MOBN-610 | Remove unused css
    
    * Siva | MOBN-610 | Add margin for bed occupancy
    
    * Vinay | MOBN-610 | Non empty check for print table row
    
    * Vinay | MOBN-610 | Configurable print icon
    
    * Vinay | MOBN-610 | Lint issues and tests fix due to moment
    
    * Vinay | MOBN-610 | Add margin for ward status section
    
    * Vinay | MOBN - 610 | Remove CSS important property
    
    Co-authored-by: Siva R <sivarach@thoughtworks.com>

--- ui/app/bedmanagement/controllers/roomController.js
+++ ui/app/bedmanagement/controllers/roomController.js
@@ -1,13 +1,14 @@
 'use strict';
 
 angular.module('bahmni.ipd')
-    .controller('RoomController', ['$scope', '$rootScope', '$state', '$translate', 'appService',
-        function ($scope, $rootScope, $state, $translate, appService) {
+    .controller('RoomController', ['$scope', '$rootScope', '$state', '$translate', 'appService', 'printer',
+        function ($scope, $rootScope, $state, $translate, appService, printer) {
             var init = function () {
                 $scope.defaultTags = ['AVAILABLE', 'OCCUPIED'];
                 var appDescriptor = appService.getAppDescriptor();
                 $rootScope.bedTagsColorConfig = appDescriptor.getConfigValue("colorForTags") || [];
                 $rootScope.currentView = $rootScope.currentView || "Grid";
+                $scope.showPrintIcon = appDescriptor.getConfigValue("wardListPrintEnabled") || false;
                 $scope.currentView = $rootScope.currentView;
 
                 if ($rootScope.bedDetails) {
@@ -32,6 +33,40 @@ angular.module('bahmni.ipd')
                 $rootScope.currentView = ($rootScope.currentView === "Grid") ? "List" : "Grid";
                 $scope.currentView = $rootScope.currentView;
             };
+
+            $scope.printWardList = function () {
+                var printTemplateUrl = appService.getAppDescriptor()
+                  .getConfigValue('wardListPrintViewTemplateUrl') || 'views/wardListPrint.html';
+                var configuredTableHeader = appService.getAppDescriptor()
+                  .getConfigValue('wardListPrintAttributes');
+                if (configuredTableHeader && configuredTableHeader.length > 0) {
+                    $scope.tableHeader = configuredTableHeader;
+                }
+                printer.print(printTemplateUrl, {
+                    wardName: $scope.room.name,
+                    date: moment().format('DD-MMM-YYYY'),
+                    totalBeds: $scope.room.totalBeds,
+                    occupiedBeds: $scope.room.totalBeds - $scope.room.availableBeds,
+                    tableData: $scope.tableData,
+                    tableHeader: $scope.tableHeader,
+                    isEmptyRow: $scope.isEmptyRow
+                });
+            };
+
+            $scope.isEmptyRow = function (row) {
+                for (var i = 0; i < $scope.tableHeader.length; i++) {
+                    var header = $scope.tableHeader[i];
+                    if (row[header]) {
+                        return false;
+                    }
+                }
+                return true;
+            };
+
+            $scope.$on("event:getTableData", function (event, data) {
+                $scope.tableData = data.tableData;
+                $scope.tableHeader = data.tableHeader;
+            });
 
             $scope.getTagName = function (tag) {
                 if (tag === 'AVAILABLE') {
