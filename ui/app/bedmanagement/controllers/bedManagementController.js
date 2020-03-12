95db2467a32b3f61a905f06d9b28bee3617dc699
venkatavineela <40136044+venkatavineela@users.noreply.github.com>
2020-03-12 17:29:05 +0530

    BAH-974 Redirection link to clinical module (#263)
    * Bindu | MOBN-689 | Added new config variable for ipd patient forward url
    
    * Bindu | MOBN-689 | changed config variable name
    
    * Sowmika | MOBN-689 | Fix build failure by fixing tests
    
    Co-authored-by: tbindu <tbindu@thoughtworks.com>
    Co-authored-by: sowmika <sowmika148@gmail.com>

--- ui/app/bedmanagement/controllers/bedManagementController.js
+++ ui/app/bedmanagement/controllers/bedManagementController.js
@@ -6,7 +6,6 @@ angular.module('bahmni.ipd')
             $scope.wards = null;
             $scope.ward = {};
             $scope.editTagsPrivilege = Bahmni.IPD.Constants.editTagsPrivilege;
-
             var links = {
                 "dashboard": {
                     "name": "inpatient",
@@ -14,6 +13,7 @@ angular.module('bahmni.ipd')
                     "url": "../bedmanagement/#/patient/{{patientUuid}}/visit/{{visitUuid}}/dashboard"
                 }
             };
+            var patientForwardUrl = appService.getAppDescriptor().getConfigValue("patientForwardUrl") || links.dashboard.url;
 
             var isDepartmentPresent = function (department) {
                 if (!department) return false;
@@ -174,7 +174,7 @@ angular.module('bahmni.ipd')
             $scope.goToAdtPatientDashboard = function () {
                 getVisitInfoByPatientUuid($scope.patient.uuid).then(function (visitUuid) {
                     var options = {patientUuid: $scope.patient.uuid, visitUuid: visitUuid};
-                    var url = appService.getAppDescriptor().formatUrl(links.dashboard.url, options);
+                    var url = appService.getAppDescriptor().formatUrl(patientForwardUrl, options);
                     window.open(url);
                 });
                 if (window.scrollY > 0) {
