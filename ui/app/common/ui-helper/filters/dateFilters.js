7afd63f63f771d759847dc34792771f72702cb5a
binduak <tbindu@thoughtworks.com>
2020-03-16 15:02:17 +0530

    Bindu | MOBN-1150 | Fix eslint issue

--- ui/app/common/ui-helper/filters/dateFilters.js
+++ ui/app/common/ui-helper/filters/dateFilters.js
@@ -6,27 +6,27 @@ angular.module('bahmni.common.uiHelper')
             return Bahmni.Common.Util.DateUtil.diffInDays(startDate, endDate);
         };
     }).filter('bahmniDateTime', function () {
-    return function (date) {
-        return Bahmni.Common.Util.DateUtil.formatDateWithTime(date);
-    };
-}).filter('bahmniDate', function () {
-    return function (date) {
-        return Bahmni.Common.Util.DateUtil.formatDateWithoutTime(date);
-    };
-}).filter('bahmniTime', function () {
-    return function (date) {
-        return Bahmni.Common.Util.DateUtil.formatTime(date);
-    };
-}).filter('bahmniDateInStrictMode', function () {
-    return function (date) {
-        return Bahmni.Common.Util.DateUtil.formatDateInStrictMode(date);
-    };
-}).filter('bahmniDateTimeWithFormat', function () {
-    return function (date, format) {
-        return Bahmni.Common.Util.DateUtil.getDateTimeInSpecifiedFormat(date, format);
-    };
-}).filter('addDays', function () {
-    return function (date, numberOfDays) {
-        return Bahmni.Common.Util.DateUtil.addDays(date, numberOfDays);
-    };
-});
+        return function (date) {
+            return Bahmni.Common.Util.DateUtil.formatDateWithTime(date);
+        };
+    }).filter('bahmniDate', function () {
+        return function (date) {
+            return Bahmni.Common.Util.DateUtil.formatDateWithoutTime(date);
+        };
+    }).filter('bahmniTime', function () {
+        return function (date) {
+            return Bahmni.Common.Util.DateUtil.formatTime(date);
+        };
+    }).filter('bahmniDateInStrictMode', function () {
+        return function (date) {
+            return Bahmni.Common.Util.DateUtil.formatDateInStrictMode(date);
+        };
+    }).filter('bahmniDateTimeWithFormat', function () {
+        return function (date, format) {
+            return Bahmni.Common.Util.DateUtil.getDateTimeInSpecifiedFormat(date, format);
+        };
+    }).filter('addDays', function () {
+        return function (date, numberOfDays) {
+            return Bahmni.Common.Util.DateUtil.addDays(date, numberOfDays);
+        };
+    });
