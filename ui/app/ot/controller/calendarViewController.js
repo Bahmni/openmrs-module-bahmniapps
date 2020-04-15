25885e8565092be8ee0bf1083afac742e318fefb
Vinisha Donthula <vinishad@thoughtworks.com>
2020-04-15 11:22:15 +0530

    Vinisha, Siva | MOBN-1153 | Fix cancel button inconsistent behaviour on surgical block details dialog

--- ui/app/ot/controller/calendarViewController.js
+++ ui/app/ot/controller/calendarViewController.js
@@ -188,6 +188,7 @@ angular.module('bahmni.ot')
                 $scope.editDisabled = false;
                 $scope.moveButtonDisabled = true;
                 $scope.addActualTimeDisabled = true;
+                $scope.cancelDisabled = true;
                 $scope.surgicalBlockSelected = surgicalBlock;
                 $scope.surgicalAppointmentSelected = {};
 
