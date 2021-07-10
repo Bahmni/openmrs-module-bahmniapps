bbe065cacc5377b10052af98e49ed9b75f855ffe
angshuman sarkar <angshuonline@gmail.com>
2021-07-10 13:20:44 +0530

    BAH-1225 | Changing invocation and launch of tele consultation (#362)

--- ui/app/common/app-framework/services/appService.js
+++ ui/app/common/app-framework/services/appService.js
@@ -11,7 +11,6 @@ angular.module('bahmni.common.appFramework')
             var customUrl = Bahmni.Common.Constants.customUrl;
             var appDescriptor = null;
             $rootScope.meetId = null;
-            $rootScope.showTeleConsultationWindow = false;
             var loadConfig = function (url) {
                 return loadConfigService.loadConfig(url, appDescriptor.contextPath);
             };
@@ -182,13 +181,6 @@ angular.module('bahmni.common.appFramework')
 
             this.getAppName = function () {
                 return this.appName;
-            };
-
-            this.setTeleConsultationVars = function (meetId, show) {
-                if (!meetId || !$rootScope.meetId) {
-                    $rootScope.meetId = meetId;
-                    $rootScope.showTeleConsultationWindow = show;
-                }
             };
 
             this.checkPrivilege = function (privilegeName) {
