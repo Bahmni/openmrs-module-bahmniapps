8fc451f35906b089b8402a8d4de1e84ed9e470b1
MOHANKUMAR T <mohan13081999@gmail.com>
2021-12-16 15:35:47 +0530

    BAH-1307|Mohan/Sanofer|Fix| Lint errors

--- ui/app/common/mf-app-shell/microFrontendLoader.js
+++ ui/app/common/mf-app-shell/microFrontendLoader.js
@@ -2,13 +2,13 @@ const cleanUpContainer = function (container) {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
-}
+};
 
 const createDOM = function (container, mfName) {
     let dom = document.createElement('div');
     dom.id = mfName;
     container.appendChild(dom);
-}
+};
 
 const loadApp = function (container, mf) {
     for (const scriptURL of mf.urls) {
@@ -16,19 +16,19 @@ const loadApp = function (container, mf) {
         scriptTag.src = scriptURL;
         container.appendChild(scriptTag);
     }
-}
+};
 
 const getEnabledMfs = function (containerName) {
-    return importMap[containerName].filter(mf => mf.enabled)
-}
+    return importMap[containerName].filter(mf => mf.enabled);
+};
 
 const callback = function (mutationsList) {
     for (const mutation of mutationsList) {
         for (const node of mutation.addedNodes) {
             if (node instanceof HTMLElement) {
-                let mfContainers = node.querySelectorAll('[mf-container]')
+                let mfContainers = node.querySelectorAll('[mf-container]');
                 for (const mfContainer of mfContainers) {
-                    let containerName = mfContainer.getAttribute('mf-container')
+                    let containerName = mfContainer.getAttribute('mf-container');
                     if (importMap[containerName]) {
                         cleanUpContainer(mfContainer);
                         const enabledMfs = getEnabledMfs(containerName);
@@ -39,12 +39,9 @@ const callback = function (mutationsList) {
                     }
                 }
             }
-
         }
     }
 };
-
-
 
 const observer = new MutationObserver(callback);
 
