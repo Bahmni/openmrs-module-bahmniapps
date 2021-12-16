4f3f84095df1b9839e3ec8841d6191c5fdfda30d
MOHANKUMAR T <mohan13081999@gmail.com>
2021-12-16 15:40:12 +0530

    BAH-1307|Mohan/Sanofer|Add| Test import map

---
+++ ui/test/unit/common/mf-app-shell/testImportMap.js
@@ -0,0 +1,21 @@
+const importMap = {
+    'test-container-1': [
+        {
+            name: "test-mf1",
+            enabled: true,
+            urls: ["test-mf1.js"],
+        }
+    ],
+    'test-container-2': [
+        {
+            name: "test-mf2",
+            enabled: true,
+            urls: ["test-mf2.js"],
+        },
+        {
+            name: "test-mf3",
+            enabled: false,
+            urls: ["test-mf3.js"],
+        }
+    ],
+};
