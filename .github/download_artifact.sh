7fdd8d571634e4983e724c659371f501bb63694f
SanoferSameera <sanofersameera25@gmail.com>
2022-03-29 12:19:21 +0530

    BAH-1439 | Add. updated workflow to pick ndhm-react.zip
    Co-authored-by: Kavitha S <kavitha.s@thoughtworks.com>

--- .github/download_artifact.sh
+++ .github/download_artifact.sh
@@ -9,7 +9,7 @@ echo "Invalid Arguments. Need repository_name, artifact_name, github_pat"
 exit 2
 fi
 
-curl -s https://api.github.com/repos/Bahmni/$1/actions/artifacts | \
+curl -s https://api.github.com/repos/Bahmni-Covid19/$1/actions/artifacts | \
     jq '[.artifacts[] | select (.name == '\"$2\"')]' | jq -r '.[0] | .archive_download_url' | \
     xargs curl -L -o $2.zip -H "Authorization: token $3"
 unzip -d package/resources/ $2.zip && rm $2.zip
