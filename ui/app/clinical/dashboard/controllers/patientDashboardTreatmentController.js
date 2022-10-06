3cb4dbc478fe2a36aeabeecdde31593772de0feb
Soorya Kumaran C <90232857+SooryaKumaranC-tw@users.noreply.github.com>
2022-10-06 13:27:33 +0530

    BAH-2189 | Generate pdf for prescription
    Co-authored-by: deepthi-m <deepthi.mantena@thoughtworks.com>

--- ui/app/clinical/dashboard/controllers/patientDashboardTreatmentController.js
+++ ui/app/clinical/dashboard/controllers/patientDashboardTreatmentController.js
@@ -4,10 +4,14 @@ angular.module('bahmni.clinical')
     .controller('PatientDashboardTreatmentController', ['$scope', 'ngDialog',
         function ($scope, ngDialog) {
             var treatmentConfigParams = $scope.dashboard.getSectionByType("treatment") || {};
+            console.log('treatmentConfigParams - ', treatmentConfigParams);
             var patientUuidparams = {"patientUuid": $scope.patient.uuid};
 
+            // $scope.visitTabConfig = visitConfig;
             $scope.dashboardConfig = {};
             $scope.expandedViewConfig = {};
+            // $scope.drugOrdersSection = drugOrdersSection;
+            // $drugOrder = drugOrder;
 
             _.extend($scope.dashboardConfig, treatmentConfigParams.dashboardConfig || {}, patientUuidparams);
             _.extend($scope.expandedViewConfig, treatmentConfigParams.expandedViewConfig || {}, patientUuidparams);
@@ -25,4 +29,19 @@ angular.module('bahmni.clinical')
             });
 
             $scope.$on("$destroy", cleanUpListener);
+
+            $scope.export = function () {
+                console.log("11111 - inside export");
+                html2canvas(document.getElementById('prescription-pdf'), {
+                    onrendered: function (canvas) {
+                        var data = canvas.toDataURL();
+                        var docDefinition = {
+                            content: [{ image: data, width: 500 }]
+                        };
+                        var document = pdfMake.createPdf(docDefinition).getStream();
+                        console.log('document ', document);
+                    }
+                });
+            };
+            console.log('Exported - ', $scope.export);
         }]);
