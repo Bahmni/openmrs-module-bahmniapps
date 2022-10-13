3c53bcbfa0d87a3866d7ca085885ae3db48bbb6a
rohit-yawalkar <82825674+rohit-yawalkar@users.noreply.github.com>
2022-10-13 17:33:05 +0530

    BAH-2185-feature - [ Rohit ] - Configure the template for printing the visit details from the patient dashboard

--- ui/app/common/patient/filters/age.js
+++ ui/app/common/patient/filters/age.js
@@ -4,11 +4,11 @@ angular.module('bahmni.common.patient')
 .filter('age', function () {
     return function (age) {
         if (age.years) {
-            return age.years + " y";
+            return age.years + " year" + (age.years > 1 ? "s" : "");
         }
         if (age.months) {
-            return age.months + " m";
+            return age.months + " month" + (age.months > 1 ? "s" : "");
         }
-        return age.days + " d";
+        return age.days + " day" + (age.days > 1 ? "s" : "");
     };
 });
--- ui/test/unit/common/patient/filters/dateToAge.spec.js
+++ ui/test/unit/common/patient/filters/dateToAge.spec.js
@@ -9,28 +9,28 @@ describe("dateToAge Filter", function () {
     }));
 
     it("should return years when age is more than a year w.r.t reference date", function() {
-      expect(dateToAgeFilter('2013-09-01', '2014-10-01')).toBe('1 y');
-      expect(dateToAgeFilter('2012-09-01', '2014-11-11')).toBe('2 y');
-      expect(dateToAgeFilter('2012-09-10', '2015-09-05')).toBe('2 y');
+      expect(dateToAgeFilter('2013-09-01', '2014-10-01')).toBe('1 year');
+      expect(dateToAgeFilter('2012-09-01', '2014-11-11')).toBe('2 years');
+      expect(dateToAgeFilter('2012-09-10', '2015-09-05')).toBe('2 years');
     });
 
     it("should return months when age is less than a year w.r.t reference date", function() {
-      expect(dateToAgeFilter('2014-09-01', '2014-10-01')).toBe('1 m');
-      expect(dateToAgeFilter('2013-11-01', '2014-10-01')).toBe('11 m');
-      expect(dateToAgeFilter('2012-09-10', '2013-09-05')).toBe('11 m');
+      expect(dateToAgeFilter('2014-09-01', '2014-10-01')).toBe('1 month');
+      expect(dateToAgeFilter('2013-11-01', '2014-10-01')).toBe('11 months');
+      expect(dateToAgeFilter('2012-09-10', '2013-09-05')).toBe('11 months');
     });
 
     it("should return days when age is less than a month w.r.t reference date", function() {
-      expect(dateToAgeFilter('2014-09-30', '2014-10-01')).toBe('1 d');
-      expect(dateToAgeFilter('2014-09-02', '2014-10-01')).toBe('29 d');
-      expect(dateToAgeFilter('2014-02-28', '2014-03-01')).toBe('1 d');
-      expect(dateToAgeFilter('2014-03-01', '2014-03-01')).toBe('0 d');
+      expect(dateToAgeFilter('2014-09-30', '2014-10-01')).toBe('1 day');
+      expect(dateToAgeFilter('2014-09-02', '2014-10-01')).toBe('29 days');
+      expect(dateToAgeFilter('2014-02-28', '2014-03-01')).toBe('1 day');
+      expect(dateToAgeFilter('2014-03-01', '2014-03-01')).toBe('0 day');
     });
 
     it("should age w.r.t current time when reference date is not given", function() {
       spyOn(Bahmni.Common.Util.DateUtil, 'now').and.returnValue('2014-10-01');
       
-      expect(dateToAgeFilter('2014-09-30')).toBe('1 d');
-      expect(dateToAgeFilter('2013-09-30')).toBe('1 y');
+      expect(dateToAgeFilter('2014-09-30')).toBe('1 day');
+      expect(dateToAgeFilter('2013-09-30')).toBe('1 year');
     });
 });
