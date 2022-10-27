c8cc836dc1cd1fb99088c5e10b63f5659f7009fe
Siva Reddy <sivareddy.pathuri@thoughtworks.com>
2022-10-27 15:58:45 +0530

    Siva Reddy | BAH-2464 | Refactoring patient lucene search code to bahmni-commons

--- ui/app/registration/services/defaultPatientServiceStrategy.js
+++ ui/app/registration/services/defaultPatientServiceStrategy.js
@@ -9,7 +9,7 @@ angular.module('bahmni.registration')
             var defer = $q.defer();
             var patientSearchUrl = Bahmni.Common.Constants.bahmniCommonsSearchUrl + "/patient";
             if (config && config.params.identifier) {
-                patientSearchUrl = Bahmni.Common.Constants.bahmniSearchUrl + "/patient/lucene";
+                patientSearchUrl = Bahmni.Common.Constants.bahmniCommonsSearchUrl + "/patient/lucene";
             }
             var onResults = function (result) {
                 defer.resolve(result);
