33b27a33c00540727bd994f1c58b75e7ba4eb0da
KK <krishna.kiran@thoughtworks.com>
2022-11-08 17:56:34 +0530

    [BAH-2527] Add logic to display arrows on need

--- ui/app/common/patient-search/directives/scrollableTabs.js
+++ ui/app/common/patient-search/directives/scrollableTabs.js
@@ -8,20 +8,35 @@ angular.module("bahmni.common.patientSearch")
         replace: true,
         template:
         `<div class="scrollable-tabs">
-          <button type="btn" class="arrow-left" ng-click="scrollLeft()"><span class="fa fa-angle-left"></span></button>
-          <button type="btn" class="arrow-right" ng-click="scrollRight()"><span class="fa fa-angle-right"></span></button>
+          <button type="btn" class="arrow-left" ng-click="scrollLeft()" ng-show="showLeft"><span class="fa fa-angle-left"></span></button>
+          <button type="btn" class="arrow-right" ng-click="scrollRight()" ng-show="showRight"><span class="fa fa-angle-right"></span></button>
           <ng-transclude />
         </div>`,
         link: function(scope, element) {
           var scrollContainer = element.find('ul');
+          var scrollingWidth;
           
           scope.scrollLeft = function() {
-            scrollContainer.animate({left: scrollContainer.position().left + 100 + 'px'});
+            var scrollPos = scrollContainer.position().left + 100;
+            scrollContainer.animate({left: scrollPos + 'px'});
+            scope.showLeft = scrollPos < 0;
+            scope.showRight = true;
           }
           
           scope.scrollRight = function() {
-            scrollContainer.animate({left: scrollContainer.position().left - 100 + 'px'});
+            var scrollPos = scrollContainer.position().left - 100;
+            scrollingWidth = scrollContainer.outerWidth() - element.outerWidth();
+            console.log(scrollingWidth);
+            scrollContainer.animate({left: scrollPos + 'px'});
+            scope.showLeft = true;
+            scope.showRight = scrollingWidth > Math.abs(scrollPos);
           }
+
+          setTimeout(function() {
+            if(scrollContainer.outerWidth() - element.outerWidth() > 0) {
+              scope.showRight = true;
+            }
+          })
         }
     };
   })
