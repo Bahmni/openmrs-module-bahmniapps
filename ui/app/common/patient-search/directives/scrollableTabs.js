37b0e9c5118231fdf87c5e4fb77460e27506c962
KK <krishna.kiran@thoughtworks.com>
2022-11-09 15:53:32 +0530

    [BAH-2527] Fix indentation & lint files

--- ui/app/common/patient-search/directives/scrollableTabs.js
+++ ui/app/common/patient-search/directives/scrollableTabs.js
@@ -1,60 +1,60 @@
 "use strict";
 
 angular.module("bahmni.common.patientSearch")
-  .directive("scrollableTabs", function () {
-    return {
-      restrict: "E",
-      transclude: true,
-      replace: true,
-      template:
-      `<div class="scrollable-tabs">
-        <button type="btn" class="arrow-left" ng-click="scrollLeft()" ng-show="showLeft">
-          <span class="fa fa-angle-left"></span>
-        </button>
-        <ng-transclude></ng-transclude>
-        <button type="btn" class="arrow-right" ng-click="scrollRight()" ng-show="showRight">
-          <span class="fa fa-angle-right"></span>
-        </button>
-      </div>`,
-      link: function(scope, element) {
-        var scrollContainer = element.find('ul');
-        var scrollingWidth;
-        
-        scope.scrollLeft = function() {
-          var scrollPos = Math.min(scrollContainer.position().left + element.outerWidth() / 3, 0);
-          scrollPos = Math.abs(scrollPos) < (element.outerWidth() / 15) ? 0 : scrollPos;
-          scrollContainer.animate({left: scrollPos + 'px'});
-          scope.showLeft = scrollPos < 0;
-          scope.showRight = true;
-        }
-        
-        scope.scrollRight = function() {
-          scrollingWidth = scrollContainer.outerWidth() - element.outerWidth();
-          var scrollPos = Math.min(Math.abs(scrollContainer.position().left - element.outerWidth() / 3), scrollingWidth);
-          scrollPos = (scrollingWidth - scrollPos) < (element.outerWidth() / 15) ? scrollingWidth : scrollPos;
-          scrollContainer.animate({left: -scrollPos + 'px'});
-          scope.showLeft = true;
-          scope.showRight = scrollingWidth > scrollPos;
-        }
+    .directive("scrollableTabs", ['$timeout', function ($timeout) {
+        return {
+            restrict: "E",
+            transclude: true,
+            replace: true,
+            template:
+                '<div class="scrollable-tabs">' +
+                    '<button type="btn" class="arrow-left" ng-click="scrollLeft()" ng-show="showLeft">' +
+                    '<span class="fa fa-angle-left"></span>' +
+                    '</button>' +
+                    '<ng-transclude></ng-transclude>' +
+                    '<button type="btn" class="arrow-right" ng-click="scrollRight()" ng-show="showRight">' +
+                    '<span class="fa fa-angle-right"></span>' +
+                    '</button>' +
+                '</div>',
+            link: function (scope, element) {
+                var scrollContainer = element.find('ul');
+                var scrollingWidth;
 
-        setTimeout(function() {
-          if(scrollContainer.outerWidth() - element.outerWidth() > 0) {
-            scope.showRight = true;
-          }
-        });
+                scope.scrollLeft = function () {
+                    var scrollPos = Math.min(scrollContainer.position().left + element.outerWidth() / 3, 0);
+                    scrollPos = Math.abs(scrollPos) < (element.outerWidth() / 15) ? 0 : scrollPos;
+                    scrollContainer.animate({ left: scrollPos + 'px' });
+                    scope.showLeft = scrollPos < 0;
+                    scope.showRight = true;
+                };
 
-        Bahmni.Common.Util.SwipeUtil.detectSwipe(function(evt, direction) {
-          if($(evt.target).closest('.scrollable-tabs').length) {
-            if(direction === Bahmni.Common.Util.SwipeUtil.DIRECTIONS.RIGHT) {
-              scope.scrollRight();
+                scope.scrollRight = function () {
+                    scrollingWidth = scrollContainer.outerWidth() - element.outerWidth();
+                    var scrollPos = Math.min(Math.abs(scrollContainer.position().left - element.outerWidth() / 3), scrollingWidth);
+                    scrollPos = (scrollingWidth - scrollPos) < (element.outerWidth() / 15) ? scrollingWidth : scrollPos;
+                    scrollContainer.animate({ left: -scrollPos + 'px' });
+                    scope.showLeft = true;
+                    scope.showRight = scrollingWidth > scrollPos;
+                };
+
+                $timeout(function () {
+                    if (scrollContainer.outerWidth() - element.outerWidth() > 0) {
+                        scope.showRight = true;
+                    }
+                });
+
+                Bahmni.Common.Util.SwipeUtil.detectSwipe(function (evt, direction) {
+                    if ($(evt.target).closest('.scrollable-tabs').length) {
+                        if (direction === Bahmni.Common.Util.SwipeUtil.DIRECTIONS.RIGHT) {
+                            scope.scrollRight();
+                        }
+
+                        if (direction === Bahmni.Common.Util.SwipeUtil.DIRECTIONS.LEFT) {
+                            scope.scrollLeft();
+                        }
+                    }
+                    scope.$apply();
+                });
             }
-
-            if(direction === Bahmni.Common.Util.SwipeUtil.DIRECTIONS.LEFT) {
-              scope.scrollLeft();
-            }
-          }
-          scope.$apply();
-        });
-      }
-  };
-})
+        };
+    }]);
