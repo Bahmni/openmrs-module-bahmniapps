d646e125e7b3727109906103cdf876ac6ce1f6c8
Kavitha S <kavitha.s@thoughtworks.com>
2022-11-23 12:59:43 +0530

    BAH-2179 | refactored to read clinicName from configuration
    Co-authored-by: SanoferSameera <sanofersameera25@gmail.com>

--- ui/app/registration/constants.js
+++ ui/app/registration/constants.js
@@ -23,7 +23,8 @@ Bahmni.Registration.Constants = {
     enableWhatsAppButton: false,
     enableDashboardRedirect: false,
     dashboardUrl: "/bahmni/clinical/index.html#/default/patient/{{patientUuid}}/dashboard",
-    enableRegistrationSMSAlert: false
+    enableRegistrationSMSAlert: false,
+    clinicName: "Bahmni"
 };
 
 Bahmni.Registration.Constants.Errors = {
--- ui/app/registration/services/patientService.js
+++ ui/app/registration/services/patientService.js
@@ -91,7 +91,9 @@ angular.module('bahmni.registration')
         };
 
         var getRegistrationMessage = function (patientId, name, age, gender) {
+            var clinicName = appService.getAppDescriptor().getConfigValue("clinicName");
             var message = $translate.instant(appService.getAppDescriptor().getConfigValue("registrationMessage") || Bahmni.Registration.Constants.registrationMessage);
+            message = message.replace("#clinicName", clinicName);
             message = message.replace("#patientId", patientId);
             message = message.replace("#name", name);
             message = message.replace("#age", age + " years");
