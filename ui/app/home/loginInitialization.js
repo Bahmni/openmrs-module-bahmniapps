bb0cc2e12198f06ef0d42f18162e0a55eabe049f
rohit-yawalkar <82825674+rohit-yawalkar@users.noreply.github.com>
2022-12-19 20:20:58 +0530

    BAH-2407 - Adds dummy get calls to get valid jsession id after logout and session timeout

--- ui/app/home/loginInitialization.js
+++ ui/app/home/loginInitialization.js
@@ -1,22 +1,24 @@
 'use strict';
 
 angular.module('bahmni.home')
-    .factory('loginInitialization', ['$rootScope', '$q', 'locationService', 'spinner', 'messagingService',
-        function ($rootScope, $q, locationService, spinner, messagingService) {
+    .factory('loginInitialization', ['$rootScope', '$q', 'locationService', 'spinner', 'messagingService', 'sessionService',
+        function ($rootScope, $q, locationService, spinner, messagingService, sessionService) {
             var init = function () {
                 var deferrable = $q.defer();
-                locationService.getAllByTag("Login Location").then(
-                    function (response) {
-                        deferrable.resolve({locations: response.data.results});
-                    },
-                    function (response) {
-                        deferrable.reject();
-                        if (response.status) {
-                            response = 'MESSAGE_START_OPENMRS';
+                sessionService.get().then(function (response) {
+                    locationService.getAllByTag("Login Location").then(
+                        function (response) {
+                            deferrable.resolve({locations: response.data.results});
+                        },
+                        function (response) {
+                            deferrable.reject();
+                            if (response.status) {
+                                response = 'MESSAGE_START_OPENMRS';
+                            }
+                            messagingService.showMessage('error', response);
                         }
-                        messagingService.showMessage('error', response);
-                    }
-                );
+                    );
+                });
                 return deferrable.promise;
             };
 
