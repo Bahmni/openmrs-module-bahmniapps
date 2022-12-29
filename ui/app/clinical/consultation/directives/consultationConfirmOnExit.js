8263e41cc354a1ce440ebdef09f180b6a4235a7e
Kushboo <kushboojain.r@thoughtworks.com>
2022-12-29 15:03:30 +0530

    BAH-2359 | Checking number of medications added to check dirty

--- ui/app/clinical/consultation/directives/consultationConfirmOnExit.js
+++ ui/app/clinical/consultation/directives/consultationConfirmOnExit.js
@@ -3,29 +3,29 @@
 angular.module('bahmni.clinical')
     .directive('consultationConfirmOnExit', ['clinicalAppConfigService', 'messagingService', 'spinner',
         function (clinicalAppConfigService, messagingService, spinner) {
-        return {
-            link: function ($scope, elem, attrs) {
-                window.onbeforeunload = function () {
-                    console.log("change");
-                };
-                $scope.$on('$stateChangeStart', function (event, next, current) {
-                    var navigating = next.url.split("/")[1];
-                    var allConsultationBoards = clinicalAppConfigService.getAllConsultationBoards();
-                    var outOfConsultationBoard = true;
-                    allConsultationBoards.map(function (board) {
-                        var consultationLink = board.url.split("/")[0];
-                        if (navigating.includes(consultationLink)) {
-                            outOfConsultationBoard = false;
+            return {
+                link: function ($scope, elem, attrs) {
+                    window.onbeforeunload = function () {
+                        console.log("change");
+                    };
+                    $scope.$on('$stateChangeStart', function (event, next, current) {
+                        var navigating = next.url.split("/")[1];
+                        var allConsultationBoards = clinicalAppConfigService.getAllConsultationBoards();
+                        var outOfConsultationBoard = true;
+                        allConsultationBoards.map(function (board) {
+                            var consultationLink = board.url.split("/")[0];
+                            if (navigating.includes(consultationLink)) {
+                                outOfConsultationBoard = false;
+                            }
+                        });
+                        var noOfOrders = $scope.consultation.orders.length;
+                        var noOfMedications = $scope.consultation.newlyAddedTabTreatments[$scope.tabConfigName].treatments.length;
+                        if (outOfConsultationBoard && (noOfOrders > 0 || noOfMedications > 0 || ($scope[attrs.name] && $scope[attrs.name].$dirty))) {
+                            messagingService.showMessage('error', "{{'CONSULTATION_TAB_OBSERVATION_ERROR ' | translate }}");
+                            event.preventDefault();
+                            spinner.hide(next.spinnerToken);
                         }
                     });
-                    var noOfOrders = $scope.consultation.orders.length;
-                    var noOfObservations = $scope.consultation.observations.length;
-                    if (outOfConsultationBoard && (noOfOrders > 0 || noOfObservations > 0 || ($scope[attrs.name] && $scope[attrs.name].$dirty))) {
-                        messagingService.showMessage('error', "{{'CONSULTATION_TAB_OBSERVATION_ERROR ' | translate }}");
-                        event.preventDefault();
-                        spinner.hide(next.spinnerToken);
-                    }
-                });
-            }
-        };
-    }]);
+                }
+            };
+        }]);
