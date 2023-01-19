797587d9a49a8e25cee447bd2ad4fd039f691668
Kavitha S <kavitha.s@thoughtworks.com>
2023-01-19 14:32:22 +0530

    BAH-2179 | added share prescription buttons for mail and download

--- ui/app/clinical/dashboard/controllers/patientDashboardTreatmentController.js
+++ ui/app/clinical/dashboard/controllers/patientDashboardTreatmentController.js
@@ -4,13 +4,16 @@ angular.module('bahmni.clinical')
     .controller('PatientDashboardTreatmentController', ['$scope', 'ngDialog',
         function ($scope, ngDialog) {
             var treatmentConfigParams = $scope.dashboard.getSectionByType("treatment") || {};
-            var patientUuidparams = {"patientUuid": $scope.patient.uuid};
+            $scope.isEmailPresent = $scope.patient.email ? true : false;
+            $scope.isPhoneNumberPresent = $scope.patient.phoneNumber ? true : false;
+            var patientParams = {"patientUuid": $scope.patient.uuid, "isEmailPresent": $scope.isEmailPresent, "isPhoneNumberPresent": $scope.isPhoneNumberPresent};
+            
 
             $scope.dashboardConfig = {};
             $scope.expandedViewConfig = {};
 
-            _.extend($scope.dashboardConfig, treatmentConfigParams.dashboardConfig || {}, patientUuidparams);
-            _.extend($scope.expandedViewConfig, treatmentConfigParams.expandedViewConfig || {}, patientUuidparams);
+            _.extend($scope.dashboardConfig, treatmentConfigParams.dashboardConfig || {}, patientParams);
+            _.extend($scope.expandedViewConfig, treatmentConfigParams.expandedViewConfig || {}, patientParams);
 
             $scope.openSummaryDialog = function () {
                 ngDialog.open({
