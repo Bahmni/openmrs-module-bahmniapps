9e8205f04499509bbb9320ddb9041342ddea31aa
Kushboo <kushboojain.r@thoughtworks.com>
2023-02-02 00:03:34 +0530

    BAh-2359 | Updating specs

--- ui/test/unit/clinical/directives/consultationConfirmOnExit.spec.js
+++ ui/test/unit/clinical/directives/consultationConfirmOnExit.spec.js
@@ -3,6 +3,9 @@
 describe("consultationConfirmOnExit", function () {
     var element, scope, compile;
     var state = jasmine.createSpyObj('$state', ['go']);
+    state.params = {
+        patientUuid: 'patientUuid'
+    }
     var clinicalAppConfigService = jasmine.createSpyObj('clinicalAppConfigService', ['getAllConsultationBoards']);
     var spinner = jasmine.createSpyObj('spinner', ['hide']);
     var messagingService = jasmine.createSpyObj('messagingService', ['showMessage']);
@@ -19,6 +22,7 @@ describe("consultationConfirmOnExit", function () {
             scope = $rootScope.$new();
             compile = $compile;
         })
+        clinicalAppConfigService.getAllConsultationBoards.and.returnValue([{url: 'consultation'}, {url: 'observations'}]);
     });
 
     it("should set dirtyConsultationForm to be false if nothing is set", function() {
@@ -26,11 +30,14 @@ describe("consultationConfirmOnExit", function () {
         element = compile(element)(scope);
         scope.$digest();
         scope.consultation = {
-            orders: []
+            orders: [],
+            investigations: []
         };
         scope.$emit('$stateChangeStart', {url: '/consultation'}, {patientUuid: 'patientUuid'});
 
         expect(state.dirtyConsultationForm).toBe(undefined);
+        expect(messagingService.showMessage).not.toHaveBeenCalled();
+        expect(spinner.hide).not.toHaveBeenCalled();
     })
 
     it("should not set dirtyConsultationForm if the form is not dirty", function() {
@@ -38,7 +45,8 @@ describe("consultationConfirmOnExit", function () {
         element = compile(element)(scope);
         scope.$digest();
         scope.consultation = {
-            orders: []
+            orders: [],
+            investigations: []
         };
         scope.dummyForm = {
             $dirty: false
@@ -53,7 +61,8 @@ describe("consultationConfirmOnExit", function () {
         element = compile(element)(scope);
         scope.$digest();
         scope.consultation = {
-            orders: []
+            orders: [],
+            investigations: []
         };
         scope.dummyForm = {
             $dirty: true
@@ -69,6 +78,7 @@ describe("consultationConfirmOnExit", function () {
         scope.$digest();
         scope.consultation = {
             orders: [],
+            investigations: [],
             newlyAddedTreatments : {
                 meds: {
                     treatments: [{
@@ -83,18 +93,67 @@ describe("consultationConfirmOnExit", function () {
         expect(state.dirtyConsultationForm).toBe(true);
     });
 
-    it("should set dirtyConsultationForm to be true if number of orders is more than 1", function() {
+    it("should set dirtyConsultationForm to be true if number of orders is not same as number of investigations", function() {
         element = angular.element('<form consultation-confirm-on-exit name="dummyForm"><input value="text"/></form>');
         element = compile(element)(scope);
         scope.$digest();
         scope.consultation = {
             orders: [{
                 id: 345
-            }]
+            }],
+            investigations: [
+                {
+                    uuid:14
+                },
+                {
+                    uuid: 345
+                }
+            ]
         };
         scope.$emit('$stateChangeStart', {url: '/consultation'}, {patientUuid: 'patientUuid'});
 
         expect(state.dirtyConsultationForm).toBe(true);
     });
+
+    it('should not show error message if there are no dirty changes and navigating out of patient dashboard', function () {
+        scope.$emit('$stateChangeStart', {url: '/dashboard/uuid'}, {patientUuid: 'patientUuid'});
+
+        expect(messagingService.showMessage).not.toHaveBeenCalled();
+        expect(spinner.hide).not.toHaveBeenCalled();
+    });
+
+    it('should not show error message if there are dirty changes but navigating inside patient dashboard', function () {
+        element = angular.element('<form consultation-confirm-on-exit name="dummyForm"><input value="text"/></form>');
+        element = compile(element)(scope);
+        scope.$digest();
+
+        scope.$emit('$stateChangeStart', {url: '/dashboard/patientUuid'}, {patientUuid: 'patientUuid'});
+
+        expect(messagingService.showMessage).not.toHaveBeenCalled();
+        expect(spinner.hide).not.toHaveBeenCalled();
+    });
+
+    it('should not show error message if there are dirty changes but navigating inside observation tabs', function () {
+        element = angular.element('<form consultation-confirm-on-exit name="dummyForm"><input value="text"/></form>');
+        element = compile(element)(scope);
+        scope.$digest();
+
+        scope.$emit('$stateChangeStart', {url: '/consultation'}, {patientUuid: 'patientUuid'});
+
+        expect(messagingService.showMessage).not.toHaveBeenCalled();
+        expect(spinner.hide).not.toHaveBeenCalled();
+    });
+
+    it('should show error message if there are dirty changes and navigating out of patient dashboard', function () {
+        element = angular.element('<form consultation-confirm-on-exit name="dummyForm"><input value="text"/></form>');
+        element = compile(element)(scope);
+        scope.$digest();
+
+        scope.$emit('$stateChangeStart', {url: '/dashboard/patientUuid'}, {patientUuid: 'uuid'});
+
+        expect(messagingService.showMessage).toHaveBeenCalled();
+        expect(spinner.hide).toHaveBeenCalled();
+    });
+
 
 });
