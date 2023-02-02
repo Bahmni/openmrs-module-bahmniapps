b0cedd6a558d4bfd477ae29fa92403b9ae90070c
Patrick Nyatindo <nyatindopatrick@gmail.com>
2023-02-02 08:23:00 +0300

    BS-75 | Patrick | Update diagnosis Coded Answer UUID delimiter

--- ui/app/clinical/consultation/mappers/encounterTransactionMapper.js
+++ ui/app/clinical/consultation/mappers/encounterTransactionMapper.js
@@ -41,7 +41,7 @@ Bahmni.Clinical.EncounterTransactionMapper = function () {
 
         if (consultation.newlyAddedDiagnoses && consultation.newlyAddedDiagnoses.length > 0) {
             encounterData.bahmniDiagnoses = consultation.newlyAddedDiagnoses.map(function (diagnosis) {
-                var conceptSystem = diagnosis.codedAnswer.conceptSystem ? diagnosis.codedAnswer.conceptSystem + "$" : "";
+                var conceptSystem = diagnosis.codedAnswer.conceptSystem ? diagnosis.codedAnswer.conceptSystem + "/" : "";
                 return {
                     codedAnswer: { uuid: !diagnosis.isNonCodedAnswer ? conceptSystem + diagnosis.codedAnswer.uuid : undefined},
                     freeTextAnswer: diagnosis.isNonCodedAnswer ? diagnosis.codedAnswer.name : undefined,
