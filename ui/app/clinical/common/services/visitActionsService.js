dfdd1f3148b1b5df6e043e4226ad465406ebd487
Kavitha S <kavitha.s@thoughtworks.com>
2023-02-09 21:51:29 +0530

    BAH-2189 | added toast notification and print headers for email

--- ui/app/clinical/common/services/visitActionsService.js
+++ ui/app/clinical/common/services/visitActionsService.js
@@ -3,8 +3,8 @@
 angular.module('bahmni.clinical')
     .factory('visitActionsService', ['printer', function (printer) {
         return {
-            printPrescription: function (patient, visitDate, visitUuid) {
-                printer.print('common/views/prescriptionPrint.html', {patient: patient, visitDate: visitDate, visitUuid: visitUuid});
+            printPrescription: function (patient, visitDate, visitUuid, printParams = null) {
+                printer.print('common/views/prescriptionPrint.html', {patient: patient, visitDate: visitDate, visitUuid: visitUuid, printParams: printParams});
             }
         };
     }]);
--- ui/app/common/services/transmissionService.js
+++ ui/app/common/services/transmissionService.js
@@ -1,7 +1,7 @@
  'use strict';
 
  angular.module('bahmni.common.util')
-    .factory('transmissionService', ['$http', '$rootScope', 'locationService', '$bahmniCookieStore', '$translate', 'appService', 'visitService', '$filter', function ($http, $rootScope, locationService, $bahmniCookieStore, $translate, appService, visitService, $filter) {
+    .factory('transmissionService', ['$http', '$q', '$rootScope', 'locationService', '$bahmniCookieStore', '$translate', 'appService', 'visitService', '$filter', 'messagingService', function ($http, $q, $rootScope, locationService, $bahmniCookieStore, $translate, appService, visitService, $filter, messagingService) {
         var sendEmail = function (pdfContent, recipient, visit) {
             var subject = "Sending Prescriptions";
             var body = getSharePrescriptionMailContent(recipient.name, visit);
@@ -14,10 +14,19 @@
                 "cc": [],
                 "bcc": []
             };
-            return $http.post(Bahmni.Common.Constants.sendViaEmailUrl, params, {
+            var deferred = $q.defer();
+            $http.post(Bahmni.Common.Constants.sendViaEmailUrl, params, {
                 withCredentials: true,
                 headers: {"Accept": "application/json", "Content-Type": "application/json"}
+            }).then(function (response) {
+                if(response.status != 200) {
+                    messagingService.showMessage("error", "Unable to send email");
+                } else {
+                    messagingService.showMessage("info", "Mail sent successfully");
+                }
+                deferred.resolve(response);
             });
+            return deferred.promise;
         };
 
         var getSharePrescriptionMailContent = function (recipientName, visit) {
