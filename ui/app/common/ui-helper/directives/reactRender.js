5322af99061e509eaafb6ef187991a490657278d
KK <krishna.kiran@thoughtworks.com>
2023-02-17 22:45:31 +0530

    [BAH-2812] Pass patient and visit details while mounting

--- ui/app/common/ui-helper/directives/reactRender.js
+++ ui/app/common/ui-helper/directives/reactRender.js
@@ -6,8 +6,9 @@ angular.module('bahmni.common.uiHelper')
             restrict: 'A',
             link: function (scope, elem) {
                 if (!(elem[0].children && elem[0].children.length)) {
+                    console.log(scope.$parent.visitHistory.activeVisit);
                     // eslint-disable-next-line no-undef
-                    mountConsultation(elem[0]);
+                    mountConsultation(elem[0], 'Diagnosis', {patient: scope.$parent.patient, visit: scope.$parent.visitHistory.activeVisit});
                 }
 
                 // eslint-disable-next-line angular/window-service
