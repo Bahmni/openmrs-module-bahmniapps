ed01fcb2e3a511ef366d7b6ac90c55b06201b13d
KK <krishna.kiran@thoughtworks.com>
2023-02-27 19:30:46 +0530

    [BAH-2812] Update event data from app

--- ui/app/common/ui-helper/directives/reactRender.js
+++ ui/app/common/ui-helper/directives/reactRender.js
@@ -6,7 +6,6 @@ angular.module('bahmni.common.uiHelper')
             restrict: 'A',
             link: function (scope, elem) {
                 if (!(elem[0].children && elem[0].children.length)) {
-                    console.log(scope.$parent.visitHistory.activeVisit);
                     // eslint-disable-next-line no-undef
                     mountConsultation(elem[0], 'Diagnosis', {patient: scope.$parent.patient, visit: scope.$parent.visitHistory.activeVisit});
                 }
@@ -14,7 +13,7 @@ angular.module('bahmni.common.uiHelper')
                 // eslint-disable-next-line angular/window-service
                 window.addEventListener('ADD_DIAGNOSIS', function (event) {
                     console.log("Event Detail:");
-                    console.log("Search Value:", event.detail.searchValue, "Order value:", event.detail.orderValue);
+                    console.log("Saved Diagnosis:", event.detail.uuid, "Order value:", event.detail.order, "Certainty value", event.detail.certainty);
                 });
             }
         };
