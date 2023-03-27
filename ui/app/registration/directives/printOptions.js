93268f8783aeb954c9147d3a333873372fa6a454
kavitha-sundararajan <90255023+kavitha-sundararajan@users.noreply.github.com>
2023-03-27 15:24:13 +0530

    BAH-2941 | fix facility location and print header null issue (#552)

--- ui/app/registration/directives/printOptions.js
+++ ui/app/registration/directives/printOptions.js
@@ -21,13 +21,13 @@ angular.module('bahmni.registration')
                 };
 
                 $scope.print = function (option) {
-                    var locationName = $rootScope.loggedInLocation.name;
+                    var location = $rootScope.facilityVisitLocation ? $rootScope.facilityVisitLocation : $rootScope.loggedInLocation;
                     var locationAddress = "";
-                    var attributeDisplay = $rootScope.loggedInLocation.attributes[0].display.split(": ");
-                    if (attributeDisplay[0] === Bahmni.Registration.Constants.certificateHeader) {
+                    var attributeDisplay = location.attributes[0] ? location.attributes[0].display.split(": ") : null;
+                    if (attributeDisplay && attributeDisplay[0] === Bahmni.Registration.Constants.certificateHeader) {
                         locationAddress = attributeDisplay[1];
                     }
-                    return registrationCardPrinter.print(option.templateUrl, $scope.patient, mapRegistrationObservations(), $scope.encounterDateTime, { "name": locationName, "address": locationAddress });
+                    return registrationCardPrinter.print(option.templateUrl, $scope.patient, mapRegistrationObservations(), $scope.encounterDateTime, { "name": location.name, "address": locationAddress });
                 };
 
                 $scope.buttonText = function (option, type) {
