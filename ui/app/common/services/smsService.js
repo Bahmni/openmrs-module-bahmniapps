79810464276049bb7174ede453b4c7617836cab8
Kavitha S <kavitha.s@thoughtworks.com>
2023-04-04 14:25:13 +0530

    BAH-2816 | refactor sms url to be read from global property

--- ui/app/common/services/smsService.js
+++ ui/app/common/services/smsService.js
@@ -1,17 +1,37 @@
 'use strict';
 
 angular.module('bahmni.common.util')
-    .factory('smsService', ['$http', function ($http) {
-        var otpServiceUrl = Bahmni.Common.Constants.otpServiceUrl;
+    .factory('smsService', ['$http', 'messagingService', function ($http, messagingService) {
         var sendSMS = function (phoneNumber, message) {
-            const url = otpServiceUrl + "/notification/sms";
-            var data = {
-                "phoneNumber": phoneNumber,
-                "message": message
-            };
-            return $http.post(url, data, {
+            getSMSUrl().then(function (smsEndpoint) {
+                if(smsEndpoint.data != '') {
+                    var data = {
+                        "phoneNumber": phoneNumber,
+                        "message": message
+                    };
+                    return $http.post(smsEndpoint.data, data, {
+                        withCredentials: true,
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Accept": "text/plain"
+                        }
+                    });
+                } else {
+                    messagingService.showMessage("error", "SMS endpoint not configured for sending SMS notification");
+                }
+            });
+        };
+
+        var getSMSUrl = function () {
+            return $http.get(Bahmni.Common.Constants.globalPropertyUrl, {
+                method: "GET",
+                params: {
+                    property: 'sms.endpoint'
+                },
                 withCredentials: true,
-                headers: {"Content-Type": "application/json"}
+                headers: {
+                    Accept: 'text/plain'
+                }
             });
         };
         return {
