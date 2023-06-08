e4df838eda1f4605804e0c03589127a1a8dede8c
Pasunuri Srinidhi <93040752+PasunuriSrinidhi@users.noreply.github.com>
2023-06-09 00:42:20 +0530

    Updated init.js
    'bahmni.reports.analytics' module has been added to the existing dependencies of the 'bahmni.reports' module.

--- ui/app/reports/init.js
+++ ui/app/reports/init.js
@@ -6,4 +6,5 @@ Bahmni.Reports = Bahmni.Reports || {};
 angular.module('bahmni.reports', ['ui.router', 'bahmni.common.config', 'bahmni.common.uiHelper', 'bahmni.common.i18n',
     'bahmni.common.domain', 'authentication', 'bahmni.common.appFramework', 'bahmni.common.routeErrorHandler',
     'httpErrorInterceptor', 'pasvaz.bindonce', 'infinite-scroll', 'bahmni.common.util', 'ngSanitize',
-    'pascalprecht.translate', 'ngCookies', 'angularFileUpload', 'bahmni.common.logging', 'ngDialog']);
+    'pascalprecht.translate', 'ngCookies', 'angularFileUpload', 'bahmni.common.logging', 'ngDialog',
+    'bahmni.reports.analytics']); 
