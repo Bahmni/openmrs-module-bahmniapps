952fa26bf328b69fddef21af4ae69fcabbb732ff
rahu1ramesh <rahul.ramesh@thoughtworks.com>
2023-06-20 20:03:31 +0530

    [Rahul] | Fix. eol-last Issue

--- ui/app/common/ui-helper/controllers/messageController.js
+++ ui/app/common/ui-helper/controllers/messageController.js
@@ -1,31 +1,34 @@
-'use strict';
+"use strict";
 
-angular.module('bahmni.common.uiHelper')
-    .controller('MessageController', ['$scope', 'messagingService', '$translate',
-        function ($scope, messagingService, $translate) {
-            $scope.messages = messagingService.messages;
+angular.module("bahmni.common.uiHelper").controller("MessageController", [
+    "$scope",
+    "messagingService",
+    "$translate",
+    function ($scope, messagingService, $translate) {
+        $scope.messages = messagingService.messages;
 
-            $scope.getMessageText = function (level) {
-                var string = "";
-                $scope.messages[level].forEach(function (message) {
-                    string = string.concat(message.value);
-                });
-                var translatedMessage = $translate.instant(string);
+        $scope.getMessageText = function (level) {
+            var string = "";
+            $scope.messages[level].forEach(function (message) {
+                string = string.concat(message.value);
+            });
+            var translatedMessage = $translate.instant(string);
 
-                navigator.clipboard.writeText(translatedMessage);
+            navigator.clipboard.writeText(translatedMessage);
 
-                return translatedMessage;
-            };
+            return translatedMessage;
+        };
 
-            $scope.hideMessage = function (level) {
-                messagingService.hideMessages(level);
-            };
+        $scope.hideMessage = function (level) {
+            messagingService.hideMessages(level);
+        };
 
-            $scope.isErrorMessagePresent = function () {
-                return $scope.messages.error.length > 0;
-            };
+        $scope.isErrorMessagePresent = function () {
+            return $scope.messages.error.length > 0;
+        };
 
-            $scope.isInfoMessagePresent = function () {
-                return $scope.messages.info.length > 0;
-            };
-        }]);
+        $scope.isInfoMessagePresent = function () {
+            return $scope.messages.info.length > 0;
+        };
+    }
+]);
