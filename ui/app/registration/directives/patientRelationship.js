18c9b8d8f33e83780bc2f8b64423498ed18f1e08
Arjun G <91885483+Arjun-Go@users.noreply.github.com>
2023-06-21 19:21:40 +0530

    [Arjun/Ashish] | BAH-3057 | Ability to edit a person in person management app (#608)
    * modify existing relationships view in registration page
    
    * update. httpd conf to serve static files for person management app
    
    * update. view of patient relationship section
    
    * update UI according to new mockups
    
    * update. css to fix alignment issues

--- ui/app/registration/directives/patientRelationship.js
+++ ui/app/registration/directives/patientRelationship.js
@@ -96,6 +96,8 @@ angular.module('bahmni.registration')
                             }
                         });
                     }
+                    $scope.showPopupWindow = false;
+                    $scope.$apply();
                 }, false);
             };
 
@@ -155,12 +157,13 @@ angular.module('bahmni.registration')
             };
 
             $scope.openPatientDashboardInNewTab = function (relationship) {
-                var personRelatedTo = getPersonRelatedTo(relationship);
-                $window.open(getPatientRegistrationUrl(personRelatedTo.uuid), '_blank');
-            };
-
-            var getPatientRegistrationUrl = function (patientUuid) {
-                return '#/patient/' + patientUuid;
+                var iframe = $document[0].getElementById("relationship-extension-popup");
+                iframe.src = Bahmni.Registration.Constants.personManagementEditPersonURL + "/" + relationship.personB.uuid;
+                $scope.showPopupWindow = true;
+                $window.addEventListener("message", function () {
+                    $scope.showPopupWindow = false;
+                    $scope.$apply();
+                }, false);
             };
 
             $scope.getProviderList = function () {
