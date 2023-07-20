402f1b00cdecee802d2d3feb2a8d5409c2efe1e7
Arjun G <91885483+Arjun-Go@users.noreply.github.com>
2023-07-20 11:30:47 +0530

    Arjun/Ashish | BAH-3106 | Ability to bring up the Add to drug chart modal with its elements from Treatments display control in Patient Dashboard by clicking on Add to drug chart link  (#645)
    * integrate DrugChartModal from ipd in dashboard
    
    * add. changes to modify Drug Chart Controller values
    
    * add. changes to communicate between modal and add to drug chart link
    
    * make schedule and start time frequencies configurable

--- ui/app/clinical/displaycontrols/treatmentData/directives/treatmentTableRow.js
+++ ui/app/clinical/displaycontrols/treatmentData/directives/treatmentTableRow.js
@@ -3,7 +3,13 @@
 angular.module('bahmni.clinical')
     .directive('treatmentTableRow', function () {
         var controller = function ($scope, $rootScope, appService) {
+            $scope.selectedDrugOrder = {};
+            $scope.openModal = false;
             $scope.enableIPDFeature = appService.getAppDescriptor().getConfigValue("enableIPDFeature");
+            if ($scope.enableIPDFeature === true) {
+                $scope.drugChartModalScheduleFrequencies = appService.getAppDescriptor().getConfigValue("drugChartScheduleFrequencies");
+                $scope.drugChartModalStartTimeFrequencies = appService.getAppDescriptor().getConfigValue("drugChartStartTimeFrequencies");
+            }
             $scope.showDetails = false;
             if ($scope.params.showProvider === undefined) {
                 $scope.params.showProvider = true;
@@ -11,7 +17,16 @@ angular.module('bahmni.clinical')
             $scope.toggle = function () {
                 $scope.showDetails = !$scope.showDetails;
             };
+            $scope.openDrugChartModal = function (drugOrder) {
+                $scope.selectedDrugOrder = drugOrder;
+                $scope.openModal = true;
+            };
+            $scope.closeDrugChartModal = function () {
+                $scope.openModal = false;
+                $scope.$apply();
+            };
         };
+
         return {
             restrict: 'A',
             controller: controller,
