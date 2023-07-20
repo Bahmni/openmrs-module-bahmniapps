afa9f36acfed594a840d1d079616a1aacda78961
parvathy00 <parvathy.babu@thoughtworks.com>
2023-07-20 19:40:30 +0530

    Parvathy | BAH-2359 | Add. Alert box with review and discard button

--- ui/app/common/ui-helper/controllers/messageController.js
+++ ui/app/common/ui-helper/controllers/messageController.js
@@ -1,16 +1,14 @@
 'use strict';
 
-angular.module("bahmni.common.uiHelper").controller("MessageController", [ "$scope", "messagingService", "$translate", "$state", "exitAlertService",
-    function ($scope, messagingService, $translate, $state, exitAlertService) {
+angular.module("bahmni.common.uiHelper").controller("MessageController", [
+    "$scope",
+    "messagingService",
+    "$translate",
+    "$location",
+    "$state",
+    function ($scope, messagingService, $translate, $location, $state) {
         $scope.messages = messagingService.messages;
-        $scope.getMessageText = function (level) {
-            var string = "";
-            $scope.messages[level].forEach(function (message) {
-                string = string.concat(message.value);
-            });
-            navigator.clipboard.writeText(string);
-            return string;
-        };
+        $scope.isNavigating = false;
 
         $scope.hideMessage = function (level) {
             messagingService.hideMessages(level);
@@ -28,10 +26,26 @@ angular.module("bahmni.common.uiHelper").controller("MessageController", [ "$sco
             return $scope.messages.alert.length > 0;
         };
 
-        $scope.discardChanges = function (level) {
-            $state.discardChanges = true;
-            $scope.hideMessage(level);
-            exitAlertService.redirectUrl();
+            $scope.isErrorMessagePresent = function () {
+                return $scope.messages.error.length > 0;
+            };
+
+        $scope.isInfoMessagePresent = function () {
+            return $scope.messages.info.length > 0;
         };
+
+        $scope.discardChanges = function (level) {
+            $scope.isNavigating = false;
+            $state.dirtyConsultationForm = false;
+            $scope.hideMessage(level);
+            $location.path('/default/patient/search');
+        }
+
+        $scope.$on("$stateChangeStart", function (event, next, current) {
+            if (next.url.includes("/patient/search")) {
+                $scope.isNavigating = true;
+            }
+        })
+
     }
 ]);
