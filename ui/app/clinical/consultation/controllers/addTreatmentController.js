d51b16f9b6653ab980fe7cfc511d81b233f90813
parvathy00 <parvathy.babu@thoughtworks.com>
2023-07-25 17:04:06 +0530

    Parvathy | BAH-2359 | Refactor. Improvements to code

--- ui/app/clinical/consultation/controllers/addTreatmentController.js
+++ ui/app/clinical/consultation/controllers/addTreatmentController.js
@@ -96,14 +96,13 @@ angular.module('bahmni.clinical')
                 });
             };
         
-            $scope. $on ('$stateChangeStart', function (event, next, current) {
+            $scope.$on('$stateChangeStart', function () {
                 if ($scope.addForm.$dirty) {
                     $state.dirtyConsultationForm = true;
                   }
             });
 
-            $scope.$on("event:changes-saved", function (event) {
-                $scope.addForm.$setSubmitted();
+            $scope.$on("event:changes-saved", function () {
                 $scope.addForm.$dirty = false;
             });
 
