9123c2105b646b5a779db2f7e850aaccf8b36656
parvathy00 <parvathy.babu@thoughtworks.com>
2023-07-29 00:35:39 +0530

    Parvathy | BAH-2359 | Fix. Eslint issues and test cases

--- ui/app/clinical/consultation/directives/alertOnExit.js
+++ ui/app/clinical/consultation/directives/alertOnExit.js
@@ -9,7 +9,7 @@ angular.module('bahmni.clinical')
                         var uuid = $state.params.patientUuid;
                         var currentUuid = current.patientUuid;
                         var isNavigating = exitAlertService.setIsNavigating(next, uuid, currentUuid);
-                        $state.dirtyConsultationForm =  $state.discardChanges ? false : $state.dirtyConsultationForm;
+                        $state.dirtyConsultationForm = $state.discardChanges ? false : $state.dirtyConsultationForm;
                         exitAlertService.showExitAlert(isNavigating, $state.dirtyConsultationForm, event, next.spinnerToken);
                     });
                 }
