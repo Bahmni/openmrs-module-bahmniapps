c8b8bd40ccc6ff63a9a39d677bb283b0ed9fd4d5
Arjun-Go <arjun.g@thoughtworks.com>
2023-09-04 20:06:48 +0530

    add. grunt changes to copy i18n from micro-frontends

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -300,7 +300,8 @@ module.exports = function (grunt) {
                             'styles/fonts/**/*',
                             'clinical/config/*.json',
                             'i18n/**/*.json',
-                            'lib/**/*'
+                            'lib/**/*',
+                            "micro-frontends-dist/i18n/**/*.json"
                         ]
                     }
                 ]
@@ -355,14 +356,14 @@ module.exports = function (grunt) {
                         src: ['clinical.*.js'],
                         dest: '<%= yeoman.dist %>/clinical/'
                     },
-                    {expand: true, cwd: '<%= yeoman.dist %>', src: ['adt.*.js'], dest: '<%= yeoman.dist %>/adt/'},
+                    { expand: true, cwd: '<%= yeoman.dist %>', src: ['adt.*.js'], dest: '<%= yeoman.dist %>/adt/' },
                     {
                         expand: true,
                         cwd: '<%= yeoman.dist %>',
                         src: ['orders.*.js'],
                         dest: '<%= yeoman.dist %>/orders/'
                     },
-                    {expand: true, cwd: '<%= yeoman.dist %>', src: ['home.*.js'], dest: '<%= yeoman.dist %>/home/'},
+                    { expand: true, cwd: '<%= yeoman.dist %>', src: ['home.*.js'], dest: '<%= yeoman.dist %>/home/' },
                     {
                         expand: true,
                         cwd: '<%= yeoman.dist %>',
@@ -375,7 +376,7 @@ module.exports = function (grunt) {
                         src: ['admin.*.js'],
                         dest: '<%= yeoman.dist %>/admin/'
                     },
-                    {expand: true, cwd: '<%= yeoman.root %>', src: ['common.*.js'], dest: '<%= yeoman.dist %>/'},
+                    { expand: true, cwd: '<%= yeoman.root %>', src: ['common.*.js'], dest: '<%= yeoman.dist %>/' },
                     {
                         expand: true,
                         cwd: '<%= yeoman.dist %>',
@@ -406,7 +407,7 @@ module.exports = function (grunt) {
                         src: ['clinical.*.css'],
                         dest: '<%= yeoman.dist %>/clinical/'
                     },
-                    {expand: true, cwd: '<%= yeoman.dist %>', src: ['adt.*.css'], dest: '<%= yeoman.dist %>/adt/'},
+                    { expand: true, cwd: '<%= yeoman.dist %>', src: ['adt.*.css'], dest: '<%= yeoman.dist %>/adt/' },
                     {
                         expand: true,
                         cwd: '<%= yeoman.dist %>',
@@ -455,7 +456,7 @@ module.exports = function (grunt) {
                         src: ['document-upload.*.css'],
                         dest: '<%= yeoman.dist %>/document-upload/'
                     },
-                    {expand: true, cwd: '<%= yeoman.dist %>', src: ['ot.*.css'], dest: '<%= yeoman.dist %>/ot/'}
+                    { expand: true, cwd: '<%= yeoman.dist %>', src: ['ot.*.css'], dest: '<%= yeoman.dist %>/ot/' }
                 ]
             }
         },
