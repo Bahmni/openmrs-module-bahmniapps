e11a5f02f787654ef97d117eb1f37508c7d0fd13
Arjun G <91885483+Arjun-Go@users.noreply.github.com>
2023-09-13 13:12:23 +0530

    Arjun | A-1205281688916688 | Default values of triage program state (#698)
    * add. changes to bring up initial program workflow state and start date
    
    * add. changes to open patient dashboard in new tab
    
    * fix. linting issues
    
    * fix. tests and code to handle redirections and pre-fill values for state and program based on config
    
    * add. feedback to update text color

--- ui/app/common/domain/services/programService.js
+++ ui/app/common/domain/services/programService.js
@@ -4,7 +4,7 @@ angular.module('bahmni.common.domain')
         var PatientProgramMapper = new Bahmni.Common.Domain.PatientProgramMapper();
 
         var getAllPrograms = function () {
-            return $http.get(Bahmni.Common.Constants.programUrl, {params: {v: 'default'}}).then(function (response) {
+            return $http.get(Bahmni.Common.Constants.programUrl, { params: { v: 'default' } }).then(function (response) {
                 var allPrograms = programHelper.filterRetiredPrograms(response.data.results);
                 _.forEach(allPrograms, function (program) {
                     program.allWorkflows = programHelper.filterRetiredWorkflowsAndStates(program.allWorkflows);
@@ -26,7 +26,7 @@ angular.module('bahmni.common.domain')
                     dateEnrolled: moment(dateEnrolled).format(Bahmni.Common.Constants.ServerDateTimeFormat),
                     attributes: attributeFormatter.removeUnfilledAttributes(attributeFormatter.getMrsAttributes(patientProgramAttributes, (programAttributeTypes || [])))
                 },
-                headers: {"Content-Type": "application/json"}
+                headers: { "Content-Type": "application/json" }
             };
             if (!_.isEmpty(stateUuid)) {
                 req.content.states = [
@@ -45,7 +45,7 @@ angular.module('bahmni.common.domain')
                 patientProgramUuid: patientProgramUuid,
                 patient: patientUuid
             };
-            return $http.get(Bahmni.Common.Constants.programEnrollPatientUrl, {params: params}).then(function (response) {
+            return $http.get(Bahmni.Common.Constants.programEnrollPatientUrl, { params: params }).then(function (response) {
                 var patientPrograms = response.data.results;
                 return getProgramAttributeTypes().then(function (programAttributeTypes) {
                     if (filterAttributesForProgramDisplayControl) {
@@ -61,7 +61,7 @@ angular.module('bahmni.common.domain')
             var req = {
                 url: Bahmni.Common.Constants.programEnrollPatientUrl + "/" + patientProgramUuid,
                 content: content,
-                headers: {"Content-Type": "application/json"}
+                headers: { "Content-Type": "application/json" }
             };
             return $http.post(req.url, req.content, req.headers);
         };
@@ -73,13 +73,13 @@ angular.module('bahmni.common.domain')
                     "!purge": "",
                     "reason": "User deleted the state."
                 },
-                headers: {"Content-Type": "application/json"}
+                headers: { "Content-Type": "application/json" }
             };
             return $http.delete(req.url, req.content, req.headers);
         };
 
         var getProgramAttributeTypes = function () {
-            return $http.get(Bahmni.Common.Constants.programAttributeTypes, {params: {v: 'custom:(uuid,name,description,datatypeClassname,datatypeConfig,concept)'}}).then(function (response) {
+            return $http.get(Bahmni.Common.Constants.programAttributeTypes, { params: { v: 'custom:(uuid,name,description,datatypeClassname,datatypeConfig,concept)' } }).then(function (response) {
                 var programAttributesConfig = appService.getAppDescriptor().getConfigValue("program");
 
                 var mandatoryProgramAttributes = [];
@@ -101,6 +101,16 @@ angular.module('bahmni.common.domain')
             return config ? config.showProgramStateInTimeline : false;
         };
 
+        var getDefaultProgram = function () {
+            var defaultProgram = appService.getAppDescriptor().getConfigValue('defaultProgram') || null;
+            return defaultProgram;
+        };
+
+        var getProgramRedirectionConfig = function () {
+            var config = appService.getAppDescriptor().getConfigValue('programRedirection');
+            return config ? config : null;
+        };
+
         var getEnrollmentInfoFor = function (patientUuid, representation) {
             var params = {
                 patient: patientUuid,
@@ -120,6 +130,8 @@ angular.module('bahmni.common.domain')
             deletePatientState: deletePatientState,
             getProgramAttributeTypes: getProgramAttributeTypes,
             getProgramStateConfig: getProgramStateConfig,
-            getEnrollmentInfoFor: getEnrollmentInfoFor
+            getEnrollmentInfoFor: getEnrollmentInfoFor,
+            getDefaultProgram: getDefaultProgram,
+            getProgramRedirectionConfig: getProgramRedirectionConfig
         };
     }]);
