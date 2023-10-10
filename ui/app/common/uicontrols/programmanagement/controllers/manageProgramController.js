016818111a8d2fd3861e599a49115a51ddd44325
ayush34957 <ayush.sharma1@thoughtworks.com>
2023-10-10 18:51:40 +0530

    test: added test and feedback [Ayush]

--- ui/app/common/uicontrols/programmanagement/controllers/manageProgramController.js
+++ ui/app/common/uicontrols/programmanagement/controllers/manageProgramController.js
@@ -17,11 +17,12 @@ angular.module('bahmni.common.uicontrols.programmanagment')
             var id = "#programEnrollmentContainer";
             const defaultProgram = programService.getDefaultProgram();
             const programRedirectionConfig = programService.getProgramRedirectionConfig();
+            const observationFormsConfig = programService.getObservationFormsConfig() || {};
 
             $scope.observationFormData = {
                 patientUuid: $scope.patient.uuid,
-                showEditForActiveEncounter: programService.getObservationFormsConfig().showEditForActiveEncounter || true,
-                numberOfVisits: programService.getObservationFormsConfig().numberOfVisits || 10,
+                showEditForActiveEncounter: observationFormsConfig.showEditForActiveEncounter || true,
+                numberOfVisits: observationFormsConfig.numberOfVisits || 10,
                 hasNoHierarchy: $scope.hasNoHierarchy
             };
 
