ca1dbe129766ed088c573db05a664a97f6fb46a9
Phanindra-tw <v.tadikonda@thoughtworks.com>
2023-11-09 09:53:57 +0530

    Phani | Add the React IPD Dashboard to ADT Patient Dashboard (#772)
    * Phani | Add the new React IPD Dashboard to ADT Patient Dashboard
    
    * Phani | Update Host data
    
    * Phani | Cleanup unused imports

--- ui/app/adt/app.js
+++ ui/app/adt/app.js
@@ -67,17 +67,8 @@ angular.module('adt').config(['$stateProvider', '$httpProvider', '$urlRouterProv
             },
             abstract: true,
             views: {
-                'header': {
-                    templateUrl: 'views/headerAdt.html',
-                    controller: function ($scope) {
-                        $scope.showClinicalDashboardLink = true;
-                    }
-                },
                 'content': {
                     template: '<ui-view/>'
-                },
-                'additional-header': {
-                    templateUrl: '../common/patient/header/views/header.html'
                 }
             },
 
