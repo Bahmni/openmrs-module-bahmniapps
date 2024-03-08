1ef8d6ece1915341a3f81baa8a95e331d9390e22
Abinaya U <abinaya.u@thoughtworks.com>
2024-03-08 14:39:29 +0530

    fix. eslint and test coverage

--- ui/app/clinical/consultation/controllers/drugOrderHistoryController.js
+++ ui/app/clinical/consultation/controllers/drugOrderHistoryController.js
@@ -98,11 +98,11 @@ angular.module('bahmni.clinical')
                 });
             };
 
-            $scope.selectAllDrugs = function (drugOrderGroup,index) {
-                    if(!$scope.autoSelectNotAllowed) {
-                    angular.forEach(drugOrderGroup.drugOrders, function (drugOrder){
-                        $scope.selectedDrugs[`${index}/${drugOrder.uuid}`] = true
-                    })
+            $scope.selectAllDrugs = function (drugOrderGroup, index) {
+                if (!$scope.autoSelectNotAllowed) {
+                    angular.forEach(drugOrderGroup.drugOrders, function (drugOrder) {
+                        $scope.selectedDrugs[index + "/" + drugOrder.uuid] = true;
+                    });
                 }
             };
 
