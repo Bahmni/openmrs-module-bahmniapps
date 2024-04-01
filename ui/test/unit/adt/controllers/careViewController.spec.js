ed4677b958fe228efe9841e71ce7a7104744522e
Phanindra-tw <v.tadikonda@thoughtworks.com>
2024-04-01 14:31:58 +0530

    Phani | Update test files

--- ui/test/unit/adt/controllers/careViewController.spec.js
+++ ui/test/unit/adt/controllers/careViewController.spec.js
@@ -1,13 +1,17 @@
 'use strict';
 
 describe("CareViewController", function () {
-    var scope, controller;
+    var scope, controller, auditLogService, sessionService;
     var state = jasmine.createSpyObj('$state', ['go']);
     beforeEach(module('bahmni.adt'));
     beforeEach(inject(function ($controller, $rootScope) {
         controller = $controller;
         scope = $rootScope.$new();
     }));
+    auditLogService = jasmine.createSpyObj('auditLogService', ['log']);
+    sessionService = jasmine.createSpyObj('sessionService', ['destroy']);
+    auditLogService.log.and.returnValue({});
+    sessionService.destroy.and.returnValue({});
     let mockProvider = {name: "__test__provider"}
     var createController = function () {
 
@@ -15,6 +19,8 @@ describe("CareViewController", function () {
             $scope: scope,
             $rootScope: {currentProvider: mockProvider},
             $state: state,
+            auditLogService: auditLogService,
+            sessionService: sessionService
         });
     };
     it('should create host data and host api', function (){
