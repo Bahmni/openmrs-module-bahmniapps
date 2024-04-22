e9d72e85a0494277302b59c813e12757a1c0aa88
kavitha-sundararajan <90255023+kavitha-sundararajan@users.noreply.github.com>
2024-04-22 14:28:56 +0530

    BAH-3053 | add pre-patient attribute in registration page (#600) (#638)
    * BAH-3053 | add pre-patient attribute in registration page (#600)
    
    * BAH-3053 | add pre-patient attribute in registration page
    
    * BAH-3053 | added check for pre-registration attribute
    
    * BAH-3053 | refactored to drive by single config
    
    * Kavitha | refactored pre-patient flow based on configs
    
    * Kavitha | BAH-3065 | add additional search param for lucene search (#604)
    
    * Update package.json (#884)
    
    * [Rahul] | BAH-3710 | Fix. Remove Transifex Script
    
    * [Bahmni Infra] | Add. Update Translation Resources
    
    * Parvathy | BAH-2833 | Add. Display Error Message When Document Size Limit Exceed (#875)
    
    * [Rahu] | BAH-3522 | Fix. Improve Test Coverage For DateUtils
    
    * [Bahmni Infra] | Add. Update Translation Resources
    
    * Parvathy | BAH-2833 | Fix. Handle Document Size Limit Error Message
    
    * Revert "Update package.json (#884)"
    
    This reverts commit 30091b30b4a0f33597f859c4279e04f0341dad22.
    
    * Revert "[Rahul] | BAH-3710 | Fix. Remove Transifex Script"
    
    This reverts commit bbc704df83ead8767dbc261e6c50a335b4b888c3.
    
    * Revert "Parvathy | BAH-2833 | Add. Display Error Message When Document Size Limit Exceed (#875)"
    
    This reverts commit 67a004b84dfb0574409a98aa1c75bfa7f9bdded9.
    
    * Revert "[Rahu] | BAH-3522 | Fix. Improve Test Coverage For DateUtils"
    
    This reverts commit 1bf9ff7224f6f3e297006d6bbe661ec36d419869.
    
    * Revert "[Bahmni Infra] | Add. Update Translation Resources"
    
    This reverts commit 95d96c41ccd4c7ff8e1dded8275da44395552bd3.
    
    * Revert "Parvathy | BAH-2833 | Fix. Handle Document Size Limit Error Message"
    
    This reverts commit 95fdfac2196d94dcc0a705263d5af823ac7988fc.
    
    * Kavitha | refactor to accept grouped configs
    
    * Revert unnecessary commit
    
    * BAH-3053 | renamed configuration names
    
    * BAH-3053 | renamed lables to related patient identifier
    
    ---------

--- ui/app/common/patient/services/patientService.js
+++ ui/app/common/patient/services/patientService.js
@@ -38,11 +38,12 @@ angular.module('bahmni.common.patient')
                 q: query,
                 startIndex: offset,
                 identifier: identifier,
-                loginLocationUuid: sessionService.getLoginLocationUuid(),
-                patientSearchResultsConfig: customAttribute
+                loginLocationUuid: sessionService.getLoginLocationUuid()
             };
-            if (appService.getAppDescriptor().getConfigValue("filterAttributeForAllSearch")) {
-                searchParams.attributeToFilterOut = appService.getAppDescriptor().getConfigValue("filterAttributeForAllSearch");
+            var filterOutAttributeForAllSearch = appService.getAppDescriptor().getConfigValue("filterOutAttributeForAllSearch") || [];
+            if (filterOutAttributeForAllSearch && filterOutAttributeForAllSearch.length > 0) {
+                searchParams.attributeToFilterOut = filterOutAttributeForAllSearch[0].attrName;
+                searchParams.attributeValueToFilterOut = filterOutAttributeForAllSearch[0].attrValue;
             }
             return $http.get(Bahmni.Common.Constants.bahmniCommonsSearchUrl + "/patient/lucene", {
                 method: "GET",
