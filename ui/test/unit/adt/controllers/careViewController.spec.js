ec6680c453182080212875283372e01691ea5b3f
srinithishg <srinithish.g@thoughtworks.com>
2024-04-25 00:10:25 +0530

    [Sri] - Add shortcut keys for quick logout

--- ui/test/unit/adt/controllers/careViewController.spec.js
+++ ui/test/unit/adt/controllers/careViewController.spec.js
@@ -1,12 +1,13 @@
 'use strict';
 
 describe("CareViewController", function () {
-    var scope, controller, auditLogService, sessionService;
+    var scope, controller, auditLogService, sessionService, $window;
     var state = jasmine.createSpyObj('$state', ['go']);
     beforeEach(module('bahmni.adt'));
-    beforeEach(inject(function ($controller, $rootScope) {
+    beforeEach(inject(function ($controller, $rootScope,_$window_) {
         controller = $controller;
         scope = $rootScope.$new();
+        $window = _$window_;
     }));
     auditLogService = jasmine.createSpyObj('auditLogService', ['log']);
     sessionService = jasmine.createSpyObj('sessionService', ['destroy']);
@@ -21,21 +22,28 @@ describe("CareViewController", function () {
 
         controller('CareViewController', {
             $scope: scope,
-            $rootScope: {currentProvider: mockProvider},
+            $rootScope: {currentProvider: mockProvider, quickLogoutComboKey: 'Escape'},
             $state: state,
             auditLogService: auditLogService,
-            sessionService: sessionService
+            sessionService: sessionService,
+            $window: $window
         });
     };
     it('should create host data and host api', function (){
         createController();
         expect(scope.hostData).toEqual({provider: mockProvider});
         expect(scope.hostApi).not.toBeNull();
-    })
+    });
     it('should call auditLogService.log and sessionService.destroy on logout', function (){
         createController();
         scope.hostApi.onLogOut();
         expect(auditLogService.log).toHaveBeenCalled();
         expect(sessionService.destroy).toHaveBeenCalled();
+    });
+    it('should call handleLogoutShortcut on keydown event', function (){
+        createController();
+        spyOn(scope.hostApi, 'onLogOut');
+        $window.dispatchEvent(new KeyboardEvent('keydown', {'key': 'Escape', 'metaKey': true, 'ctrlKey': false}));
+        expect(scope.hostApi.onLogOut).toHaveBeenCalled();
     });
 });
