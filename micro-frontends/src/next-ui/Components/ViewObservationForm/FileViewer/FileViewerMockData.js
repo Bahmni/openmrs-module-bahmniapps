b75b426edbc7bf53e3977b4ca7cda9f14b15acab
Soorya Kumaran C <90232857+SooryaKumaranC-tw@users.noreply.github.com>
2024-04-24 18:33:08 +0530

    Write few more test cases for grouping of files concepts

--- micro-frontends/src/next-ui/Components/ViewObservationForm/FileViewer/FileViewerMockData.js
+++ micro-frontends/src/next-ui/Components/ViewObservationForm/FileViewer/FileViewerMockData.js
@@ -64,200 +64,217 @@ export const mockVideoItem = {
 };
 
 export const observationList = [
-    {
-      "encounterDateTime": 1713856719000,
-      "groupMembers": [],
-      "providers": [
-        {
-          "uuid": "c1c26908-3f10-11e4-adec-0800271c1b75",
-          "name": "test provider",
-          "encounterRoleUuid": "a0b03050-c99b-11e0-9572-0800200c9a66"
-        }
-      ],
-      "isAbnormal": null,
-      "duration": null,
-      "type": "Complex",
-      "encounterUuid": "276f3f45-2bdb-4166-b495-8e3463774034",
-      "interpretation": null,
-      "complexData": {
-        "title": "51196-Consultation-3c01db16-e63a-4b83-9159-dc3f4e8ebb01.mp4",
-        "mimeType": "video/mp4",
+  {
+    encounterDateTime: 1713856719000,
+    groupMembers: [],
+    providers: [
+      {
+        uuid: "c1c26908-3f10-11e4-adec-0800271c1b75",
+        name: "test provider one",
+        encounterRoleUuid: "a0b03050-c99b-11e0-9572-0800200c9a66",
       },
-      "conceptNameToDisplay": "Patient Video",
-      "concept": {
-        "uuid": "eef8e969-269d-40c4-b7c9-182da11c2653",
-        "name": "Patient Video",
-        "dataType": "Complex",
-        "shortName": "Patient Video",
-      },
-      "conceptUuid": "eef8e969-269d-40c4-b7c9-182da11c2653",
-      "comment": "Sample comments are added",
-      "value": "51200/51196-Consultation-3c01db16-e63a-4b83-9159-dc3f4e8ebb01.mp4"
+    ],
+    isAbnormal: null,
+    duration: null,
+    type: "Complex",
+    encounterUuid: "276f3f45-2bdb-4166-b495-8e3463774034",
+    interpretation: null,
+    complexData: {
+      title: "51196-Consultation-3c01db16-e63a-4b83-9159-dc3f4e8ebb01.mp4",
+      mimeType: "video/mp4",
     },
-    {
-      "encounterDateTime": 1713856719000,
-      "groupMembers": [],
-      "providers": [
-        {
-          "uuid": "c1c26908-3f10-11e4-adec-0800271c1b75",
-          "name": "test provider",
-        }
-      ],
-      "isAbnormal": null,
-      "type": "Complex",
-      "interpretation": null,
-      "complexData": {
-        "title": "51196-Consultation-37e2a799-b44c-412f-9a16-1f04ae8f9a34.mp4",
-        "mimeType": "video/mp4",
-      },
-      "conceptNameToDisplay": "Patient Video",
-      "concept": {
-        "uuid": "eef8e969-269d-40c4-b7c9-182da11c2653",
-        "name": "Patient Video",
-        "dataType": "Complex",
-        "shortName": "Patient Video",
-      },
-      "conceptUuid": "eef8e969-269d-40c4-b7c9-182da11c2653",
-      "comment": "Sample comments are added",
-      "value": "51200/51196-Consultation-37e2a799-b44c-412f-9a16-1f04ae8f9a34.mp4"
+    conceptNameToDisplay: "Patient Video",
+    concept: {
+      uuid: "eef8e969-269d-40c4-b7c9-182da11c2653",
+      name: "Patient Video",
+      dataType: "Complex",
+      shortName: "Patient Video",
     },
+    conceptUuid: "eef8e969-269d-40c4-b7c9-182da11c2653",
+    comment: "Sample comments are added one",
+    value: "51200/51196-Consultation-3c01db16-e63a-4b83-9159-dc3f4e8ebb01.mp4",
+  },
+  {
+    encounterDateTime: 1713856719000,
+    groupMembers: [],
+    providers: [
+      {
+        uuid: "c1c26908-3f10-11e4-adec-0800271c1b75",
+        name: "test provider one",
+      },
+    ],
+    isAbnormal: null,
+    type: "Complex",
+    interpretation: null,
+    complexData: {
+      title: "51196-Consultation-37e2a799-b44c-412f-9a16-1f04ae8f9a34.mp4",
+      mimeType: "video/mp4",
+    },
+    conceptNameToDisplay: "Patient Video",
+    concept: {
+      uuid: "eef8e969-269d-40c4-b7c9-182da11c2653",
+      name: "Patient Video",
+      dataType: "Complex",
+      shortName: "Patient Video",
+    },
+    conceptUuid: "eef8e969-269d-40c4-b7c9-182da11c2653",
+    comment: "Sample comments are added two",
+    value: "51200/51196-Consultation-37e2a799-b44c-412f-9a16-1f04ae8f9a34.mp4",
+  },
+  {
+    encounterDateTime: 1713856719000,
+    groupMembers: [],
+    providers: [
+      {
+        uuid: "c1c26908-3f10-11e4-adec-0800271c1b75",
+        name: "test provider one",
+      },
+    ],
+    isAbnormal: null,
+    type: "Complex",
+    interpretation: null,
+    complexData: {
+      title: "51196-Consultation-445dd7fc-a7c7-4e9c-8ea6-70a3f567d8d4.jpeg",
+      mimeType: "image/jpeg",
+    },
+    conceptNameToDisplay: "Image",
+    concept: {
+      uuid: "6ca8ddf3-4e3d-4116-bd4e-a5147e580931",
+      name: "Image",
+      dataType: "Complex",
+      shortName: "Image",
+    },
+    comment: "Sample comments are added three",
+    value: "51200/51196-Consultation-445dd7fc-a7c7-4e9c-8ea6-70a3f567d8d4.jpeg",
+  },
+  {
+    encounterDateTime: 1713856719000,
+    groupMembers: [],
+    providers: [
+      {
+        uuid: "c1c26908-3f10-11e4-adec-0800271c1b75",
+        name: "test provider one",
+      },
+    ],
+    isAbnormal: null,
+    type: "Complex",
+    interpretation: null,
+    complexData: {
+      title: "51196-Consultation-46b1cbfc-00a7-4cbb-afdf-71ac2b040182.jpeg",
+      mimeType: "image/jpeg",
+    },
+    conceptNameToDisplay: "Image",
+    concept: {
+      uuid: "6ca8ddf3-4e3d-4116-bd4e-a5147e580931",
+      name: "Image",
+      dataType: "Complex",
+      shortName: "Image",
+    },
+    comment: "Sample comments are added four",
+    value: "51200/51196-Consultation-46b1cbfc-00a7-4cbb-afdf-71ac2b040182.jpeg",
+  },
+  {
+    encounterDateTime: 1713856719000,
+    groupMembers: [],
+    providers: [
+      {
+        uuid: "c1c26908-3f10-11e4-adec-0800271c1b75",
+        name: "test provider one",
+      },
+    ],
+    isAbnormal: null,
+    type: "Complex",
+    interpretation: null,
+    complexData: {
+      title: "51196-Consultation-aa0a417a-0a61-4c97-b4be-fe3fc43ea371.pdf",
+      mimeType: "application/pdf",
+    },
+    conceptNameToDisplay: "Image",
+    concept: {
+      uuid: "6ca8ddf3-4e3d-4116-bd4e-a5147e580931",
+      name: "Image",
+      dataType: "Complex",
+      shortName: "Image",
+    },
+    uuid: "8a643679-fcb2-4246-9b10-01a3d1eb4eb6",
+    comment: null,
+    value: "51200/51196-Consultation-aa0a417a-0a61-4c97-b4be-fe3fc43ea371.pdf",
+  },
+];
+
+export const observationListWithGroupMembers = [{
+  "encounterDateTime": 1713953624000,
+  "groupMembers": [
     {
-      "encounterDateTime": 1713856719000,
+      "encounterDateTime": 1713953624000,
       "groupMembers": [],
       "providers": [
         {
           "uuid": "c1c26908-3f10-11e4-adec-0800271c1b75",
-          "name": "test provider",
+          "name": "provider one",
         }
       ],
-      "isAbnormal": null,
       "type": "Complex",
       "interpretation": null,
       "complexData": {
-        "title": "51196-Consultation-b1eae63b-98a0-4396-80c7-0beb06f5fd00.jpeg",
+        "title": "51196-Consultation-e1e88b46-5db9-43c4-97fc-34096bdaad25.jpeg",
         "mimeType": "image/jpeg",
       },
-      "conceptNameToDisplay": "Image",
+      "conceptNameToDisplay": "Image 0-9-3",
+      "valueAsString": "51200/51196-Consultation-e1e88b46-5db9-43c4-97fc-34096bdaad25.jpeg",
       "concept": {
-        "uuid": "6ca8ddf3-4e3d-4116-bd4e-a5147e580931",
-        "name": "Image",
+        "uuid": "4fbf4b32-4411-436c-9c3b-1a907685a0a0",
+        "name": "Image 0-9-3",
         "dataType": "Complex",
-        "shortName": "Image",
+        "shortName": "Image 0-9-3",
       },
-      "comment": "Sample comments are added",
-      "value": "51200/51196-Consultation-b1eae63b-98a0-4396-80c7-0beb06f5fd00.jpeg"
+      "comment": "Notes 1",
+      "value": "51200/51196-Consultation-e1e88b46-5db9-43c4-97fc-34096bdaad25.jpeg"
     },
     {
-      "encounterDateTime": 1713856719000,
+      "encounterDateTime": 1713953624000,
       "groupMembers": [],
       "providers": [
         {
           "uuid": "c1c26908-3f10-11e4-adec-0800271c1b75",
-          "name": "test provider",
-          "encounterRoleUuid": "a0b03050-c99b-11e0-9572-0800200c9a66"
+          "name": "provider one",
         }
       ],
-      "isAbnormal": null,
-      "type": "Complex",
-      "encounterUuid": "276f3f45-2bdb-4166-b495-8e3463774034",
-      "interpretation": null,
-      "complexData": {
-        "title": "51196-Consultation-445dd7fc-a7c7-4e9c-8ea6-70a3f567d8d4.jpeg",
-        "mimeType": "image/jpeg",
-      },
-      "conceptNameToDisplay": "Image",
-      "concept": {
-        "uuid": "6ca8ddf3-4e3d-4116-bd4e-a5147e580931",
-        "name": "Image",
-        "dataType": "Complex",
-        "shortName": "Image",
-      },
-      "comment": "Sample comments are added",
-      "value": "51200/51196-Consultation-445dd7fc-a7c7-4e9c-8ea6-70a3f567d8d4.jpeg"
-    },
-    {
-      "encounterDateTime": 1713856719000,
-      "groupMembers": [],
-      "providers": [
-        {
-          "uuid": "c1c26908-3f10-11e4-adec-0800271c1b75",
-          "name": "test provider",
-        }
-      ],
-      "isAbnormal": null,
       "type": "Complex",
       "interpretation": null,
       "complexData": {
-        "title": "51196-Consultation-46b1cbfc-00a7-4cbb-afdf-71ac2b040182.jpeg",
-        "mimeType": "image/jpeg",
+        "title": "51196-Consultation-1cb9db39-987b-4f36-bf9b-3f4538e9b478.png",
+        "mimeType": "image/png",
       },
-      "conceptNameToDisplay": "Image",
+      "conceptNameToDisplay": "Image 0-9-3",
+      "valueAsString": "51200/51196-Consultation-1cb9db39-987b-4f36-bf9b-3f4538e9b478.png",
       "concept": {
-        "uuid": "6ca8ddf3-4e3d-4116-bd4e-a5147e580931",
-        "name": "Image",
+        "uuid": "4fbf4b32-4411-436c-9c3b-1a907685a0a0",
+        "name": "Image 0-9-3",
         "dataType": "Complex",
-        "shortName": "Image",
+        "shortName": "Image 0-9-3",
       },
-      "conceptUuid": "6ca8ddf3-4e3d-4116-bd4e-a5147e580931",
-      "comment": "Sample comments are added",
-      "value": "51200/51196-Consultation-46b1cbfc-00a7-4cbb-afdf-71ac2b040182.jpeg"
-    },
-    {
-      "encounterDateTime": 1713856719000,
-      "visitStartDateTime": null,
-      "targetObsRelation": null,
-      "groupMembers": [],
-      "providers": [
-        {
-          "uuid": "c1c26908-3f10-11e4-adec-0800271c1b75",
-          "name": "test provider",
-          "encounterRoleUuid": "a0b03050-c99b-11e0-9572-0800200c9a66"
-        }
-      ],
-      "isAbnormal": null,
-      "duration": null,
-      "type": "Complex",
-      "encounterUuid": "276f3f45-2bdb-4166-b495-8e3463774034",
-      "obsGroupUuid": null,
-      "creatorName": "test provider",
-      "conceptSortWeight": 1,
-      "parentConceptUuid": null,
-      "hiNormal": null,
-      "lowNormal": null,
-      "formNamespace": "Bahmni",
-      "formFieldPath": "Demo Final.1/1-2",
-      "interpretation": null,
-      "status": "AMENDED",
-      "encounterTypeName": null,
-      "complexData": {
-        "data": null,
-        "title": "51196-Consultation-aa0a417a-0a61-4c97-b4be-fe3fc43ea371.pdf",
-        "mimeType": "application/pdf",
-        "length": null
-      },
-      "unknown": false,
-      "orderUuid": null,
-      "abnormal": null,
-      "observationDateTime": 1713856755000,
-      "conceptNameToDisplay": "Image",
-      "valueAsString": "51200/51196-Consultation-aa0a417a-0a61-4c97-b4be-fe3fc43ea371.pdf",
-      "concept": {
-        "uuid": "6ca8ddf3-4e3d-4116-bd4e-a5147e580931",
-        "name": "Image",
-        "dataType": "Complex",
-        "shortName": "Image",
-        "units": null,
-        "conceptClass": "Misc",
-        "hiNormal": null,
-        "lowNormal": null,
-        "set": false,
-        "mappings": []
-      },
-      "conceptUuid": "6ca8ddf3-4e3d-4116-bd4e-a5147e580931",
-      "voided": false,
-      "voidReason": null,
-      "uuid": "8a643679-fcb2-4246-9b10-01a3d1eb4eb6",
-      "comment": null,
-      "value": "51200/51196-Consultation-aa0a417a-0a61-4c97-b4be-fe3fc43ea371.pdf"
+      "conceptUuid": "4fbf4b32-4411-436c-9c3b-1a907685a0a0",
+      "comment": "Notes 2",
+      "value": "51200/51196-Consultation-1cb9db39-987b-4f36-bf9b-3f4538e9b478.png"
     }
-  ];
+  ],
+  "providers": [
+    {
+      "uuid": "c1c26908-3f10-11e4-adec-0800271c1b75",
+      "name": "provider one",
+    }
+  ],
+  "type": null,
+  "interpretation": null,
+  "conceptNameToDisplay": "Consultation Images",
+  "concept": {
+    "uuid": "c3ae29e3-9836-4df9-bcef-8d01b776854d",
+    "name": "Consultation Images",
+    "dataType": "N/A",
+    "shortName": "Consultation Images",
+  },
+  "conceptUuid": "c3ae29e3-9836-4df9-bcef-8d01b776854d",
+  "comment": null,
+  "value": "51200/51196-Consultation-1cb9db39-987b-4f36-bf9b-3f4538e9b478.png, 51200/51196-Consultation-e1e88b46-5db9-43c4-97fc-34096bdaad25.jpeg"
+}];
--- micro-frontends/src/next-ui/Components/ViewObservationForm/ViewObservationForm.spec.jsx
+++ micro-frontends/src/next-ui/Components/ViewObservationForm/ViewObservationForm.spec.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import ViewObservationForm from "./ViewObservationForm.jsx";
-import { observationList } from "./FileViewer/FileViewerMockData";
+import { observationList, observationListWithGroupMembers } from "./FileViewer/FileViewerMockData";
 const initialProps = {
   formName: "Vitals",
   formNameTranslations: "Vitals",
@@ -76,12 +76,63 @@ describe("ViewObservationForm", () => {
     expect(screen.queryAllByText("Active loading indicator")).toHaveLength(2);
   });
 
-  // it("should render for complex type like image", () => {
-  //   const updatedProps = { ...initialProps, formData: observationList};
-  //   render(<ViewObservationForm {...updatedProps} />);
-  //   screen.debug();
-  //   screen.logTestingPlaygroundURL();
+  it("should render and group complex type like video/image/pdf which is in first level of hierarchy", () => {
+    const updatedProps = { ...initialProps, formData: observationList};
+    const { container } = render(<ViewObservationForm {...updatedProps} />);
 
-  //   expect(screen.queryAllByText("Active loading indicator")).toHaveLength(2);
-  // });
+    const fileSections = container.querySelectorAll(".file-section");
+    expect(fileSections).toHaveLength(2);
+    const video = fileSections[0];
+    
+    expect(video).not.toBeNull();
+    expect(within(video).getByText(/Patient Video/i)).toBeTruthy();
+    expect(within(video).getByText(/Sample comments are added one/i)).toBeTruthy();
+    expect(within(video).getByText(/Sample comments are added two/i)).toBeTruthy();
+    expect(within(video).getAllByText(/test provider one/i)).toHaveLength(2);
+    expect(within(video).getAllByText(/23 Apr 2024 12:48 pm/i)).toHaveLength(2);
+    
+    expect(container.querySelectorAll(".file-section .video-viewer")).toHaveLength(2);
+    expect(container.querySelectorAll(".file-section .video-viewer.video-0")).toHaveLength(1);
+    expect(container.querySelectorAll(".file-section .video-viewer.video-1")).toHaveLength(1);
+    
+    const image = fileSections[1];
+    expect(image).not.toBeNull();
+
+    expect(within(image).getByText(/Image/i)).toBeTruthy();
+    expect(within(image).getByText(/Sample comments are added three/i)).toBeTruthy();
+    expect(within(image).getByText(/Sample comments are added four/i)).toBeTruthy();
+    expect(within(image).getAllByText(/test provider one/i)).toHaveLength(3);
+    expect(within(image).getAllByText(/23 Apr 2024 12:48 pm/i)).toHaveLength(3);
+    
+    expect(container.querySelectorAll(".file-section .image-viewer")).toHaveLength(3);
+    expect(container.querySelectorAll(".file-section .image-viewer.img-0")).toHaveLength(1);
+    expect(container.querySelectorAll(".file-section .image-viewer.img-1")).toHaveLength(1);
+    expect(container.querySelectorAll(".file-section .image-viewer.img-2 .pdf-link")).toHaveLength(1);
+
+  });
+
+  it("should render and group complex type like image which is in second level of hierarchy", () => {
+    const updatedProps = { ...initialProps, formData: observationListWithGroupMembers};
+    const { container } = render(<ViewObservationForm {...updatedProps} />);
+
+    const sectionHeader = container.querySelector(".section-header");
+    expect(within(sectionHeader).getByText(/Consultation Images/i)).toBeTruthy();
+
+    expect(container.querySelector(".row-label")).toBeTruthy();
+    expect(container.querySelector(".row-label").textContent).toBe("Image 0-9-3");
+
+    expect(container.querySelectorAll(".row-value .image-viewer")).toHaveLength(2);
+
+    const imageRowOne = container.querySelector(".row-value .image-viewer.img-0");
+    expect(imageRowOne).toBeTruthy();
+    expect(within(imageRowOne).getByText(/Notes 1/i)).toBeTruthy();
+    expect(within(imageRowOne).getByText(/provider one/i)).toBeTruthy();
+    expect(within(imageRowOne).getByText(/24 Apr 2024 03:43 pm/i)).toBeTruthy();
+
+    const imageRowTwo = container.querySelector(".row-value .image-viewer.img-1");
+    expect(imageRowTwo).toBeTruthy();
+    expect(within(imageRowTwo).getByText(/Notes 2/i)).toBeTruthy();
+    expect(within(imageRowTwo).getByText(/provider one/i)).toBeTruthy();
+    expect(within(imageRowTwo).getByText(/24 Apr 2024 03:43 pm/i)).toBeTruthy();
+  });
 });
