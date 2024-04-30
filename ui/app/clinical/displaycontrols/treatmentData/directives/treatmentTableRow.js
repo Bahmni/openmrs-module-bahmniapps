0f5ea0a6c8910049297da739998b01b5136e4a97
binduak <tbindu@thoughtworks.com>
2024-04-30 15:56:38 +0530

    Bindu | Removed unnecessary code from the PR

--- ui/app/clinical/displaycontrols/treatmentData/directives/treatmentTableRow.js
+++ ui/app/clinical/displaycontrols/treatmentData/directives/treatmentTableRow.js
@@ -2,68 +2,13 @@
 
 angular.module('bahmni.clinical')
     .directive('treatmentTableRow', function () {
-        var controller = function ($scope, $rootScope, appService) {
-            $scope.selectedDrugOrder = {};
-            $scope.openModal = false;
-            $scope.enableIPDFeature = appService.getAppDescriptor().getConfigValue("enableIPDFeature");
-            const enable24HourTimers = appService.getAppDescriptor().getConfigValue("enable24HourTimers");
-            var drugChartModalScheduleFrequencies = [];
-            var drugChartModalStartTimeFrequencies = [];
-            if ($scope.enableIPDFeature === true) {
-                drugChartModalScheduleFrequencies = appService.getAppDescriptor().getConfigValue("drugChartScheduleFrequencies");
-                drugChartModalStartTimeFrequencies = appService.getAppDescriptor().getConfigValue("drugChartStartTimeFrequencies");
-            }
+        var controller = function ($scope) {
             $scope.showDetails = false;
             if ($scope.params.showProvider === undefined) {
                 $scope.params.showProvider = true;
             }
             $scope.toggle = function () {
                 $scope.showDetails = !$scope.showDetails;
-            };
-
-            $scope.drugChartModal = {
-                hostData: {
-                    drugOrder: {},
-                    patientId: $scope.params.patientUuid,
-                    scheduleFrequencies: drugChartModalScheduleFrequencies,
-                    startTimeFrequencies: drugChartModalStartTimeFrequencies,
-                    enable24HourTimers: enable24HourTimers
-                },
-                hostApi: {
-                    onModalClose: function () {
-                        $scope.openModal = false;
-                        $scope.$apply();
-                    },
-                    onModalSave: function () {
-                        $scope.openModal = false;
-                        $scope.drugChartModalNotification.hostData.notificationKind = "success";
-                        $scope.showModalWarningMessage = true;
-                        $scope.$apply();
-                    },
-                    onModalCancel: function () {
-                        $scope.openModal = false;
-                        $scope.drugChartModalNotification.hostData.notificationKind = "warning";
-                        $scope.showModalWarningMessage = true;
-                        $scope.$apply();
-                    }
-                }
-            };
-
-            $scope.drugChartModalNotification = {
-                hostData: {
-                    notificationKind: ""
-                },
-                hostApi: {
-                    onClose: function () {
-                        $scope.showModalWarningMessage = false;
-                        $scope.$apply();
-                    }
-                }
-            };
-
-            $scope.openDrugChartModal = function (drugOrder) {
-                $scope.drugChartModal.hostData.drugOrder = drugOrder;
-                $scope.openModal = true;
             };
         };
 
