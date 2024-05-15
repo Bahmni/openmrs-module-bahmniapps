815fc154bfde5ceabe7d55bbf012ca820cd42155
Arjun G <91885483+Arjun-Go@users.noreply.github.com>
2024-05-15 10:32:22 +0530

    Arjun | To hide show/hide IPD button in treatments tab (#942)
    * update. treatments added based on active visit type
    
    * update. logic to add IPD medications in an IPD visit
    
    * add. texts to denote different type of medication
    
    * fix. uglify build errors
    
    * update. set default behaviour as adding medication based on visit type

--- ui/app/clinical/consultation/controllers/drugOrderHistoryController.js
+++ ui/app/clinical/consultation/controllers/drugOrderHistoryController.js
@@ -12,11 +12,11 @@ angular.module('bahmni.clinical')
             var prescribedDrugOrders = [];
             $scope.dispensePrivilege = Bahmni.Clinical.Constants.dispensePrivilege;
             $scope.scheduledDate = DateUtil.getDateWithoutTime(DateUtil.addDays(DateUtil.now(), 1));
-            $scope.enableIPDFeature = appService.getAppDescriptor().getConfigValue("enableIPDFeature");
+            $scope.allMedicinesInPrescriptionAvailableForIPD = appService.getAppDescriptor().getConfigValue("allMedicinesInPrescriptionAvailableForIPD") !== null ? appService.getAppDescriptor().getConfigValue("allMedicinesInPrescriptionAvailableForIPD") : true;
             $scope.printPrescriptionFeature = appService.getAppDescriptor().getConfigValue("printPrescriptionFeature");
             $scope.selectedDrugs = {};
 
-            if ($scope.enableIPDFeature) {
+            if (!$scope.allMedicinesInPrescriptionAvailableForIPD) {
                 $scope.updateOrderType = function (drugOrder) {
                     var updatedDrugOrder = angular.copy(drugOrder);
                     updatedDrugOrder.careSetting = updatedDrugOrder.careSetting === Bahmni.Clinical.Constants.careSetting.outPatient ? Bahmni.Clinical.Constants.careSetting.inPatient : Bahmni.Clinical.Constants.careSetting.outPatient;
@@ -220,7 +220,7 @@ angular.module('bahmni.clinical')
                 });
             };
 
-            $scope.enableIPDFeature && spinner.forPromise(treatmentService.getMedicationSchedulesForOrders($stateParams.patientUuid, getActiveAndPrescribedDrugOrdersUuids()).then(function (response) {
+            !$scope.allMedicinesInPrescriptionAvailableForIPD && spinner.forPromise(treatmentService.getMedicationSchedulesForOrders($stateParams.patientUuid, getActiveAndPrescribedDrugOrdersUuids()).then(function (response) {
                 $scope.medicationSchedules = response.data;
             }));
 
--- ui/app/clinical/consultation/directives/newDrugOrders.js
+++ ui/app/clinical/consultation/directives/newDrugOrders.js
@@ -2,9 +2,9 @@
 
 angular.module('bahmni.clinical')
     .directive('newDrugOrders', ['messagingService', function (messagingService) {
-        var controller = function ($scope, $rootScope, appService) {
-            $scope.enableIPDFeature = appService.getAppDescriptor().getConfigValue("enableIPDFeature");
-            if ($scope.enableIPDFeature) {
+        var controller = function ($scope, $rootScope, $stateParams, appService, visitService) {
+            $scope.allMedicinesInPrescriptionAvailableForIPD = appService.getAppDescriptor().getConfigValue("allMedicinesInPrescriptionAvailableForIPD") !== null ? appService.getAppDescriptor().getConfigValue("allMedicinesInPrescriptionAvailableForIPD") : true;
+            if (!$scope.allMedicinesInPrescriptionAvailableForIPD) {
                 $rootScope.$on("event:setEncounterId", function (event, encounterId) {
                     $scope.encounterId = encounterId;
                 });
