5518cd8c74f159442a0688da661df22087bf37aa
binduak <tbindu@thoughtworks.com>
2024-05-24 06:26:45 +0530

    Ayush |test: added test and feedback

--- ui/test/unit/common/uicontrols/programmanagement/controllers/manageProgramController.spec.js
+++ ui/test/unit/common/uicontrols/programmanagement/controllers/manageProgramController.spec.js
@@ -22,7 +22,7 @@ describe("ManageProgramController", function () {
         _provide = $provide;
         programService = jasmine.createSpyObj('programService', ['getPatientPrograms', 'getAllPrograms',
             'deletePatientState', 'getProgramAttributeTypes', 'updatePatientProgram',
-            'getDefaultProgram', 'getProgramRedirectionConfig', 'disableProgramOutcomeEditOption']);
+            'getDefaultProgram', 'getProgramRedirectionConfig', 'disableProgramOutcomeEditOption', 'getObservationFormsConfig']);
 
         programService.getDefaultProgram.and.callFake(function () {
             deferred = q.defer();
