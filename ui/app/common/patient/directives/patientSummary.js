b38175907935c19b102c74667ec955e434da914c
Vikramdas Vaishnav <vikramdas.vaishnav@thoughtworks.com>
2025-02-13 11:44:47 +0530

    A - 1209055939299991 |  Added Supprt for partient ADT page

--- ui/app/common/patient/directives/patientSummary.js
+++ ui/app/common/patient/directives/patientSummary.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('bahmni.common.patient').directive('patientSummary', function () {
+angular.module('bahmni.common.patient').directive('patientSummary', function ($translate) {
     var link = function ($scope) {
         $scope.showPatientDetails = false;
         $scope.togglePatientDetails = function () {
@@ -12,6 +12,33 @@ angular.module('bahmni.common.patient').directive('patientSummary', function () 
                 $scope.onImageClickHandler();
             }
         };
+
+        function calculateAge(birthDate) {
+            if (!birthDate) return "";
+
+            var DateUtil = Bahmni.Common.Util.DateUtil;
+            var age = DateUtil.diffInYearsMonthsDays(birthDate, DateUtil.now());
+
+            var ageInString = "";
+            if (age.years) {
+                ageInString += age.years + " <span> " + $translate.instant("CLINICAL_YEARS_TRANSLATION_KEY") + " </span>";
+            }
+            if (age.months) {
+                ageInString += " " + age.months + " <span> " + $translate.instant("CLINICAL_MONTHS_TRANSLATION_KEY") + " </span>";
+            }
+            if (age.days) {
+                ageInString += " " + age.days + " <span> " + $translate.instant("CLINICAL_DAYS_TRANSLATION_KEY") + " </span>";
+            }
+
+            return ageInString.trim();
+        }
+
+        function computeAgeDisplay() {
+            if (!$scope.patient || !$scope.patient.birthdate) return;
+            $scope.displayAge = calculateAge($scope.patient.birthdate);
+        }
+
+        $scope.$watch('patient', computeAgeDisplay);
     };
 
     return {
