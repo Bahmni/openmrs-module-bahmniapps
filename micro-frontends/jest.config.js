bbbf46b437281458f167106f97c71accf4a2c10b
Vikramdas Vaishnav <vikramdas.vaishnav@thoughtworks.com>
2025-03-03 10:09:30 +0530

    updating jest config

--- micro-frontends/jest.config.js
+++ micro-frontends/jest.config.js
@@ -6,4 +6,16 @@ module.exports = {
     "\\.(css|scss|sass)$": "<rootDir>/src/__mocks__/styleMock.js",
   },
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+
+  collectCoverage: true,
+  coverageReporters: ["text", "lcov", "html"],
+  coverageThreshold: {
+    global: {
+      branches: 58,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
+
 };
