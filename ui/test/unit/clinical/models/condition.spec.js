bfa9754f60492426e5bb5f4e2560fd98dbf3158f
Samriddhi-98 <samridhi.agrawal@thoughtworks.com>
2025-10-06 11:47:25 +0530

    BAH-4203 | Fix the condition and condtionService test

--- ui/test/unit/clinical/models/condition.spec.js
+++ ui/test/unit/clinical/models/condition.spec.js
@@ -20,7 +20,11 @@ describe("condition", function () {
 
     describe("clearConcept", function () {
         it("should reset concept uuid", function () {
-            var condition = new Condition({concept: {uuid: 'uuid'}});
+            var condition = new Condition({
+                condition: {
+                    coded: { uuid: 'uuid', display: 'Test Condition' }
+                }
+            });
             condition.clearConcept();
             expect(condition.concept.uuid).toBeUndefined();
         });
@@ -29,21 +33,23 @@ describe("condition", function () {
     describe("isValidConcept", function () {
         it("should be okay to not have concept name", function () {
             var condition = new Condition({
-                concept: {uuid: 'uuid'},
+                condition: {
+                    coded: { uuid: 'uuid', display: 'Test' }
+                },
                 isNonCoded: false
             });
             expect(condition.isValidConcept()).toBeTruthy();
         });
         it("shouldn't be okay to have coded concept without uuid", function () {
             var condition = new Condition({
-                concept: {name: 'concept name'},
+                display: 'concept name',
                 isNonCoded: false
             });
             expect(condition.isValidConcept()).toBeFalsy();
         });
         it("should be okay to have non coded concept", function () {
             var condition = new Condition({
-                concept: {name: 'concept name'},
+                display: 'concept name',
                 isNonCoded: true
             });
             expect(condition.isValidConcept()).toBeTruthy();
@@ -53,30 +59,32 @@ describe("condition", function () {
     describe("isValid", function () {
         it("should be okay to have coded concept with a status", function () {
             var condition = new Condition({
-                concept: {uuid: 'uuid'},
+                condition: {
+                    coded: { uuid: 'uuid', display: 'concept name' }
+                },
                 isNonCoded: false,
-                status: 'ACTIVE'
+                clinicalStatus: 'ACTIVE'
             });
             expect(condition.isValid()).toBeTruthy();
         });
         it("should be okay to have non coded concept with a status", function () {
             var condition = new Condition({
-                concept: {name: 'concept name'},
+                display: 'concept name',
                 isNonCoded: true,
-                status: 'ACTIVE'
+                clinicalStatus: 'ACTIVE'
             });
             expect(condition.isValid()).toBeTruthy();
         });
         it("shouldn't be okay to have non coded concept without status", function () {
             var condition = new Condition({
-                concept: {name: 'concept name'},
+                display: 'concept name',
                 isNonCoded: true
             });
             expect(condition.isValid()).toBeFalsy();
         });
         it("shouldn't be okay to have coded concept without status", function () {
             var condition = new Condition({
-                concept: {name: 'concept name'},
+                display: 'concept name',
                 isNonCoded: false
             });
             expect(condition.isValid()).toBeFalsy();
@@ -90,21 +98,21 @@ describe("condition", function () {
         });
         it("should be empty when status,Concept or onsetDate is given ", function () {
             var condition = new Condition({
-                concept: {name: 'concept name'}
+                display: 'concept name'
             });
             expect(condition.isEmpty()).toBeFalsy();
 
             var condition = new Condition({
-                onSetDate:new Date()
+                onsetDate: new Date()
             });
             expect(condition.isEmpty()).toBeFalsy();
 
             var condition = new Condition({
-                status:'ACTIVE'
+                clinicalStatus: 'ACTIVE'
             });
             expect(condition.isEmpty()).toBeFalsy();
             var condition = new Condition({
-                additionalDetail:'some notes'
+                additionalDetail: 'some notes'
             });
             expect(condition.isEmpty()).toBeFalsy();
         });
@@ -119,7 +127,7 @@ describe("condition", function () {
         });
         it("should be okay if status is ACTIVE", function () {
             var condition = new Condition({
-                status: 'ACTIVE'
+                clinicalStatus: 'ACTIVE'
             });
             expect(condition.isActive()).toBeTruthy();
         });
@@ -128,19 +136,27 @@ describe("condition", function () {
     describe("displayString", function () {
         it("should be the name of non coded condition", function () {
             var condition = new Condition({
-                conditionNonCoded: 'conditionNonCoded name'
+                condition: {
+                    nonCoded: 'conditionNonCoded name'
+                }
             });
             expect(condition.displayString()).toBe('conditionNonCoded name');
         });
         it("should be the short name of coded condition", function () {
             var condition = new Condition({
-                concept: {shortName: 'coded short name'}
+                condition: {
+                    coded: {
+                        uuid: 'uuid',
+                        display: 'coded full name',
+                        name: { name: 'coded short name' }
+                    }
+                }
             });
             expect(condition.displayString()).toBe('coded short name');
         });
         it("should be the full name of coded condition", function () {
             var condition = new Condition({
-                concept: {name: 'coded full name'}
+                display: 'coded full name'
             });
             expect(condition.displayString()).toBe('coded full name');
         });
@@ -163,11 +179,11 @@ describe("condition", function () {
             var conditions = [
                 new Condition({
                     uuid: '1st condition uuid',
-                    previousConditionUuid: '2nd condition uuid'
+                    previousVersion: { uuid: '2nd condition uuid' }
                 }),
                 new Condition({
                     uuid: '2nd condition uuid',
-                    previousConditionUuid: '3rd condition uuid'
+                    previousVersion: { uuid: '3rd condition uuid' }
                 }),
                 new Condition({
                     status: 'ACTIVE',
@@ -175,66 +191,76 @@ describe("condition", function () {
                 })
             ];
 
-            expect(Conditions.getPreviousActiveCondition(conditions[0],conditions)).toBe(conditions[2]);
+            expect(Conditions.getPreviousActiveCondition(conditions[0], conditions)).toBe(conditions[2]);
         });
 
     });
 
     describe("fromConditionHistories", function () {
         it("should consider only condition which is not voided", function () {
-            var conditionHistory = {};
-            conditionHistory.conditions = [
+            var conditionHistories = [
                 {
                     uuid: '001',
-                    concept: {uuid: "Headache UUID"},
-                    status: 'ACTIVE',
-                    onSetDate: DateUtil.parse('2015-01-01'),
+                    condition: {
+                        coded: { uuid: "Headache UUID", display: 'Headache' }
+                    },
+                    clinicalStatus: 'ACTIVE',
+                    onsetDate: DateUtil.parse('2015-01-01'),
                     voided: true
                 },
                 {
                     uuid: '002',
-                    concept: {uuid: "Headache UUID"},
-                    status: 'HISTORY_OF',
-                    onSetDate: DateUtil.parse('2016-01-01'),
+                    condition: {
+                        coded: { uuid: "Headache UUID", display: 'Headache' }
+                    },
+                    clinicalStatus: 'HISTORY_OF',
+                    onsetDate: DateUtil.parse('2016-01-01'),
                     voided: true
                 },
                 {
                     uuid: '003',
-                    concept: {uuid: "Headache UUID"},
-                    status: 'ACTIVE',
-                    onSetDate: DateUtil.parse('2017-01-01')
+                    condition: {
+                        coded: { uuid: "Headache UUID", display: 'Headache' }
+                    },
+                    clinicalStatus: 'ACTIVE',
+                    onsetDate: DateUtil.parse('2017-01-01')
                 }
             ];
 
-            var latestConditions = Conditions.fromConditionHistories([conditionHistory]);
+            var latestConditions = Conditions.fromConditionHistories(conditionHistories);
             expect(latestConditions.length).toBe(1);
             expect(latestConditions[0].uuid).toBe('003');
 
         });
         it("should take recently onSet condition", function () {
-            var conditionHistory = {};
-            conditionHistory.conditions = [
+            var conditionHistories = [
                 {
                     uuid: '001',
-                    concept: {uuid: "Headache UUID"},
-                    status: 'ACTIVE',
-                    onSetDate: DateUtil.parse('2015-01-01')
+                    condition: {
+                        coded: { uuid: "Headache UUID", display: 'Headache' }
+                    },
+                    clinicalStatus: 'ACTIVE',
+                    onsetDate: DateUtil.parse('2015-01-01')
                 },
                 {
                     uuid: '002',
-                    concept: {uuid: "Headache UUID"},
-                    status: 'HISTORY_OF',
-                    onSetDate: DateUtil.parse('2016-01-01')
+                    condition: {
+                        coded: { uuid: "Headache UUID", display: 'Headache' }
+                    },
+                    clinicalStatus: 'HISTORY_OF',
+                    onsetDate: DateUtil.parse('2016-01-01')
                 },
                 {
                     uuid: '003',
-                    concept: {uuid: "Headache UUID"},
-                    status: 'ACTIVE',
-                    onSetDate: DateUtil.parse('2017-01-01')
+                    condition: {
+                        coded: { uuid: "Headache UUID", display: 'Headache' }
+                    },
+                    clinicalStatus: 'ACTIVE',
+                    onsetDate: DateUtil.parse('2017-01-01')
                 }
             ];
 
-            var latestConditions = Conditions.fromConditionHistories([conditionHistory]);
+            var latestConditions = Conditions.fromConditionHistories(conditionHistories);
             expect(latestConditions.length).toBe(1);
             expect(latestConditions[0].uuid).toBe('003');
 
--- ui/test/unit/common/domain/services/conditionsService.spec.js
+++ ui/test/unit/common/domain/services/conditionsService.spec.js
@@ -35,11 +35,40 @@ describe("conditionsService", function () {
 
     describe("getConditionHistory",function () {
         it("should fetch from condition History Url",function (done) {
-            _$http.get.and.returnValue(specUtil.respondWithPromise(Q, {data: []}));
+            _$http.get.and.returnValue(specUtil.respondWithPromise(Q, {data: {results: []}}));
 
             conditionsService.getConditionHistory('patientUuid').then(function () {
                 expect(_$http.get.calls.mostRecent().args[0]).toEqual(Bahmni.Common.Constants.conditionHistoryUrl);
                 expect(_$http.get.calls.mostRecent().args[1].params.patientUuid).toBe('patientUuid');
+                expect(_$http.get.calls.mostRecent().args[1].params.v).toBe('full');
+                expect(_$http.get.calls.mostRecent().args[1].params.includeInactive).toBe(true);
+            }).catch(notifyError).finally(done);
+        });
+    });
+    
+    describe("getConditions",function () {
+        it("should fetch and transform conditions",function (done) {
+            var mockResponse = {
+                data: {
+                    results: [
+                        {
+                            uuid: '001',
+                            condition: {
+                                coded: {uuid: 'concept-uuid', display: 'Headache'}
+                            },
+                            clinicalStatus: 'ACTIVE',
+                            onsetDate: '2017-01-01',
+                            voided: false
+                        }
+                    ]
+                }
+            };
+            _$http.get.and.returnValue(specUtil.respondWithPromise(Q, mockResponse));
+
+            conditionsService.getConditions('patientUuid').then(function (conditions) {
+                expect(_$http.get.calls.mostRecent().args[0]).toEqual(Bahmni.Common.Constants.conditionHistoryUrl);
+                expect(conditions).toBeDefined();
+                expect(conditions.length).toBeGreaterThan(0);
             }).catch(notifyError).finally(done);
         });
     });
