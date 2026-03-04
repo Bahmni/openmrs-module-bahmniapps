dfc200ffcce18f2553989c711f51a50a5741cd54
Pooja Sastry <pooja.sastry@thoughtworks.com>
2026-03-04 19:59:32 +0530

    Pooja | Bugfix | Fix for primary diagnosis not seen in OT calendar view (#75)

--- ui/app/ot/directives/otCalendarSurgicalBlock.js
+++ ui/app/ot/directives/otCalendarSurgicalBlock.js
@@ -105,6 +105,7 @@ angular.module('bahmni.ot')
                         getCalendarEndDateTime($scope.viewDate) < surgicalAppointment.derivedAttributes.expectedEndDatetime
                             ? getCalendarEndDateTime($scope.viewDate) : surgicalAppointment.derivedAttributes.expectedEndDatetime
                     );
+                    surgicalAppointment.primaryDiagnosis = new Bahmni.OT.SurgicalBlockMapper().mapPrimaryDiagnoses(surgicalAppointment.patientObservations);
                     nextAppointmentStartDatetime = surgicalAppointment.derivedAttributes.expectedEndDatetime;
                     return surgicalAppointment;
                 });
