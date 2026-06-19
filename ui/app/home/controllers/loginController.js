448e990effc5e090b51ea1871e9f5b8530b43ff4
Lingeswaran Subramaniyam <lingeswaran.s@thoughtworks.com>
2026-06-19 12:03:37 +0530

    BAH-4755 Home page as config (#1063)
    * BAH-4755 Initial changes
    
    * BAH-4755 use home page url via local storgae
    
    * BAH-4791. Add home page config for OT
    
    * BAH-4791. fix: indentation
    
    * BAH-4755. Sync localStorage on config load failure in loginInitialization
    
    ---------
    
    Co-authored-by: arshiyaTW2021 <arshiya.shehzad@thoughtworks.com>

--- ui/app/home/controllers/loginController.js
+++ ui/app/home/controllers/loginController.js
@@ -105,7 +105,7 @@ angular.module('bahmni.home')
             var redirectToLandingPageIfAlreadyAuthenticated = function () {
                 sessionService.get().then(function (data) {
                     if (data.authenticated) {
-                        $window.location.href = Bahmni.Common.Constants.newHomeURL;
+                        $window.location.href = $rootScope.homeURL || Bahmni.Common.Constants.homeUrl;
                     }
                 });
             };
@@ -235,7 +235,7 @@ angular.module('bahmni.home')
                                 if (res) {
                                     $window.location.replace(redirectUrl);
                                 } else {
-                                    $window.location.href = Bahmni.Common.Constants.newHomeURL;
+                                    $window.location.href = $rootScope.homeURL || Bahmni.Common.Constants.homeUrl;
                                 }
                             });
                         } else {
