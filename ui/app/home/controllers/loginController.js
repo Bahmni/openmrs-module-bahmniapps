d8c9a87ce1470cd8e2de8f8039d4427e3cc143e7
Varun Prasad <varun.prasad@thoughtworks.com>
2026-09-17 09:42:44 +0530

    BAH-4777 | fix typo savedLoacale to savedLocale

--- ui/app/home/controllers/loginController.js
+++ ui/app/home/controllers/loginController.js
@@ -72,8 +72,8 @@ angular.module('bahmni.home')
                     });
 
                     if (_.isEmpty($scope.locales)) {
-                        var savedLoacale = $translate.use() || "en";
-                        $scope.locales = [findLanguageByLocale(savedLoacale) || {"code": savedLoacale, "nativeName": savedLoacale}];
+                        var savedLocale = $translate.use() || "en";
+                        $scope.locales = [findLanguageByLocale(savedLocale) || {"code": savedLocale, "nativeName": savedLocale}];
                     }
                     $scope.selectedLocale = $translate.use() ? $translate.use() : $scope.locales[0].code;
                 });
